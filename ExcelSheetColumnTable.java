15798c44fbbaae4f4a4301a3ab69c086acd04860
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-08-01 01:27:50 +0530

    Update ExcelSheetColumnTable.java

--- ExcelSheetColumnTable.java
+++ ExcelSheetColumnTable.java
@@ -1,5 +1,7 @@
 class ExcelSheetColumnTable {
-    public String convertToTitle(int columnNumber) {
+    public String convertToTitleWithStringBuilder(int columnNumber) {
+
+        // Here we used StringBuilder so that performance will be faster
         StringBuilder title = new StringBuilder();
         while (columnNumber > 0) {
             // Adjust index to be 0-based for the calculation
@@ -11,5 +13,15 @@ class ExcelSheetColumnTable {
         }
         // The result is built backwards, so reverse it before returning
         return title.reverse().toString();
+    }
+    // Following will be roughly 10th to 100th times slower than String concatenation due to the reduction in overhead and memory allocation.
+    public String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while(columnNumber!=0){
+            columnNumber--;
+            sb.append((char) ('A'+columnNumber%26));
+            columnNumber/=26;
+        }
+        return sb.reverse().toString();
     }
 }
