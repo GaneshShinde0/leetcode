1af94cd98ab4c8618e88825a48e1beba4495d05c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-08-03 23:45:54 +0530

    Create N21MergeTwoSortedLists.java

---
+++ N21MergeTwoSortedLists.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class N21MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
+        ListNode currNode = new ListNode();
+        ListNode head= currNode;
+        while(head1!=null&&head2!=null){
+            if(head1.val<head2.val){
+                currNode.next=new ListNode(head1.val);
+                head1=head1.next;
+            }else{
+                currNode.next=new ListNode(head2.val);
+                head2=head2.next;
+            }
+            currNode=currNode.next;
+        }
+        if(head1==null) currNode.next=head2;
+        if(head2==null) currNode.next=head1;
+        return head.next;
+    }
+}
