6137ef424e450e2d2a9296af3404b190fab75282
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-08-16 22:35:55 +0530

    Create RepeatedSubString.java

---
+++ RepeatedSubString.java
@@ -0,0 +1,21 @@
+class RepeatedSubString {
+    public boolean repeatedSubstringPatternUsingContains(String s) {
+        String duplicate = s.substring(1)+s.substring(0,s.length()-1);
+        return duplicate.contains(s); 
+    }
+
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                String substring = s.substring(0, i);
+                StringBuilder repeated = new StringBuilder();
+                for (int j = 0; j < n / i; j++) {
+                    repeated.append(substring);
+                }
+                if (repeated.toString().equals(s)) return true;
+            }
+        }
+        return false;
+    }
+}
