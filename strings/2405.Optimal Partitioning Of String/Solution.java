2971f36c869305613896ef6c4d37ba0d60cc0e2d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-08-30 22:09:37 +0530

    Update Solution.java

--- strings/2405.Optimal Partitioning Of String/Solution.java
+++ strings/2405.Optimal Partitioning Of String/Solution.java
@@ -3,8 +3,29 @@ import java.util.Set;
 
 class Solution {
 
+
+/* Other Solution
+Problem name is Optimal Partition of String.
+
+ab|ac|ab|a
+
+So we start form left and continue partitioning and we just need to check the last partition.
+
+Method 1. Use set or strig to see if elements are repeated.
+    Time Complexity: O(n)
+    Space Complexity: O(26) => O(1)
+
+Method 2. Instead of set, we can use an array to store and check if the characters have occurred again or not.
+- This will be litte bit faster than set.
+    Time Complexity: O(n)
+    Space Complexity: O(26) => O(1)
+    
+Method 3. For the same operation to check if the character has occured or not we use bits.
+*/
+
+
     // Following approach takes 29 ms
-    public int partitionString(String s) {
+    public int partitionString1(String s) {
         Set<Character> seen = new HashSet<>();
         int count = 1;
 
@@ -18,4 +39,40 @@ class Solution {
         }
         return count;
     }
+
+    // Following takes around 9ms
+    public int partitionStringUsingArray(String s){
+
+        int[] lastPositions = new int[26];
+        int partitions =0, lastPartitionEnd = 0;
+
+        for(int i=0;i<s.length();i++){
+            if(lastPositions[s.charAt(i)-'a']>=lastPartitionEnd){
+                partitions++;
+                lastPartitionEnd = i+1;
+            }
+            lastPositions[s.charAt(i)-'a']=i+1;
+        }
+        return partitions;
+    }
+
+    public int partitionString(String s) {
+        int count = 1;
+        int bitMask = 0;
+        byte[] charss = new byte[s.length()];
+        s.getBytes(0, s.length(), charss, 0);
+
+        for (byte currentChar : charss) {
+            int currentBit = 1 << currentChar;
+            if ((currentBit & bitMask) != 0) {
+                count++;
+                bitMask = currentBit;
+            } else {
+                bitMask = currentBit | bitMask;
+            }
+        }
+        return count;
+    }
+
+
 }
