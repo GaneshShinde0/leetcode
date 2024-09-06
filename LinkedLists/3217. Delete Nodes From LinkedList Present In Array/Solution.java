5f034fef27a5f5f429da4cbb7df88e281573ecbd
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-09-06 22:40:46 +0530

    Create Solution.java

---
+++ LinkedLists/3217. Delete Nodes From LinkedList Present In Array/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode dummy = temp;
+        while(set.contains(dummy.val)){
+            if (set.contains(dummy.val)) dummy = dummy.next;
+        }
+
+        while(dummy.next!=null){
+            if(set.contains(dummy.next.val)) dummy.next=dummy.next.next;
+            else dummy = dummy.next;
+        }
+        return temp.next;
+    }
+}
