08f45ea6827022f0420701a3a12c7f29010c7820
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-09-06 22:45:29 +0530

    Update Solution.java

--- LinkedLists/3217. Delete Nodes From LinkedList Present In Array/Solution.java
+++ LinkedLists/3217. Delete Nodes From LinkedList Present In Array/Solution.java
@@ -9,7 +9,7 @@
  * }
  */
 class Solution {
-    public ListNode modifiedList(int[] nums, ListNode head) {
+    public ListNode modifiedList01(int[] nums, ListNode head) {
         Set<Integer> set = new HashSet<>();
         for(int i:nums){
             set.add(i);
@@ -21,6 +21,23 @@ class Solution {
         while(set.contains(dummy.val)){
             if (set.contains(dummy.val)) dummy = dummy.next;
         }
+
+        while(dummy.next!=null){
+            if(set.contains(dummy.next.val)) dummy.next=dummy.next.next;
+            else dummy = dummy.next;
+        }
+        return temp.next;
+    }
+
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        Set<Integer> set = new HashSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+
+        ListNode temp = new ListNode(0,head);
+        ListNode dummy = temp;
+        
 
         while(dummy.next!=null){
             if(set.contains(dummy.next.val)) dummy.next=dummy.next.next;
