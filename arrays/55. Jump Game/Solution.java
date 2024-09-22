3c7b4931f9923ccb29aeff26a94b52aa41ef2c70
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-09-22 22:22:49 +0530

    Create Solution.java

---
+++ arrays/55. Jump Game/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canJumpNaive(int[] nums) {
+        boolean[] arr = new boolean[nums.length];
+        int n = nums.length;
+        if(n==1) return true;
+        arr[0] = true;
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(arr[i] && j<=(i+nums[i])&&j<n){
+                arr[j]=true;
+                j++;
+            }
+            if(arr[i]==false) return false;
+        }
+        return arr[n-1];
+    }
+
+    public boolean canJump(int[] nums){
+        int reachable = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i>reachable) return false;
+            reachable = Math.max(nums[i]+i,reachable);
+        }
+        return true;
+    }
+}
