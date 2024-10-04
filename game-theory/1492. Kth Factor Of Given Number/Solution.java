e0cfe96a115cc5830975d3f9bc3b8c6dc0b20c30
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2024-10-04 17:46:10 +0530

    Update Solution.java

--- game-theory/1492. Kth Factor Of Given Number/Solution.java
+++ game-theory/1492. Kth Factor Of Given Number/Solution.java
@@ -1,9 +1,22 @@
 class Solution {
-    public int kthFactor(int n, int k) {
+    public int kthFactorOofN(int n, int k) {
         int counter=0;
         for(int i=1;i<=n;i++){
             if(n%i==0) counter++;
             if(counter==k) return i;
+        }
+        return -1;
+    }
+
+    public int kthFactor(int n, int k) {
+        int counter=0;
+        for(int i=1;i<Math.sqrt(n);i++){
+            if(n%i==0) ++counter;
+            if(counter==k) return i;
+        }
+        for(int i=(int)Math.sqrt(n);i>=1;i--){
+            if(n%(n/i)==0) counter++;
+            if(counter==k) return n/i;
         }
         return -1;
     }
