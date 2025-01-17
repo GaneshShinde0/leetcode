99c5ecd9d08ccc3585395d921050ab492c1208df
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-01-17 08:25:03 +0530

    Create Solution.java

---
+++ bit-manipulation/2683 Neighboring Bitwise XOR/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public boolean doesValidArrayExistSimulation(int[] derived) {
+        // Create an original array initialized with 0.
+        int[] original = new int[derived.length + 1];
+        original[0] = 0;
+        for (int i = 0; i < derived.length; i++) {
+            original[i + 1] = derived[i] ^ original[i];
+        }
+        // Store the validation results in checkForZero and checkForOne respectively.
+        boolean checkForZero = (original[0] == original[original.length - 1]);
+
+        original[0] = 1;
+        for (int i = 0; i < derived.length; i++) {
+            original[i + 1] = derived[i] ^ original[i];
+        }
+        boolean checkForOne = (original[0] == original[original.length - 1]);
+
+        return checkForZero || checkForOne;
+    }
+
+    // Cumulative XOR
+    public boolean doesValidArrayExist(int[] derived) {
+        int XOR = 0;
+        for (int element : derived) {
+            XOR = XOR ^ element;
+        }
+        return XOR == 0;
+    }
+
+    // Using Sum
+    public boolean doesValidArrayExistUsingSum(int[] derived) {
+        int sum = 0;
+        for (int num : derived) {
+            sum += num;
+        }
+        return sum % 2 == 0;
+    }
+}
