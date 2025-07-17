fc102b749b0660a090aef02f73b0b02d79341a44
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-07-17 21:49:31 +0530

    Time: 1 ms (94.66%) | Memory: 41.2 MB (69.17%) - LeetSync

---
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int count = 0;
+        int currSub = 0;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=currSub){
+                currSub = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
