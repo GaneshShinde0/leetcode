7fcec9fd99086e31892bd8cb92145556abc23be3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-07-17 21:53:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (42.31%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minimumOperations(int[] nums) {
+    public int minimumOperationsInitial(int[] nums) {
         int count = 0;
         int currSub = 0;
         Arrays.sort(nums);
@@ -11,4 +11,18 @@ class Solution {
         }
         return count;
     }
+    public int minimumOperations(int[] nums) {
+        int[] x=new int[101];
+        int count =0;
+        for (int i=0; i<nums.length; i++){
+            int num = nums[i];
+            if(num != 0 && x[num] == 0){
+                x[num]= 1;
+                count++;
+            }
+            
+        }
+        return (count);
+    }
+
 }
