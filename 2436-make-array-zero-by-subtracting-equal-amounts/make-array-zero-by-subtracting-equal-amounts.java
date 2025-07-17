8ccefe6ea7f580ed669b173a15b471df657baec1
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-07-17 21:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (13.96%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -16,8 +16,8 @@ class Solution {
         int count =0;
         for (int i=0; i<nums.length; i++){
             int num = nums[i];
-            if(num != 0 && x[num] == 0){
-                x[num]= 1;
+            if(num != 0 && x[num] == 0){ // x[num]==0 means we check if the element was previously substracted.
+                x[num]= 1; // If the element was substracted we mark that as yes ... so that it will not come up in later subtractions
                 count++;
             }
             
