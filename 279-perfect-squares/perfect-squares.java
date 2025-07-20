0cae91b8dda1ace6a86272a6081ee6cd942b8240
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-07-20 09:42:57 +0530

    Time: 36 ms (52.73%) | Memory: 42.7 MB (68.89%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -6,6 +6,7 @@ class Solution {
         for(int i=1;i<=n;i++){
             for(int j=1;j*j<=i;j++){
                 dp[i] = Math.min(dp[i],dp[i-j*j]+1); // So basically we are checking our current solution with previous integer's solution. if there exists solution for i-j*j we will use that .. otherwise no
+                /* For 0 it will be 0 ... for 1 it will be 1, for 2 it will be 1+1, for three it will be 1+1+1 for four it will be 1 (as 2*2 = 4)  */
             }
         }
         return dp[n];
