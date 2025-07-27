0862bb40cdd553f4576890e679398c85a404239e
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-07-27 18:29:10 +0530

    Time: 3 ms (58.83%) | Memory: 45.3 MB (18.64%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,4 +1,15 @@
 class Solution {
+    public int minimumTotalInitialSolution(List<List<Integer>> triangle) {
+        for(int i=triangle.size()-1;i>0;i--){
+            List<Integer> li = triangle.get(i);
+            for(int j=0;j<li.size()-1;j++){
+                Integer temp = Math.min(li.get(j),li.get(j+1))+triangle.get(i-1).get(j);
+                triangle.get(i-1).set(j,temp);
+            }
+        }
+        return (int) triangle.get(0).get(0);
+    }
+
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int[] dp = new int[n + 1];
