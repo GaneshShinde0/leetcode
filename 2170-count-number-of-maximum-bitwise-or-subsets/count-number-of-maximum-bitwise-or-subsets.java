76c942057acfa41c4e4bd7ce62871b450265e5ce
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-07-28 06:21:21 +0530

    Time: 67 ms (26.87%) | Memory: 41.2 MB (64.39%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int countMaxOrSubsets(int[] nums) {
+    public int countMaxOrSubsetsRecursion(int[] nums) {
         int maxOrValue = 0, n = nums.length;
         for(int i=0;i<n; i++){
             maxOrValue |= nums[i];
@@ -11,5 +11,38 @@ class Solution {
         int countWithout = countSubsets(nums, index+1, currentOr,targetOr);
         int countWith = countSubsets(nums,index+1, currentOr|nums[index], targetOr);
         return countWithout+countWith;
+    }
+
+    public int countMaxOrSubsetsMemoization(int[] nums){
+        int n = nums.length, maxOrValue = 0;
+        for(int i:nums) maxOrValue |= i;
+        Integer[][] memo = new Integer[n][maxOrValue+1];
+        return countSubsetsRecursive(nums,0,0,maxOrValue, memo);
+    }
+    public int countSubsetsRecursive(int[] nums, int index, int currentOr, int targetOr, Integer[][] memo){
+        if(index == nums.length) return currentOr==targetOr?1:0;
+        if(memo[index][currentOr]!=null){
+            return memo[index][currentOr];
+        }
+        int countWithout = countSubsetsRecursive(nums,index+1, currentOr, targetOr, memo);
+        int countWith = countSubsetsRecursive(nums,index+1, currentOr|nums[index], targetOr, memo);
+        return memo[index][currentOr] = countWithout + countWith;
+    }
+    // Not getting this approach
+    public int countMaxOrSubsets(int[] nums){
+        int n = nums.length, maxOrValue = 0;
+        for(int i:nums) maxOrValue |= i;
+        int totalSubsets = 1<<nums.length;
+        int subsetsWithMaxOr = 0;
+        // Iterate through all possible subsets
+        for(int subsetMask = 0; subsetMask<totalSubsets; subsetMask++){
+            int currentOrValue = 0;
+            // Calculate OR value for the current subset
+            for(int i=0; i<nums.length; i++){
+                if(((subsetMask>>i)&1)==1) currentOrValue |=nums[i];
+            }
+            if(currentOrValue==maxOrValue) subsetsWithMaxOr++;
+        }
+        return subsetsWithMaxOr;
     }
 }
