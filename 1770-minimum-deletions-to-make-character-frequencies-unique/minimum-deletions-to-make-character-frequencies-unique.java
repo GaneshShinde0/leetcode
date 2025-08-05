d00a0e287b4ee400291b71597c4e5da6c05a0c00
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-05 23:41:30 +0530

    Time: 9 ms (42.54%) | Memory: 45.5 MB (66.16%) - LeetSync

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minDeletions(String s) {
+        int count = 0;
+        int[] freq = new int[26];
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
+        }
+        Arrays.sort(freq);
+        Set<Integer> set = new HashSet<>();
+        for(int i:freq){
+            int temp =i;
+            while(set.contains(i) && i>0){
+                i--;
+            }
+            set.add(i);
+            count +=(temp-i);
+            // System.out.println(set);
+        }
+        return count;
+    }
+}
