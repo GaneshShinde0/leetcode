df9b650fdeca1ae54fdde498df6984cda8ec96ad
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-10 11:31:18 +0530

    Time: 11 ms (94.53%) | Memory: 47.3 MB (39.55%) - LeetSync

--- 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.java
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.java
@@ -3,11 +3,11 @@ class Solution {
         int[] freqS = new int[26];
         int[] freqT = new int[26];
         int distA=0, distB=0;
-        for(int i=0; i<s.length();i++){
-            freqS[s.charAt(i)-'a']++;
+        for(char c:s.toCharArray()){
+            freqS[c-'a']++;
         }
-        for(int i=0; i<t.length();i++){
-            freqT[t.charAt(i)-'a']++;
+        for(char c:t.toCharArray()){
+            freqT[c-'a']++;
         }
         int res = 0;
         for(int i=0;i<26;i++){
