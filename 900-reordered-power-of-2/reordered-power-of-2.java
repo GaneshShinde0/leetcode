ec85d988ff172fc44b99c5f25239277397cae8f5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-10 10:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (85.79%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.java
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -1,16 +1,32 @@
 class Solution {
+
     public boolean reorderedPowerOf2(int n) {
+        int count = counter(n);
+
+        for (int i = 0; i < 30; ++i)
+        if (counter(1 << i) == count)
+            return true;
+
+        return false;
+    }
+
+    private int counter(int n) {
+        int count = 0;
+
+        for (; n > 0; n /= 10)
+        count += Math.pow(10, n % 10);
+
+        return count;
+    }
+    public boolean reorderedPowerOf2_3ms(int n) {
         Set<String> set = new HashSet<>();
         int curr = 1;
         for(int i=0;i<=32;i++){
             char[] s = String.valueOf(curr).toCharArray();
-            // System.out.println(Math.pow(2,i));
-            
             Arrays.sort(s);
             set.add(new String(s));
             curr = curr*2;
         }
-        // System.out.println(set);
         char[] nArr = String.valueOf(n).toCharArray();
         Arrays.sort(nArr);
         return set.contains((new String(nArr)));
