6180f56dbd238c38666355490b9e4b0eebf3cea2
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-11 08:04:10 +0530

    Time: 4 ms (91.40%) | Memory: 90.5 MB (43.01%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -1,6 +1,6 @@
 class Solution {
     private static final int MOD = 1000000007;
-    public int[] productQueries(int n, int[][] queries) {
+    public int[] productQueriesUsingLI(int n, int[][] queries) {
         List<Long> li = new ArrayList<>();
         long curr = 1;
         while(n>0){
@@ -24,6 +24,40 @@ class Solution {
         for(int i=0;i<queries.length;i++){
             res[i]=(int) temp[queries[i][0]][queries[i][1]]%MOD;
         }
+        return res;
+    }
+
+    public int[] productQueries(int n, int[][] queries) {
+        int count = Integer.bitCount(n);
+        long[] arr = new long[count];
+        long curr = 1;
+        int index = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                arr[index++] = curr;
+            }
+            curr *= 2;
+            n >>= 1;
+        }
+
+        int size = arr.length;
+        long[][] temp = new long[size][size];
+        
+        for (int i = 0; i < size; i++) {
+            temp[i][i] = arr[i] % MOD; // Initialize diagonal
+            for (int j = i + 1; j < size; j++) {
+                temp[i][j] = (temp[i][j - 1] * arr[j]) % MOD;
+            }
+        }
+
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
+            res[i] = (int)(temp[l][r] % MOD);
+        }
+
         return res;
     }
 }
