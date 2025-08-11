eca31dcea5288b114283ca8ec47ce698a0cbcbab
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-11 08:02:25 +0530

    Time: 10 ms (87.10%) | Memory: 85.8 MB (48.39%) - LeetSync

---
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.java
@@ -0,0 +1,29 @@
+class Solution {
+    private static final int MOD = 1000000007;
+    public int[] productQueries(int n, int[][] queries) {
+        List<Long> li = new ArrayList<>();
+        long curr = 1;
+        while(n>0){
+            if(n%2==1) li.add(curr);
+            curr*=2;
+            n/=2;
+        }
+        int size = li.size();
+        long[][] temp = new long[size][size];
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                temp[i][j] = 1;
+                for(int k=i;k<=j;k++){
+                    temp[i][j]= (temp[i][j]*li.get(k))%MOD;
+                }
+            }
+            System.out.println(Arrays.toString(temp[i]));
+        }
+
+        int[] res = new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            res[i]=(int) temp[queries[i][0]][queries[i][1]]%MOD;
+        }
+        return res;
+    }
+}
