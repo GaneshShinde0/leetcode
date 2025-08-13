365e18966d925a29f7b8c1f194a5d90f617c9228
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-13 07:26:04 +0530

    Time: 8 ms (84.41%) | Memory: 44.1 MB (67.49%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -13,11 +13,6 @@ class Solution {
         while(n%3==0&&n>1){
             n=n/3;
         }
-        if(n==1){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return n==1;
     }
 }
