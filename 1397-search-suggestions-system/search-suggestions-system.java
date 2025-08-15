bf13ea3a0393f9cd73b9995e6a79b2d2aef73841
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-15 22:13:48 +0530

    Time: 19 ms (65.67%) | Memory: 46.7 MB (92.21%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.java
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -1,12 +1,18 @@
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
         Arrays.sort(products);
+        int start=0;
         List<List<String>> res = new ArrayList<>();
         for(int i=0;i<searchWord.length();i++){
             String s = searchWord.substring(0,i+1);
             List<String> li = new ArrayList<>();
-            for(int j=0;j<products.length && li.size()<3;j++){
+            boolean first= false;
+            for(int j=start;j<products.length && li.size()<3;j++){
                 if(products[j].startsWith(s)){
+                    if(!first){
+                        start = j;
+                        first = true;
+                    }
                     li.add(products[j]);
                 }
             }
