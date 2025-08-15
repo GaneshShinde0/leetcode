346b2bd9110a4d6f675084ef29399fe0ef597fa4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-15 22:20:07 +0530

    Time: 28 ms (46.93%) | Memory: 46.7 MB (92.21%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.java
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+    public List<List<String>> suggestedProductsSort(String[] products, String searchWord) {
         Arrays.sort(products);
         int start=0;
         List<List<String>> res = new ArrayList<>();
@@ -19,5 +19,28 @@ class Solution {
             res.add(li);
         }
         return res;
+    }
+
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+    PriorityQueue<String> pq = new PriorityQueue<>(3, (s1,s2) -> s1.compareTo(s2)); 
+    List<List<String>> list = new ArrayList<>();
+    
+    for(int i = 1; i<=searchWord.length(); i++){
+        String temp = searchWord.substring(0, i);
+        for(String s : products){
+        if(s.startsWith(temp)){
+            pq.offer(s);
+        }
+        }
+        List<String> temp_list = new ArrayList<>();
+        for(int j = 0; j<3; j++){
+        if(pq.peek() != null){
+            temp_list.add(pq.poll());
+        }
+        }
+        pq.clear();
+        list.add(temp_list);
+    }
+    return list;
     }
 }
