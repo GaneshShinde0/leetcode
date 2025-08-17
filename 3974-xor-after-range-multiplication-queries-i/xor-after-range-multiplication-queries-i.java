e5e2f086f9894135498922792ff5e94c07d342b2
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-17 20:04:20 +0530

    Time: 438 ms (5.04%) | Memory: 75.3 MB (6.54%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.java
@@ -1,6 +1,5 @@
 class Solution {
-    private static final int MOD = 1000000007;
-    public int xorAfterQueries(int[] nums, int[][] queries) {
+    public int xorAfterQueries1(int[] nums, int[][] queries) {
         for(int[] query:queries){
             int idx = query[0];
             int r = query[1];
@@ -13,6 +12,45 @@ class Solution {
         int res =0;
         for(int i:nums){
             res^=i;
+        }
+        return res;
+    }
+
+    private static final int MOD = 1_000_000_007;
+
+    public int xorAfterQueries(int[] nums, int[][] queries) {
+        int n = nums.length;
+        // Step 1: For each index, track all multipliers
+        List<List<Integer>> multipliers = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            multipliers.add(new ArrayList<>());
+        }
+
+        for (int[] query : queries) {
+            int idx = query[0];
+            int r = query[1];
+            int k = query[2];
+            int v = query[3];
+
+            // Only go through affected indices
+            for (int i = idx; i <= r; i += k) {
+                multipliers.get(i).add(v);
+            }
+        }
+
+        // Step 2: Apply all multipliers per index
+        for (int i = 0; i < n; i++) {
+            long val = nums[i];
+            for (int v : multipliers.get(i)) {
+                val = (val * v) % MOD;
+            }
+            nums[i] = (int) val;
+        }
+
+        // Step 3: Compute XOR
+        int res = 0;
+        for (int val : nums) {
+            res ^= val;
         }
         return res;
     }
