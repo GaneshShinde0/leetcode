3d35d7fa0bf5a3a69b06657277d820e3fc8a7350
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-24 08:37:08 +0530

    Time: 3 ms (77.92%) | Memory: 56.4 MB (87.86%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestSubarray(int[] nums) {
+    public int longestSubarrayInitialSolution(int[] nums) {
         // Store index Of last 0 
         List<List<Integer>> li = new ArrayList<>();
         int n = nums.length, curr=1;
@@ -32,5 +32,27 @@ class Solution {
         }
         // System.out.println(li);
         return res;
+    }
+
+    public int longestSubarray(int[] nums) {
+        // Number of zero's in the window.
+        int zeroCount = 0;
+        int longestWindow = 0;
+        // Left end of the window.
+        int start = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            zeroCount += (nums[i] == 0 ? 1 : 0);
+                          
+            // Shrink the window until the count of zero's
+            // is less than or equal to 1.
+            while (zeroCount > 1) {
+                zeroCount -= (nums[start] == 0 ? 1 : 0);
+                start++;
+            }
+              
+            longestWindow = Math.max(longestWindow, i - start);
+        }
+        return longestWindow;
     }
 }
