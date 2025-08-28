90595a69d4dd2090a74fee0b711db6988aedbc74
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-28 08:48:40 +0530

    Time: 5 ms (69.27%) | Memory: 45.6 MB (78.44%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[][] sortMatrix(int[][] grid) {
-        int n= grid.length;
-
+        int n = grid.length;
         for(int i=0;i<n;i++){
             List<Integer> temp = new ArrayList<>();
             for(int j=0;i+j<n;j++){
@@ -9,18 +8,18 @@ class Solution {
             }
             temp.sort(Collections.reverseOrder());
             for(int j=0;i+j<n;j++){
-                grid[i+j][j] = temp.get(j);
+                grid[i+j][j]=temp.get(j);
             }
         }
 
         for(int j=1;j<n;j++){
             List<Integer> temp = new ArrayList<>();
-            for(int i=0;i+j<n; i++){
+            for(int i=0;i+j<n;i++){
                 temp.add(grid[i][i+j]);
             }
             Collections.sort(temp);
-            for(int i=0; i+j<n; i++){
-                grid[i][i+j] = temp.get(i);
+            for(int i=0;i+j<n;i++){
+                grid[i][i+j]=temp.get(i);
             }
         }
         return grid;
