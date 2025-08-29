9131b0beeb00902e7ab92d6da7da237c5eff4a79
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-29 20:31:43 +0530

    Time: 25 ms (49.22%) | Memory: 62.8 MB (14.42%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean checkSubarraySumFailsThreeTestCases(int[] nums, int k) {
         int currSum = 0;
-        int res = 0;
         int n=nums.length;
         HashMap<Integer, Integer> hm = new HashMap<>();
         for(int i=0;i<nums.length;i++){
@@ -15,9 +14,8 @@ class Solution {
     }
 
     
-    public boolean checkSubarraySum(int[] nums, int k) {
+    public boolean checkSubarraySumMine(int[] nums, int k) {
         int currSum = 0;
-        int res = 0;
         int n=nums.length;
         HashMap<Integer, Integer> hm = new HashMap<>();
         hm.put(0, -1); // important: handles subarray starting at index 0
@@ -27,6 +25,21 @@ class Solution {
             hm.putIfAbsent((k-currSum%k)%k,i);
         }
         // System.out.println(hm);
+        return false;
+    }
+
+    public boolean checkSubarraySum(int[] nums, int k){
+        int n = nums.length;
+        int remainder = 0;
+        int currSum=0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0,-1);// This will handle when array is starting from 0;
+        for(int i=0;i<nums.length;i++){
+            currSum+=nums[i];
+            remainder = (k+currSum%k)%k;
+            if(hm.containsKey(remainder) && i-hm.get(remainder)>=2)return true;
+            hm.putIfAbsent(remainder,i);
+        }
         return false;
     }
 }
