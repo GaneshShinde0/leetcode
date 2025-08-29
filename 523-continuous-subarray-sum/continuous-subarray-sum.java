8ef18bf8216eacbc46f733c4cba986b93996c060
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-29 20:23:31 +0530

    Time: 95 ms (5.02%) | Memory: 65.9 MB (6.40%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkSubarraySumFailsThreeTestCases(int[] nums, int k) {
+        int currSum = 0;
+        int res = 0;
+        int n=nums.length;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            currSum+=nums[i];
+            hm.putIfAbsent((k-currSum%k)%k,i);
+            if(i>0 && currSum%k==0) return true;
+            if((hm.containsKey(currSum%k)||nums[i]==0) &&(i-hm.getOrDefault(k-currSum%k,0))>=1) return true;
+        }
+        System.out.println(hm);
+        return false;
+    }
+
+    
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int currSum = 0;
+        int res = 0;
+        int n=nums.length;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(0, -1); // important: handles subarray starting at index 0
+        for(int i=0;i<nums.length;i++){
+            currSum+=nums[i];
+            if(hm.containsKey((k-currSum%k)%k) && (i-hm.get((k-currSum%k)%k)>1)) return true;
+            hm.putIfAbsent((k-currSum%k)%k,i);
+        }
+        System.out.println(hm);
+        return false;
+    }
+}
