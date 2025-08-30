935a44acb1eda03ae0f235c75f5b91f899ea8949
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-30 22:44:30 +0530

    Time: 6 ms (98.69%) | Memory: 48.1 MB (43.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,22 +1,33 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public List<List<String>> groupAnagramsInitial(String[] strs) {
         List<List<String>> res = new ArrayList<>();
         HashMap<String, Integer> hm = new HashMap<>();
         int curr = 0;
         for(String s: strs){
             char[] ch = s.toCharArray();
             Arrays.sort(ch);
-            String tempS = new String(ch);
-            int temp = hm.getOrDefault(tempS,curr);
+            String sortedString = new String(ch);
+            int temp = hm.getOrDefault(sortedString,curr);
             if(temp>=res.size()){
                 res.add(new ArrayList<>());
                 res.get(curr).add(s);
-                hm.put(tempS,temp);
+                hm.put(sortedString,temp);
                 curr++;
             }else{
                 res.get(temp).add(s);
             }
         }
         return res;
+    }
+    
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
     }
 }
