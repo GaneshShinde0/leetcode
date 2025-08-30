3ea8be300b502733347b43e3f924214ae715aa6e
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-08-30 07:11:19 +0530

    Time: 2 ms (77.42%) | Memory: 44.3 MB (89.38%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,27 +1,27 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character>[] rows = new HashSet[9];
-        HashSet<Character>[] cols = new HashSet[9];
-        HashSet<Character>[] boxes = new HashSet[9];
-
-        for(int i=0;i<9;i++){
+        int n = board.length;
+        HashSet<Character>[] rows = new HashSet[n];
+        HashSet<Character>[] cols = new HashSet[n];
+        HashSet<Character>[] boxes = new HashSet[n];
+        for(int i=0;i<n;i++){
             rows[i] = new HashSet<>();
             cols[i] = new HashSet<>();
             boxes[i] = new HashSet<>();
         }
-        for(int r = 0; r<9; r++){
-            for(int c = 0; c<9; c++){
-                if(board[r][c]=='.') continue;
-                char val = board[r][c];
-                int boxIndex = (r/3)*3+(c/3);
-                if(rows[r].contains(val) || cols[c].contains(val) || boxes[boxIndex].contains(val)){
-                    return false;
-                }
-                rows[r].add(val);
-                cols[c].add(val);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='.') continue;
+                char val = board[i][j];
+                int boxIndex = (i/3)*3+j/3;
+                if(rows[i].contains(val)||cols[j].contains(val)||boxes[boxIndex].contains(val)) return false;
+                rows[i].add(val);
+                cols[j].add(val);
                 boxes[boxIndex].add(val);
             }
         }
+
         return true;
     }
 }
