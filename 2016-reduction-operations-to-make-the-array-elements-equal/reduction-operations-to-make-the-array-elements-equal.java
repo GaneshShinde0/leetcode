d2d72ca6b16656a91d11ff5ff632f1c8db03444d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-05 21:03:08 +0530

    Time: 34 ms (58.92%) | Memory: 56 MB (43.78%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
@@ -1,15 +1,15 @@
 class Solution {
     public int reductionOperations(int[] nums) {
         Arrays.sort(nums);
-        int result = 0;
+        int totalOperations = 0;
         int n = nums.length;
         int currrentOperatedWindowLength = 1;
         for(int i=n-1;i>0;i--){
             if(nums[i]!=nums[i-1]){
-                result+=currrentOperatedWindowLength;
+                totalOperations+=currrentOperatedWindowLength;
             }
             currrentOperatedWindowLength++;
         }
-        return result;
+        return totalOperations;
     }
 }
