89358d7c9b872af1955e49342858135476143265
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-05 18:57:57 +0530

    Time: 693 ms (8.71%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,2 +1,22 @@
 /* Write your T-SQL query statement below */
-SELECT MAX(SALARY) as SecondHighestSalary from employee WHERE salary< (select max(salary) from employee);
+--SELECT MAX(SALARY) as SecondHighestSalary from employee WHERE salary< (select max(salary) from employee);
+/*
+SELECT ISNULL((SELECT DISTINCT SALARY FROM EMPLOYEE ORDER BY SALARY DESC
+OFFSET 1 ROWS FETCH NEXT 1 ROWS ONLY),NULL) AS SecondHighestSalary;
+*/
+/*
+SELECT TOP 1 lead(salary) over (order by salary desc) as SecondHighestSalary
+FROM (Select DISTINCT SALARY FROM EMPLOYEE) a
+ORDER By SALARY DESC
+*/
+/*
+SELECT DISTINCT SALARY as SecondHighestSalary FROM
+(SELECT Salary, dense_rank() over (order by salary desc) as rank from employee) a
+right join (select 1 as rank union select 2 as rank) b
+on a.rank = b.rank
+where b.rank = 2;
+*/
+
+SELECT max(a.salary) as SecondHighestSalary from Employee a
+RIGHT join EMPLOYEE B
+on a.salary<b.salary;
