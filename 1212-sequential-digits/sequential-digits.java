35eae7fc007542e6bb2ecad91f2a865046afc739
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-05 14:18:25 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (47.94%) - LeetSync

--- 1212-sequential-digits/sequential-digits.java
+++ 1212-sequential-digits/sequential-digits.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<Integer> sequentialDigits(int low, int high) {
+    public List<Integer> sequentialDigitsSimpleSolution(int low, int high) {
         int[] allNums = {12,23,34,45,56,67,78,89,
                          123,234,345,456,567,678,789,
                          1234,2345,3456,4567,5678,6789,
@@ -14,6 +14,21 @@ class Solution {
             if (allNums[i] < low) continue;
             if (allNums[i] > high) break;
             res.add(allNums[i]);
+        }
+        return res;
+    }
+
+    public List<Integer> sequentialDigits(int low, int high){
+        String digits = "123456789";
+        List<Integer> res = new ArrayList<>();
+        int nl = String.valueOf(low).length();
+        int nh = String.valueOf(high).length();
+
+        for(int i = nl; i<=nh; i++){
+            for(int j=0; j<10-i;j++){
+                int num = Integer.parseInt(digits.substring(j, j+i));
+                if(num>=low && num<=high) res.add(num);
+            }
         }
         return res;
     }
