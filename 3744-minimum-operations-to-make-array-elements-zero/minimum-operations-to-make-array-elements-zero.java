424a6215b1bb34f3ea784549647a4e9cac4975b7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-06 07:46:02 +0530

    Time: 48 ms (5.56%) | Memory: 101.6 MB (8.33%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.java
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.java
@@ -1,5 +1,5 @@
 class Solution {
-    public long minOperations(int[][] queries) {
+    public long minOperations1(int[][] queries) {
         long res = 0;
         for(int[] q : queries){
             long count1 = get(q[1]);
@@ -20,5 +20,17 @@ class Solution {
             base *= 2;
         }
         return count;
+    }
+
+    long getCount(long n){
+        long cnt = 1 + ((n == 0)?0: (long)(Math.log(n) / Math.log(4)));   // this is just 1 + log n to base 4 i.e log(a)/log(b) = log(n)/log(4)
+        long sumOfGp = ((cnt <= 0)?0: (long)((Math.pow(4, cnt) - 1)/3));  // sum of gp = a.(r^t -1)/(r - 1) and here a = 1, r = 4 and t = cnt
+        return cnt * (n + 1) - sumOfGp;                                   // apply the formula we deduced
+    } 
+
+    long minOperations(int[][] queries) {
+        long ans = 0;
+        for(int[] q: queries) ans += (getCount(q[1]) - getCount(q[0]-1) + 1)/2;
+        return ans;
     }
 }
