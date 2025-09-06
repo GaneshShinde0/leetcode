9ef618432aa944c4c55fa50d2353c61d5de25fd4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-06 14:34:41 +0530

    Time: 34 ms (72.71%) | Memory: 55.4 MB (72.07%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,45 @@
+class Trie {
+    private Trie[] children;
+    private boolean isWord;
+
+    public Trie() {
+        children = new Trie[26];
+        isWord = false;
+    }
+    
+    public void insert(String word) {
+        Trie node = this;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (node.children[idx] == null) {
+                node.children[idx] = new Trie();
+            }
+            node = node.children[idx];
+        }
+        node.isWord = true;
+    }
+    
+    public boolean search(String word) {
+        Trie node = this;
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+            if (node.children[idx] == null) {
+                return false;
+            }
+            node = node.children[idx];
+        }
+        return node.isWord;
+    }
+    
+    public boolean startsWith(String prefix) {
+        Trie node = this;
+        for (char c : prefix.toCharArray()) {
+            int idx = c - 'a';
+            if (node.children[idx] == null) {
+                return false;
+            }
+            node = node.children[idx];
+        }
+        return true;
+    }
+}
