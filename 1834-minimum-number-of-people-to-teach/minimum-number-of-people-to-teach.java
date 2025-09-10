0e27d6aef494d61cf8618d4a63d9839714842041
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-10 09:06:20 +0530

    Time: 46 ms (75.86%) | Memory: 66.1 MB (5.17%) - LeetSync

--- 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.java
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.java
@@ -1,31 +1,47 @@
 class Solution {
     public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
         Set<Integer> cannotCommunicate = new HashSet<>();
-        for(int[] friendship : friendships){
-            Map<Integer,Integer> hm = new HashMap<>();
-            boolean comm = false;
-            for(int lan:languages[friendship[0]-1]){
-                hm.put(lan,1);
+        for(int[] friendship:friendships){
+            boolean areCommunicating = false;
+            Set<Integer> langKnown = new HashSet<>();
+            for(int i:languages[friendship[0]-1]){
+                langKnown.add(i);
             }
-            for(int lan:languages[friendship[1]-1]){
-                if(hm.containsKey(lan)){
-                    comm = true;
+            for(int j:languages[friendship[1]-1]){
+                if(langKnown.contains(j)){
+                    areCommunicating = true;
                     break;
                 }
             }
-            if(!comm){
+            if(!areCommunicating){
                 cannotCommunicate.add(friendship[0]-1);
                 cannotCommunicate.add(friendship[1]-1);
             }
         }
-        int max_cnt = 0;
-        int[] cnt = new int[n+1];
-        for(int friendship:cannotCommunicate){
-            for(int lan:languages[friendship]){
-                cnt[lan]++;
-                max_cnt = Math.max(max_cnt, cnt[lan]);
+        int maxLangKnown = 0;
+        int[] langCount = new int[n+1];
+        for(int i: cannotCommunicate){
+            for(int j: languages[i]){
+                langCount[j]++;
+                maxLangKnown = Math.max(maxLangKnown,langCount[j]);
             }
         }
-        return cannotCommunicate.size()-max_cnt;
+        return cannotCommunicate.size()-maxLangKnown;
     }
 }
+
+
+        // Map<Integer,Integer> hm = new HashMap<>();
+        // for(int[] lang:languages){
+        //     for(int i:lang){
+        //         hm.put(i,hm.getOrDefault(i,0)+1);
+        //     }
+        // }
+        // int res =languages[0][0];
+        // for(Map.Entry<Integer,Integer> e: hm.entrySet()){
+        //     if(e.getValue()>hm.get(res)){
+        //         res = e.getKey();
+        //     }
+        // }
+
+        // return res;
