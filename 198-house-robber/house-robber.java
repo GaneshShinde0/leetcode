e6962f08c09a4f79c050fa7fdfb78e43362f38f0
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-11 06:05:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (46.96%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,16 @@
 class Solution {
-    int[] memo;
     public int rob(int[] nums) {
-        memo = new int[nums.length + 1];
-        Arrays.fill(memo, -1);
-        return rob(nums, nums.length - 1);
-    }
-
-    private int rob(int[] nums, int i) {
-        if (i < 0) {
-            return 0;
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]); // Fix: correct initialization
+        
+        for(int i = 2; i < n; i++){
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]); // Fix: compare with dp[i-1]
         }
-        if (memo[i] >= 0) {
-            return memo[i];
-        }
-        int result = Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
-        memo[i] = result;
-        return result;
+        
+        return dp[n-1]; // Fix: return the last element
     }
 }
