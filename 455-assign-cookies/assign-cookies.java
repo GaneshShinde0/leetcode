249c1f5306d73d3f037de9ea08a78c2428a4f300
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-11 12:58:52 +0530

    Time: 9 ms (98.60%) | Memory: 48.6 MB (60.03%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int childrens = g.length, chocolates = s.length;
+        int currentChild = 0;
+        int res =0;
+        for(int i=0;i<s.length;i++){
+            // while(currentChild<childrens && s[i]<g[currentChild]){
+            //     currentChild++;
+            // }
+            if(currentChild<childrens && s[i]>=g[currentChild]){
+                res++;
+                currentChild++;
+            }
+        }
+        return res;
+    }
+}
