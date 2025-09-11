68cd540a36aabb6da7793e317eeb50bcd1ffb2b9
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-11 13:11:02 +0530

    Time: 3 ms (43.11%) | Memory: 45.9 MB (43.82%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,9 +1,34 @@
 class Solution {
-    public boolean canJump(int[] nums) {
+    public boolean canJumpAlternate(int[] nums) {
         int reachable = 0;
         for(int i=0;i<nums.length;i++){
             if(reachable>=i) reachable = Math.max(nums[i]+i,reachable);
         }
         return reachable>=nums.length-1;
     }
-} 
+
+    public boolean canJumpNaive(int[] nums) {
+        boolean[] arr = new boolean[nums.length];
+        int n = nums.length;
+        if(n==1) return true;
+        arr[0] = true;
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(arr[i] && j<=(i+nums[i])&&j<n){
+                arr[j]=true;
+                j++;
+            }
+            if(arr[i]==false) return false;
+        }
+        return arr[n-1];
+    }
+
+    public boolean canJump(int[] nums){
+        int reachable = 0;
+        for(int i=0;i<nums.length;i++){
+            if(i>reachable) return false;
+            reachable = Math.max(nums[i]+i,reachable);
+        }
+        return true;
+    }
+}
