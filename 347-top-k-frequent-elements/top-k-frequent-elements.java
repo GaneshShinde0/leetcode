50db447cf863d281c31d6b40bae7b2639b743755
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-12 10:42:07 +0530

    Time: 13 ms (75.82%) | Memory: 49.3 MB (13.48%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    public int[] topKFrequentNLogN(int[] nums, int k) {
         int[][] freq = new int[20001][2];
         for(int i=-10000;i<=10000;i++){
             freq[i+10000][0]= i;
@@ -11,6 +11,24 @@ class Solution {
         int[] res = new int[k];
         for(int i=0;i<k;i++){
             res[i]=freq[i][0]-1;
+        }
+        return res;
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> counter = new HashMap<>();
+        for(int num:nums){
+            counter.put(num,counter.getOrDefault(num,0)+1);
+        }
+        PriorityQueue<Map.Entry<Integer,Integer>> heap = new PriorityQueue<>(
+            (a,b)->Integer.compare(b.getValue(), a.getValue())
+        );
+        for(Map.Entry<Integer,Integer> entry:counter.entrySet()){
+            heap.offer(entry);
+        }
+        int[] res = new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=heap.poll().getKey();
         }
         return res;
     }
