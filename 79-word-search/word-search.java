e94a78ff88a547ec467580c231f084e28cecb83e
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-12 18:19:27 +0530

    Time: 176 ms (30.27%) | Memory: 41.2 MB (99.27%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -5,32 +5,34 @@ class Solution {
         {0,-1},
         {-1,0}
     };
+
     public boolean exist(char[][] board, String word) {
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[i].length;j++){
-                if (dfs(board,word,i,j,0)) return true;
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[i].length; j++) {
+                if(dfs(board, word, i, j, 0)) return true;
             }
         }
         return false;
     }
 
-    private boolean dfs(char[][] board, String word, int row, int col,int i){
-        if(i==word.length()) return true;
-        if((col<0 || col>=board[0].length) || (row<0 || row>=board.length)){
-            return false;
-        }
-        boolean check = false;
+    private boolean dfs(char[][] board, String word, int row, int col, int index) {
+        if(index == word.length()) return true;
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length) return false;
+        if(board[row][col] != word.charAt(index)) return false;
+
         char temp = board[row][col];
-        if(board[row][col]==word.charAt(i)){
-            board[row][col]='/';
-            for(int[] dir:directions){
-                int newRow = row+dir[0];
-                int newCol = col+dir[1];
-                check |= dfs(board,word,newRow,newCol,i+1);
-                if(check) return true;
+        board[row][col] = '/';  // mark visited
+
+        for(int[] dir : directions) {
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+            if(dfs(board, word, newRow, newCol, index + 1)) {
+                board[row][col] = temp;  // restore before returning
+                return true;
             }
-            board[row][col]=temp;
         }
+
+        board[row][col] = temp;  // restore
         return false;
     }
 }
