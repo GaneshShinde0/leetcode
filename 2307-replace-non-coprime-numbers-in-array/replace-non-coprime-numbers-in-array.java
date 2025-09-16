f877f5c783ae7f4f7887d642e335777e3230652f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-16 08:50:22 +0530

    Time: 75 ms (25.77%) | Memory: 65.1 MB (26.80%) - LeetSync

---
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> replaceNonCoprimes(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> stk = new Stack<>();
+        for(int num:nums){
+            while(!stk.isEmpty()){
+                int temp = stk.peek();
+                int x = gcd(temp,num);
+                if(x==1) break;
+                num *= temp/x;
+                stk.pop();
+            }
+            stk.add(num);
+        }
+        List<Integer> result = new ArrayList<>();
+        while(!stk.isEmpty()){
+            result.add(stk.pop());
+        }
+        Collections.reverse(result);
+        return result;
+    }
+
+    private int gcd(int a, int b){
+        return b==0?a:gcd(b,a%b);
+    }
+}
