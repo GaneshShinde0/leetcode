43cf154328265d9029d883d81d3b7067d1fc7d63
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-21 18:29:54 +0530

    Time: 1 ms (93.39%) | Memory: 45.6 MB (42.11%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.java
+++ 419-battleships-in-a-board/battleships-in-a-board.java
@@ -5,7 +5,7 @@ class Solution {
         int n = board[0].length;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                // j == n-1, i==m-1 considers cases when edges have 0 length
+                // j == n-1, i==m-1 considers cases when we are dealing with last row and last column
                 if(board[i][j]=='X' && (j==n-1 ||board[i][j+1]=='.') && (i==m-1 || board[i+1][j]=='.')) count++;
             }
         }
