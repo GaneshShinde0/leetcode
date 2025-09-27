fc01d3e497f1ae9c4d513c5883fdea2a7c436ceb
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-27 06:55:04 +0530

    Time: 1 ms (100.00%) | Memory: 53.3 MB (16.61%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -16,6 +16,22 @@ class Solution {
         }
         return res;
     }
+
+    public int[][] restoreMatrixSpaceComplexityGreedy(int[] rowSum, int[] colSum) {
+        int N = rowSum.length;
+        int M = colSum.length;
+
+        int[][] origMatrix = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                origMatrix[i][j] = Math.min(rowSum[i], colSum[j]);
+
+                rowSum[i] -= origMatrix[i][j];
+                colSum[j] -= origMatrix[i][j];
+            }
+        }
+        return origMatrix;
+    }
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
         int numrows = rowSum.length;
         int colrows = colSum.length;
