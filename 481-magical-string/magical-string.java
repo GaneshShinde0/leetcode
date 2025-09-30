c6415ede11dd58b14580dfc27f69989728e9429f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-09-30 09:12:58 +0530

    Time: 2 ms (98.05%) | Memory: 41 MB (96.31%) - LeetSync

--- 481-magical-string/magical-string.java
+++ 481-magical-string/magical-string.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int magicalString(int n) {
+    public int magicalStringInitial(int n) {
         StringBuilder sb = new StringBuilder();
         sb.append("122");
         int curr = 1;
@@ -16,5 +16,31 @@ class Solution {
             if(sb.charAt(i)=='1') res++;
         }
         return res;
+    }
+
+    static int N = (int)1e5 + 1;
+    static int[] prefixSum;
+
+    public int magicalString(int n) {
+
+        if(prefixSum == null){
+            int[] nums = new int[N];
+            nums[0] = 1;
+            nums[1] = 2;
+            nums[2] = 2;
+            int size = 3, index = 2, x = 2;
+            while(size < N){
+                x ^= 3;
+                int count = nums[index++];
+                while(size < N && count-- > 0)
+                    nums[size++] = x;
+            }
+
+            prefixSum = new int[N];
+            for(int i = 0; i < N - 1; ++i)
+                prefixSum[i + 1] = prefixSum[i] + 2 - nums[i];
+        }
+        
+        return prefixSum[n];
     }
 }
