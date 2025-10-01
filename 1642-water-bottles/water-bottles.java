e2f2cbbf0b5738508f258bbab8bb9428ade06865
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-01 19:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (43.30%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int res =0;
+        int emptyBottles = 0;
+        while(numBottles>0){
+            res+=numBottles;
+            emptyBottles+=numBottles;
+            numBottles = emptyBottles/numExchange;
+            emptyBottles = emptyBottles%numExchange;
+        }
+        return res;
+    }
+}
