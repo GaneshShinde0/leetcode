7c112bf4ac41fcfe3fe0997d34de2807c111bc81
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-01 22:43:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (91.50%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.java
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maxBottlesDrunk(int numBottles, int numExchange) {
+    public int maxBottlesDrunkInitial(int numBottles, int numExchange) {
         int emptyBottles = numBottles, bottlesDrank = numBottles;
         numBottles = 0;
         while(numBottles>0||emptyBottles>=numExchange){
@@ -13,6 +13,19 @@ class Solution {
                 numBottles=0;
             }
         }
+        return bottlesDrank;
+    }
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int emptyBottles = numBottles;
+        int bottlesDrank = numBottles;
+
+        while (emptyBottles >= numExchange) {
+            emptyBottles -= numExchange; // trade for one new bottle
+            numExchange++;              // exchange cost increases
+            bottlesDrank++;             // drink that bottle
+            emptyBottles++;             // empty bottle from drinking
+        }
+
         return bottlesDrank;
     }
 }
