b794021fc968f3f9f295176eab7d544762a416c4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-02 15:50:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (78.98%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int reverseBits(int n) {
+    public int reverseBitsInitial(int n) {
         String s = Integer.toBinaryString(n);
         // System.out.println("Original: "+s);
 
@@ -10,5 +10,15 @@ class Solution {
         // System.out.println(rev.toString());
         return Integer.parseInt(rev.toString(),2);
 
+    }
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        return ans;
     }
 }
