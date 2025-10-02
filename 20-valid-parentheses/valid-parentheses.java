ed27a4ea78ebf951497df749ddf5f0e49f351ee2
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-02 15:23:15 +0530

    Time: 2 ms (96.90%) | Memory: 42 MB (32.81%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isValid(String s) {
+    public boolean isValidInitial(String s) {
         Stack<Character> stk = new Stack<>();
         for(int i=0;i<s.length();i++){
             if(!stk.isEmpty()){
@@ -16,6 +16,23 @@ class Solution {
                 stk.push(s.charAt(i));
             }
         }   
+        return stk.isEmpty();
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stk.push(c);
+            } else {
+                if (stk.isEmpty()) return false;
+                char top = stk.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
         return stk.isEmpty();
     }
 }
