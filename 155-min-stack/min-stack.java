499d43e47786bd5eae2446c5c8c361e75a5d98fc
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-04 12:00:29 +0530

    Time: 4 ms (98.12%) | Memory: 45 MB (48.51%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -42,6 +42,35 @@ class MinStack{
         return st.isEmpty()?-1:st.peek();
     }
 }
+
+// class MinStack{
+//     Stack<Long> s = new Stack<>();
+//     long minEle;
+//     public void push(int val){
+//         if(s.isEmpty()){
+//             s.push((long) val);
+//             minEle=val;
+//         }else if(val<minEle){
+//             s.push(2L*val-minEle);
+//             minEle = val;
+//         }else{
+//             s.push((long) val);
+//         }
+//     }
+
+//     public void pop(){
+//         long t = s.pop();
+//         if(t<minEle) minEle = 2*minEle-t;
+//     }
+
+//     public int top(){
+//         long t = s.peek();
+//         return t<minEle? (int)minEle:(int)t;
+//     }
+//     public int getMin(){
+//         return (int) minEle;
+//     }
+// }
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
