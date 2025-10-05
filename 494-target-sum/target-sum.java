6a8e57a08566335e232b3e952a3027767ac9e8a8
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-05 13:28:58 +0530

    Time: 7 ms (67.08%) | Memory: 44.8 MB (30.01%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
+    public int findTargetSumWaysInitial(int[] nums, int target) {
         Map<String, Integer> dp = new HashMap<>();
         return backtrack(nums,0,0,dp,target);
     }
@@ -9,5 +9,27 @@ class Solution {
         if(dp.containsKey(i+"-"+total)) return dp.get(i+"-"+total);
         dp.put(i+"-"+total, backtrack(nums,i+1,total+nums[i],dp,target)+backtrack(nums,i+1,total-nums[i],dp,target));
         return dp.get(i+"-"+total);
+    }
+    public int findTargetSumWays(int[] nums, int target) {
+        int total = 0;
+        for (int num : nums) total += num;
+        
+        // If target is out of possible range, return 0 early
+        if (Math.abs(target) > total) return 0;
+
+        // memo[i][sum + total] = ways
+        Integer[][] memo = new Integer[nums.length][2 * total + 1];
+        return dfs(nums, 0, 0, target, total, memo);
+    }
+
+    private int dfs(int[] nums, int i, int sum, int target, int offset, Integer[][] memo) {
+        if (i == nums.length) return sum == target ? 1 : 0;
+        if (memo[i][sum + offset] != null) return memo[i][sum + offset];
+
+        int add = dfs(nums, i + 1, sum + nums[i], target, offset, memo);
+        int subtract = dfs(nums, i + 1, sum - nums[i], target, offset, memo);
+
+        memo[i][sum + offset] = add + subtract;
+        return memo[i][sum + offset];
     }
 }
