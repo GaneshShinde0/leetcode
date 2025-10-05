c07ac9a7b716ab3b8f7a3bfe524950e0a8f4c9f6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-05 14:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (5.71%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int climbStairs(int n) {
+    public int climbStairsUsingTwoVariables(int n) {
         int n1 = 0, n2 =1;
         int i=1;
         if(n==1) return 1;
@@ -10,5 +10,24 @@ class Solution {
             n1=temp;
         }
         return n2;
+    }
+    public int climbStairsDPArray(int n) {
+        int[] dp = new int[n+2];
+        dp[1] = 1;
+        for(int i = 2;i<=n+1;i++){
+            dp[i]=dp[i-2]+dp[i-1];
+        }
+        return dp[n+1];
+    }
+
+    public int climbStairs(int n){
+        int one=1;
+        int two =1;
+        for(int i=1;i<n;i++){
+            int temp = one;
+            one = one+two;
+            two = temp;
+        }
+        return one;
     }
 }
