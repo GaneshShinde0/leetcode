07ad7b9f2b819f175ca741526c3f1aaa9becbdc8
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-05 17:43:53 +0530

    Time: 2 ms (45.46%) | Memory: 41.5 MB (51.68%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.java
@@ -5,6 +5,8 @@ class Solution {
         if(nums[0]>=n) return n;
 
         for(int i=1;i<=n;i++){
+            // Following condition checks if number at index n-i greater than or equal to i,
+            // It also makes sure that number at index n-i-1 is less than i;
             if(nums[n-i]>=i && nums[n-i-1]<i){
                 return i;
             }
