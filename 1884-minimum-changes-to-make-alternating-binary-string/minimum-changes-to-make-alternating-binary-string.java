fbfb8f8aa7ce5640ba18a8d467316ec5d18ae6c2
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-05 18:50:53 +0530

    Time: 3 ms (88.40%) | Memory: 42.8 MB (23.97%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minOperations(String s) {
+        StringBuilder sb = new StringBuilder();
+        boolean turn = true;
+        int count = 0,count2=0;
+        for(char c:s.toCharArray()){
+            if(turn&&c=='0') ;
+            else if(!turn&&c=='1') ;
+            else count++;
+            turn = !turn;
+        }
+        for(char c:s.toCharArray()){
+            if(!turn&&c=='0') ;
+            else if(turn&&c=='1') ;
+            else count2++;
+            turn = !turn;
+        }
+        int res = Math.min(count,count2);
+        return Math.min(res,s.length()-res);
+    }
+}
