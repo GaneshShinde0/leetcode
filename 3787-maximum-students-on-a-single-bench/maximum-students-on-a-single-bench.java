7b3cc41737289f39cc73bf7b72ecf89a472d515c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-06 21:28:54 +0530

    Time: 5 ms (78.38%) | Memory: 45.7 MB (8.11%) - LeetSync

--- 3787-maximum-students-on-a-single-bench/maximum-students-on-a-single-bench.java
+++ 3787-maximum-students-on-a-single-bench/maximum-students-on-a-single-bench.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maxStudentsOnBench(int[][] students) {
+    public int maxStudentsOnBenchInitial(int[][] students) {
         Map<Integer, TreeSet<Integer>> hm = new HashMap<>();
         int res = 0;
         for(int[] s:students){
@@ -8,4 +8,15 @@ class Solution {
         }
         return res;
     }
+    
+    public int maxStudentsOnBench(int[][] students) {
+        Map<Integer, HashSet<Integer>> hm = new HashMap<>();
+        int res = 0;
+        for(int[] s:students){
+            hm.computeIfAbsent(s[1],k->new HashSet<Integer>()).add(s[0]);
+            res = Math.max(res,hm.get(s[1]).size());
+        }
+        return res;
+    }
+
 }
