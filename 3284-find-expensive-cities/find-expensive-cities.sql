9ddeefeac8935b7dae744dfa1358723061e90738
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-06 22:30:18 +0530

    Time: 364 ms (69.23%) | Memory: 0B (100.00%) - LeetSync

--- 3284-find-expensive-cities/find-expensive-cities.sql
+++ 3284-find-expensive-cities/find-expensive-cities.sql
@@ -7,3 +7,49 @@ ORDER By city ASC
 SELECT  city FROM Listings group by city
 HAVING  sum(price)/count(*)>(SELECT SUM(price)/count(1) as nat FROM Listings)
 ORDER BY city asc
+
+/*
+-- Method 1
+-- SELECT city
+-- FROM Listings
+-- GROUP BY city
+-- HAVING AVG(price) > (SELECT AVG(price) FROM Listings)
+-- ORDER BY city
+ 
+
+
+-- Method 2
+WITH NationalAverage
+AS (
+    SELECT AVG(price) AS national_average
+    FROM Listings
+),
+
+CityAverage 
+AS (
+    SELECT city, AVG(price) as city_average
+    FROM Listings
+    GROUP BY city
+)
+
+SELECT city
+FROM CityAverage
+JOIN NationalAverage 
+ON TRUE
+WHERE city_average > national_average
+ORDER BY city
+*/
+
+/*
+WITH expense_window AS (
+    SELECT DISTINCT city
+    , AVG(price) OVER (PARTITION BY city) AS city_average
+    , AVG(price) OVER () AS national_average
+    FROM Listings
+)
+
+SELECT city
+FROM expense_window
+WHERE city_average > national_average
+ORDER BY city ASC
+*/
