d8407c87969affd7cc3fee5908a3fa33897fab13
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-06 20:58:34 +0530

    Time: 249 ms (90.00%) | Memory: 0B (100.00%) - LeetSync

--- 1438-find-the-team-size/find-the-team-size.sql
+++ 1438-find-the-team-size/find-the-team-size.sql
@@ -1,2 +1,4 @@
 /* Write your T-SQL query statement below */
-SELECT employee_id,team_size FROM Employee e left join (SELECT team_id,COUNT(*) as team_size from Employee GROUP by team_id)as src on e.team_id=src.team_id
+/*SELECT employee_id,team_size FROM Employee e left join (SELECT team_id,COUNT(*) as team_size from Employee GROUP by team_id)as src on e.team_id=src.team_id
+*/
+select employee_id, count(*) OVER (partition by team_id) as team_size from employee
