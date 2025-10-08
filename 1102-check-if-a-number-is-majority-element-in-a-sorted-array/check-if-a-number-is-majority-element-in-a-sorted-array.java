103056b20e652025707f11e658702e2237db83b7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-08 10:13:18 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (90.14%) - LeetSync

--- 1102-check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.java
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.java
@@ -1,10 +1,38 @@
 class Solution {
-    public boolean isMajorityElement(int[] nums, int target) {
+    public boolean isMajorityElementInitial(int[] nums, int target) {
         Map<Integer, Integer> hm = new HashMap<>();
         int maxFreq = Integer.MIN_VALUE;
         for(int i:nums){
             hm.put(i,hm.getOrDefault(i,0)+1);
         }
         return hm.getOrDefault(target,0)>(nums.length/2);
+    }
+    // As nums is sorted;
+    public boolean isMajorityElement(int[] nums, int target) {
+        if(nums[nums.length / 2] != target) {
+            return false;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        int start = left;
+        right = nums.length - 1;
+        while(left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        int end = nums[right] == target ? right : left;
+        return end - start + 1 > nums.length / 2;
     }
 }
