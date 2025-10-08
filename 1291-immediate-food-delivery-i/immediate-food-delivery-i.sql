c81107d66ab6875f37117c1283b360ac05f9d9ae
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-08 10:36:27 +0530

    Time: 967 ms (32.43%) | Memory: 0B (100.00%) - LeetSync

--- 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
+++ 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
@@ -1,3 +1,9 @@
+-- /* Write your T-SQL query statement below */
+-- SELECT 100*ROUND((SELECT COUNT(*) FROM Delivery WHERE order_date = customer_pref_delivery_date)
+-- /(SELECT count(*)*1.0 from delivery),4) as immediate_percentage
 /* Write your T-SQL query statement below */
-SELECT 100*ROUND((SELECT COUNT(*) FROM Delivery WHERE order_date = customer_pref_delivery_date)
-/(SELECT count(*)*1.0 from delivery),4) as immediate_percentage
+
+
+select 
+    round(sum(case when order_date = customer_pref_delivery_date then 1.0 else 0 end) / count(*), 4) * 100 as immediate_percentage
+from Delivery
