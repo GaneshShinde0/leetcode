57d4f81ae6823c7ff5b28475d49a5f2c947b08c3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-08 10:11:42 +0530

    Time: 2 ms (7.74%) | Memory: 42.4 MB (7.24%) - LeetSync

---
+++ 1102-check-if-a-number-is-majority-element-in-a-sorted-array/check-if-a-number-is-majority-element-in-a-sorted-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isMajorityElement(int[] nums, int target) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        int maxFreq = Integer.MIN_VALUE;
+        for(int i:nums){
+            hm.put(i,hm.getOrDefault(i,0)+1);
+        }
+        return hm.getOrDefault(target,0)>=(nums.length/2+1);
+    }
+}
