368a25e5d62b498c92862da4a2d6e80f676d05e4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-09 20:09:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (51.98%) - LeetSync

--- 246-strobogrammatic-number/strobogrammatic-number.java
+++ 246-strobogrammatic-number/strobogrammatic-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isStrobogrammatic(String num) {
+    public boolean isStrobogrammaticInitial(String num) {
         String num2 = (new StringBuilder(num).reverse().toString());
         for(int i=0;i<num.length();i++){
             boolean check1 = (num.charAt(i)=='6'&&num2.charAt(i)=='9');
@@ -11,5 +11,30 @@ class Solution {
             else return false;
         }
         return true;
+    }
+    Map<Character, Character> map = Map.of('0','0','1','1','6','9','8','8','9','6');
+    public boolean isStrobogrammatic(String num) {
+        int left = 0;
+        int right = num.length() - 1;
+
+        while (left < right) {
+            char cLeft = num.charAt(left);
+            char cRight = num.charAt(right);
+
+            if (!map.containsKey(cLeft) || map.get(cLeft) != cRight) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        if (left == right) {
+            char c = num.charAt(left);
+            return c == '0' || c == '1' || c == '8';
+        }
+
+        return true;
+
+
     }
 }
