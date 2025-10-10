c4b0dd9e86f56c2f74ba80ea56c9c4507e413401
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-11 05:23:45 +0530

    Time: 3 ms (94.44%) | Memory: 55.2 MB (94.44%) - LeetSync

--- 2029-minimize-product-sum-of-two-arrays/minimize-product-sum-of-two-arrays.java
+++ 2029-minimize-product-sum-of-two-arrays/minimize-product-sum-of-two-arrays.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minProductSum(int[] nums1, int[] nums2) {
+    public int minProductSumInitial(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         int res = 0, n = nums1.length;
@@ -7,5 +7,26 @@ class Solution {
             res+=nums1[i]*nums2[n-i-1];
         }
         return res;
+    }
+    public int minProductSum(int[] nums1, int[] nums2) {
+        int[] count1 = new int[101]; // since nums[i] <= 100
+        int[] count2 = new int[101];
+
+        for (int x : nums1) count1[x]++;
+        for (int x : nums2) count2[x]++;
+
+        int sum = 0;
+        int i = 0, j = 100;
+        while (i <= 100 && j >= 0) {
+            if (count1[i] == 0) { i++; continue; }
+            if (count2[j] == 0) { j--; continue; }
+
+            int freq = Math.min(count1[i], count2[j]);
+            sum += freq * i * j;
+            count1[i] -= freq;
+            count2[j] -= freq;
+        }
+
+        return sum;
     }
 }
