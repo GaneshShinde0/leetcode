843660a9cff746960e7b41d62f1d454c7af87a22
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-10 21:53:41 +0530

    Time: 2 ms (100.00%) | Memory: 41.6 MB (93.43%) - LeetSync

--- 1083-two-sum-less-than-k/two-sum-less-than-k.java
+++ 1083-two-sum-less-than-k/two-sum-less-than-k.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int twoSumLessThanK(int[] nums, int k) {
+    public int twoSumLessThanK1(int[] nums, int k) {
         int res = -1;
         for(int i=0;i<nums.length-1;i++){
             for(int j=i+1;j<nums.length;j++){
@@ -9,5 +9,20 @@ class Solution {
             }
         }
         return res;
+    }
+    public int twoSumLessThanK(int[] nums, int k) {
+        Arrays.sort(nums);
+        int l = 0, r = nums.length - 1;
+        int res = Integer.MIN_VALUE;
+        while (r > l) {
+            if (nums[l] + nums[r] >= k) {
+                r--;
+            } else {
+                res = Math.max(res, nums[l] + nums[r]);
+                l++;
+            }
+        }
+        
+        return res == Integer.MIN_VALUE ? -1 : res;
     }
 }
