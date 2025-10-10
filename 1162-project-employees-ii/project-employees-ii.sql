692161293ff9e37d16fd9199564b024166501b8c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-10 22:22:22 +0530

    Time: 1401 ms (5.08%) | Memory: 0B (100.00%) - LeetSync

--- 1162-project-employees-ii/project-employees-ii.sql
+++ 1162-project-employees-ii/project-employees-ii.sql
@@ -1,2 +1,10 @@
-# Write your MySQL query statement below
-SELECT project_id FROM Project GROUP BY project_id having count(*)= (SELECT max(count) from (SELECT count(*) as count,project_id from Project group by project_id) src)
+-- # Write your MySQL query statement below
+-- SELECT project_id FROM Project GROUP BY project_id having count(*)= (SELECT max(count) from (SELECT count(*) as count,project_id from Project group by project_id) src)
+
+select project_id
+from(
+select project_id,rank() over(order by count(distinct employee_id)desc ) rnk
+from Project
+group by project_id    
+)t
+where rnk =1 
