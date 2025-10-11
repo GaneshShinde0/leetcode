e1cfe4719d3bdbb8e594714db193ab3b9121308b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-11 18:39:52 +0530

    Time: 3 ms (91.84%) | Memory: 73.3 MB (14.95%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,5 +1,5 @@
 class Solution {
-    public double averageWaitingTime(int[][] customers) {
+    public double averageWaitingTimeInitial(int[][] customers) {
         double totalWaitTime = 0;
         double currEndTime = 0;
         for(int i=0;i<customers.length;i++){
@@ -10,6 +10,15 @@ class Solution {
                 totalWaitTime+=currEndTime-customers[i][0]+customers[i][1];
                 currEndTime = currEndTime+customers[i][1];
             }
+        }
+        return totalWaitTime/customers.length;
+    }
+
+    public double averageWaitingTime(int[][] customers) {
+        double totalWaitTime=0, currentEndTime=0;
+        for(int[] customer:customers){
+            currentEndTime = Math.max(currentEndTime,customer[0])+customer[1];
+            totalWaitTime += currentEndTime-customer[0];
         }
         return totalWaitTime/customers.length;
     }
