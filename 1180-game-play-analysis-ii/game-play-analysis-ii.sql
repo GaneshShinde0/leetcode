fad4d3c4570ffdb65378eca0ca6e379e5e0c8754
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-12 21:48:03 +0530

    Time: 1187 ms (76.47%) | Memory: 0B (100.00%) - LeetSync

--- 1180-game-play-analysis-ii/game-play-analysis-ii.sql
+++ 1180-game-play-analysis-ii/game-play-analysis-ii.sql
@@ -1,3 +1,8 @@
-SELECT a.player_id,a.device_id FROM (SELECT player_id, MIN(event_Date) as min_Date FROM Activity GROUP BY player_id) dt
-JOIN Activity a ON a.player_id = dt.player_id
-AND a.event_Date = dt.min_date
+-- SELECT a.player_id,a.device_id FROM (SELECT player_id, MIN(event_Date) as min_Date FROM Activity GROUP BY player_id) dt
+-- JOIN Activity a ON a.player_id = dt.player_id
+-- AND a.event_Date = dt.min_date
+
+WITH Players AS
+(SELECT player_id, event_date as first_login, device_id, ROW_NUMBER() OVER(partition by player_id ORDER BY event_Date) as player_index FROM Activity)
+
+SELECT player_id,device_id FROM players where player_index = 1;
