31b1fd8a393e7adb99870735636c6a54aff2c987
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-12 21:31:23 +0530

    Time: 1314 ms (69.90%) | Memory: 0B (100.00%) - LeetSync

--- 1181-game-play-analysis-iii/game-play-analysis-iii.sql
+++ 1181-game-play-analysis-iii/game-play-analysis-iii.sql
@@ -1,5 +1,7 @@
-select a.player_id, aa.event_date, sum(a.games_played) as games_played_so_far
-from activity as a
-     inner join activity as aa
-     on a.player_id = aa.player_id and a.event_date <= aa.event_date
-group by a.player_id, aa.event_date
+-- select a.player_id, aa.event_date, sum(a.games_played) as games_played_so_far
+-- from activity as a
+--      inner join activity as aa
+--      on a.player_id = aa.player_id and a.event_date <= aa.event_date
+-- group by a.player_id, aa.event_date
+
+SELECT a.player_id, a.event_date,sum(games_played) over(partition by a.player_id order by event_date) as games_played_so_far from activity as a  
