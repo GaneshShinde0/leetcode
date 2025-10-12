f7cd1660724298c061273531f6d9583876918eb5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-12 21:00:59 +0530

    Time: 1282 ms (76.70%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1181-game-play-analysis-iii/game-play-analysis-iii.sql
@@ -0,0 +1,5 @@
+select a.player_id, aa.event_date, sum(a.games_played) as games_played_so_far
+from activity as a
+     inner join activity as aa
+     on a.player_id = aa.player_id and a.event_date <= aa.event_date
+group by a.player_id, aa.event_date
