5fcc6102efdffb921669ab5ab447f7b262402967
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-12 22:23:03 +0530

    Time: 2592 ms (47.22%) | Memory: 0B (100.00%) - LeetSync

--- 1172-sales-analysis-i/sales-analysis-i.sql
+++ 1172-sales-analysis-i/sales-analysis-i.sql
@@ -1,4 +1,15 @@
-SELECT TOP 1 WITH TIES seller_id
-FROM Sales
-GROUP BY seller_id
-ORDER BY SUM(price) DESC;
+WITH SellerSales AS (
+    SELECT
+        seller_id,
+        SUM(price) AS total_sales
+    FROM
+        Sales
+    GROUP BY
+        seller_id
+)
+SELECT
+    seller_id
+FROM
+    SellerSales
+WHERE
+    total_sales = (SELECT MAX(total_sales) FROM SellerSales);
