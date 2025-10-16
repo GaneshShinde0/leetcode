4644d98a57a568a7083839187d58a9f1d6ab8979
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-16 21:34:14 +0530

    Time: 3 ms (100.00%) | Memory: 46.4 MB (29.94%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minMeetingRooms(int[][] intervals) {
+    public int minMeetingRoomsInitial40Min(int[][] intervals) {
         int numOfRooms = 1;
         int n = intervals.length;
         TreeMap<Integer,Integer> hm = new TreeMap<>();
@@ -14,5 +14,26 @@ class Solution {
             numOfRooms = Math.max(counter,numOfRooms);
         }
         return numOfRooms;
+    }
+    public int minMeetingRooms(int[][] intervals) {
+        int length = intervals.length;
+        int[] start = new int[length];
+        int[] end = new int[length];
+        for(int i=0; i<intervals.length; i++){
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int i=0, j=0, count=0;
+        while(i<length && j<length){
+            if(start[i]<end[j]){
+                count++;
+                i++;
+            }else{
+                i++; j++;
+            }
+        }
+        return count;
     }
 }
