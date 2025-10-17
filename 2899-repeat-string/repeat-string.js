7dbfb3963a0dbd332284471373703182cd3a6a95
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-17 21:50:06 +0530

    Time: 78 ms (15.74%) | Memory: 67.8 MB (23.15%) - LeetSync

--- 2899-repeat-string/repeat-string.js
+++ 2899-repeat-string/repeat-string.js
@@ -7,10 +7,18 @@
 //     return this + this.replicate(times-1);
 // }
 
+// String.prototype.replicate = function(times) {
+//     let result = "";
+//     for(let i = 0;i<times;i++){
+//         result+=this;
+//     }
+//     return result;
+// }
+
 String.prototype.replicate = function(times) {
-    let result = "";
-    for(let i = 0;i<times;i++){
-        result+=this;
+    const result = [];
+    for(let i=0;i<times;i++){
+        result.push(this);
     }
-    return result;
-}
+    return result.join('');
+};
