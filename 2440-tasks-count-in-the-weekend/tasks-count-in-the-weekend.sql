e5b841756260bd7237216035b0332387250bdea0
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-19 06:41:02 +0530

    Time: 376 ms (88.24%) | Memory: 0B (100.00%) - LeetSync

--- 2440-tasks-count-in-the-weekend/tasks-count-in-the-weekend.sql
+++ 2440-tasks-count-in-the-weekend/tasks-count-in-the-weekend.sql
@@ -1,2 +1,8 @@
 /* Write your T-SQL query statement below */
-SELECT count(*) as weekend_cnt, (SELECT COUNT(*) FROM TASKS)-COUNT(*) as working_cnt FROM Tasks WHERE DATENAME(WEEKDAY,submit_date) IN ('Saturday','Sunday')
+-- SELECT count(*) as weekend_cnt, (SELECT COUNT(*) FROM TASKS)-COUNT(*) as working_cnt FROM Tasks WHERE DATENAME(WEEKDAY,submit_date) IN ('Saturday','Sunday')
+
+select sum(case when wkdy=7 or wkdy=1 then 1 else 0 end) as weekend_cnt, sum(case when wkdy=7 or wkdy=1 then 0 else 1 end) as working_cnt 
+from (
+select *, DATEPART(weekday, submit_date) as wkdy
+from Tasks 
+) a
