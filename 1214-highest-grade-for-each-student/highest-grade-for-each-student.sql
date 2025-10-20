e1bd6a268c3f52dcd4cec361170ce42ebf037672
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-20 07:24:52 +0530

    Time: 1183 ms (81.82%) | Memory: 0B (100.00%) - LeetSync

--- 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
+++ 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
@@ -1,3 +1,12 @@
+-- /* Write your T-SQL query statement below */
+-- WITH CTE AS (SELECT student_id,course_id, MAX(GRADE) over(PARTITION BY student_id) as grade, rank() OVER (PARTITION BY student_id order by grade desc, course_id asc) rank FROM Enrollments)
+-- SELECT student_id,course_id,grade FROM CTE WHERE rank = 1;
+
 /* Write your T-SQL query statement below */
-WITH CTE AS (SELECT student_id,course_id, MAX(GRADE) over(PARTITION BY student_id) as grade, rank() OVER (PARTITION BY student_id order by grade desc, course_id asc) rank FROM Enrollments)
-SELECT student_id,course_id,grade FROM CTE WHERE rank = 1;
+with cte as (
+select * , row_number() over (partition by student_id order by  grade desc , course_id   ) ranked
+from Enrollments as e
+)
+select student_id , course_id  , grade
+from cte
+where ranked=1
