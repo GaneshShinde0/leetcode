b8323cf4e818e9c6a2300b5d5f76dd06a398be9e
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-22 22:46:20 +0530

    Time: 409 ms (90.00%) | Memory: 0B (100.00%) - LeetSync

--- 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,4 +1,14 @@
-/* Write your T-SQL query statement below */
-SELECT customer_id,customer_name FROM Customers WHERE customer_id in (SELECT customer_id FROM Orders WHERE product_name = 'A') 
-AND customer_id in (SELECT customer_id FROM Orders WHERE product_name = 'B')
-AND customer_id not in (SELECT customer_id FROM Orders WHERE product_name = 'C')
+-- /* Write your T-SQL query statement below */
+-- SELECT customer_id,customer_name FROM Customers WHERE customer_id IN (SELECT customer_id FROM Orders WHERE product_name = 'A') 
+-- AND customer_id in (SELECT customer_id FROM Orders WHERE product_name = 'B')
+-- AND customer_id not in (SELECT customer_id FROM Orders WHERE product_name = 'C')
+
+
+SELECT c.customer_id, c.customer_name
+FROM Customers c
+JOIN Orders o ON o.customer_id = c.customer_id
+GROUP BY c.customer_id, c.customer_name
+HAVING SUM(CASE WHEN o.product_name = 'A' THEN 1 ELSE 0 END) > 0
+   AND SUM(CASE WHEN o.product_name = 'B' THEN 1 ELSE 0 END) > 0
+   AND SUM(CASE WHEN o.product_name = 'C' THEN 1 ELSE 0 END) = 0
+ORDER BY c.customer_id;
