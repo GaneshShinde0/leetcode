9ac889f99ad73a3bf400754118409db66c23aa88
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-23 11:50:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (9.52%) - LeetSync

--- 1980-faulty-sensor/faulty-sensor.java
+++ 1980-faulty-sensor/faulty-sensor.java
@@ -11,8 +11,8 @@ Algorithm:
 class Solution {
     public int badSensor(int[] s1, int[] s2) {
         int i = 0, n = s1.length;
-        while(i<n && s1[i]==s2[i]) i++;
-        while(i+1<n && s1[i]==s2[i+1] && s1[i+1]==s2[i]) i++;
+        while(i<n && s1[i]==s2[i])i++;
+        while(i+1<n&&s1[i]==s2[i+1] && s2[i]==s1[i+1]) i++;
         return i>=n-1?-1:s1[i]==s2[i+1]?1:2;
     }
 }
