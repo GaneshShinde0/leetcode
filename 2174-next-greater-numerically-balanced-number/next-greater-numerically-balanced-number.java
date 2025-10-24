29cf02bd4056d556e0525f8c74904ac5b84cd3c7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-24 08:34:06 +0530

    Time: 36 ms (77.69%) | Memory: 44.5 MB (44.62%) - LeetSync

--- 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
+++ 2174-next-greater-numerically-balanced-number/next-greater-numerically-balanced-number.java
@@ -1,25 +1,29 @@
-class Solution {
+import java.util.*;
 
-    public int nextBeautifulNumber(int n) {
-        for (int i = n + 1; i <= 1224444; ++i) {
-            if (isBalance(i)) {
-                return i;
-            }
+class Solution {
+    private static final List<Integer> beautiful = new ArrayList<>();
+
+    static {
+        for (int i = 1; i <= 1224444; i++) {
+            if (isBalance(i)) beautiful.add(i);
         }
-        return -1;
     }
 
-    private boolean isBalance(int x) {
+    private static boolean isBalance(int x) {
         int[] count = new int[10];
         while (x > 0) {
             count[x % 10]++;
             x /= 10;
         }
         for (int d = 0; d < 10; ++d) {
-            if (count[d] > 0 && count[d] != d) {
-                return false;
-            }
+            if (count[d] > 0 && count[d] != d) return false;
         }
         return true;
+    }
+
+    public int nextBeautifulNumber(int n) {
+        int idx = Collections.binarySearch(beautiful, n + 1);
+        if (idx < 0) idx = -idx - 1; // insertion point
+        return idx < beautiful.size() ? beautiful.get(idx) : -1;
     }
 }
