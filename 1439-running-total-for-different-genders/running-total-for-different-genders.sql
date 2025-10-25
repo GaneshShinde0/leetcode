3bb92f225caa6f24b1317a310f0f17fd7806c772
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-25 13:25:46 +0530

    Time: 891 ms (62.07%) | Memory: 0B (100.00%) - LeetSync

--- 1439-running-total-for-different-genders/running-total-for-different-genders.sql
+++ 1439-running-total-for-different-genders/running-total-for-different-genders.sql
@@ -1,6 +1,12 @@
-/* Write your T-SQL query statement below */
-SELECT s1.gender, s1.day, sum(s2.score_points) as total
-FROM scores s1, scores s2 
-WHERE s1.gender = s2.gender and s1.day>=s2.day 
-GROUP by s1.gender, s1.day
-ORDER BY gender, day
+-- /* Write your T-SQL query statement below */
+-- SELECT s1.gender, s1.day, sum(s2.score_points) as total
+-- FROM scores s1, scores s2 
+-- WHERE s1.gender = s2.gender and s1.day>=s2.day 
+-- GROUP by s1.gender, s1.day
+-- ORDER BY gender, day
+
+select
+    gender,
+    day,
+    sum(score_points) over (partition by gender order by day) as total
+from scores
