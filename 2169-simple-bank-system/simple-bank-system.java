37f4c7796b96b1f7d347198dcc7445aeaad307c2
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-26 14:11:55 +0530

    Time: 101 ms (34.94%) | Memory: 80 MB (99.27%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.java
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -2,43 +2,41 @@ class Bank {
 
     private long[] balance;
     private int n;
+
     public Bank(long[] balance) {
-        this.n = balance.length+1;
-        this.balance  = new long[this.n];
-        for(int i=0;i<balance.length;i++){
-            this.balance[i+1]=balance[i];
+        this.n = balance.length + 1;
+        this.balance = new long[this.n];
+        for (int i = 0; i < balance.length; i++) {
+            this.balance[i + 1] = balance[i];
         }
         // System.out.println(Arrays.toString(this.balance));
     }
-    
+
     public boolean transfer(int account1, int account2, long money) {
         // System.out.println(Arrays.toString(this.balance));
-        if(account1<=this.n && account2<=this.n && account1>0 &&account2>0){
-            if(this.balance[account1]>=money){
-                this.balance[account1]-=money;
-                this.balance[account2]+=money;
-                // System.out.println(Arrays.toString(this.balance));
+        if (account1 <= this.n && account2 <= this.n && account1 > 0 && account2 > 0
+                && this.balance[account1] >= money) {
+            this.balance[account1] -= money;
+            this.balance[account2] += money;
+            // System.out.println(Arrays.toString(this.balance));
             return true;
-            }else{
-                return false;
-            }
-        }else{
+        } else {
             return false;
         }
     }
-    
+
     public boolean deposit(int account, long money) {
-        if(account>0 && account<=this.n){
-            this.balance[account]+=money;
+        if (account > 0 && account <= this.n) {
+            this.balance[account] += money;
             // System.out.println(Arrays.toString(this.balance));
             return true;
         }
         return false;
     }
-    
+
     public boolean withdraw(int account, long money) {
-        if(account>0 && account<=n && this.balance[account]>=money){
-            this.balance[account]-=money;
+        if (account > 0 && account <= n && this.balance[account] >= money) {
+            this.balance[account] -= money;
             // System.out.println(Arrays.toString(this.balance));
             return true;
         }
