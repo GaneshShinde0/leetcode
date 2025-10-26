6c2c6188190b3d70492365609c8feb38632b3ca5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-26 09:43:05 +0530

    Time: 101 ms (34.94%) | Memory: 80.4 MB (96.16%) - LeetSync

---
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -0,0 +1,55 @@
+class Bank {
+
+    private long[] balance;
+    private int n;
+    public Bank(long[] balance) {
+        this.n = balance.length+1;
+        this.balance  = new long[this.n];
+        for(int i=0;i<balance.length;i++){
+            this.balance[i+1]=balance[i];
+        }
+        // System.out.println(Arrays.toString(this.balance));
+    }
+    
+    public boolean transfer(int account1, int account2, long money) {
+        // System.out.println(Arrays.toString(this.balance));
+        if(account1<=this.n && account2<=this.n && account1>0 &&account2>0){
+            if(this.balance[account1]>=money){
+                this.balance[account1]-=money;
+                this.balance[account2]+=money;
+                // System.out.println(Arrays.toString(this.balance));
+            return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+    
+    public boolean deposit(int account, long money) {
+        if(account>0 && account<=this.n){
+            this.balance[account]+=money;
+            // System.out.println(Arrays.toString(this.balance));
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean withdraw(int account, long money) {
+        if(account>0 && account<=n && this.balance[account]>=money){
+            this.balance[account]-=money;
+            // System.out.println(Arrays.toString(this.balance));
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank obj = new Bank(balance);
+ * boolean param_1 = obj.transfer(account1,account2,money);
+ * boolean param_2 = obj.deposit(account,money);
+ * boolean param_3 = obj.withdraw(account,money);
+ */
