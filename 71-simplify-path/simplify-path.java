8fbd3cec542ef906f3bcf067d8d164578b93f6c3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-27 18:47:12 +0530

    Time: 11 ms (6.78%) | Memory: 43.6 MB (72.76%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,5 +1,21 @@
 class Solution {
     public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+        for (String s : path.split("/+")) {
+            if (s.equals("") || s.equals(".")) continue;
+            if (s.equals("..")) {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(s);
+            }
+        }
+        // build final path
+        List<String> parts = new ArrayList<>(stack);
+        Collections.reverse(parts);
+        return "/" + String.join("/", parts);
+    }
+
+    public String simplifyPathInitial(String path) {
         StringBuilder sb = new StringBuilder();
         Stack<String> stk = new Stack<>();
         String[] arr = path.split("/+");
