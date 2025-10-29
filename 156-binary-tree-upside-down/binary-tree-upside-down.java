38d77e0426b3d6c6441c36fb10084b708d001a64
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-29 09:01:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (10.69%) - LeetSync

--- 156-binary-tree-upside-down/binary-tree-upside-down.java
+++ 156-binary-tree-upside-down/binary-tree-upside-down.java
@@ -19,13 +19,32 @@ If you see the simulated diagrams you will see previously tree is starting from 
 We need tree which is startign from left most node and projecting all the way to root and right nodes.
 */
 class Solution {
-    public TreeNode upsideDownBinaryTree(TreeNode root) {
+    public TreeNode upsideDownBinaryTreeRecursive(TreeNode root) {
         if(root == null || root.left==null) return root;
-        TreeNode newRoot = upsideDownBinaryTree(root.left);
-        root.left.left = root.right;
-        root.left.right = root;
+        TreeNode newRoot = upsideDownBinaryTree(root.left); 
+        root.left.left = root.right; // Node to left children.
+        root.left.right = root; // Node to right children.
         root.left = null;
         root.right = null;
         return newRoot;
+    }
+
+    public TreeNode upsideDownBinaryTree(TreeNode root){
+        TreeNode curr = root;
+        TreeNode next = null;
+        TreeNode temp = null;
+        TreeNode prev = null;
+
+        while(curr!=null){
+            next = curr.left;
+
+            // Swapping nodes now, need temp to keep the previous right children
+            curr.left = temp;;
+            temp = curr.right;
+            curr.right = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
