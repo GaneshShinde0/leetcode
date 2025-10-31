5a92d799ca5175ba12b46dee10827670f9bf44a1
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-31 11:42:27 +0530

    Time: 9 ms (86.27%) | Memory: 48.9 MB (8.50%) - LeetSync

---
+++ 251-flatten-2d-vector/flatten-2d-vector.java
@@ -0,0 +1,42 @@
+class Vector2D {
+
+    private int[][] vec;
+    private int i,j;
+    private int m,n;
+    public Vector2D(int[][] vec) {
+        this.vec = vec;
+        this.i=0;
+        this.j=0;
+        this.m = vec.length;
+    }
+    
+    public int next() {
+        int res = -1;
+        if(hasNext()){
+            res = this.vec[this.i][this.j];
+            this.j++;
+            if(this.j>=vec[i].length){
+                this.j=0;
+                this.i++;
+            }
+        }
+        return res;
+    }
+    
+    public boolean hasNext() {
+        while(i<m){
+            if(j<vec[i].length){
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D obj = new Vector2D(vec);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
