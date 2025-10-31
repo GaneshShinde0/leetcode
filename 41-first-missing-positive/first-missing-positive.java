1475ab10bd4c69e41b4d0f8976a6da4c7fe049a5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-10-31 11:20:55 +0530

    Time: 1 ms (100.00%) | Memory: 57.6 MB (19.79%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
+    public int firstMissingPositiveInitial(int[] nums) {
         // int min = Integer.MAX_VALUE, max=Integer.MIN_VALUE;
         int n = nums.length;
         int[] freq = new int[n+1];
@@ -11,5 +11,58 @@ class Solution {
             if(freq[i]==0) return i;
         }
         return n+1;
+    }
+    
+    // Index as a Hash Key
+    public int firstMissingPositiveIndexHashKey(int[] nums) {
+        int n = nums.length;
+        boolean contains1 = false;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1) contains1 = true;
+            if(nums[i]<=0||nums[i]>n){
+                nums[i]=1;
+            }
+        }
+        if(!contains1) return 1;
+        for(int i=0;i<n;i++){
+            int curr = Math.abs(nums[i]);
+            if(curr==n) nums[0] = -Math.abs(nums[0]);
+            else nums[curr] = -Math.abs(nums[curr]);
+        }
+
+        // First Positive in nums is smallest positive integer.
+        // As we have set everything else to -ve;
+        for(int i=1;i<n;i++){
+            if(nums[i]>0) return i;
+        }
+        if(nums[0]>0) return n;
+        return n+1;
+
+    }
+    // Approach 3: Cycle Sort
+    public int firstMissingPositive(int[] nums){
+        int n = nums.length;
+        int i=0;
+        while(i<n){
+            int correctIdx = nums[i]-1;
+            if(nums[i]>0 && nums[i]<=n && nums[i]!=nums[correctIdx]){
+                swap(nums,i, correctIdx);
+            }else{
+                i++;
+            }
+        }
+
+        for(i=0;i<n;i++){
+            if(nums[i]!=i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+
+    private void swap(int[] nums, int index1, int index2){
+        int temp = nums[index1];
+        nums[index1]=nums[index2];
+        nums[index2] = temp;
     }
 }
