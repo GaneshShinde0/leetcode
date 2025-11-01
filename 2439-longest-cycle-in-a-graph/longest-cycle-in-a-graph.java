9974eb6436e18f51237faf1b7fc74f822b0d90d5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-01 21:13:27 +0530

    Time: 42 ms (52.51%) | Memory: 128.3 MB (5.02%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.java
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.java
@@ -7,10 +7,10 @@
     // For each node you will either hit dead-end or come back to initial node.
 class Solution {
     public int longestCycle(int[] edges) {
-        HashMap<Integer, List<Integer>> graph = new HashMap<>();
-        for(int i=0;i<edges.length;i++){
-            graph.computeIfAbsent(i,k->new ArrayList<>()).add(edges[i]);
-        }
+        // HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        // for(int i=0;i<edges.length;i++){
+        //     graph.computeIfAbsent(i,k->new ArrayList<>()).add(edges[i]);
+        // }
         int res = -1;
         boolean[] visited = new boolean[edges.length];
         for(int i=0;i<edges.length;i++){
