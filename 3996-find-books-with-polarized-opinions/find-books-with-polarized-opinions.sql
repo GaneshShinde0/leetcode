61d058df0c88fe5ee69475ae44b1fb3dff6d2675
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-01 23:42:40 +0530

    Time: 476 ms (76.19%) | Memory: 0B (100.00%) - LeetSync

--- 3996-find-books-with-polarized-opinions/find-books-with-polarized-opinions.sql
+++ 3996-find-books-with-polarized-opinions/find-books-with-polarized-opinions.sql
@@ -1,28 +1,25 @@
 /* Write your T-SQL query statement below */
-with cte as(
-select
-book_id
-, SUM(case when session_rating >= 4 then 1 else 0 end) as good_ratings
-, SUM(case when session_rating  <=2 then 1 else 0 end) as bad_ratings
-, MAX(session_rating) as highest_rating
-, MIN(session_rating) as lowest_rating
-, COUNT(*) as total_sessions
+
+
+with calcul as(
+SELECT book_id, 
+        MAX(session_rating) may, MIN(session_rating) men ,
+        SUM(CASE WHEN session_rating <=2 then 1 else 0 end) as pmin,
+        SUM(CASE WHEN session_rating >=4 then 1 else 0 end )as pmax,
+        COUNT(*) tot
 from reading_sessions
 group by book_id
-having count(*) >= 5
-)
-, cte2 as(
-select
-b.*
-, highest_rating - lowest_rating as rating_spread
-, ROUND((good_ratings + bad_ratings) / cast(total_sessions as float), 2) as polarization_score
-from cte
-JOIN books as b
-ON cte.book_id = b.book_id
-where cte.good_ratings >= 1 and cte.bad_ratings >= 1
 )
 
-select *
-from cte2
-where polarization_score >= 0.6
-order by polarization_score desc, title desc
+SELECT b.book_id,b.title,b.author,b.genre,b.pages,
+        c.may-c.men as rating_spread,
+        ROUND(( c.pmin + c.pmax *1.00 ) / tot,2) as polarization_score 
+from books b 
+join calcul c 
+on c.book_id = b.book_id
+where ROUND(( c.pmin + c.pmax *1.00) / tot,2) >= 0.6
+        AND c.tot >=5
+        AND  c.pmin>0 AND c.pmax>0 
+
+order by ( c.pmin + c.pmax *1.00 ) / tot desc, b.title desc
+        
