5dd2e95378c3c8f1ac7832dece1aa5d636d2d8e1
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-01 15:45:56 +0530

    Time: 2 ms (84.62%) | Memory: 43.3 MB (7.69%) - LeetSync

---
+++ 1199-hexspeak/hexspeak.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String toHexspeak(String num) {
+        String s = Long.toHexString(Long.parseLong(num));
+        StringBuilder sb = new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(c=='0'){
+                sb.append('O');
+            }else if (c=='1'){
+                sb.append('I');
+            }else if(Character.isDigit(c)){
+                return "ERROR";
+            }else{
+                sb.append(c);
+            }
+        }
+        return sb.toString().toUpperCase();
+    }
+}
