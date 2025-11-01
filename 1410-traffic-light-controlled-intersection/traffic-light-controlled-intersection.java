db1fac1612c1462dd04613c8ce5a627973c329f8
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-01 11:20:50 +0530

    Time: 10 ms (99.15%) | Memory: 50.6 MB (7.23%) - LeetSync

---
+++ 1410-traffic-light-controlled-intersection/traffic-light-controlled-intersection.java
@@ -0,0 +1,21 @@
+class TrafficLight {
+    private int curr;
+    public TrafficLight() {
+        this.curr=1;
+    }
+    
+    public void carArrived(
+        int carId,           // ID of the car
+        int roadId,          // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        int direction,       // Direction of the car
+        Runnable turnGreen,  // Use turnGreen.run() to turn light to green on current road
+        Runnable crossCar    // Use crossCar.run() to make car cross the intersection 
+    ) {
+        if(this.curr!=roadId){
+            turnGreen.run();
+            this.curr = roadId;
+        }
+        crossCar.run();
+        
+    }
+}
