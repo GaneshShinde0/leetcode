3c2c90636bc769434786c08822022e1c8aeb5adc
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-01 08:36:36 +0530

    Time: 5 ms (96.97%) | Memory: 130.5 MB (6.34%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -9,6 +9,7 @@
  * }
  */
 class Solution {
+    // 78ms
     public ListNode removeNodesStack(ListNode head) {
         ListNode curr = head;
         Stack<ListNode> stk = new Stack<>();
@@ -29,13 +30,50 @@ class Solution {
         }
         return curr;
     }
-
-    public ListNode removeNodes(ListNode head){
+    // 15ms
+    public ListNode removeNodesRecursion(ListNode head){
         if(head == null) return null; // Base condition for recursion
         ListNode node = head;
-        ListNode nextGreater = removeNodes(node.next);
+        ListNode nextGreater = removeNodesRecursion(node.next);
         node.next = nextGreater;
         if(nextGreater==null || node.val>=nextGreater.val) return node;
         return nextGreater;
+    }
+
+    // Reverse And Filter Approach
+    public ListNode removeNodes(ListNode head){
+        ListNode prev = null, curr = head;
+        // Reverse
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+        curr = prev;
+        
+        while(curr!=null){
+            if(curr.val>=temp.val){
+                temp.next = curr;
+                temp = curr;
+                curr = curr.next;
+            }else{
+                curr = curr.next;
+            }
+        }
+
+        temp.next = null;
+
+        ListNode newPrev = null, newCurr = dummy.next;
+        while(newCurr!=null){
+            ListNode next = newCurr.next;
+            newCurr.next = newPrev;
+            newPrev = newCurr;
+            newCurr = next;
+        }
+        return newPrev;
     }
 }
