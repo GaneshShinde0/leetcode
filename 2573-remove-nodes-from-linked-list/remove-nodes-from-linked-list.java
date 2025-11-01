46143355c7fed88d8db521c115c56475d415bdef
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-01 08:31:35 +0530

    Time: 15 ms (41.55%) | Memory: 135.5 MB (6.34%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -9,7 +9,7 @@
  * }
  */
 class Solution {
-    public ListNode removeNodes(ListNode head) {
+    public ListNode removeNodesStack(ListNode head) {
         ListNode curr = head;
         Stack<ListNode> stk = new Stack<>();
         while(curr!=null){
@@ -20,6 +20,7 @@ class Solution {
             curr = curr.next;
         }
 
+        // Reversing the stack.
         ListNode next = null;
         while(!stk.isEmpty()){
             curr = stk.pop();
@@ -27,5 +28,14 @@ class Solution {
             next = curr;
         }
         return curr;
+    }
+
+    public ListNode removeNodes(ListNode head){
+        if(head == null) return null; // Base condition for recursion
+        ListNode node = head;
+        ListNode nextGreater = removeNodes(node.next);
+        node.next = nextGreater;
+        if(nextGreater==null || node.val>=nextGreater.val) return node;
+        return nextGreater;
     }
 }
