97fc16f471e32fd3ac769d3759861d6a37c48c8d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-02 17:18:34 +0530

    Time: 91 ms (30.51%) | Memory: 67.4 MB (98.31%) - LeetSync

---
+++ 2861-create-object-from-two-arrays/create-object-from-two-arrays.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Array} keysArr
+ * @param {Array} valuesArr
+ * @return {Object}
+ */
+var createObject = function(keysArr, valuesArr) {
+    const obj = {};
+    keysArr.forEach((key,index)=>{
+        if(!(key in obj)){
+            obj[key]=valuesArr[index];
+        }
+    });
+    return obj;
+};
