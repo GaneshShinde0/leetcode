88e8923687839e2287a21fdb005012643338f09b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-02 21:24:59 +0530

    Time: 3 ms (56.31%) | Memory: 44.4 MB (9.71%) - LeetSync

--- 734-sentence-similarity/sentence-similarity.java
+++ 734-sentence-similarity/sentence-similarity.java
@@ -9,7 +9,7 @@ class Solution {
         for(int i=0;i<sentence1.length;i++){
             if(sentence1[i].equals(sentence2[i])) continue;
             if((similar.containsKey(sentence1[i]) && similar.get(sentence1[i]).contains(sentence2[i]))) continue;
-            if(similar.containsKey(sentence2[i]) && similar.get(sentence2[i]).contains(sentence1[i])) continue;
+            // if(similar.containsKey(sentence2[i]) && similar.get(sentence2[i]).contains(sentence1[i])) continue;
             return false;
         }
         return true;
