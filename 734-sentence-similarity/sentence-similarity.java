c960608ddf4e73af7245c65887e22df4f4b2bd75
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-02 21:24:33 +0530

    Time: 3 ms (56.31%) | Memory: 44.3 MB (9.71%) - LeetSync

---
+++ 734-sentence-similarity/sentence-similarity.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean areSentencesSimilar(String[] sentence1, String[] sentence2, List<List<String>> similarPairs) {
+        if(sentence1.length!=sentence2.length) return false;
+        HashMap<String,Set<String>> similar = new HashMap<>();
+        for(List<String> pair:similarPairs){
+            similar.computeIfAbsent(pair.get(0),k->new HashSet<String>()).add(pair.get(1));
+            similar.computeIfAbsent(pair.get(1),k->new HashSet<String>()).add(pair.get(0));
+        }
+        for(int i=0;i<sentence1.length;i++){
+            if(sentence1[i].equals(sentence2[i])) continue;
+            if((similar.containsKey(sentence1[i]) && similar.get(sentence1[i]).contains(sentence2[i]))) continue;
+            if(similar.containsKey(sentence2[i]) && similar.get(sentence2[i]).contains(sentence1[i])) continue;
+            return false;
+        }
+        return true;
+    }
+}
