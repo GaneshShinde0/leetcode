ef3c490a796e033e2459acc1e51d265bf98da600
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-03 08:31:55 +0530

    Time: 54 ms (97.04%) | Memory: 47 MB (5.69%) - LeetSync

--- 447-number-of-boomerangs/number-of-boomerangs.java
+++ 447-number-of-boomerangs/number-of-boomerangs.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int numberOfBoomerangs(int[][] points) {
+    public int numberOfBoomerangsInitial(int[][] points) {
         int n = points.length;
         int res = 0;
         for(int i=0;i<n;i++){
@@ -21,5 +21,15 @@ class Solution {
             }
         }
         return res;
+    }
+    public int numberOfBoomerangs(int[][] points) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int boomerangs = 0;
+        for (int[] p1 : points) {
+            for (int[] p2 : points)
+                boomerangs += map.merge((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]), 1, Integer::sum) - 1;
+            map.clear();
+        }
+        return boomerangs * 2;
     }
 }
