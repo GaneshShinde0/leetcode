8919b2798d0c3e3320c3422f9d742ebb14375dd4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-03 05:50:06 +0530

    Time: 31 ms (6.99%) | Memory: 87.8 MB (5.68%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -0,0 +1,21 @@
+// If multiple characters are same then take all times expect highest.
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int n = colors.length();
+        int res = 0;
+        int[] freq = new int[26];
+        int prev = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(neededTime[0]);
+        for(int i=1;i<n;i++){
+            if(colors.charAt(i)==colors.charAt(i-1)){
+                pq.add(neededTime[i]);
+                res+=pq.poll();
+            }else{
+                pq.clear();
+                pq.add(neededTime[i]);
+            }
+        }
+        return res;
+    }
+}
