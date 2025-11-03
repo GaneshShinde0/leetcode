cc76eac0dc9ae2d63fa6d5d527d3c1e58adec933
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-03 12:59:03 +0530

    Time: 58 ms (41.18%) | Memory: 54.4 MB (70.59%) - LeetSync

--- 2908-custom-interval/custom-interval.js
+++ 2908-custom-interval/custom-interval.js
@@ -31,3 +31,48 @@ function customClearInterval(id) {
     clearTimeout(timerId);
     intervalMap.delete(id);
 }
+
+/*
+
+const CustomInterval = (() => {
+   let intervalIdCounter = 0;
+   const intervalMap = new Map();
+
+   function setCustomInterval(fn, delay, period) {
+      let count = 0;
+      const customId = intervalIdCounter++;
+
+      function dfs() {
+         const timerId = setTimeout(() => {
+            fn();
+            dfs();
+         }, delay + period * count);
+         count++;
+         intervalMap.set(customId, timerId);
+      }
+
+      dfs();
+
+      return customId;
+   }
+
+   function clearCustomInterval(id) {
+      const timerId = intervalMap.get(id);
+      clearTimeout(timerId);
+      intervalMap.delete(id);
+   }
+
+   return {
+      setCustomInterval,
+      clearCustomInterval
+   };
+})();
+
+function customInterval(fn, delay, period) {
+   return CustomInterval.setCustomInterval(fn, delay, period);
+}
+
+function customClearInterval(id) {
+   CustomInterval.clearCustomInterval(id);
+}
+*/
