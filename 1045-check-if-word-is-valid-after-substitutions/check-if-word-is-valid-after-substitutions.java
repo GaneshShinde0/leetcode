922da2ad8d15026ec5db7b0021258f13fff4fe16
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-04 12:57:23 +0530

    Time: 12 ms (71.80%) | Memory: 46.1 MB (6.40%) - LeetSync

--- 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.java
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.java
@@ -1,10 +1,25 @@
 class Solution {
-    public boolean isValid(String s) {
+    public boolean isValidSimple(String s) {
         int n = s.length()/3;
         while(n>0){
             s = s.replace("abc","");
             n--;
         }
         return s.equals("");
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='c'){
+                if(stk.size()>=2){
+                    if(!(stk.pop()=='b')) return false;
+                    if(!(stk.pop()=='a')) return false;
+                }else return false;
+            }else{
+                stk.push(ch);
+            }
+        }
+        return stk.isEmpty();
     }
 }
