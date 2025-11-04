6c653f9969c287207a6dc164bcf1bf15dbfafa21
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-04 14:21:12 +0530

    Time: 30 ms (16.24%) | Memory: 72.4 MB (5.98%) - LeetSync

---
+++ 1143-find-smallest-common-element-in-all-rows/find-smallest-common-element-in-all-rows.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int smallestCommonElement(int[][] mat) {
+        int i=0,j=0;
+        int m = mat.length;
+        int n = mat[0].length;
+        TreeMap<Integer,Integer> tm = new TreeMap<>();
+        for(j=0;j<n;j++) tm.put(mat[0][j],tm.getOrDefault(mat[0][j],0)+1);
+        for(i=1;i<m;i++){
+            for(j=0;j<n;j++){
+                if(tm.containsKey(mat[i][j])){
+                    tm.put(mat[i][j],tm.get(mat[i][j])+1);
+                }
+            }
+        }
+        for(Map.Entry<Integer,Integer> e: tm.entrySet()){
+            if(e.getValue()==m) return e.getKey();
+        }
+        return -1;
+    }
+}
