a4cf68daa08f4556720c5eacfbcb1485924e7e78
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-08 19:49:53 +0530

    Time: 812 ms (16.63%) | Memory: 0B (100.00%) - LeetSync

--- 1541-top-travellers/top-travellers.sql
+++ 1541-top-travellers/top-travellers.sql
@@ -1,5 +1,8 @@
-/* Write your T-SQL query statement below */
-SELECT name, travelled_distance from (SELECT u.id,u.name, COALESCE(sum(r.distance),0) as travelled_distance from Users u left JOIN Rides r on u.id = r.user_id 
-GROUP by u.id,u.name)
-SRC
- ORDER BY 2 DESC;
+-- /* Write your T-SQL query statement below */
+-- SELECT name, travelled_distance from (SELECT u.id,u.name, COALESCE(sum(r.distance),0) as travelled_distance from Users u left JOIN Rides r on u.id = r.user_id 
+-- GROUP by u.id,u.name)
+-- SRC
+--  ORDER BY 2 DESC;
+
+SELECT DISTINCT u.name, COALESCE(sum(r.distance) OVER(PARTITION BY u.id),0) as travelled_distance from Users u left JOIN Rides r on u.id = r.user_id  
+ORDER BY 2 DESC
