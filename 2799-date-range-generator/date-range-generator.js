b9f256f60a319e571a70d187a79a16132c57d430
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-09 10:59:16 +0530

    Time: 68 ms (27.27%) | Memory: 58.5 MB (9.09%) - LeetSync

--- 2799-date-range-generator/date-range-generator.js
+++ 2799-date-range-generator/date-range-generator.js
@@ -5,24 +5,24 @@
  * @yields {string}
  */
 
-// var dateRangeGenerator = function (start, end, step) {
-//     const startDate = new Date(start);
-//     const endDate = new Date(end);
-//     const dateList = [];
-//     while(startDate<=endDate){
-//         dateList.push(formatDate(startDate));
-//         startDate.setDate(startDate.getDate()+step);
-//     }
-//     return dateList;
-// };
+var dateRangeGenerator0 = function (start, end, step) {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    const dateList = [];
+    while(startDate<=endDate){
+        dateList.push(formatDate(startDate));
+        startDate.setDate(startDate.getDate()+step);
+    }
+    return dateList;
+};
 
-// function formatDate(date){
-//     const year = date.getFullYear();
-//     const month = String(date.getMonth()+1).padStart(2,'0');
-//     const day = String(date.getDate()).padStart(2,'0');
-//     return `${year}-${month}-${day}`;
-// }
-var dateRangeGenerator = function*(start, end, step){
+function formatDate(date){
+    const year = date.getFullYear();
+    const month = String(date.getMonth()+1).padStart(2,'0');
+    const day = String(date.getDate()).padStart(2,'0');
+    return `${year}-${month}-${day}`;
+}
+var dateRangeGenerator1 = function*(start, end, step){
     const startDate = new Date(start);
     const endDate = new Date(end).getTime();
 
@@ -35,6 +35,16 @@ var dateRangeGenerator = function*(start, end, step){
         startDate.setDate(next);
     }
 };
+
+var dateRangeGenerator = function*(start, end, step){
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    while(startDate<=endDate){
+        yield startDate.toISOString().split('T')[0].trim();
+        startDate.setDate(startDate.getDate()+step);
+    }
+}
 /**
  * const g = dateRangeGenerator('2023-04-01', '2023-04-04', 1);
  * g.next().value; // '2023-04-01'
