7c34567ce00e8936efdeeecfc81a48cbe6e2d444
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-09 14:04:17 +0530

    Time: 1 ms (63.46%) | Memory: 42.1 MB (11.54%) - LeetSync

--- 651-4-keys-keyboard/4-keys-keyboard.java
+++ 651-4-keys-keyboard/4-keys-keyboard.java
@@ -6,7 +6,7 @@ class Solution {
             dp[i]=i+1;
         }
         for(int i=0;i<n;i++){
-            for(int j=1;j<=i-3;j++){
+            for(int j=0;j<i-3;j++){
                 dp[i]= Math.max(dp[i],(i-j-1)*dp[j]);
             }
         }
