21d9b111a27ccd710b79040b3becd262b5febeeb
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-09 10:57:17 +0530

    Time: 41 ms (100.00%) | Memory: 56.6 MB (100.00%) - LeetSync

--- 2799-date-range-generator/date-range-generator.js
+++ 2799-date-range-generator/date-range-generator.js
@@ -4,24 +4,37 @@
  * @param {number} step
  * @yields {string}
  */
-var dateRangeGenerator = function (start, end, step) {
+
+// var dateRangeGenerator = function (start, end, step) {
+//     const startDate = new Date(start);
+//     const endDate = new Date(end);
+//     const dateList = [];
+//     while(startDate<=endDate){
+//         dateList.push(formatDate(startDate));
+//         startDate.setDate(startDate.getDate()+step);
+//     }
+//     return dateList;
+// };
+
+// function formatDate(date){
+//     const year = date.getFullYear();
+//     const month = String(date.getMonth()+1).padStart(2,'0');
+//     const day = String(date.getDate()).padStart(2,'0');
+//     return `${year}-${month}-${day}`;
+// }
+var dateRangeGenerator = function*(start, end, step){
     const startDate = new Date(start);
-    const endDate = new Date(end);
-    const dateList = [];
-    while(startDate<=endDate){
-        dateList.push(formatDate(startDate));
-        startDate.setDate(startDate.getDate()+step);
+    const endDate = new Date(end).getTime();
+
+    while(startDate.getTime()<=endDate){
+        const date = String(startDate.getDate()).padStart(2,'0');
+        const month = String(startDate.getMonth()+1).padStart(2,'0');
+        const year = String(startDate.getFullYear()).padStart(2,'0');
+        yield `${year}-${month}-${date}`;
+        const next = startDate.getDate()+step;
+        startDate.setDate(next);
     }
-    return dateList;
 };
-
-function formatDate(date){
-    const year = date.getFullYear();
-    const month = String(date.getMonth()+1).padStart(2,'0');
-    const day = String(date.getDate()).padStart(2,'0');
-    return `${year}-${month}-${day}`;
-}
-
 /**
  * const g = dateRangeGenerator('2023-04-01', '2023-04-04', 1);
  * g.next().value; // '2023-04-01'
