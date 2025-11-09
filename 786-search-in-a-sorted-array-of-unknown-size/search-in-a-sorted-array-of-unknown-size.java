988bda9371e5e6925813bc800b1cdb1264be5184
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-09 23:08:00 +0530

    Time: 1 ms (98.44%) | Memory: 48 MB (6.88%) - LeetSync

--- 786-search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.java
+++ 786-search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.java
@@ -7,7 +7,7 @@
  */
 
 class Solution {
-    public int search(ArrayReader reader, int target) {
+    public int searchInitial(ArrayReader reader, int target) {
         int low = 0;
         int high = Integer.MAX_VALUE;
         int mid = 0;
@@ -15,13 +15,31 @@ class Solution {
             mid = low+(high-low)/2;
             int curr = reader.get(mid);
             if(curr>target){
-                high= mid;
+                high= mid; // If I do high = mid-1, it will not work properly because of integer division for mid.
             }else if(curr<target){
-                low=mid+1;
+                low=mid+1; // we we increase low then atleast we get proper division.
+                // Example low = 5, high = 6
+                // If we increase low by 1 next time we get mid = 6... that is not case with decrementing high
             }else if (curr==target){
                 return mid;
             }
         }
         return -1;
+    }
+
+    public int search(ArrayReader reader, int target) {
+        int low = 0;
+        int high = Integer.MAX_VALUE;
+        int mid = 0;
+        while(low<high){
+            mid = low+(high-low)/2;
+            int curr = reader.get(mid);
+            if(curr>=target){
+                high= mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return reader.get(low)==target?low:-1;
     }
 }
