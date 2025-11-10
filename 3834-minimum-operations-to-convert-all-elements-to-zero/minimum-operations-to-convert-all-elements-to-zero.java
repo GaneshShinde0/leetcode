c3c38a7d4ac427bc6b942164042d26375230b8a5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-10 08:50:21 +0530

    Time: 142 ms (38.73%) | Memory: 123.5 MB (12.68%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.java
@@ -1,15 +1,28 @@
+/*
+Minimum Operations to Convert all elements to Zero
+
+What we can do 
+    - If current is same as previous continue;
+    - If current is 0 continue;
+
+    - If we have not processed anything increase result by 1
+    - If we have previous element greater than current... Then increase the result by 1... as we have processed larger element and we need to process (current -> smaller) element.
+    - If we have previous element smaller than current element... we will think about processing current element.
+
+
+*/
 class Solution {
     public int minOperations(int[] nums) {
         Stack<Integer> stk = new Stack<>();
         int res = 0;
         for(int i:nums){
-            while(!stk.isEmpty()&&stk.peek()>i){
-                stk.pop();
+            while(!stk.isEmpty() && stk.peek()>i){
+                stk.pop(); // As we had previously added elements and increased result... we have found one less element... We will have to remove them.
             }
             if(i==0) continue;
-            if(stk.isEmpty() ||stk.peek()<i){
-                res++;
+            if(stk.isEmpty()|| stk.peek()<i){
                 stk.add(i);
+                res++;
             }
         }
         return res;
