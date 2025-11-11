61235b8b64ca3c35cbb19a2bd36e9715d05e6d8c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-11 11:35:28 +0530

    Time: 124 ms (14.82%) | Memory: 53.9 MB (5.82%) - LeetSync

---
+++ 436-find-right-interval/find-right-interval.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+        TreeSet<int[]> set = new TreeSet<>((a,b)->{
+                    if(a[1]==b[0]) return Integer.compare(a[0],b[0]);
+                    return Integer.compare(a[1],b[0]); 
+                }
+            );
+        int idx = 0;
+        for(int[] i:intervals){
+            set.add(new int[]{i[0],i[1],idx});
+            idx++;
+        }
+        for(int[] i: set){
+            System.out.println(Arrays.toString(i));
+        }
+        int n = intervals.length;
+        int[] res = new int[n];
+        for(int i=0;i<n;i++){
+            int[] temp = set.ceiling(intervals[i]);
+            if(temp==null){
+                res[i]=-1; 
+                continue;
+            }
+            if(Arrays.equals(intervals[i],temp)){
+                res[i] = -1;
+            }else{
+                res[i] = temp[2];
+            }
+        }
+        return res;
+    }
+}
