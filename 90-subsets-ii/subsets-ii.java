ce3909689b623f7e21e4f2af7dde7a2d84fe56ed
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-12 10:53:28 +0530

    Time: 3 ms (24.03%) | Memory: 44.8 MB (17.69%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -3,23 +3,23 @@ class Solution {
         Set<List<Integer>> set = new HashSet<>();
         Arrays.sort(nums);
         List<Integer> li = new ArrayList<>();
-        recurse(set, nums,li, 0, nums.length);
+        recurseInitial(set, nums,li, 0, nums.length);
         return new ArrayList<>(set);
     }
 
-    private void recurse(Set<List<Integer>> set, int[] nums,List<Integer> li, int start, int end){
+    private void recurseInitial(Set<List<Integer>> set, int[] nums,List<Integer> li, int start, int end){
         // Collections.sort(li); // This is not needed and adds about 8 ms to result.
         set.add(new ArrayList<>(li));
         for(int i= start; i<end;i++){
             li.add(nums[i]);
-            recurse(set, nums, li, i+1, end);
+            recurseInitial(set, nums, li, i+1, end);
             li.remove(li.size()-1);
-            recurse(set, nums, li, i+1, end);
+            recurseInitial(set, nums, li, i+1, end);
         }
     }
 
-    
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
+    // 4 ms beats 12%
+    public List<List<Integer>> subsetsWithDupWithoutForLoop(int[] nums) {
         Set<List<Integer>> rs = new HashSet<>();
         Arrays.sort(nums);
         List<Integer> li = new ArrayList<>();
@@ -36,5 +36,23 @@ class Solution {
         recurseWithoutForLoop(res, nums, li, start+1, end);
         li.remove(li.size()-1);
         recurseWithoutForLoop(res, nums, li, start+1, end);
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        List<Integer> li = new ArrayList<>();
+        recurse(res, nums,li, 0, nums.length);
+        return res;
+    }
+
+    private void recurse(List<List<Integer>> res, int[] nums,List<Integer> li, int start, int end){
+        res.add(new ArrayList<>(li));
+        for(int i= start; i<end;i++){
+            if(i>start && nums[i]==nums[i-1]) continue;
+            li.add(nums[i]);
+            recurse(res, nums, li, i+1, end);
+            li.remove(li.size()-1);
+        }
     }
 }
