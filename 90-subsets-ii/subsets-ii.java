d8f309c48ac95d12fd3af105a89a573609364492
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-12 10:21:47 +0530

    Time: 25 ms (6.70%) | Memory: 46.7 MB (6.39%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Set<List<Integer>> set = new HashSet<>();
+        Arrays.sort(nums);
+        List<Integer> li = new ArrayList<>();
+        recurse(set, nums,li, 0, nums.length);
+        return new ArrayList<>(set);
+    }
+
+    private void recurse(Set<List<Integer>> set, int[] nums,List<Integer> li, int start, int end){
+        Collections.sort(li);
+        set.add(new ArrayList<>(li));
+        for(int i= start; i<end;i++){
+            li.add(nums[i]);
+            recurse(set, nums, li, i+1, end);
+            li.remove(li.size()-1);
+            recurse(set, nums, li, i+1, end);
+        }
+    }
+}
