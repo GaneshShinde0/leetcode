25453abeb067a60f91011dd6e1a5e82e48d11c52
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-13 21:49:23 +0530

    Time: 169 ms (18.44%) | Memory: 107.5 MB (5.34%) - LeetSync

---
+++ 4073-lexicographically-smallest-string-after-reverse/lexicographically-smallest-string-after-reverse.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String lexSmallest(String s) {
+        TreeSet<String> set = new TreeSet<>();
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            StringBuilder sb = new StringBuilder(s.substring(0,i+1));
+            sb.reverse();
+            sb.append(s.substring(i+1));
+            // System.out.println(sb.toString());
+            set.add(sb.toString());
+            sb = new StringBuilder(s.substring(i+1));
+            sb.reverse();
+            sb.insert(0,s.substring(0,i+1));
+            set.add(sb.toString());
+        }
+        return set.first();
+    }
+}
