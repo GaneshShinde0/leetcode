d720ccdbb7406a0c9c6f5e5bb52e1ec9edf0688f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-13 21:56:25 +0530

    Time: 105 ms (57.84%) | Memory: 46.9 MB (9.94%) - LeetSync

--- 4073-lexicographically-smallest-string-after-reverse/lexicographically-smallest-string-after-reverse.java
+++ 4073-lexicographically-smallest-string-after-reverse/lexicographically-smallest-string-after-reverse.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String lexSmallest(String s) {
+    public String lexSmallestInitial(String s) {
         TreeSet<String> set = new TreeSet<>();
         int n = s.length();
         for(int i=0;i<n;i++){
@@ -12,12 +12,27 @@ class Solution {
             sb.reverse();
             sb.insert(0,s.substring(0,i+1));
             set.add(sb.toString());
-
-            if(set.size()>=3){
-                set.pollLast();
-                set.pollLast();
-            }
+            // Following 4 lines make code faster by 45 ms
+            // if(set.size()>=3){
+            //     set.pollLast();
+            //     set.pollLast();
+            // }
         }
         return set.first();
+    }
+    public String lexSmallest(String s) {
+        String best = s;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb1 = new StringBuilder(s.substring(0, i + 1)).reverse().append(s.substring(i + 1));
+            StringBuilder sb2 = new StringBuilder(s.substring(i + 1)).reverse().insert(0, s.substring(0, i + 1));
+            best = min(best, sb1.toString());
+            best = min(best, sb2.toString());
+        }
+        return best;
+    }
+
+    private String min(String a, String b) {
+        return a.compareTo(b) <= 0 ? a : b;
     }
 }
