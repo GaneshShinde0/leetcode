1d40dbe35fdf1cf8c97dba236d0131332acc30b7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-15 17:29:22 +0530

    Time: 1161 ms (52.34%) | Memory: 51 MB (22.43%) - LeetSync

--- 3479-count-the-number-of-substrings-with-dominant-ones/count-the-number-of-substrings-with-dominant-ones.java
+++ 3479-count-the-number-of-substrings-with-dominant-ones/count-the-number-of-substrings-with-dominant-ones.java
@@ -17,7 +17,7 @@ We process each i as described above, accumulating the number of valid left boun
 */
 
 class Solution{
-    public int numberOfSubstrings(String s){
+    public int numberOfSubstringsInitial(String s){
         int n = s.length();
         int[] pre = new int[n+1];
         pre[0] = 0;
@@ -28,7 +28,7 @@ class Solution{
                 pre[i+1] = pre[i];
             }
         }
-
+        System.out.println(Arrays.toString(pre));
         int res = 0;
         for(int i=1;i<=n;i++){
             int cnt0 = s.charAt(i-1)=='0'?1:0;
@@ -43,5 +43,75 @@ class Solution{
             }
         }
         return res;
+    }
+
+    public int numberOfSubstringsSecond(String s) {
+
+        int n = s.length();
+        int[] prefix0 = new int[n + 1];
+        int[] prefix1 = new int[n + 1];
+
+        // build prefix zero & one counts
+        for (int i = 1; i <= n; i++) {
+            prefix0[i] = prefix0[i - 1] + (s.charAt(i - 1) == '0' ? 1 : 0);
+            prefix1[i] = prefix1[i - 1] + (s.charAt(i - 1) == '1' ? 1 : 0);
+        }
+
+        int result = 0;
+
+        // iterate left pointer
+        for (int i = 0; i < n; i++) {
+
+            // We stop when zeros grow too large
+            // Because once zeros > sqrt(n), they cannot be dominated anymore
+            int maxZerosToTry = (int) Math.sqrt(n) + 2;
+
+            // expand right pointer
+            for (int j = i; j < n; j++) {
+
+                int zeros = prefix0[j + 1] - prefix0[i];
+                int ones = prefix1[j + 1] - prefix1[i];
+
+                // if zeros too large → break early
+                if (zeros > maxZerosToTry) break;
+
+                // Check condition
+                if (ones >= zeros * zeros) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+    public int numberOfSubstrings(String s) {
+        int n = s.length();
+        int dominantCount = 0;
+        List<Integer> zeros = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='0') zeros.add(i);
+        }
+        if(zeros.size()==0) return n*(n+1)/2;
+        int zeroInd = 0;
+        for(int i=0;i<n;i++){
+            while(zeroInd<zeros.size() && zeros.get(zeroInd)<i) zeroInd++;
+
+            List<Integer> validZero = new ArrayList<>();
+            for(int z = zeroInd; z<zeros.size() && z<(zeroInd+201); z++){
+                validZero.add(zeros.get(z));
+            }
+            validZero.add(n);
+            int zeroCount = 0;
+            int last = i;
+
+            for(int vz:validZero){
+                int minOne = zeroCount*zeroCount;
+                int minInd = Math.max(i+minOne+zeroCount-1,last); // Minimum Index After which everything would become valid;
+                if(minInd<vz) dominantCount += (vz-minInd);
+                last = vz;
+                zeroCount++;
+            }
+        }
+        return dominantCount;
     }
 }
