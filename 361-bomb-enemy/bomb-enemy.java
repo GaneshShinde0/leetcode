b0414eded374a465702447e132f9d1b0e6bc7f96
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-22 22:05:15 +0530

    Time: 6 ms (91.45%) | Memory: 65.3 MB (39.32%) - LeetSync

--- 361-bomb-enemy/bomb-enemy.java
+++ 361-bomb-enemy/bomb-enemy.java
@@ -2,7 +2,7 @@ class Solution {
     private int[] dir = {1,-1};
     int m;
     int n;
-    public int maxKilledEnemies(char[][] grid) {
+    public int maxKilledEnemiesInitial(char[][] grid) {
         this.m = grid.length;
         this.n = grid[0].length;
         int res = 0;
@@ -34,5 +34,32 @@ class Solution {
             }
         }
         return currRes;
+    }
+    public int maxKilledEnemies(char[][] grid) {
+        int[][] dp     = new int[grid.length][grid[0].length];
+        int[] colCount = new int[grid[0].length];
+
+        for (int r = 0; r < grid.length; r++) {
+            int rowCount = 0;
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c] == 'E') { rowCount++; colCount[c]++; }
+                else if (grid[r][c] == 'W') { rowCount = colCount[c] = 0; }
+                else dp[r][c] = rowCount + colCount[c];
+            }
+        }
+
+        Arrays.fill(colCount, 0);
+
+        int max = 0;
+        for (int r = grid.length-1; r > -1; r--) {
+            int rowCount = 0;
+            for (int c = grid[0].length-1; c > -1; c--) {
+                if (grid[r][c] == 'E') { rowCount++; colCount[c]++; }
+                else if (grid[r][c] == 'W') { rowCount = colCount[c] = 0; }
+                else { dp[r][c] += rowCount + colCount[c]; max = Math.max(max, dp[r][c]); }
+            }
+        }
+
+        return max;
     }
 }
