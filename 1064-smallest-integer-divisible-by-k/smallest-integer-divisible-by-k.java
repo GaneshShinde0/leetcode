29b60a21b606333696a9ffa93ca05a3d4650b18e
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-22 10:36:41 +0530

    Time: 2 ms (96.99%) | Memory: 42.2 MB (14.06%) - LeetSync

--- 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.java
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int smallestRepunitDivByK(int k) {
+    public int smallestRepunitDivByKInitial(int k) {
         int remainder = 1, lengthOfN = 1;
         Set<Integer> seenRemainders = new HashSet<>();
         while(remainder%k!=0){
@@ -12,6 +12,22 @@ class Solution {
             }
         }
         return lengthOfN;
+    }
+    public int smallestRepunitDivByK(int k) {
+        int count = 1;
+        int remainder = 1%k;
+
+        if(k%2 == 0 || k%5 == 0)
+        {
+            return -1;
+        }
+
+        while(remainder != 0){
+            remainder = (remainder*10 + 1)%k;
+            count++;
+        }
+
+        return count;
     }
 }
 /*
