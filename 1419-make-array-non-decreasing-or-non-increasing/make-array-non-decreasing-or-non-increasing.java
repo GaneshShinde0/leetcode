d1a12c544c969ccb0c21c801555348ce3cb1c6f6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-23 21:42:52 +0530

    Time: 6 ms (100.00%) | Memory: 45.3 MB (18.18%) - LeetSync

---
+++ 1419-make-array-non-decreasing-or-non-increasing/make-array-non-decreasing-or-non-increasing.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int convertArray(int[] nums) {
+        int costNonDecreasing = getMinCostNonDecreasing(nums);
+        int[] negativeNums = new int[nums.length];
+        for (int i = 0; i < nums.length; i++)
+            negativeNums[i] = -nums[i];
+        int costNonIncreasing = getMinCostNonDecreasing(negativeNums);
+        return Math.min(costNonDecreasing, costNonIncreasing);
+    }
+
+    private int getMinCostNonDecreasing(int[] arr){
+        int cost = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num:arr){
+            pq.offer(num);
+            if(pq.peek()<num){
+                cost += (num-pq.peek());
+                pq.poll();
+                pq.offer(num);
+            }
+        }
+        return cost;
+    }
+}
