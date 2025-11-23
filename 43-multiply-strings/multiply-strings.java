42019da0c61ce6acbc13eb3d187b936d2c19608b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-23 19:04:19 +0530

    Time: 2 ms (98.87%) | Memory: 43.6 MB (22.87%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String multiply(String num1, String num2) {
+    public String multiplyInitial(String num1, String num2) {
         StringBuilder res = new StringBuilder();
         for(int i=num2.length()-1;i>=0;i--){
             StringBuilder sb = new StringBuilder();
@@ -36,5 +36,24 @@ class Solution {
             res.deleteCharAt(index); // Delete the leading '0'
         }
         return res.toString();
+    }
+
+    public String multiply(String num1, String num2){
+        if(num1.equals("0")||num2.equals("0")) return "0";
+        int n1 = num1.length(), n2 = num2.length();
+        int[] result = new int[n1+n2];
+        for(int i = n1-1;i>=0;i--){
+            int d1 = num1.charAt(i)-'0';
+            for(int j= n2-1;j>=0;j--){
+                result[i+j+1] +=d1*(num2.charAt(j)-'0');
+            }
+        }
+        for(int i=result.length-1, carry=0,sum;i>=0;i--){
+            sum = result[i]+carry;
+            carry = sum/10;
+            result[i] = sum%10+'0';
+        }
+        int i = result[0] == '0'?1:0;
+        return new String(result,i,result.length-i);
     }
 }
