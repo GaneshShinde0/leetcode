bce296068103fd6b77e3c427be50efe7bb5c41bf
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-24 21:18:54 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (26.55%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.java
+++ 1009-pancake-sorting/pancake-sorting.java
@@ -3,7 +3,7 @@ class Solution {
         List<Integer> res = new ArrayList<>();
         for(int i=arr.length;i>0;i--){
             int idx = findIdx(arr,i);
-            if(idx==i-1) continue;
+            if(idx==i-1) continue; // It is present at right index;
             if(idx!=0){
                 res.add(idx+1);
                 flip(arr,idx+1);
