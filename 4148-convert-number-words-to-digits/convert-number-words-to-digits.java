12f349e0aae30b7a9c70e8b299d514f0c4d6427a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-11-30 17:46:18 +0530

    Time: 93 ms (32.39%) | Memory: 48.2 MB (14.08%) - LeetSync

--- 4148-convert-number-words-to-digits/convert-number-words-to-digits.java
+++ 4148-convert-number-words-to-digits/convert-number-words-to-digits.java
@@ -3,9 +3,9 @@ class Solution {
         Map<String, Integer> hm = Map.of("zero",0,"one",1,"two",2,"three",3, "four", 4, "five",5, "six",6, "seven",7, "eight",8,"nine",9);
         int n = s.length();
         StringBuilder sb = new StringBuilder();
-        System.out.println(hm);
+        // System.out.println(hm);
         for(int i=0;i<=n-3;i++){
-            System.out.println(s.substring(i,Math.min(n,i+3)));
+            // System.out.println(s.substring(i,Math.min(n,i+3)));
             if(hm.containsKey(s.substring(i,Math.min(n,i+3)))){
                 sb.append(hm.get(s.substring(i,Math.min(n,i+3))));
                 i+=2;
