ba6df61ee5118036ac028ac1c6d4ad77dee172b6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-04 07:38:02 +0530

    Time: 14 ms (64.11%) | Memory: 47.6 MB (26.61%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int countCollisions(String directions) {
+    public int countCollisionsInitial(String directions) {
         Stack<Character> stk = new Stack<>();
         // System.out.println();
         int res = 0;
@@ -20,6 +20,32 @@ class Solution {
                 res+=1;
             }else if(c!='L'){
                 stk.push(c);
+            }
+        }
+        return res;
+    }
+
+    
+    public int countCollisions(String directions) {
+        int res = 0;
+        int flag = -1;
+        for (char c : directions.toCharArray()) {
+            if (c == 'L') {
+                if (flag >= 0) {
+                    res += flag + 1;
+                    flag = 0;
+                }
+            } else if (c == 'S') {
+                if (flag > 0) {
+                    res += flag;
+                }
+                flag = 0;
+            } else {
+                if (flag >= 0) {
+                    flag++;
+                } else {
+                    flag = 1;
+                }
             }
         }
         return res;
