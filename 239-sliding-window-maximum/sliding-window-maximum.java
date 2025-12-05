b7c12d86a7bcfd094a2d7da559628f8c305435cf
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-05 23:45:20 +0530

    Time: 31 ms (59.99%) | Memory: 143.5 MB (45.89%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] maxSlidingWindow(int[] nums, int k) {
+    public int[] maxSlidingWindowUsingHm(int[] nums, int k) {
         int n = nums.length;
         int[] res = new int[n-k+1];
         Arrays.fill(res,Integer.MIN_VALUE);
@@ -13,6 +13,26 @@ class Solution {
             }
             res[Math.max(i-k+1,0)] = Math.max(res[Math.max(i-k+1,0)],tm.lastEntry().getKey());
             
+        }
+        return res;
+    }
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n-k+1];
+        Arrays.fill(res,Integer.MIN_VALUE);
+        if(k==1) return nums;
+        Deque<Integer> dq = new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            // Remove Elements outside window
+            if(!dq.isEmpty() && dq.peekFirst()<=i-k) dq.pollFirst();
+
+            while(!dq.isEmpty() && nums[dq.peekLast()]<=nums[i]){
+                dq.pollLast();
+            }
+            dq.add(i);
+            if(i-k+1>=0){
+                res[i-k+1]=nums[dq.peekFirst()];
+            }
         }
         return res;
     }
