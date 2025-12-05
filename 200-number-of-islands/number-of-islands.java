36c8c95aae9f150f52be893669390e0d7aedbf33
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-05 23:56:23 +0530

    Time: 3 ms (85.59%) | Memory: 52.3 MB (39.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -5,7 +5,8 @@ class Solution {
         {1,0},
         {-1,0}
     };
-    public int numIslands(char[][] grid) {
+    
+    public int numIslandsInitial(char[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         int res = 0;
@@ -14,14 +15,14 @@ class Solution {
             for(int j=0;j<n;j++){
                 if(visited[i][j]==false&&grid[i][j]=='1'){
                     res++;
-                    dfs(grid, visited,i,j,m,n);
+                    dfsInitial(grid, visited,i,j,m,n);
                 }
             }
         }
         return res;
     }
 
-    private void dfs(char[][] grid, boolean[][] visited, int i, int j,int m, int n){
+    private void dfsInitial(char[][] grid, boolean[][] visited, int i, int j,int m, int n){
         if(visited[i][j]) return;
         visited[i][j]=true;
         if(grid[i][j]=='1'){
@@ -29,8 +30,56 @@ class Solution {
                 int newI = i+dir[0];
                 int newJ = j+dir[1];
                 if(newI>=m||newI<0||newJ>=n||newJ<0) continue;
-                dfs(grid,visited,newI,newJ,m,n);
+                dfsInitial(grid,visited,newI,newJ,m,n);
             }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    public int numIslands(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void dfs(char[][] grid, int i, int j){
+        grid[i][j] = '0';
+        for(int[] dir:directions){
+            int nx = i+dir[0];
+            int ny = j+dir[1];
+            if(nx>=grid.length||ny>=grid[0].length||nx<0||ny<0||grid[nx][ny]=='0') continue;
+            dfs(grid,nx,ny);
         }
     }
 }
