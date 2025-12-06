6119531eab998e6806f63a4ac8537320c76fae2d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-06 16:45:24 +0530

    Time: 18 ms (77.86%) | Memory: 54.1 MB (50.15%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -2,13 +2,11 @@ class Solution {
     public int numDistinct(String s, String t) {
         int slen = s.length(), tlen = t.length();
         int[][] dp = new int[slen+1][tlen+1];
-        dp[0][0] = 1;
-        //(non-empty string cannot be subsequence of empty)
-        // for (int i = 0; i <= slen; i++) {
-        //     dp[i][0] = 1;
-        // }
+        dp[0][0] = 1; // At begining empty string can match with empty string.
+        for (int i = 0; i <= slen; i++) {
+            dp[i][0] = 1; //base: empty target can always be formed exactly 1 way
+        }
         for(int i=0;i<slen;i++){
-            dp[i][0] = 1;
             for(int j=0;j<tlen;j++){
                 if(s.charAt(i)==t.charAt(j)){
                     dp[i+1][j+1] = dp[i][j+1]+dp[i][j];
