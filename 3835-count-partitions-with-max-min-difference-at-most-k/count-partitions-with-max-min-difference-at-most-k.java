9dac0b7b8563c61d123bed142bfddcbb47e5dbbd
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-06 08:52:20 +0530

    Time: 38 ms (95.06%) | Memory: 69.3 MB (61.73%) - LeetSync

--- 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.java
+++ 3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.java
@@ -25,7 +25,7 @@ class Solution {
         return (int) dp[n];
     }
 
-    public int countPartitions(int[] nums, int k) {
+    public int countPartitionsLinkedList(int[] nums, int k) {
         int n = nums.length;
         long mod = (long) 1e9 + 7;
         long[] dp = new long[n + 1];
@@ -67,7 +67,55 @@ class Solution {
 
         return (int) dp[n];
     }
+    public int countPartitions(int[] A, int k) {
+        int n = A.length, mod = 1_000_000_007, acc = 1;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        Deque<Integer> minq = new ArrayDeque<>();
+        Deque<Integer> maxq = new ArrayDeque<>();
+        for (int i = 0, j = 0; j < n; ++j) {
+            while (!maxq.isEmpty() && A[j] > A[maxq.peekLast()])
+                maxq.pollLast();
+            maxq.addLast(j);
+
+            while (!minq.isEmpty() && A[j] < A[minq.peekLast()])
+                minq.pollLast();
+            minq.addLast(j);
+
+            while (A[maxq.peekFirst()] - A[minq.peekFirst()] > k) {
+                acc = (acc - dp[i++] + mod) % mod;
+                if (minq.peekFirst() < i)
+                    minq.pollFirst();
+                if (maxq.peekFirst() < i)
+                    maxq.pollFirst();
+            }
+
+            dp[j + 1] = acc;
+            acc = (acc + dp[j + 1]) % mod;
+        }
+
+        return dp[n];
+    }
 }
+/*
+Example 1:
+
+Input: nums = [9,4,1,3,7], k = 4
+
+Output: 6
+
+Explanation:
+
+There are 6 valid partitions where the difference between the maximum and minimum elements in each segment is at most k = 4:
+
+[[9], [4], [1], [3], [7]]
+[[9], [4], [1], [3, 7]]
+[[9], [4], [1, 3], [7]]
+[[9], [4, 1], [3], [7]]
+[[9], [4, 1], [3, 7]]
+[[9], [4, 1, 3], [7]]
+*/
 
 /*
 9 9 9 9 9
