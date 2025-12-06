410f6b38eeedcffe115af4748336765b7490ed7f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-06 16:41:21 +0530

    Time: 20 ms (47.65%) | Memory: 54 MB (50.15%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -4,10 +4,11 @@ class Solution {
         int[][] dp = new int[slen+1][tlen+1];
         dp[0][0] = 1;
         //(non-empty string cannot be subsequence of empty)
-        for (int i = 0; i <= slen; i++) {
-            dp[i][0] = 1;
-        }
+        // for (int i = 0; i <= slen; i++) {
+        //     dp[i][0] = 1;
+        // }
         for(int i=0;i<slen;i++){
+            dp[i][0] = 1;
             for(int j=0;j<tlen;j++){
                 if(s.charAt(i)==t.charAt(j)){
                     dp[i+1][j+1] = dp[i][j+1]+dp[i][j];
