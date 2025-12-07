dfb1666a3031cc410a938c58f7c73e0ba3f681e6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-07 13:26:35 +0530

    Time: 3 ms (68.68%) | Memory: 42.5 MB (59.08%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean isInterleave(String s1, String s2, String s3) {
+    public boolean isInterleaveMN(String s1, String s2, String s3) {
         int m = s1.length(), n = s2.length();
         if((m+n)!=s3.length()) return false;
         boolean[][] dp = new boolean[m+1][n+1];
@@ -14,6 +14,27 @@ class Solution {
             }
         }
         return dp[m][n];
+    }
+
+    public boolean isInterleave(String s1, String s2, String s3){
+        int m = s1.length(), n = s2.length();
+        if(m+n!=s3.length()) return false;
+        boolean[] dp = new boolean[n+1];
+        dp[0]=true;
+        // Initialize first row using only s2;
+        for(int j=1;j<=n;j++){
+            dp[j] = dp[j-1] && s2.charAt(j-1)==s3.charAt(j-1);        
+        }
+
+        for(int i=1;i<=m;i++){
+            // Update first column using only s1
+            dp[0] = dp[0]&&s1.charAt(i-1)==s3.charAt(i-1);
+            for(int j=1;j<=n;j++){
+                dp[j] = (dp[j] && s1.charAt(i-1)==s3.charAt(i+j-1))
+                    || (dp[j-1] && s2.charAt(j-1)==s3.charAt(i+j-1));
+            }
+        }
+        return dp[n];
     }
 }
 
