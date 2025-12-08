3e0e6715f67740a765dd88d7c2fe6e8935cf18bd
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-08 15:07:10 +0530

    Time: 725 ms (66.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2014-orders-with-maximum-quantity-above-average/orders-with-maximum-quantity-above-average.sql
@@ -0,0 +1,2 @@
+/* Write your T-SQL query statement below */
+SELECT DISTINCT od.order_id from OrdersDetails od left join (SELECT order_id , AVG(quantity*1.0) as average,MAX(quantity) as max FROM OrdersDetails GROUP by order_id) src on od.order_id = src.order_id WHERE src.max>(SELECT max(average) FROM (SELECT order_id , AVG(quantity*1.0) as average,MAX(quantity) as max FROM OrdersDetails GROUP by order_id) src )--(SELECT AVG(quantity*1.0) as average from OrdersDetails);
