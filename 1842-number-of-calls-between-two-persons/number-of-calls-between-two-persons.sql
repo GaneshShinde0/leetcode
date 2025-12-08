3112a7336b989d5c7766e71a1b945c0c60b7f34f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-08 18:30:24 +0530

    Time: 1680 ms (6.06%) | Memory: 0B (100.00%) - LeetSync

--- 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -1,9 +1,9 @@
 SELECT 
     CASE WHEN from_id<to_id THEN from_id ELSE to_id END as person1,
-    CASE WHEN from_id>to_id THEN from_id ELSE to_id END as person2,
+    CASE WHEN from_id<to_id THEN to_id ELSE from_id END as person2,
     COUNT(*) as call_count,
     SUM(duration) as total_duration
 FROM Calls
 GROUP BY
     CASE WHEN from_id<to_id THEN from_id ELSE to_id END,
-    CASE WHEN from_id>to_id THEN from_id ELSE to_id END
+    CASE WHEN from_id<to_id THEN to_id ELSE from_id END
