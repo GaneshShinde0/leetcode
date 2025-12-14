02db3fe8bba12b3a94a68f872c40b82d0ffe855c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-14 16:18:23 +0530

    Time: 8 ms (61.35%) | Memory: 65.4 MB (13.38%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<List<String>> partition(String s) {
+    public List<List<String>> partitionWithoutDP(String s) {
         List<List<String>> result = new ArrayList<>();
         dfs(0, result, new ArrayList<String>(), s);
         return result;
@@ -23,5 +23,25 @@ class Solution {
             end--;
         }
         return true;
+    }
+
+    public List<List<String>> partition(String s){
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        List<List<String>> result = new ArrayList<>();
+        dfs(result, s, 0, new ArrayList<>(), dp);
+        return result;
+    }
+
+    void dfs(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp){
+        if(start>=s.length()) result.add(new ArrayList<>(currentList));
+        for(int end = start; end<s.length();end++){
+            if(s.charAt(start)==s.charAt(end)&&(end-start<=2 || dp[start+1][end-1])){
+                dp[start][end] = true;
+                currentList.add(s.substring(start, end+1));
+                dfs(result, s, end+1, currentList, dp);
+                currentList.remove(currentList.size()-1);
+            }
+        }
     }
 }
