1460c633ddb9d9888f9b1ac1408bbe702557b600
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-19 23:06:02 +0530

    Time: 7 ms (99.62%) | Memory: 151.1 MB (29.54%) - LeetSync

--- 2165-plates-between-candles/plates-between-candles.java
+++ 2165-plates-between-candles/plates-between-candles.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] platesBetweenCandles(String s, int[][] queries) {
+    public int[] platesBetweenCandlesInitialONLogN(String s, int[][] queries) {
         int n = s.length();
         int[] plates = new int[n];
         int curr = 0;
@@ -19,11 +19,47 @@ class Solution {
         }
         return res;
     }
-    public int getPlatesCount(int start, int end, String s){
-        int count =0;
-        for(int i=start;i<=Math.min(end,s.length()-1);i++){
-            if(s.charAt(i)=='*') count++;
+
+    public int[] platesBetweenCandles(String S, int[][] queries) {
+        int n = S.length();
+        char[] s = S.toCharArray();
+        int[] prefixSum = new int[n];
+        prefixSum[0] = (s[0] == '*') ? 1 : 0;
+        for(int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + (s[i] == '*' ? 1 : 0);
         }
-        return count;
+        int[] nextCandle = new int[n];
+        int next = -1;
+        for(int i = n - 1; i >= 0; i--) {
+            if(s[i] == '|') {
+                next = i;
+            }
+            nextCandle[i] = next;
+        }
+        int[] prevCandle = new int[n];
+        int prev = -1;
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '|') {
+                prev = i;
+            }
+            prevCandle[i] = prev;
+        }
+        int[] res = new int[queries.length];
+        for(int i = 0; i < queries.length; i++) {
+            int x = queries[i][0];
+            int y = queries[i][1];
+            int leftCandle = nextCandle[x];
+            int rightCandle = prevCandle[y];
+            if(leftCandle == -1 || rightCandle == -1 || leftCandle >= rightCandle) continue;
+            res[i] = prefixSum[rightCandle] - prefixSum[leftCandle];
+        }
+        return res;
     }
+    // public int getPlatesCount(int start, int end, String s){
+    //     int count =0;
+    //     for(int i=start;i<=Math.min(end,s.length()-1);i++){
+    //         if(s.charAt(i)=='*') count++;
+    //     }
+    //     return count;
+    // }
 }
