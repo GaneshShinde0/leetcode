8cd9f35b82357ef8233ea6db155226cd4987e27b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-20 10:05:09 +0530

    Time: 54 ms (35.14%) | Memory: 47.1 MB (10.81%) - LeetSync

--- 2995-number-of-unique-categories/number-of-unique-categories.java
+++ 2995-number-of-unique-categories/number-of-unique-categories.java
@@ -7,24 +7,39 @@
  */
 class Solution {
 	public int numberOfCategories(int n, CategoryHandler categoryHandler) {
-    	int[] freq = new int[100];
-        int res = 0;
-        int temp = n;
+    	List<Integer>[] adjList = new ArrayList[n];
+        for(int i=0;i<n;i++){
+            adjList[i] = new ArrayList<Integer>();
+        }
+
+        // Iterate over every pair and add an undirected edge if both belong to the same catergory.
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(categoryHandler.haveSameCategory(i,j)){
-                    if(freq[i]==0) res++;
-                    freq[i]++;
-                    temp--;
-                    break;
-                }else{
-                    freq[i]++;
-                    freq[j]++;
-                    if(freq[i]==0) res++;
-                    if(freq[j]==0) res++;
+                    adjList[i].add(j);
+                    adjList[j].add(i);
                 }
             }
         }
-        return temp;
+
+        int components = 0;
+        boolean[] vis = new boolean[n];
+        // Each DFS means that a new category is being accessed.
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                components++;
+                dfs(adjList,vis, i);
+            }
+        }
+        return components;
 	}
+
+    private void dfs(List<Integer>[] adjList, boolean[] vis, int src){
+        vis[src] = true;
+        for(int i=0;i<adjList[src].size();i++){
+            if(!vis[adjList[src].get(i)]){
+                dfs(adjList, vis, adjList[src].get(i));
+            }
+        }
+    }
 }
