44fcc3767faf0c648ba3d7f18b79866ef95d1d80
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-21 17:42:35 +0530

    Time: 24 ms (76.55%) | Memory: 48.9 MB (25.48%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,50 +1,13 @@
 class Solution {
-    public int subarraySumInitialSolution(int[] nums, int k) {
-        int n = nums.length;
-        if(n==1&&nums[0]!=k)return 0;
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        int[] prefSum = new int[n];
-        int curr =0;
-        int res =0;
-        for(int i=0;i<nums.length;i++){
-            curr+=nums[i];
-            prefSum[i]+=curr;
-            if(hm.containsKey(prefSum[i]-k)) res++;
-            if((prefSum[i]-k)==0)res++;
-            hm.put(curr,i);
-        }
-        System.out.println(hm);
-        return res;
-    }
-
-
-    public int subarraySumold(int[] nums, int k) {
-        int total = 0;
-        int res = 0;
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        hm.put(0,1);
-        for(int n:nums){
-            total+=n;
-            if(hm.containsKey(total-k)){
-                res+=hm.get(total-k);
-            }
-            hm.put(total, hm.getOrDefault(total,0)+1);
-        }
-        return res;
-    }
-
-
     public int subarraySum(int[] nums, int k) {
-        int res =0;
-        int total = 0;
-        Map<Integer, Integer> hm = new HashMap<>();
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int sum = 0;
+        int res = 0;
         hm.put(0,1);
-        for(int i:nums){
-            total+=i;
-            if(hm.containsKey(total-k)){
-                res+=hm.get(total-k);
-            }
-            hm.put(total, hm.getOrDefault(total,0)+1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            res +=hm.getOrDefault(sum-k,0);
+            hm.put(sum,hm.getOrDefault(sum,0)+1);
         }
         return res;
     }
