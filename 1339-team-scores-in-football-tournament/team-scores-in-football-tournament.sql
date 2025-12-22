dbda3f405dc920781a103c67d2e91531f99b984b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-22 15:16:53 +0530

    Time: 376 ms (63.89%) | Memory: 0B (100.00%) - LeetSync

--- 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
+++ 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -12,6 +12,7 @@
 -- GROUP BY t.team_id, team_name
 -- ORDER BY num_points DESC
 
+/*
 WITH ALL_MATCHES AS (
     SELECT host_team as team_id, case 
         when host_goals>guest_goals then 3
@@ -27,6 +28,24 @@ WITH ALL_MATCHES AS (
 )
 SELECT t.team_id, t.team_name, COALESCE(SUM(points),0) as num_points 
 FROM Teams t LEFT JOIN all_matches a
-on a.team_id = t.team_id
+on t.team_id = a.team_id
 GROUP BY t.team_id, t.team_name
 ORDER BY num_points DESC, TEAM_ID ASC
+
+*/
+
+/* Write your T-SQL query statement below */
+SELECT team_id, team_name, 
+SUM( 
+    CASE WHEN team_id = host_team AND host_goals > guest_goals THEN 3 
+         WHEN team_id = guest_team AND guest_goals > host_goals THEN 3 
+         WHEN host_goals = guest_goals THEN 1 
+         ELSE 0 
+    END           
+) AS num_points
+FROM Teams t 
+LEFT JOIN Matches m ON t.team_id = m.host_team OR t.team_id = m.guest_team 
+
+GROUP BY team_id, team_name 
+
+ORDER BY num_points DESC, team_id 
