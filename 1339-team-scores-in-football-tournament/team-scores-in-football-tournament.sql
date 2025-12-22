2902b5b7323a6363f6a6f79b00e2062028b537ed
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-22 15:14:08 +0530

    Time: 414 ms (47.22%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -0,0 +1,32 @@
+/* Write your T-SQL query statement below */
+
+-- SELECT t.team_id, team_name, SUM(
+--     CASE WHEN m.host_goals>m.guest_goals THEN 3
+--     WHEN m.host_goals=m.guest_goals THEN 1
+--     WHEN m2.host_goals<m2.guest_goals THEN 3
+--     WHEN m2.host_goals=m2.guest_goals THEN 1
+--     ELSE 0 END
+--     ) as num_points
+-- FROM Teams t FULL OUTER join MATCHES m on t.team_id = m.host_team
+-- FULL OUTER JOIN MATCHES m2 on t.team_id=m2.guest_team
+-- GROUP BY t.team_id, team_name
+-- ORDER BY num_points DESC
+
+WITH ALL_MATCHES AS (
+    SELECT host_team as team_id, case 
+        when host_goals>guest_goals then 3
+        WHEN host_goals=guest_goals then 1
+        ELSE 0 END as points
+    FROM matches
+    UNION ALL
+    SELECT guest_team as team_id, case 
+        when host_goals<guest_goals then 3
+        WHEN host_goals=guest_goals then 1
+        ELSE 0 END as points
+    FROM matches
+)
+SELECT t.team_id, t.team_name, COALESCE(SUM(points),0) as num_points 
+FROM Teams t LEFT JOIN all_matches a
+on a.team_id = t.team_id
+GROUP BY t.team_id, t.team_name
+ORDER BY num_points DESC, TEAM_ID ASC
