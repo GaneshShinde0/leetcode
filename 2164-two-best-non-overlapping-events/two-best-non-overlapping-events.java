5f26ed94c8c70fcf9461a9f1dd2259e281c638a3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-23 13:21:58 +0530

    Time: 70 ms (25.00%) | Memory: 197.2 MB (5.00%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -30,7 +30,7 @@ class Solution {
         return dp[idx][count];
     }
 
-    public int maxTwoEvents(int[][] events) {
+    public int maxTwoEventsPQ(int[][] events) {
         PriorityQueue<Pair<Integer,Integer>> pq = new PriorityQueue<>(
             Comparator.comparingInt(Pair::getKey)
         );
@@ -38,6 +38,7 @@ class Solution {
         int maxVal = 0, maxSum = 0;
         
         for(int[] event: events){
+            // Take Maximum Value That is ending before latest event start time
             while(!pq.isEmpty() && pq.peek().getKey()<event[0]){ // If Event is ending before start of current event;
                 maxVal = Math.max(maxVal, pq.peek().getValue());
                 pq.poll();
@@ -48,5 +49,31 @@ class Solution {
             // System.out.println(pq);
         }
         return maxSum;
+    }
+
+    public int maxTwoEvents(int[][] events){
+        List<int[]> times = new ArrayList<>();
+
+        for(int[] event:events){
+            // 1 dennotes start time
+            times.add(new int[]{event[0],1, event[2]});
+            // 0 denotes end time;
+            times.add(new int[]{event[1]+1,0,event[2]});
+        }
+        times.sort((a,b)->
+            a[0]==b[0]? Integer.compare(a[1],b[1])
+            : Integer.compare(a[0],b[0])
+        );
+        int ans = 0, maxValue = 0;
+
+        // Process the sorted times
+        for(int[] timeValue: times){
+            if(timeValue[1]==1){
+                ans = Math.max(ans, timeValue[2]+maxValue);
+            }else{
+                maxValue = Math.max(maxValue, timeValue[2]);
+            }
+        }
+        return ans;
     }
 }
