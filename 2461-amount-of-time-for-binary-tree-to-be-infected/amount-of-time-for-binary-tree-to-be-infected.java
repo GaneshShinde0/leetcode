ef2ab9d2859a616a1d03a88a6897581b66100e47
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-23 16:34:07 +0530

    Time: 8 ms (97.78%) | Memory: 90.5 MB (86.23%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -31,7 +31,7 @@ class Solution {
         convert(current.right, current.val, map);
     }
 
-    public int amountOfTime(TreeNode root, int start){
+    public int amountOfTimeGraph(TreeNode root, int start){
         Map<Integer, Set<Integer>> map = new HashMap<>();
         convert(root, 0, map);
         Queue<Integer> queue = new LinkedList<>();
@@ -56,6 +56,29 @@ class Solution {
         return minute-1;
     }
 
+    private int maxDistance = 0;
+    public int amountOfTime(TreeNode root, int start){
+        traverse(root, start);
+        return maxDistance;
+    }
+
+    public int traverse(TreeNode root, int start){
+        int depth = 0;
+        if(root==null) return depth;
+        int leftDepth = traverse(root.left,start);
+        int rightDepth = traverse(root.right, start);
+        if(root.val==start){
+            maxDistance = Math.max(leftDepth, rightDepth);
+            depth = -1;
+        }else if(leftDepth>=0 && rightDepth>=0){
+            depth = Math.max(leftDepth, rightDepth) + 1;
+        }else{
+            int distance = Math.abs(leftDepth) + Math.abs(rightDepth);
+            maxDistance = Math.max(maxDistance, distance);
+            depth = Math.min(leftDepth, rightDepth) - 1;  
+        }
+        return depth;
+    }
 
 
 
