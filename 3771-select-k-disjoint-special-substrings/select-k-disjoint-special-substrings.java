efb71516d3e5566eb84b7c828c9175ac7397ccf0
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-23 18:28:08 +0530

    Time: 10 ms (85.52%) | Memory: 47.4 MB (63.45%) - LeetSync

--- 3771-select-k-disjoint-special-substrings/select-k-disjoint-special-substrings.java
+++ 3771-select-k-disjoint-special-substrings/select-k-disjoint-special-substrings.java
@@ -12,12 +12,13 @@ class Solution {
             int start = startEnd[i][0];
             int end = startEnd[i][1];
             int j=start;
+            // To Make Sure String is valid check if all characters inside it are not present outside.
             for(;j<=end;j++){
                 int idx = s.charAt(j)-'a';
                 if(startEnd[idx][1]>end) end = startEnd[idx][1];
-                if(startEnd[idx][0]<start) break;
+                if(startEnd[idx][0]<start) break; // We already went to left so we dont have to go to left once more
             }
-            if(j==end+1 && !(start==0 && end ==s.length()-1)){
+            if(j==end+1 && !(start==0 && end ==s.length()-1)){ // !(start==0 && end ==s.length()-1) We check this to make sure our substring is not entire substring ... because in question it is asked that substring is not entire substring.
                 intervals.add(new int[]{start,end});
             }
         }
