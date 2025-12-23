1c8be1e5520fabc710d548eea92543b5a0aaae44
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-24 05:25:07 +0530

    Time: 21 ms (49.55%) | Memory: 47.6 MB (19.54%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String minRemoveToMakeValid(String s) {
+    public String minRemoveToMakeValidUsingStringBuilder(String s) {
         StringBuilder sb = new StringBuilder();
         int opens = 0;
         for(char c:s.toCharArray()){
@@ -17,6 +17,29 @@ class Solution {
                 sb.deleteCharAt(i);
             }
             i--;
+        }
+        return sb.toString();
+    }
+    public String minRemoveToMakeValid(String s){
+        Set<Integer> indexesToRemove = new HashSet<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                stack.push(i);
+            }
+            if(s.charAt(i)==')'){
+                if(stack.isEmpty()) indexesToRemove.add(i); // Stack is Empty and we have openning parenthesis.
+                else stack.pop();
+            }
+        }
+
+        // Put any indexes remaining on stack into the set.
+        while(!stack.isEmpty()) indexesToRemove.add(stack.pop());
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(!indexesToRemove.contains(i)){
+                sb.append(s.charAt(i));
+            }
         }
         return sb.toString();
     }
