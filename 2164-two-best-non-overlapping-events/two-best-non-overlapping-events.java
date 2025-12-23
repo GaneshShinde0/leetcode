9e15f26b064ccbd6ef66dd8a98659b503def3668
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-23 11:28:00 +0530

    Time: 62 ms (29.50%) | Memory: 182.5 MB (19.50%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maxTwoEvents(int[][] events) {
+    public int maxTwoEvents1(int[][] events) {
         Arrays.sort(events,(a,b)->a[0]-b[0]);
         int[][] dp = new int[events.length][3]; // Three Choices
         for(int[] d:dp) Arrays.fill(d,-1);
@@ -28,5 +28,24 @@ class Solution {
             dp[idx][count] = Math.max(include, exclude); 
         }
         return dp[idx][count];
+    }
+
+    public int maxTwoEvents(int[][] events) {
+        PriorityQueue<Pair<Integer,Integer>> pq = new PriorityQueue<>(
+            Comparator.comparingInt(Pair::getKey)
+        );
+
+        Arrays.sort(events, (a,b)-> a[0]-b[0]);
+        int maxVal = 0, maxSum = 0;
+        
+        for(int[] event: events){
+            while(!pq.isEmpty() && pq.peek().getKey()<event[0]){
+                maxVal = Math.max(maxVal, pq.peek().getValue());
+                pq.poll();
+            }
+            maxSum = Math.max(maxSum, maxVal+event[2]);
+            pq.add(new Pair<>(event[1],event[2]));
+        }
+        return maxSum;
     }
 }
