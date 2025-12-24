d4b87806dd8be14d9dba34343c3c9fa6233a3a05
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-24 16:19:33 +0530

    Time: 78 ms (81.67%) | Memory: 68.6 MB (43.48%) - LeetSync

--- 1125-design-file-system/design-file-system.java
+++ 1125-design-file-system/design-file-system.java
@@ -1,6 +1,6 @@
-class FileSystem {
+class FileSystemInitial {
     HashMap<String,Integer> hm;
-    public FileSystem() {
+    public FileSystemInitial() {
         this.hm = new HashMap<>();
         hm.put("",0);
     }
@@ -29,3 +29,35 @@ class FileSystem {
  * boolean param_1 = obj.createPath(path,value);
  * int param_2 = obj.get(path);
  */
+
+ class FileSystem {
+    private Map<String, Integer> map;
+
+    public FileSystem() {
+        map = new HashMap<>();
+        map.put("/", 0); // root
+    }
+
+    public boolean createPath(String path, int value) {
+        if (path.equals("/") || map.containsKey(path)) {
+            return false;
+        }
+
+        int idx = path.lastIndexOf("/");
+        String parent = path.substring(0, idx);
+        if (parent.isEmpty()) {
+            parent = "/";
+        }
+
+        if (!map.containsKey(parent)) {
+            return false;
+        }
+
+        map.put(path, value);
+        return true;
+    }
+
+    public int get(String path) {
+        return map.getOrDefault(path, -1);
+    }
+}
