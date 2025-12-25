2e8c7364fe1a510c803eebfd77b4c26db90f7e23
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-25 21:03:00 +0530

    Time: 3 ms (43.33%) | Memory: 82.1 MB (6.67%) - LeetSync

---
+++ 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long minimumHealth(int[] damage, int armor) {
+        long res = 0, usedArmor=0;
+        for(int d:damage){
+            res+=d;
+            if(d<=armor){
+                usedArmor = Math.max(d,usedArmor);
+            }else{
+                usedArmor = armor;
+            }
+        }
+        if(usedArmor==0)usedArmor = Math.max(usedArmor,armor);
+        return Math.max(res-usedArmor+1,1);
+    }
+}
