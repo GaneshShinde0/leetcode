a0b7a75fddf0e6b1f0e893b76a112e8410958ad9
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-25 23:58:48 +0530

    Time: 1 ms (100.00%) | Memory: 50.5 MB (58.82%) - LeetSync

--- 2653-check-if-there-is-a-path-with-equal-number-of-0s-and-1s/check-if-there-is-a-path-with-equal-number-of-0s-and-1s.java
+++ 2653-check-if-there-is-a-path-with-equal-number-of-0s-and-1s/check-if-there-is-a-path-with-equal-number-of-0s-and-1s.java
@@ -1,6 +1,7 @@
 class Solution {
     int m,n,res;
-    public boolean isThereAPath(int[][] grid) {
+    // 25ms
+    public boolean isThereAPathInitial(int[][] grid) {
        this.m = grid.length;
        this.n = grid[0].length;
        if((m+n)%2==0) return false;
@@ -25,5 +26,29 @@ class Solution {
         }
         memo[i][j][curr] = temp;
         return temp;
+    }
+
+    Boolean[][][] memo;
+    int k;
+    public boolean isThereAPath(int[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
+        if ((n+m) % 2 == 0) return false;
+        k = (n+m) / 2; // Target sum
+        // Memo up to k, no need for after k since we know it'll be False
+        memo = new Boolean[n][m][k+1];
+        return isThereAPath(grid, 0, 0, 0);
+    }
+
+    public boolean isThereAPath(int[][] grid, int x, int y, int sum) {
+        if (x >= n || y >= m) return false;
+        sum += grid[x][y];
+        // Sum shouldn't be more than k.
+        // Also, n+m is max possible sum and x+y is what we've seen so far. Their difference
+        // is the remaining cells, which must be at least k-sum (otherwise we won't get to k == sum)
+        if (sum > k || n+m-x-y < k-sum) return false; // Optimization/Optional
+        if (x == n-1 && y == m-1) return memo[x][y][sum] = sum == k;
+        if (memo[x][y][sum] != null) return memo[x][y][sum];
+        return memo[x][y][sum] = isThereAPath(grid, x+1, y, sum) || isThereAPath(grid, x, y+1, sum);
     }
 }
