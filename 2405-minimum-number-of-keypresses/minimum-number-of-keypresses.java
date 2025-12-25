8aa87f78842e912d07ba50bfa4a64a1988f2d014
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-25 18:14:00 +0530

    Time: 4 ms (100.00%) | Memory: 46.8 MB (34.62%) - LeetSync

--- 2405-minimum-number-of-keypresses/minimum-number-of-keypresses.java
+++ 2405-minimum-number-of-keypresses/minimum-number-of-keypresses.java
@@ -1,13 +1,23 @@
 class Solution {
     public int minimumKeypresses(String s) {
         int[] freq = new int[26];
-        for(char c:s.toCharArray()) freq[c-'a']++;
+        for(char c : s.toCharArray()) freq[c - 'a']++;
+        
         Arrays.sort(freq);
+        
         int res = 0;
-        for(int i=0;i<26;i++){
-            // if(freq[i]==0) continue;
-            int mult = (i>=17)?1:(i>=8)?2:3;
-            res += mult*freq[i];
+        // Iterate from largest frequency (index 25) down to smallest (index 0)
+        // j represents how many characters we have processed so far (0, 1, 2...)
+        for (int i = 25, j = 0; i >= 0; i--, j++) {
+            // Optimization: If freq is 0, we can stop early (no more chars in string)
+            if (freq[i] == 0) break;
+            
+            // Math trick: 
+            // First 9 items (0-8) get multiplier 1
+            // Next 9 items (9-17) get multiplier 2
+            int mult = (j / 9) + 1;
+            
+            res += freq[i] * mult;
         }
         return res;
     }
