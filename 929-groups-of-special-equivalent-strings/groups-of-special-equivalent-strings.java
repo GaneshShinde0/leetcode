ad3dc63c0d81c909d380542fbdb8f16ecea81e12
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-26 12:34:40 +0530

    Time: 16 ms (28.92%) | Memory: 46.3 MB (53.01%) - LeetSync

--- 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.java
+++ 929-groups-of-special-equivalent-strings/groups-of-special-equivalent-strings.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int numSpecialEquivGroups(String[] words) {
+    public int numSpecialEquivGroupsInitial(String[] words) {
         HashMap<String,Integer> map = new HashMap<>();
         int res = 0;
         for(String s:words){
@@ -9,7 +9,21 @@ class Solution {
                 if(i%2==0) evenFreq[s.charAt(i)-'a']++;
                 else oddFreq[s.charAt(i)-'a']++;
             }
-            String temp = Arrays.toString(oddFreq)+Arrays.toString(evenFreq);
+            String temp = Arrays.toString(oddFreq)+Arrays.toString(evenFreq); // We know string concatenation is expensive
+            map.put(temp,map.getOrDefault(temp,0)+1);
+        }
+        return map.size();
+    }
+    public int numSpecialEquivGroups(String[] words) {
+        HashMap<String,Integer> map = new HashMap<>();
+        int res = 0;
+        for(String s:words){
+            int[] freq = new int[52]; // We will save even initially and odds later ()
+            for(int i=0;i<s.length();i++){
+                if(i%2==0) freq[s.charAt(i)-'a']++;
+                else freq[26+s.charAt(i)-'a']++;
+            }
+            String temp = Arrays.toString(freq);
             map.put(temp,map.getOrDefault(temp,0)+1);
         }
         return map.size();
