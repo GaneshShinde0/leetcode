519d2e2bf07c7a82bf1996baa2fbc968733fa48f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-28 14:44:00 +0530

    Time: 1156 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 2281-the-number-of-passengers-in-each-bus-i/the-number-of-passengers-in-each-bus-i.sql
+++ 2281-the-number-of-passengers-in-each-bus-i/the-number-of-passengers-in-each-bus-i.sql
@@ -1,3 +1,4 @@
+/*
 WITH T as (
     SELECT passenger_id, MIN(b.arrival_time) as boarding_time 
     FROM Passengers p 
@@ -8,4 +9,14 @@ WITH T as (
 
 SELECT bus_id, COUNT(T.boarding_time) as passengers_cnt
 FROM Buses b LEFT Join T on b.arrival_time = T.boarding_time
-GROUP BY bus_id ORDER BY bus_id ASC
+GROUP BY bus_id ORDER BY bus_id ASC;
+*/
+WITH CTE As (
+    Select bus_id, arrival_time, LAG(arrival_time, 1, 0) OVER (ORDER BY arrival_time) min_time FROM buses
+)
+SELECT cte.bus_id, COUNT(passenger_id) as passengers_cnt
+FROM CTE 
+LEFT JOIN Passengers p ON
+p.arrival_time>cte.min_time AND p.arrival_time<=cte.arrival_time
+GROUP BY cte.bus_id
+ORDER BY 1;
