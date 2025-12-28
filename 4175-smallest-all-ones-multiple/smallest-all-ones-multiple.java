400477ffa644791838d448e8ea7308fabee17b80
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-28 10:30:27 +0530

    Time: 9 ms (100.00%) | Memory: 42.8 MB (100.00%) - LeetSync

--- 4175-smallest-all-ones-multiple/smallest-all-ones-multiple.java
+++ 4175-smallest-all-ones-multiple/smallest-all-ones-multiple.java
@@ -1,10 +1,13 @@
 class Solution {
     public int minAllOneMultiple(int k) {
         if(k%2==0||k%5==0) return -1;
-        int remainder = 1;
+        int curr = 1; // We are looking for a number composed of digit 1, sequence starts with the number 1.
+        // We initialize curr = 1 to represent the first number in the sequence.
+        // Count = 1 means the current number has length 1.
         int count = 1;
-        while(remainder!=0){
-            remainder = (remainder*10+1)%k;
+        while(curr%k!=0){ // We are checking if current is multiple of k
+            curr = (curr*10+1)%k; // We are multiplying our number of 10 and adding 1 ..
+            // Basically we are generating the sequence 
             count++;
         }
         return count;
