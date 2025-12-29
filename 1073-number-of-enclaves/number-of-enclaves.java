3d70dbbde5789532a944915b1166d06b5e101700
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-29 12:17:20 +0530

    Time: 10 ms (35.33%) | Memory: 53.5 MB (88.77%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,30 +1,30 @@
 class Solution {
     private int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
     public int numEnclaves(int[][] grid) {
-        Stack<int[]> q = new Stack<>();
+        Stack<int[]> stk = new Stack<>();
         int m = grid.length, n = grid[0].length;
         for(int i=0;i<m;i++){
             if(grid[i][0]==1){
-                q.add(new int[]{i,0});
+                stk.add(new int[]{i,0});
                 grid[i][0] = 0; // Mark visited immediately
             }
             if(grid[i][n-1]==1){
-                q.add(new int[]{i,n-1});
+                stk.add(new int[]{i,n-1});
                 grid[i][n-1] = 0; // Mark visited immediately
             }
         }
         for(int i=0;i<n;i++){
             if(grid[0][i]==1){
-                q.add(new int[]{0,i});
+                stk.add(new int[]{0,i});
                 grid[0][i] = 0; // Mark visited immediately
             }
             if(grid[m-1][i]==1){
-                q.add(new int[]{m-1,i});
+                stk.add(new int[]{m-1,i});
                 grid[m-1][i] = 0; // Mark visited immediately
             }
         }
-        while(!q.isEmpty()){
-            int[] curr = q.pop();
+        while(!stk.isEmpty()){
+            int[] curr = stk.pop();
             int x = curr[0];
             int y = curr[1];
             for(int[] dir:dirs){
@@ -32,7 +32,7 @@ class Solution {
                 int ny = y+dir[1];
                 if(nx<0||ny<0||nx>=m||ny>=n||grid[nx][ny]==0) continue;
                 grid[nx][ny]=0;
-                q.push(new int[]{nx,ny});
+                stk.push(new int[]{nx,ny});
             }
         }
         int res = 0;
