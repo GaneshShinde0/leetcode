f1cf370a1eaa49797aa931045045f1d82cd6a0df
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2025-12-31 08:22:00 +0530

    Time: 42 ms (69.04%) | Memory: 80.8 MB (46.70%) - LeetSync

--- 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
@@ -1,6 +1,6 @@
 class Solution {
     private int[][] directions = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
-    public int latestDayToCross(int row, int col, int[][] cells) {
+    public int latestDayToCrossBinarySearchAndBFS(int row, int col, int[][] cells) {
         int left = 1;
         // int right = row*col; // Right is the point when all cells get filled.
         // Following also works fine
@@ -46,4 +46,45 @@ class Solution {
         }
         return false;
     }
+
+    // Binary Search + DFS
+    public int latestDayToCross(int row, int col, int[][] cells){
+        int left = 1, right = cells.length;
+        while(left<right){
+            int mid = right - (right-left)/2;
+            if(canCrossDFS(row, col, cells, mid)){
+                left = mid;
+            }else{
+                right = mid-1;
+            }
+        }
+        return left;
+    }
+
+    private boolean canCrossDFS(int row, int col, int[][] cells, int day){
+        int[][] grid = new int[row][col];
+        for(int i=0;i<day; i++){
+            int r = cells[i][0]-1, c = cells[i][1]-1;
+            grid[r][c]=1;
+        }
+        for(int i=0;i<col;i++){
+            if(grid[0][i]==0 && dfs(grid,0,i,row, col)){
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean dfs(int[][] grid, int r, int c, int row, int col){
+        if(r<0||r>=row||c<0||c>=col||grid[r][c]!=0){
+            return false;
+        }
+        if(r==row-1) return true;
+        grid[r][c]=-1;
+        for(int[] dir:directions){
+            int newR = r+dir[0], newC = c+dir[1];
+            if(dfs(grid, newR, newC, row, col)) return true;
+        }
+        return false;
+    }
+
 }
