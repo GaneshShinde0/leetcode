c62fedd37893e7dd13208ab1a45c8df1d28b5dc6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-02 20:53:12 +0530

    Time: 27 ms (61.46%) | Memory: 101 MB (7.60%) - LeetSync

---
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.java
@@ -0,0 +1,59 @@
+class RandomizedSet 
+{
+    ArrayList<Integer> arrlist;
+    HashMap<Integer,Integer> hm;
+    Random rand = new Random();
+    public RandomizedSet() 
+    {
+        arrlist=new ArrayList<>();
+        hm=new HashMap<>();
+        
+    }
+    
+    public boolean insert(int val) 
+    {
+        if(hm.containsKey(val))
+        {
+            return false;
+        }
+        else
+        {
+            hm.put(val,arrlist.size());
+            arrlist.add(val);
+            return true;
+        }
+        
+    }
+    
+    public boolean remove(int val) 
+    {
+        if(!hm.containsKey(val))
+        {
+            return false;
+        }
+        int position=hm.get(val);
+        if(position!=(arrlist.size()-1))
+        {
+            int lastelevalue=arrlist.get(arrlist.size()-1);
+            arrlist.set(position,lastelevalue);
+            hm.put(lastelevalue,position);
+        }
+        hm.remove(val);
+        arrlist.remove(arrlist.size()-1);
+        return true;
+    }
+    
+    public int getRandom() 
+    {
+     int randomval=rand.nextInt(arrlist.size());
+     return arrlist.get(randomval);
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
