3cefe418cea04b65fc28b168a53166ed7cc6b862
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-05 21:55:02 +0530

    Time: 26 ms (84.75%) | Memory: 45.3 MB (85.39%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -26,11 +26,10 @@ class UnionFind{
     }
 }
 class Solution {
-    public int minCostConnectPoints(int[][] points) {
+    public int minCostConnectPointsInital346ms(int[][] points) {
         
         int n = points.length;
         ArrayList<int[]> allEdges = new ArrayList<>();
-
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 int weight = Math.abs(points[i][0]-points[j][0])+Math.abs(points[i][1]-points[j][1]);
@@ -51,5 +50,49 @@ class Solution {
             }
         }
         return mostCost;
+    }
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        int mstCost = 0;
+        int edgesUsed = 0;
+        
+        // Track nodes which are visited.
+        boolean[] inMST = new boolean[n];
+        
+        int[] minDist = new int[n];
+        minDist[0] = 0;
+        
+        for (int i = 1; i < n; ++i) {
+            minDist[i] = Integer.MAX_VALUE;
+        }
+        
+        while (edgesUsed < n) {
+            int currMinEdge = Integer.MAX_VALUE;
+            int currNode = -1;
+            
+            // Pick least weight node which is not in MST.
+            for (int node = 0; node < n; ++node) {
+                if (!inMST[node] && currMinEdge > minDist[node]) {
+                    currMinEdge = minDist[node];
+                    currNode = node;
+                }
+            }
+            
+            mstCost += currMinEdge;
+            edgesUsed++;
+            inMST[currNode] = true;
+            
+            // Update adjacent nodes of current node.
+            for (int nextNode = 0; nextNode < n; ++nextNode) {
+                int weight = Math.abs(points[currNode][0] - points[nextNode][0]) + 
+                             Math.abs(points[currNode][1] - points[nextNode][1]);
+                
+                if (!inMST[nextNode] && minDist[nextNode] > weight) {
+                    minDist[nextNode] = weight;
+                }
+            }
+        }
+        
+        return mstCost;
     }
 }
