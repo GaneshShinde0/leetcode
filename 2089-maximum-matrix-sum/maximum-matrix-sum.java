e36aa670becb8cedb09fa414649a8412972e34c3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-05 08:59:48 +0530

    Time: 5 ms (91.52%) | Memory: 68.2 MB (34.55%) - LeetSync

--- 2089-maximum-matrix-sum/maximum-matrix-sum.java
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public long maxMatrixSum(int[][] matrix) {
+    public long maxMatrixSum6ms(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         int negs = 0, min = Integer.MIN_VALUE, posMin = Integer.MAX_VALUE;
         long sum = 0;
@@ -19,6 +19,40 @@ class Solution {
         }else if(negs%2==1){
             return sum - 2*posMin; // As posMin is less we can reove posMin from sum
         }else{
+            return sum;
+        }
+    }
+
+    public long maxMatrixSum(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int negs = 0;
+        
+        // Initialize minAbs to a large number so we can find the true minimum
+        int minAbs = Integer.MAX_VALUE; 
+        long sum = 0;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int val = matrix[i][j];
+                
+                // 1. Check for negative
+                if(val < 0){
+                    negs++;
+                }
+                
+                // 2. Always add absolute value to sum
+                int absVal = Math.abs(val);
+                sum += absVal;
+                
+                // 3. Track the smallest magnitude number in the ENTIRE matrix
+                minAbs = Math.min(minAbs, absVal);
+            }
+        }
+
+        // If odd negatives, we must sacrifice the smallest absolute number
+        if(negs % 2 == 1){
+            return sum - 2L * minAbs;
+        } else {
             return sum;
         }
     }
