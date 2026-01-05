8020ff60375a5236240b7e5db8b5943a9897c30f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-05 10:17:58 +0530

    Time: 1 ms (86.34%) | Memory: 47.2 MB (60.66%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,24 +1,46 @@
-class Solution {
+class UnionFind{
+    int[] parent;
+    int[] rank;
 
+    public UnionFind(int size){
+        parent = new int[size];
+        rank = new int[size];
+        for(int i=0;i<size;i++) parent[i] = i; // Initially Each Node is its own parent in UnionFind/Disjoint Sets
+    }
+
+    public int find(int x){
+        // Check Following Later
+        // return parent[x];
+        if(parent[x]!=x) parent[x] =find(parent[x]);
+        return parent[x];
+    }
+
+    public void union_set(int x, int y){
+        int xset = find(x), yset = find(y);
+        if(xset==yset) return;
+        else if(rank[xset]<rank[yset]){ // Y's parent has higher rank. Make yset as parent of x;
+            parent[xset] = yset; 
+        }else if(rank[xset]>rank[yset]){ // X's parent has higher rank. Make xset as parent of y; 
+            parent[yset] = xset;
+        }else{
+            parent[yset]=xset; // By Defaut make xset as parent of yset.
+            rank[xset]++; /// As xset became parent we will increase its rank;
+        }
+    }
+}
+class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int[] vis = new int[n];
-        int cnt = 0;
+        UnionFind uf = new UnionFind(n);
+        int numberOfComponents = n;
         for(int i=0;i<n;i++){
-            if(vis[i]==0){
-                dfs(isConnected, vis, n, i);
-                cnt++;
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1 && uf.find(i)!=uf.find(j)){ // If points are connnected and dont have common parent;
+                    numberOfComponents--;
+                    uf.union_set(i,j);
+                }
             }
         }
-        return cnt;
-    }
-    // This DFS will visit all the rows and columns for particullar index.
-    public void dfs(int[][] isConnected, int[] vis, int n, int u){
-        vis[u]=1;
-        for(int v=0;v<n;v++){
-            if(vis[v]==0&&isConnected[u][v]==1){
-                dfs(isConnected,vis,n,v);
-            }
-        }
+        return numberOfComponents;
     }
 }
