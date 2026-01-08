291c6ffbe9025cd5bf1448849ebbc6534356ca99
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-08 22:45:47 +0530

    Time: 0 ms (100.00%) | Memory: 52.9 MB (60.41%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -24,4 +24,30 @@ class Solution {
         }
         return nums[low];
     }
+
+     public int singleNonDuplicateAlternate(int[] nums) {
+        int low = 0, high = nums.length - 1;
+        
+        while (low < high) {
+            int mid = (low + high) / 2;
+            
+            // TRICK: Ensure mid is even. 
+            // If mid is odd, decrement to make it even.
+            if (mid % 2 == 1) {
+                mid--; 
+            }
+            
+            // Now we check if the pair (even, odd) matches
+            if (nums[mid] == nums[mid + 1]) {
+                // Pair is valid, so single element is to the RIGHT
+                // We can skip this pair completely (mid and mid+1)
+                low = mid + 2;
+            } else {
+                // Pair is broken (or we are at the single element)
+                // The single element is either at 'mid' or to the LEFT
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
 }
