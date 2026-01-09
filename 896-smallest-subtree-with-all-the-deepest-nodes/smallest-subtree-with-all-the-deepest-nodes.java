2cb67a00c491e0e976a82d2dccd54a0627482a58
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-09 08:15:05 +0530

    Time: 2 ms (7.32%) | Memory: 43.6 MB (50.68%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.java
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.java
@@ -36,9 +36,41 @@ class Solution {
     public TreeNode answer(TreeNode node){
         if(node==null || depth.get(node)==maxDepth) return node;
         TreeNode L = answer(node.left), R = answer(node.right);
-        if(L!=null && R != null) return node;
-        if(L != null) return L;
-        if(R != null) return R;
-        return null;
+        if(L!=null && R != null) return node; // Both Left and Right have a node with max Value (we will return node)
+        if(L != null) return L; // Left Has Node with max value; return Left
+        if(R != null) return R;  // Right has node with max value; return right;
+        return null; // Null found return null
+    }
+}
+
+class SolutionAlternate {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return dfs(root).node;
+    }
+
+    // Return the result of the subtree at this node.
+    public Result dfs(TreeNode node) {
+        if (node == null) return new Result(null, 0);
+        Result L = dfs(node.left),
+               R = dfs(node.right);
+        if (L.dist > R.dist) return new Result(L.node, L.dist + 1);
+        if (L.dist < R.dist) return new Result(R.node, R.dist + 1);
+        return new Result(node, L.dist + 1);
+    }
+}
+
+/**
+ * The result of a subtree is:
+ *       Result.node: the largest depth node that is equal to or
+ *                    an ancestor of all the deepest nodes of this subtree.
+ *       Result.dist: the number of nodes in the path from the root
+ *                    of this subtree, to the deepest node in this subtree.
+ */
+class Result {
+    TreeNode node;
+    int dist;
+    Result(TreeNode n, int d) {
+        node = n;
+        dist = d;
     }
 }
