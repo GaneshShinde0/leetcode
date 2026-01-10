0b2b2b544c47f7ee5f97295c6c9c338a435ca3a1
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-10 19:06:36 +0530

    Time: 28 ms (11.61%) | Memory: 50.1 MB (27.59%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -28,22 +28,24 @@ class Solution {
     private void dijkstra(int start, int[] dist, Map<Integer,List<List<Integer>>> adj){
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[1],b[1]));
         pq.add(new int[]{start,0});
-        boolean[] visited = new boolean[dist.length+1];
+        // boolean[] visited = new boolean[dist.length+1];
         while(!pq.isEmpty()){
             int[] curr = pq.poll();
             int node = curr[0];
             int distance = curr[1];
             if(!adj.containsKey(node)) continue;
             if(dist[node]<distance) continue;
-            if(visited[node]) continue;
+            // if(visited[node]) continue;
             dist[node]=distance;
             for(List<Integer> edge:adj.get(node)){
                 int subNode = edge.get(0);
                 int subDist = edge.get(1);
-                dist[subNode] = Math.min(dist[subNode],distance+subDist);
-                pq.add(new int[]{subNode,dist[subNode]});
+                if(dist[subNode]>distance+subDist){
+                    dist[subNode] = Math.min(dist[subNode],distance+subDist);
+                    pq.add(new int[]{subNode,dist[subNode]});
+                }
             }
-            visited[node]=true;
+            // visited[node]=true;
         }
     }
 }
