bf91ea4776d9daa02dff69d4abeba2bdfb87ff91
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-10 08:15:03 +0530

    Time: 19 ms (75.26%) | Memory: 46.6 MB (42.92%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -4,9 +4,11 @@ class Solution {
         int[][] dp = new int[m+1][n+1];
         
         // Fill Base Case Values
+        // Deleting characcters from string 1 considering string 2 is empty.
         for(int i=1;i<=m;i++){
             dp[i][0] = dp[i-1][0]+ s1.charAt(i-1);
         }
+        // Deleting characters from string 2 considering string 1 is empty.
         for(int i=1;i<=n;i++){
             dp[0][i] = dp[0][i-1]+ s2.charAt(i-1);
         }
