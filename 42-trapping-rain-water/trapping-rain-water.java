4c2158e20fe793ea28247df1d6b85c9a216e83d3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-10 20:08:22 +0530

    Time: 1 ms (58.93%) | Memory: 48.9 MB (10.55%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,21 +1,27 @@
+/*
+Input: height = [4,2,0,3,2,5]
+Left: height = [4,4,4,4,4,5]
+Right: height = [5,5,5,5,5,5]
+0,2,4,1,2
+*/
+
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        int[] leftHeight = new int[n];
-        int[] rightHeight = new int[n];
-        int res = 0;
-        leftHeight[0]=height[0];
-        rightHeight[n-1]=height[n-1];
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
         for(int i=1;i<n;i++){
-            leftHeight[i]=Math.max(height[i],leftHeight[i-1]);
-            rightHeight[n-i-1]=Math.max(height[n-i-1],rightHeight[n-i]);
+            leftMax[i] = Math.max(leftMax[i-1],height[i]);
         }
-        
-        // for(int i=n-2;i>=0;i--){
-        //     rightHeight[i] = Math.max(height[i],rightHeight[i+1]);
-        // }
+        for(int i=n-2;i>=0;i--){
+            rightMax[i] = Math.max(rightMax[i+1],height[i]);
+        }
+
+        int res = 0;
         for(int i=0;i<n;i++){
-            res+=Math.min(leftHeight[i],rightHeight[i])-height[i];
+            res+=Math.min(leftMax[i],rightMax[i])-height[i];
         }
         return res;
     }
