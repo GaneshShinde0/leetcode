816bfa1acd98c83e440e7a53e6384543cb43be6b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-10 22:15:42 +0530

    Time: 4 ms (20.64%) | Memory: 47.6 MB (20.31%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.java
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestMountain(int[] arr) {
+    public int longestMountainInitialNeedsCleaninc(int[] arr) {
         int inc = 0, dec = 0,res = 0;
         for(int i=1;i<arr.length;i++){
             if(arr[i]>arr[i-1]){
@@ -16,6 +16,17 @@ class Solution {
                 dec = 0;
             }
             if(inc>0 && dec>0)res = Math.max(inc+dec+1,res);
+        }
+        return res;
+    }
+
+    public int longestMountain(int[] A) {
+        int res = 0, inc = 0, dec = 0;
+        for (int i = 1; i < A.length; ++i) {
+            if ((dec > 0 && A[i - 1] < A[i]) || A[i - 1] == A[i]) inc = dec = 0;
+            if (A[i - 1] < A[i]) inc++;
+            if (A[i - 1] > A[i]) dec++;
+            if (inc > 0 && dec > 0 && inc + dec + 1 > res) res = inc + dec + 1;
         }
         return res;
     }
