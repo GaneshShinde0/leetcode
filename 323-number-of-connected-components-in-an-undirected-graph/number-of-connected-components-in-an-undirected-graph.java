18098c2f5edf12c862e58160af635bf55edf9f39
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-11 15:25:48 +0530

    Time: 5 ms (53.76%) | Memory: 46.8 MB (33.13%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,8 +1,13 @@
 // Build a Graph,
 // Perform a DFS.
-
+// Space Complexity 
+    // O(E) for Edges
+    // O(N) for visited,
+// Time Complextiy
+    // O(N) - Number of Nodes
+    // O(E) - Number of edges
 class Solution {
-    public int countComponents(int n, int[][] edges) {
+    public int countComponentsDFS(int n, int[][] edges) {
         HashMap<Integer,List<Integer>> hm = new HashMap<>();
         for(int[] edge:edges){
             hm.computeIfAbsent(edge[0],k->new ArrayList<Integer>()).add(edge[1]);
@@ -30,4 +35,47 @@ class Solution {
             if(!vis[j]) dfs(hm,vis,j);
         }
     }
+
+    public int countComponents(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        for(int[] edge:edges){
+            uf.union(edge[0],edge[1]);
+        }
+        System.out.println(Arrays.toString(uf.parent));
+        Set<Integer> set = new HashSet<>();
+        for(int i:uf.parent){
+            set.add(uf.find(i));
+        }
+        return set.size();
+    }
+}
+
+class UnionFind{
+    int[] parent;
+    int[] rank;
+    UnionFind(int n){
+        parent = new int[n];
+        rank = new int[n];
+        for(int i=0;i<n;i++)parent[i]=i;
+    }
+    int find(int x){
+        if(x!=parent[x]) parent[x]=find(parent[x]);
+        return parent[x];
+    }
+    void union(int x, int y){
+        int xPar=find(x), yPar = find(y);
+        if(xPar==yPar) return;
+        else if(rank[xPar]<rank[yPar]){
+            parent[xPar] = yPar;
+            // rank[xPar]=rank[yPar];
+        }else if(rank[yPar]<rank[xPar]){
+            parent[yPar]=xPar;
+            // rank[yPar] = rank[xPar];
+        }else{
+            parent[yPar]=xPar;
+            rank[xPar]++;
+        }
+    }
+
+
 }
