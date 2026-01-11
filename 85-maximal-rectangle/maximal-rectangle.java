dce6a2086eb254430f23b77e9dce73013eae499d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-11 06:44:21 +0530

    Time: 4 ms (93.88%) | Memory: 50.9 MB (5.23%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maximalRectangle(char[][] matrix) {
+    public int maximalRectangle1(char[][] matrix) {
         if (matrix.length == 0) return 0;
         int maxarea = 0;
         int[][] dp = new int[matrix.length][matrix[0].length];
@@ -18,6 +18,52 @@ class Solution {
                         maxarea = Math.max(maxarea, width * (i - k + 1));
                     }
                 }
+            }
+        }
+        return maxarea;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] left = new int[n]; // initialize left as the leftmost boundary possible
+        int[] right = new int[n];
+        int[] height = new int[n];
+
+        Arrays.fill(right, n); // initialize right as the rightmost boundary possible
+
+        int maxarea = 0;
+        for (int i = 0; i < m; i++) {
+            int cur_left = 0, cur_right = n;
+            // update height
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') height[j]++;
+                else height[j] = 0;
+            }
+            // update left
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1') left[j] = Math.max(left[j], cur_left);
+                else {
+                    left[j] = 0;
+                    cur_left = j + 1;
+                }
+            }
+            // update right
+            for (int j = n - 1; j >= 0; j--) {
+                if (matrix[i][j] == '1') right[j] = Math.min(
+                    right[j],
+                    cur_right
+                );
+                else {
+                    right[j] = n;
+                    cur_right = j;
+                }
+            }
+            // update area
+            for (int j = 0; j < n; j++) {
+                maxarea = Math.max(maxarea, (right[j] - left[j]) * height[j]);
             }
         }
         return maxarea;
