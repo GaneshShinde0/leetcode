75ccec88e8d6aef17794506bb7d6dacff67649cb
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-14 23:55:00 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (53.22%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
@@ -3,7 +3,7 @@ class Solution {
     int m,n;
     long res;
     private static final int MOD = 1_000_000_007;
-    public int maxProductPath(int[][] grid) {
+    public int maxProductPathRecursion(int[][] grid) {
         m = grid.length;
         n = grid[0].length;
         res = Integer.MIN_VALUE;
@@ -15,7 +15,7 @@ class Solution {
 
     private void recurse(long[][] dp, int[][] grid, int i, int j){
         if(grid[i][j]==0){
-            res = Math.max(res,0);
+            res = Math.max(res,0); // This pruning moves solution from TLE to working solution.
             return;
         }
         if(i==m-1&&j==n-1){
@@ -32,5 +32,52 @@ class Solution {
         }
     }
 
-    
+    public int maxProductPath(int[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        long[][] maxDP = new long[m][n];
+        long[][] minDP = new long[m][n];
+        
+        maxDP[0][0] = minDP[0][0] = grid[0][0];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) continue;
+                
+                long val = grid[i][j];
+                
+                // Initialize current max/min with a value that will definitely be overwritten
+                // Or handle edge cases where we are at the first row (i=0) or first col (j=0)
+                
+                if (i == 0) {
+                    // We can only come from the left
+                    maxDP[i][j] = maxDP[i][j-1] * val;
+                    minDP[i][j] = minDP[i][j-1] * val;
+                } else if (j == 0) {
+                    // We can only come from the top
+                    maxDP[i][j] = maxDP[i-1][j] * val;
+                    minDP[i][j] = minDP[i-1][j] * val;
+                } else {
+                    // Can come from TOP or LEFT
+                    // You need to calculate 4 potential candidates:
+                    // 1. max from top * val
+                    long topMax = maxDP[i-1][j]*val;
+                    // 2. min from top * val
+                    long topMin = minDP[i-1][j]*val;
+                    // 3. max from left * val
+                    long leftMax = maxDP[i][j-1]*val;
+                    // 4. min from left * val
+                    long leftMin = minDP[i][j-1]*val;
+                    // TODO: Set maxDP[i][j] to the max of these 4
+                    // TODO: Set minDP[i][j] to the min of these 4
+                    maxDP[i][j] = Math.max(Math.max(leftMin,topMin),Math.max(topMax,leftMax));
+                    minDP[i][j] = Math.min(Math.min(topMax,leftMax),Math.min(topMin,leftMin));
+                }
+            }
+        }
+        
+        long result = maxDP[m-1][n-1];
+        if (result < 0) return -1;
+        return (int) (result % 1_000_000_007);
+    }
 }
