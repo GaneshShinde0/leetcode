adc422a5589bc2e5692b23d98f8998330d262d7c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-19 11:13:12 +0530

    Time: 26 ms (25.60%) | Memory: 46.8 MB (89.29%) - LeetSync

--- 839-short-encoding-of-words/short-encoding-of-words.java
+++ 839-short-encoding-of-words/short-encoding-of-words.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minimumLengthEncoding(String[] words) {
+    public int minimumLengthEncodingInitial(String[] words) {
         Arrays.sort(words,(a,b)->Integer.compare(b.length(),a.length()));
         StringBuilder sb = new StringBuilder();
         for(String word:words){
@@ -8,5 +8,27 @@ class Solution {
             }
         }
         return sb.length();
+    }
+
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> set = new HashSet<>();
+        int totalLen=0;
+        for(String word:words){
+            set.add(word);
+        }
+        for(String word:words){
+            int n = word.length();
+            for(int i=1;i<n;i++){
+                String sub = word.substring(i);
+                if(set.contains(sub)){
+                    set.remove(sub);
+                }
+            }
+        }
+        for(String word:set){
+            totalLen+=word.length()+1;
+        }
+        System.out.println(set);
+        return totalLen;
     }
 }
