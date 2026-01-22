e3af94d3803072a138430cc53e9ec17b93a9bbb6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-22 12:00:05 +0530

    Time: 6 ms (57.24%) | Memory: 80.3 MB (66.55%) - LeetSync

---
+++ 2530-minimize-maximum-of-array/minimize-maximum-of-array.java
@@ -0,0 +1,18 @@
+/*
+Input: nums = [3,7,1,6]
+Input: nums = [4,6,1,6]
+Input: nums = [5,5,1,6]
+Input: nums = [3,7,1,6]
+
+*/
+class Solution {
+    public int minimizeArrayValue(int[] nums) {
+        long currSum =0;
+        long res = 0;
+        for(int i=0;i<nums.length;i++){
+            currSum+=nums[i];
+            res = Math.max((int) Math.ceil(currSum/(i+1.0)),res);
+        }
+        return (int) res;
+    }
+}
