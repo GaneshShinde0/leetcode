862c6a394609d913978d333d6d60935cd8fd6660
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-23 11:01:32 +0530

    Time: 60 ms (12.26%) | Memory: 68.7 MB (81.98%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,65 +1,16 @@
 class Solution {
-    public int totalFruit53ms(int[] fruits) {
-        int start = 0,end = 0;
-        int n = fruits.length,maxLen = 0;
-        Map<Integer,Integer> map = new HashMap<>();
-        while(end<n)
-        {
-            map.put(fruits[end],map.getOrDefault(fruits[end],0)+1);
-            while(map.size()>=3)
-            {
-                map.put(fruits[start],map.get(fruits[start])-1);
-                if(map.get(fruits[start]) == 0) map.remove(fruits[start]);
-                start++;
+    public int totalFruit(int[] fruits) {
+        HashMap<Integer,Integer> fruitToCount = new HashMap<>();
+        int left =0, result = 0;
+        for(int right =0;right<fruits.length;right++){
+            fruitToCount.put(fruits[right],fruitToCount.getOrDefault(fruits[right],0)+1);
+            if(fruitToCount.size()>2){
+                fruitToCount.put(fruits[left],fruitToCount.getOrDefault(fruits[left],0)-1);
+                if(fruitToCount.get(fruits[left])==0) fruitToCount.remove(fruits[left]);
+                left++;
             }
-            int currLen = end-start+1;
-            maxLen = Math.max(maxLen,currLen);
-            end++;
+            result= Math.max(right-left+1,result);
         }
-        return maxLen;
-    }
-
-    static {
-        for (int i = 0; i < 300; i++) {
-            totalFruit(new int[0]);
-        }
-    }
-    public static int totalFruit(int[] fruits) {
-        int max = 0;
-        int type1 = -1, type2 = -1;
-        int count1 = 0, count2 = 0;
-        int lead = -1, leadIndex = -1;
-        for (int i = 0; i < fruits.length; i++) {
-            if (type1 == -1 || type1 == fruits[i]) {
-                type1 = fruits[i];
-                count1++;
-                if (lead != type1) {
-                    lead = type1;
-                    leadIndex = i;
-                }
-            } else if (type2 == -1 || type2 == fruits[i]) {
-                type2 = fruits[i];
-                count2++;
-                if (lead != type2) {
-                    lead = type2;
-                    leadIndex = i;
-                }
-            } else {
-                max = Math.max(max, count1+count2);
-                if (lead == type1) {
-                    count1 = i-leadIndex;
-                    type2 = fruits[i];
-                    count2 = 1;
-                } else {
-                    count2 = i-leadIndex;
-                    type1 = fruits[i];
-                    count1 = 1;
-                }
-                lead = fruits[i];
-                leadIndex = i;
-            }
-        }
-
-        return Math.max(max, count1+count2);
+        return result;
     }
 }
