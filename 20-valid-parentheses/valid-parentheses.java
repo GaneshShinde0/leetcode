6fdedd5da2faf1dc6f1cae5843fb09bcd8bb1bae
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-01-23 10:03:03 +0530

    Time: 3 ms (87.67%) | Memory: 43.4 MB (48.90%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,37 +1,16 @@
 class Solution {
-    public boolean isValidInitial(String s) {
-        Stack<Character> stk = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(!stk.isEmpty()){
-                char top = stk.peek();
-                char curr = s.charAt(i);
-                if((top == '('&& curr==')' ) ||
-                    (top == '[' && curr==']' ) ||
-                    (top == '{' && curr== '}' )){
-                    stk.pop();
-                }else{
-                    stk.push(curr);
-                }
-            }else{
-                stk.push(s.charAt(i));
-            }
-        }   
-        return stk.isEmpty();
-    }
     public boolean isValid(String s) {
         Stack<Character> stk = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                stk.push(c);
-            } else {
-                if (stk.isEmpty()) return false;
-                char top = stk.pop();
-                if ((c == ')' && top != '(') ||
-                    (c == ']' && top != '[') ||
-                    (c == '}' && top != '{')) {
-                    return false;
-                }
+        for(char c:s.toCharArray()){
+            if(c=='('||c=='{'||c=='[') stk.push(c);
+            else if(!stk.isEmpty() && (
+                (stk.peek()=='(' && c==')') ||
+                (stk.peek()=='{' && c=='}') ||
+                (stk.peek()=='[' && c==']')
+            )){
+                stk.pop();
             }
+            else return false;
         }
         return stk.isEmpty();
     }
