f32896c1a460751d17f2c2594619eaca3c58f18a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-07 17:01:25 +0530

    Time: 30 ms (56.76%) | Memory: 129.7 MB (95.95%) - LeetSync

---
+++ 1347-distance-to-a-cycle-in-undirected-graph/distance-to-a-cycle-in-undirected-graph.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] distanceToCycle(int n, int[][] edges) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0;i< n;i++) graph.add(new ArrayList<>());
+        int[] res = new int[n];
+        int[] count = new int[n];
+        Queue<Integer> q = new LinkedList<>();
+        for(int[] edge:edges){
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        for(int i=0;i<n;i++){
+            count[i] = graph.get(i).size();
+            if(graph.get(i).size()==1) q.add(i);
+        }
+
+        while(!q.isEmpty()){
+            int i = q.poll();
+            res[i] = Integer.MAX_VALUE;
+            for(int j: graph.get(i)){
+                if(count[j]>1 && --count[j]==1){
+                    q.add(j);
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(count[i]>1) q.add(i);
+        }
+        
+        while(!q.isEmpty()){
+            int i = q.poll();
+            for(int j:graph.get(i)){
+                if(res[j]>res[i]+1){
+                    res[j] = res[i]+1;
+                    q.add(j);
+                }
+            }
+        }
+        return res;
+    }
+}
