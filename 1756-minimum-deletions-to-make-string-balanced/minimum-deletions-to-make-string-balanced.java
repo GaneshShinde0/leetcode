ef9c4312498fd26862c460ec9e40824361a801b2
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-07 14:33:01 +0530

    Time: 44 ms (30.92%) | Memory: 48.2 MB (5.59%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,6 +1,31 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
+        int[] countA = new int[n];
+        int[] countB = new int[n];
+        int bCount = 0, aCount = 0;
+
+        // First pass: Compute countB which stores the number of b characters to the left of the current position.
+        for(int i=0;i<n;i++){
+            countB[i] = bCount;
+            if(s.charAt(i)=='b') bCount++;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            countA[i] = aCount;
+            if(s.charAt(i)=='a') aCount++;
+        }
+
+        int minDeletion = n;
+        // Third pass: iterate throught the string to find the minimum deletion
+        for(int i=0;i<n;i++){
+            minDeletion = Math.min(minDeletion,countA[i]+countB[i]);
+        }
+        return minDeletion;
+    }
+
+    public int minimumDeletionsDP(String s) {
+        int n = s.length();
         int[] dp = new int[n+1];
         int bCount = 0;
 
@@ -11,6 +36,7 @@ class Solution {
                 bCount++;
             }else{
                 dp[i+1] = Math.min(dp[i]+1,bCount);
+                //                 minDeletions = Math.min(minDeletions + 1, bCount);
             }
         }
         return dp[n];
