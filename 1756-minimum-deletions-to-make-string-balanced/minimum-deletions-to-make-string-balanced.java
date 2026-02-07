588d9b70a826d1141a2029aa091aba674448e88a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-07 14:36:33 +0530

    Time: 42 ms (36.02%) | Memory: 47.8 MB (62.83%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,5 +1,24 @@
 class Solution {
-    public int minimumDeletions(String s) {
+    public int minimumDeletions(String s){
+        int n = s.length();
+        int[] countA = new int[n];
+        int aCount = 0;
+
+        // First Pass: Compute countA which stores the number of 'a' characters to the right of the current position
+        for(int i=n-1;i>=0;i--){
+            countA[i] = aCount;
+            if(s.charAt(i)=='a') aCount++;
+        }
+
+        int minDeletions = n;
+        int bCount = 0;
+        for(int i=0;i<n;i++){
+            minDeletions = Math.min(countA[i]+bCount, minDeletions);
+            if(s.charAt(i)=='b') bCount++;
+        }
+        return minDeletions;
+    }
+    public int minimumDeletionsThreePass(String s) {
         int n = s.length();
         int[] countA = new int[n];
         int[] countB = new int[n];
