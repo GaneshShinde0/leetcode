375520bf0ab9518978cb26b25caa15c423d29327
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-07 09:53:35 +0530

    Time: 24 ms (52.14%) | Memory: 48 MB (30.43%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+        int bCount = 0;
+
+        // dp[i]: The number of deletions required to balance substring s[0,i)
+        for(int i = 0;i<n;i++){
+            if(s.charAt(i)=='b'){
+                dp[i+1] = dp[i];
+                bCount++;
+            }else{
+                dp[i+1] = Math.min(dp[i]+1,bCount);
+            }
+        }
+        return dp[n];
+    }
+}
