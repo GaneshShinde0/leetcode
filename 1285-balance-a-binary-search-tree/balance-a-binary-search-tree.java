6fbd2898db69bdf7b746c609f3ddcfb8abbec930
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-09 10:25:07 +0530

    Time: 5 ms (10.92%) | Memory: 48.8 MB (5.50%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public TreeNode balanceBST(TreeNode root) {
+    public TreeNode balanceBSTInorderTraversalRecursiveConstruction(TreeNode root) {
         List<Integer> li =  new ArrayList<>();
         getNodesOfTree(li,root);
         return getBalancedTree(li,0, li.size()-1);
@@ -31,5 +31,66 @@ class Solution {
         TreeNode leftSubTree = getBalancedTree(li,start,mid-1);
         TreeNode rightSubTree = getBalancedTree(li,mid+1,end);
         return new TreeNode(li.get(mid),leftSubTree, rightSubTree);
+    }
+    
+    // Approach  2: Day-Stout-Warren Algorithm / In-Place Balancing
+    public TreeNode balanceBST(TreeNode root){
+        if(root == null) return null;
+
+        // Step 1: Create the backbone (vine), Temporary dummy node
+        TreeNode vineHead = new TreeNode(0);
+        vineHead.right = root;
+        TreeNode current = vineHead;
+        while(current.right!=null){
+            if(current.right.left!=null){
+                rightRotate(current, current.right);
+            }else{
+                current = current.right;
+            }
+        }
+        // Step 2: Count the Nodes
+        int nodeCount = 0;
+        current = vineHead.right;
+        while(current!=null){
+            ++nodeCount;
+            current = current.right;
+        }
+        
+        // Step 3: Create a balanced BST
+        int m = (int) Math.pow(2, Math.floor(Math.log(nodeCount+1)/Math.log(2)))-1;
+        makeRotations(vineHead, nodeCount-m);
+        while(m>1){
+            m/=2;
+            makeRotations(vineHead,m);
+        }
+
+        TreeNode balancedRoot = vineHead.right;
+        return balancedRoot;
+    }
+
+    // Perform a right rotation
+    private void rightRotate(TreeNode parent, TreeNode node){
+        TreeNode tmp = node.left;
+        node.left = tmp.right;
+        tmp.right = node;
+        parent.right = tmp;
+    }
+
+    // Perform a left rotation
+    private void leftRotate(TreeNode parent, TreeNode node){
+        TreeNode tmp = node.right;
+        node.right = tmp.left;
+        tmp.left = node;
+        parent.right = tmp;
+    }
+
+    // Function to perform a series of left rotations to balance the vine
+    private void makeRotations(TreeNode vineHead, int count){
+        TreeNode current = vineHead;
+        for(int i=0;i<count;i++){
+            TreeNode tmp = current.right;
+            leftRotate(current, tmp);
+            current = current.right;
+        }
     }
 }
