5c5d94d6d912e9edc224bf527c2ced9f955abbba
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-09 09:45:58 +0530

    Time: 2 ms (97.82%) | Memory: 48 MB (88.59%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -14,45 +14,22 @@
  * }
  */
 class Solution {
-
     public TreeNode balanceBST(TreeNode root) {
-        // Create a list to store the inorder traversal of the BST
-        List<Integer> inorder = new ArrayList<>();
-        inorderTraversal(root, inorder);
-
-        // Construct and return the balanced BST
-        return createBalancedBST(inorder, 0, inorder.size() - 1);
+        List<Integer> li =  new ArrayList<>();
+        getNodesOfTree(li,root);
+        return getBalancedTree(li,0, li.size()-1);
     }
-
-    private void inorderTraversal(TreeNode root, List<Integer> inorder) {
-        // Perform an inorder traversal to store the elements in sorted order
-        if (root == null) return;
-        inorderTraversal(root.left, inorder);
-        inorder.add(root.val);
-        inorderTraversal(root.right, inorder);
+    private void getNodesOfTree(List<Integer> li, TreeNode root){
+        if(root==null) return;
+        getNodesOfTree(li, root.left);
+        li.add(root.val);
+        getNodesOfTree(li, root.right);
     }
-
-    private TreeNode createBalancedBST(
-        List<Integer> inorder,
-        int start,
-        int end
-    ) {
-        // Base case: if the start index is greater than the end index, return null
-        if (start > end) return null;
-
-        // Find the middle element of the current range
-        int mid = start + (end - start) / 2;
-
-        // Recursively construct the left and right subtrees
-        TreeNode leftSubtree = createBalancedBST(inorder, start, mid - 1);
-        TreeNode rightSubtree = createBalancedBST(inorder, mid + 1, end);
-
-        // Create a new node with the middle element and attach the subtrees
-        TreeNode node = new TreeNode(
-            inorder.get(mid),
-            leftSubtree,
-            rightSubtree
-        );
-        return node;
+    private TreeNode getBalancedTree(List<Integer> li, int start, int end){
+        if(start>end) return null;
+        int mid = (start+end)/2;
+        TreeNode leftSubTree = getBalancedTree(li,start,mid-1);
+        TreeNode rightSubTree = getBalancedTree(li,mid+1,end);
+        return new TreeNode(li.get(mid),leftSubTree, rightSubTree);
     }
 }
