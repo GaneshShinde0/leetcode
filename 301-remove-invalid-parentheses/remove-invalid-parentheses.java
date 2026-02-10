a3c82a78a67fff1328350ec68649359633692f1d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-10 11:48:02 +0530

    Time: 152 ms (19.71%) | Memory: 44 MB (83.75%) - LeetSync

--- 301-remove-invalid-parentheses/remove-invalid-parentheses.java
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.java
@@ -1,6 +1,7 @@
 /*
 Recursion, Stack, Backtracking
-
+*/
+/*
 Approach 1 : Backtracking
 Intuition: 
 
@@ -38,8 +39,116 @@ Example: (()))
     - In this case the third closing bracket will make the expression invalid. No matter what comes afterward, this will give us an invalid expresssion and if such a thing happens, we shouldn't recurse further and simply prune the recursion tree.
     - This is why in adition to the index of the original string/ expression which we are currently processing and the expression string formed till now, we also keep track of the number of left and right parentheses. Whenever we keep a left parentheseis in the expression, we increment its counter. For a right parentheseis, we check if rightCount<leftCount. If this is the case then only we consider that right parenthesis and recurse further. Otherwise we don't as we know it will make the expression invvalid. This simple optimization saves a lot of runtime.
 
+
+
+###########################
+Complexity Analysis:
+- Time Complexity: O(2^N) since the worst case will have only left parentheses in the expression and for every bracket we will have two options i.e.e whethere to remove it or consider it. Considering that the expresiion has n parenthesis, the time compleixyt will be O(2^N)
+- Space Complexity: O(N) We are resorting to a recursive solution and for a recursive solution there is always a stack space used as internal function. States are saved onto stack during recursion. The maximum depth of recursion decides the stack space used. Since we process one character at a time and the base case for recursion is when we have processed all of the characters of the expression string. The size of the stack would be O(N). Note that twe are not considerin the space required to store the valid expressions. We only count the intermediate space here.
+
 */
-class Solution {
+
+/*
+
+Approach 2: Limited Backtracking!
+Although the previous solution does get accepted on the platform, it is a very inefficient solution because we try removing each and every possible parentheses from the expresssion and in the end we check two things.
+1. If the expression is valid or not.
+2. If the total number of removed parentheses removed in the current recursion is less than the global minimum till now or not.
+
+We cannot determine which of the parentheses are misplaced because as the problem statement puts across, we can remove multiple combinatioons and end up with a valid expression. This means there can be multiple valid expression from a single invalid expression and we have to find all of them.
+
+The one thing all these valid expressions have in common is that they will all be of the same length i.e, as compared to the original expression, all of these expressions will have the same number of characters removed.
+
+What if we could determine this count?
+
+What if in addition to determining this count of characters to be rmoved, could could also dermine the number of left parentheses and number of right parantheses to be removed from the original expression to get any valid expression?
+
+This would cut down the computatiosns immensely and the runtime would plummet as a result. The reson for this is, if we knew how many left and right parentheses are to be removed from the original expression to get a valid expression, we would cut down on so may unwanted recursive calls.
+
+Imagine the original expression to be 1000 characters with only 3 misplaced ( parentheses and 2 misplaced )/ In our previous solution we would end up trying to remove each one of left and right parentheses and try to reach valid expression in the end where as we should only be trying out removing  3 ( brackets and 2 ) brackets.
+
+This is the exact number ( and ) that have to be removed to get a valid expression, no more or less.
+
+How we can find out number of misplaced left and right parenthesses in given expression.
+    1. We process the expression one bracket a time starting from left.
+    2. Suppose we encounter an opening bracket i.e. ( it may or may not lead to an invalid expression because there can be a matching ending bracket somewhere in the remaining part of the expression. Here we simply increment the counter keeping track of left parentheses till now. Left+=1
+    3. If we encounter a closing bracket, this has two meanings.
+        - Either there was no matching opening bracket for this closing bracketand in that case we have an invalid expression. This is the case when left ==0 i.e. when there are no unmatched left brackets available. In such a case we increment another counter say right+=1 to represent mispalced right parentheses.
+        - Or we had some unmatched opening bracket avalialbe to match this closing bracket. This is the case when left>0 . In this case we simply decrement the left counter we had i.e. left -= 1
+    4. Continue processing the string until all parentheses have been processed.
+    5. In the end value of left and right would tell us the nummber of unmatched ( and ) parentheses respectively.
+
+Now that  we have two values available that tell us the total number of left i.e. ( and right i.e. ) parentheses that have to be removed to make the invalid expression valid.
+
+Follwing modifications will be done in approach 1. 
+
+Algorithm: (Almost same with changes)
+
+- The state of the recursion is now defined by five different variables:
+    1. Index which represents the current character that we have to process in the original string.
+    2. leftCount which represents the number of left parentheses that have been added to the expression we are building.
+    3. rightCount which represents the number of right parentheses that have been added to the expression we are building.
+    4. leftRem is the number of left parentheses that remain to be removed.
+    5. rightRem represents the number of right parentheses that remain to be removed. Overall, for the final expression to be valid, leftRem == 0 and rightRem == 0;
+
+- When we decide to not consider a parenthesis i.e. delete a parenthesis, be it a left or a right parentheses, we have to consider their corresponding remaining counts as well. This means that we can only discard a left parentheses if leftRem>0 and similarly for the right one we will check for rightRem>0.
+- There are no changes to checks for considering a parenthesis. Only the conditions change for discarding a parenthesis.
+- Condition for an expression being balid in the base case would now become leftRem==0 and reightRem==0 Note that we don't have to check if leftCount == rightCount anymore because in the case of a valid expression, we would have removed all the misplaed or invalid parenthesis by the time the recursion ends. So the only check we need if leftRem == 0 and rightRem == 0;
+
+
+Most important thing here is that we have completely gotten rid of checking if the number of parentheses removed is lesser than the current minimum or not. the reason for this is we always remove the smae number of parentheses as defined by leftRem + rightRem at the start of recursion.
+
+
+*/
+
+class Solution{
+    private Set<String> validExpressions = new HashSet<String>();
+    private void recurse(String s, int index, int leftCount, int rightCount, int leftRem, int rightRem, StringBuilder expression){
+        // If we reached the end of the string, just check if the resulting expression is valid or not and also if we chave removed the total number of left and right parentheses that we should have removed.
+        if(index==s.length()){
+            if(leftRem == 0 && rightRem==0) this.validExpressions.add(expression.toString());
+        }else{
+            char character = s.charAt(index);
+            int length = expression.length();
+            // The discard case. Note that here we have our pruning condition. We don't recurse if the remaining count for that parenthesis is ==0.
+            if((character == '(' && leftRem>0)|| (character == ')' && rightRem>0)){
+                this.recurse(s, index+1, leftCount, rightCount, leftRem - (character=='('?1:0), rightRem - (character == ')'?1:0), expression);
+            }
+            expression.append(character);
+
+            // Simply recurse one step further if the current character is not parenthesis. 
+            if(character!='(' && character != ')'){
+                this.recurse(s, index+1, leftCount, rightCount, leftRem, rightRem, expression);
+            }else if(character == '('){
+                // Consider an opening bracket.
+                this.recurse(s, index+1, leftCount+1, rightCount, leftRem, rightRem, expression);
+            }else if(rightCount<leftCount){
+                // Consider a closing bracket.
+                this.recurse(s, index+1, leftCount, rightCount+1, leftRem, rightRem, expression);
+            }
+            // Delete for backtracking.
+            expression.deleteCharAt(length);
+        }
+    }
+
+    public List<String> removeInvalidParentheses(String s){
+        int left = 0, right = 0;
+        // First we find out the number of misplaced left and right parentheses.
+        for(int i=0;i<s.length();i++){
+            // Simply record the left one.
+            if(s.charAt(i)=='(') left ++;
+            else if(s.charAt(i)==')'){
+                // If we dont have a matching left, then this is a misplaced right, record it.
+                right = left == 0? right+1: right;
+                // Decrement count of left parentheses because we have found a right which can be a matching one for the left.
+                left = left>0? left-1:left;
+            } 
+        }
+        this.recurse(s, 0, 0,0, left, right, new StringBuilder());
+        return new ArrayList<String>(this.validExpressions);
+    }
+}
+class Solution1 {
 
     private Set<String> validExpressions = new HashSet<String>();
     private int minimumRemoved;
