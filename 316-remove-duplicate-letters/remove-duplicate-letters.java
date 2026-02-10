fdcc621da776aa70dd70522c05270015a534e6a4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-10 12:53:43 +0530

    Time: 4 ms (21.28%) | Memory: 44.1 MB (27.66%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -1,22 +1,23 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
         int[] freq = new int[26];
-        Set<Character> present = new HashSet<>();
         Stack<Character> stk = new Stack<>();
+        Set<Character> present = new HashSet<>();
         for(char c: s.toCharArray()) freq[c-'a']++;
-        for(int i=0;i<s.length();i++){
-            char curr = s.charAt(i);
-            freq[curr-'a']--;
-            if(present.contains(curr)) continue;
-            while(!stk.isEmpty() && stk.peek()>curr && freq[stk.peek()-'a']>0){
+        for(char c:s.toCharArray()){
+            freq[c-'a']--;
+            if(present.contains(c)) continue;
+            while(!stk.isEmpty() && stk.peek()>c && freq[stk.peek()-'a']>0){
                 present.remove(stk.peek());
                 stk.pop();
             }
-            stk.push(curr);
-            present.add(curr);
+            stk.push(c);
+            present.add(c);
         }
         StringBuilder sb = new StringBuilder();
-        while(!stk.isEmpty()) sb.append(stk.pop());
+        while(!stk.isEmpty()){
+            sb.append(stk.pop());
+        }
         return sb.reverse().toString();
     }
 }
