4348a036c8113e280bccd69fcf0c56ab6c8b32ab
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-10 12:19:51 +0530

    Time: 3 ms (31.64%) | Memory: 44.3 MB (23.28%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -5,14 +5,15 @@ class Solution {
         Stack<Character> stk = new Stack<>();
         for(char c: s.toCharArray()) freq[c-'a']++;
         for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']--;
-            if(present.contains(s.charAt(i))) continue;
-            while(!stk.isEmpty() && stk.peek()>s.charAt(i) && freq[stk.peek()-'a']>0){
+            char curr = s.charAt(i);
+            freq[curr-'a']--;
+            if(present.contains(curr)) continue;
+            while(!stk.isEmpty() && stk.peek()>curr && freq[stk.peek()-'a']>0){
                 present.remove(stk.peek());
                 stk.pop();
             }
-            stk.push(s.charAt(i));
-            present.add(s.charAt(i));
+            stk.push(curr);
+            present.add(curr);
         }
         StringBuilder sb = new StringBuilder();
         while(!stk.isEmpty()) sb.append(stk.pop());
