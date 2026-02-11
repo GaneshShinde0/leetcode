b7720bf655a3991210cbf2e33dfe824f77b15e66
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-11 09:04:04 +0530

    Time: 5 ms (97.64%) | Memory: 46.7 MB (84.25%) - LeetSync

--- 3825-apply-substitutions/apply-substitutions.java
+++ 3825-apply-substitutions/apply-substitutions.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String applySubstitutions(List<List<String>> replacements, String text) {
+    public String applySubstitutionsInitial(List<List<String>> replacements, String text) {
         StringBuilder sb = new StringBuilder(text);
         while(sb.indexOf("%")!=-1){
             for(List<String> li: replacements){
@@ -9,5 +9,34 @@ class Solution {
             }
         }
         return sb.toString();
+    }
+
+    HashMap<String, String> map;
+    HashMap<String, String> memo;
+    public String applySubstitutions(List<List<String>> replacements, String text) {
+        map  = new HashMap<>();
+        memo = new HashMap<>();
+        for (List<String> r : replacements){
+            map.put(r.get(0), r.get(1));
+        }
+        return dfs(text);
+    }
+    private String dfs(String s){
+        if (memo.containsKey(s)) return memo.get(s);
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        for (int i = 0; i < n; i++){
+            if (s.charAt(i) != '%') sb.append(s.charAt(i));
+            else {
+                int j = i + 1;
+                while (j < n && s.charAt(j) != '%') j++;
+                String key = s.substring(i + 1, j); // [i + 1, j)  -> %a%
+                sb.append(dfs(map.get(key)));
+                i = j;// new start without % 
+            }
+        }
+        String res = sb.toString();
+        memo.put(s, res);
+        return res;
     }
 }
