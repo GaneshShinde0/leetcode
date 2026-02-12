f7738074727f254f4bb33f0fd242b9d4a0c5bee1
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-12 08:10:33 +0530

    Time: 67 ms (66.44%) | Memory: 87 MB (5.75%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,25 +1,42 @@
 class Solution {
-    // Space and Time O(n)
+    // This wont work as we need start with current indexes height.
+    public int largestRectangleAreaNA(int[] heights) {
+        Stack<Integer> stk = new Stack<>();
+        int maxArea = 0, n = heights.length;
+        for(int i=0;i<n;i++){
+            int start = i;
+            while(!stk.isEmpty() && heights[stk.peek()]>heights[i]){
+                maxArea = Math.max(maxArea, (i-stk.peek())*heights[i]);
+                start = stk.peek();
+                stk.pop();
+            }
+            stk.push(start);
+        }
+        while(!stk.isEmpty()){
+            maxArea = Math.max(maxArea, (n-stk.peek())*heights[stk.peek()]);
+            stk.pop();
+        }
+        return maxArea;
+    }
     public int largestRectangleArea(int[] heights) {
-        int maxArea = 0;
-        int n = heights.length;
+        int maxArea = 0, n = heights.length;
         Stack<int[]> stk = new Stack<>();
+
         for(int i=0;i<n;i++){
             int start = i;
             while(!stk.isEmpty() && stk.peek()[1]>heights[i]){
                 int index = stk.peek()[0];
                 int height = stk.peek()[1];
+                maxArea = Math.max(maxArea, height*(i-index));
                 stk.pop();
                 start = index;
-                maxArea = Math.max(maxArea, height*(i-index));
             }
-            stk.add(new int[]{start,heights[i]});
+            stk.push(new int[]{start, heights[i]});
         }
+
         while(!stk.isEmpty()){
-            int index = stk.peek()[0];
-            int height = stk.peek()[1];
+            maxArea = Math.max(maxArea, stk.peek()[1]*(n-stk.peek()[0]));
             stk.pop();
-            maxArea = Math.max(maxArea, height*(n-index));
         }
         return maxArea;
     }
