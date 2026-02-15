57638b3c19b400755d452ac5c6cf9c4574f8ff32
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-15 17:06:08 +0530

    Time: 54 ms (85.94%) | Memory: 173.8 MB (7.03%) - LeetSync

--- 2118-maximum-earnings-from-taxi/maximum-earnings-from-taxi.java
+++ 2118-maximum-earnings-from-taxi/maximum-earnings-from-taxi.java
@@ -1,22 +1,22 @@
 /*
 Algorithm:
 
-1. Initialization: Set up DP array and endI list.
+1. Initialization: Set up DP array and ridesEndingAtI list.
 2. Ride Grouping: Organize Rides by their end locations.
-3. Dynamic Programming: Iterate through each stop, calculating the maximum pearnings possible by either continuiing from the previous stop or ending a ride at the current stop.
+3. Dynamic Programming: Iterate through each stop, calculating the maximum pearnings possible by either continuiing from the previous stop or ridesEndingAtIng a ride at the current stop.
 4. Return result: Return the maximum earnings at the final stop.
 */
 
 class Solution {
     public long maxTaxiEarnings(int n, int[][] rides) {
         long[] dp = new long[n+1];
-        List<int[]>[] endI = new List[n+1];
-        Arrays.setAll(endI, i->i==0?null:new ArrayList<>());
-        for(int[] r:rides) endI[r[1]].add(r);
+        List<int[]>[] ridesEndingAtI = new List[n+1];
+        Arrays.setAll(ridesEndingAtI, i->i==0?null:new ArrayList<>());
+        for(int[] ride:rides) ridesEndingAtI[ride[1]].add(ride);
         for(int i=1;i<=n;i++){
             dp[i] = dp[i-1];
-            for(int[] r:endI[i]){
-                dp[i] = Math.max(dp[i],dp[r[0]]+r[1]-r[0]+r[2]);
+            for(int[] ride:ridesEndingAtI[i]){
+                dp[i] = Math.max(dp[ride[0]]+ride[1]-ride[0]+ride[2],dp[i]);
             }
         }
         return dp[n];
