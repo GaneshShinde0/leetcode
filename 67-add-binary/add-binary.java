733921fc0e475cbcfb05bea8a12766463f699171
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-15 07:39:10 +0530

    Time: 2 ms (49.49%) | Memory: 43.7 MB (53.97%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,4 +1,22 @@
 class Solution {
+    public String addBinaryNotIdealForInputGreaterThan32(String a, String b){
+        return Integer.toBinaryString(Integer.parseInt(a,2)+ Integer.parseInt(b,2));
+    }
+
+    public String addBinarhy(String a, String b){
+        int n = a.length(), m = b.length();
+        if(n<m) return addBinary(b,a);
+        StringBuilder sb = new StringBuilder();
+        int carry = 0, j = m-1;
+        for(int i=n-1;i>-1;i--){
+            if(a.charAt(i)=='1') carry++;
+            if(carry%2==1) sb.append('1');
+            else sb.append('0');
+        }
+        if(carry%2==1) sb.append('1');
+        sb.reverse();
+        return sb.toString();
+    }
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
         int aLen = a.length()-1, bLen = b.length()-1;
