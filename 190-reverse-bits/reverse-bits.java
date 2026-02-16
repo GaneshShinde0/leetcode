89348e2478e24306986299091ca0fe7593320dbf
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-16 09:04:20 +0530

    Time: 3 ms (10.46%) | Memory: 42.5 MB (71.14%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,8 +1,8 @@
 class Solution {
-        public int reverseBits(int n) {
-                StringBuilder sb = new StringBuilder(Integer.toBinaryString(n));
-                        sb = sb.reverse();
-                                while(sb.length()<32){sb.append("0");}
-                                        return Integer.parseInt(sb.toString(),2);
-                                    }
-                                }
+    public int reverseBits(int n) {
+        StringBuilder sb = new StringBuilder(Integer.toBinaryString(n));
+            sb = sb.reverse();
+            while(sb.length()<32){sb.append("0");}
+            return Integer.parseInt(sb.toString(),2);
+        }
+    }
