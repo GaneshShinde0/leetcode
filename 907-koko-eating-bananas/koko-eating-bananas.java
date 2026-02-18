9a4f9e8439cf4395efac75de63e99d0d527f2804
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-18 10:32:34 +0530

    Time: 21 ms (34.31%) | Memory: 48.1 MB (30.78%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,28 +1,23 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1, right = 1;
-        for(int pile:piles){
-            if(pile>right) right=pile;
+        int right = 0, left = 1;
+        for(int p:piles){
+            right = Math.max(p,right);
         }
-        int res = right;
+        while(left<right){
+            int mid = (left)+(right-left)/2;
+            int hours = getHours(mid, piles); // Hours taken to finish pile for any mid.
+            if(hours<=h) right = mid;
+            else left = mid+1;
+        }
+        return right;
+    }
 
-        while(left<=right){
-            int mid = (right-left)/2+left;
-            long hours = getHours(piles,mid);
-            if(hours<=h){
-                res = Math.min(res,mid);
-                right = mid-1;
-            }else{
-                left = mid+1;
-            }
+    private int getHours(int speed, int[] piles){
+        int res = 0;
+        for(int i:piles){
+            res+=(int)(Math.ceil(1.0*i/speed));
         }
         return res;
-    }
-    public long getHours(int[] piles, int k){
-        long hours = 0;
-        for(int pile:piles){
-            hours += Math.ceil((pile*1.0)/k);
-        }
-        return hours;
     }
 }
