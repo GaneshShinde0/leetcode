a61d33b9ee37de976418e5d599fe5e5123bd0831
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-22 17:15:17 +0530

    Time: 1 ms (99.82%) | Memory: 46.7 MB (47.28%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -9,7 +9,7 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbersUsingStack(ListNode l1, ListNode l2) {
         Stack<Integer> stk1 = new Stack<>();
         Stack<Integer> stk2 = new Stack<>();
         while(l1!=null){
@@ -51,5 +51,42 @@ class Solution {
             result = curr;
         }
         return result;
+    }
+
+    private ListNode reverseList(ListNode head){
+        ListNode prev = null, temp;
+        while(head!=null){
+            // Keep the next node.
+            temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        ListNode r1 = reverseList(l1);
+        ListNode r2 = reverseList(l2);
+
+        int totalSum = 0, carry  = 0;
+        ListNode ans = new ListNode();
+        while(r1!=null || r2!=null){
+            if(r1!=null){
+                totalSum += r1.val;
+                r1 = r1.next;
+            }
+            if(r2!=null){
+                totalSum +=r2.val;
+                r2=r2.next;
+            }
+            ans.val = totalSum%10;
+            carry = totalSum/10;
+            ListNode head = new ListNode(carry);
+            head.next = ans;
+            ans = head;
+            totalSum = carry;
+        }
+        return carry == 0?ans.next:ans;
     }
 }
