57f63bbbd133ce006864082c4b8cd0ff0d76c07c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-22 15:33:15 +0530

    Time: 10 ms (96.44%) | Memory: 50.4 MB (53.79%) - LeetSync

--- 403-frog-jump/frog-jump.java
+++ 403-frog-jump/frog-jump.java
@@ -1,6 +1,49 @@
 class Solution {
     public boolean canCross(int[] stones) {
         int n = stones.length;
+        if (stones[1] != 1) return false;
+
+        // Optimization 1: Use primitive byte array instead of Boolean wrapper
+        // 0 = unknown, 1 = true, 2 = false
+        byte[][] memo = new byte[n][n + 1];
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            hm.put(stones[i], i);
+        }
+        
+        return jumps(1, 1, stones, hm, memo);
+    }
+
+    public boolean jumps(int index, int k, int[] stones, HashMap<Integer, Integer> hm, byte[][] memo) {
+        if (index == stones.length - 1) return true;
+        
+        // Check cache (0 means not calculated yet)
+        if (memo[index][k] != 0) {
+            return memo[index][k] == 1;
+        }
+
+        // Optimization 2: Eliminate 'new int[]' allocation with a loop
+        for (int nextK = k - 1; nextK <= k + 1; nextK++) {
+            if (nextK > 0) {
+                int nextStoneVal = stones[index] + nextK;
+                
+                // Optimization 3: Single map lookup instead of containsKey + get
+                Integer nextIndex = hm.get(nextStoneVal); 
+                
+                if (nextIndex != null && jumps(nextIndex, nextK, stones, hm, memo)) {
+                    memo[index][k] = 1; // Mark as True
+                    return true;
+                }
+            }
+        }
+        
+        memo[index][k] = 2; // Mark as False
+        return false;
+    }
+
+    public boolean canCrossInitial(int[] stones) {
+        int n = stones.length;
         if(stones[1]!=1) return false;
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i=0;i<n;i++){
