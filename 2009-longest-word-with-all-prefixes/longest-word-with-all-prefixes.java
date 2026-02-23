d8abe798534cbbd393817771e9a69d0971dd79b4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-23 11:31:58 +0530

    Time: 26 ms (54.05%) | Memory: 86.1 MB (18.92%) - LeetSync

--- 2009-longest-word-with-all-prefixes/longest-word-with-all-prefixes.java
+++ 2009-longest-word-with-all-prefixes/longest-word-with-all-prefixes.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String longestWord(String[] words) {
+    public String longestWordUsingSet(String[] words) {
         Arrays.sort(words);
         // Set to store valid words
         Set<String> validWords = new HashSet<>();
@@ -18,5 +18,49 @@ class Solution {
 
         // Return the longest valid word found.
         return longestValidWord;
+    }
+
+    public String longestWord(String[] words) {
+        String result="";
+        Trie trie = new Trie();
+        for(String word:words){
+            trie.insert(word);
+        }
+        for(String word:words){
+            if(trie.hasAllPrefixes(word)){
+                if(word.length()>result.length()||(word.length()==result.length() && word.compareTo(result)<0)) result = word;
+            }
+        }
+        return result;
+    }
+    private static class Trie{
+        public static class TrieNode{
+            TrieNode[] children = new TrieNode[26];
+            boolean isEndOfWord;
+        }
+        private final TrieNode root = new TrieNode();
+        // Insert a word into the trie.
+        private void insert(String word){
+            TrieNode node = root;
+            for(char c:word.toCharArray()){
+                int index = c-'a';
+                if(node.children[index]==null){
+                    node.children[index] = new TrieNode();
+                }
+                node = node.children[index];
+            }
+            node.isEndOfWord = true;
+        }
+
+        private boolean hasAllPrefixes(String word){
+            TrieNode node = root;
+            for(char c:word.toCharArray()){
+                node = node.children[c-'a'];
+                if(node==null || !node.isEndOfWord){
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
