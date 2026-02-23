4d8facaeac224d1e961c30d4baf329a81b96d053
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-23 15:47:32 +0530

    Time: 1064 ms (82.35%) | Memory: 0B (100.00%) - LeetSync

--- 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -1,13 +1,33 @@
-/* Write your T-SQL query statement below */
+-- /* Write your T-SQL query statement below */
+-- SELECT 
+--     co.name AS country
+-- FROM 
+--     person p
+--     JOIN 
+--         country co
+--         ON SUBSTRING(phone_number, 1,3) = country_code
+--     JOIN 
+--         calls c
+--         ON p.id IN (c.caller_id,c.callee_id)
+-- GROUP BY co.name
+-- HAVING AVG(duration)>(SELECT AVG(duration) FROM CALLS)
+
+WITH CallDetails AS (
+    SELECT caller_id AS id, duration FROM Calls
+    UNION ALL
+    SELECT callee_id AS id, duration FROM Calls
+)
 SELECT 
     co.name AS country
 FROM 
-    person p
-    JOIN 
-        country co
-        ON SUBSTRING(phone_number, 1,3) = country_code
-    JOIN 
-        calls c
-        ON p.id IN (c.caller_id,c.callee_id)
-GROUP BY co.name
-HAVING AVG(duration)>(SELECT AVG(duration) FROM CALLS)
+    Person p
+JOIN 
+    Country co
+    ON SUBSTRING(p.phone_number, 1, 3) = co.country_code
+JOIN 
+    CallDetails c
+    ON p.id = c.id
+GROUP BY 
+    co.name
+HAVING 
+    AVG(c.duration) > (SELECT AVG(duration) FROM Calls);
