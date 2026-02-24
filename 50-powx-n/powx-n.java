950ef415a95309e5a13baf4006c56b11c38cd4cd
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-24 19:05:40 +0530

    Time: 1 ms (9.87%) | Memory: 47.8 MB (79.16%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,6 +1,6 @@
 class Solution {
     HashMap<Integer, Double> hm = new HashMap<>();
-    public double myPow(double x, int n) {
+    public double myPowInitial(double x, int n) {
         hm.put(0,1.0);
         hm.put(1,x);
         hm.put(-1,1/x);
@@ -15,5 +15,28 @@ class Solution {
         }
         hm.put(n,full);
         return full;
+    }
+    
+    public double myPow(double x, int n){
+        return myPow(x,(long) n);
+    }
+    public double myPow(double x, long n) {
+       if(n==0) return 1;
+       if(n<0){
+            n = -1*n;
+            x=1.0/x;
+       }
+       // Perform Binary Exponentiation
+       double result = 1;
+       while(n!=0){
+            if(n%2==1){
+                result = result*x;
+                n-=1;
+            }
+            x = x*x;
+            n = n/2;
+       }
+       return result;
+
     }
 }
