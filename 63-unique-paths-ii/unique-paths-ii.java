018a01e31f639de410ca402d426d53b1097124a7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-24 09:30:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (92.32%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     private static final int[][] dirs = {{0,1},{1,0}};
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    public int uniquePathsWithObstaclesInitial(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n=obstacleGrid[0].length;
         int[][] dp = new int[m][n];
         // Suppose a cell (i, j) is a "dead end" (it leads to obstacles and has 0 valid paths to the destination).
@@ -23,4 +23,23 @@ class Solution {
         dp[i][j] = res;
         return res;
     }
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        if(obstacleGrid[0][0]==1) return 0;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                else if(i==0 && j>0 && dp[i][j-1]==1) dp[i][j]=1;
+                else if(j==0 && i>0 && dp[i-1][j]==1) dp[i][j]=1; 
+                else if(i>0 && j>0) {
+                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
 }
