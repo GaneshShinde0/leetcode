48e78a88d7f6cfc08934f86607d3d6592e89a9e7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-24 18:07:38 +0530

    Time: 10 ms (83.53%) | Memory: 47.1 MB (70.39%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.java
@@ -1,4 +1,6 @@
 class Solution {
+    // We just have to make sure all elements are same; we dont have to worry
+    // Whether they are same or different.
     public int minMoves2(int[] nums) {
         Arrays.sort(nums);
         int median = nums[nums.length/2];
