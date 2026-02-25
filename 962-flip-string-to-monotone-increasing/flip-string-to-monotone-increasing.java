e79b90e39e6a05efa353d67c64bfe805d10ffd13
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-25 11:49:27 +0530

    Time: 14 ms (37.57%) | Memory: 47.1 MB (34.50%) - LeetSync

---
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
@@ -0,0 +1,40 @@
+/*
+"010110"
+
+011111 => Two Flips
+
+000111 => Two Flips.
+
+Number of ones to left 
+[0,1,1,2,3,3]
+
+Number of zeros to right 
+[3,2,2,1,1,1]
+
+Ones:   [0,1,1,2,3,3]
+Zeros:  [3,2,2,1,1,1]
+
+*/
+
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        int n = s.length();
+        int result = n;
+        int[] ones = new int[n];
+        int[] zeros = new int[n];
+        int curr = 0;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='1') curr++;
+            ones[i]=curr;
+        }
+        if(curr==n) return 0;
+        curr = 0;
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i)=='0') curr++;
+            int temp = curr+ones[i]-1;
+            result = Math.min(result,temp);
+        }
+        if(curr==n) return 0;
+        return result;
+    }
+}
