7860b0e9d2fb90686680acc3a9a0281e6c623fe4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-26 12:08:27 +0530

    Time: 4 ms (24.57%) | Memory: 47.8 MB (31.96%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,28 +1,39 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        left[0]=1;right[n-1]=1;
+        int[] incrementingCandies = new int[n];
+        Arrays.fill(incrementingCandies,1);
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
-                left[i]=left[i-1]+1;
-            }else{
-                left[i]=1;
+                incrementingCandies[i]+=incrementingCandies[i-1];
             }
         }
+        // System.out.println(Arrays.toString(incrementingCandies));
+        int result = incrementingCandies[n-1];
+        int prev = 1;
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
+                prev++;
             }else{
-                right[i]=1;
+                prev = 1;
             }
+            result+=Math.max(incrementingCandies[i],prev);
         }
-        int res = 0;
-        for(int i=0;i<n;i++){
-            res +=Math.max(left[i],right[i]);
-        }
-        return res;
-
+        return result;
     }
 }
+/*
+1,0,2
+1 1 2
+2 1 1
+
+
+1 2 2 
+1 2 1
+1 1 1
+
+1,3,2,2,1
+1 2 1 1 1
+1 2 1 2 1
+
+*/
