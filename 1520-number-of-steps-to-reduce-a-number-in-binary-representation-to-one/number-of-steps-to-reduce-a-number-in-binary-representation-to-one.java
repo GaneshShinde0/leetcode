51f6032f90d2133960fd5a0fbdf6ca44daec9600
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-26 08:40:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (63.13%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -16,10 +16,16 @@ class Solution {
         for(int i=n;i>0;i--){
             int curr = s.charAt(i)-'0'+carry;
             if(curr%2==0){
-                result++;
+                result++; // +1 is for dividing carry.
             }else{
-                result+=2;
-                carry=1;
+                // As current digit is one, we will have to add one to it.
+                // eg. 100001 => 100010
+                // Adding one is 1 step
+                // After adding one we will have to divide the number 
+                // That is one more step.
+                // Total Two steps and, carry tells us that current digit (i th) was one in next steps (i-1 th) calculation.
+                result+=2; 
+                carry=1; // Reset Carry as current digit is 1.
             }
         }
         return result+carry;
