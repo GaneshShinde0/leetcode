6040069f91fc784ae89817d66f08e03a599b4b10
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-28 23:14:10 +0530

    Time: 32 ms (99.63%) | Memory: 94.7 MB (83.33%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,5 +1,14 @@
 class Solution {
-    public int maxFrequency(int[] nums, int k) {
+
+    /*
+    Time Complexity: O(nlongn) => sorting nlogn , while loop is O(1), for loop is O(n)
+    Space Complexity: If you look at code it might look O(1) but.
+        Space gets used in sorting and it can be O(logn) or O(n) based on programming language.
+        - Java, Arrays.sort() for primitive is implemented using a variant of the Quick Sort algorithm, which has space complexity of O(logn)
+        - In C++, the sort() function provided by STL uses a hybrid of Quick Sort, Heap Sort and Insertion Sort, with  a worst case space complexity of O(logn)
+        - In Python, the sort() function is implemented using the Timsort algorithm which has a worst case space complexity of O(n).
+    */
+    public int maxFrequencySlidingWindow(int[] nums, int k) {
         Arrays.sort(nums); // We want to make most elements equal, sorting will help with that
         int left = 0, ans = 0; // left pointer, best answer so far, sum of elements in our window
         long currSum = 0;
@@ -19,5 +28,24 @@ class Solution {
         }
         return ans;
     }
+
+
+    // Advanced Sliding Window
+    // Basically once we got longest window we will hold on to it, until we find larger window.
+    public int maxFrequency(int[] nums, int k){
+        Arrays.sort(nums);
+        int n = nums.length, left = 0;
+        long currSum = 0;
+        for(int right = 0; right<n;right++){
+            long target = nums[right];
+            currSum+=nums[right];
+            if((right-left+1)*target-currSum>k){ // Single if makes sure we dont decrease the window size.
+                currSum -= nums[left];
+                left++;
+            }
+        }
+        return n-left;
+    }
+
 }
 
