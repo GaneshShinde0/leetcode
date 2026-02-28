e9fccb679ee6cfd0e2e922bead771bd097524e1b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-28 14:10:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (87.76%) - LeetSync

---
+++ 250-count-univalue-subtrees/count-univalue-subtrees.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int res = 0;
+    public int countUnivalSubtrees(TreeNode root) {
+        if(root==null) return  res;
+        isUniValue(root);
+        return res;
+    }
+
+    private boolean isUniValue(TreeNode root){
+        if(root.left==null&&root.right==null){
+            this.res++;
+            return true;
+        }
+        boolean left = true, right = true;
+        if(root.left!=null) left = isUniValue(root.left);
+        if(root.right!=null) right = isUniValue(root.right);
+        if(root.left!=null && root.left.val!=root.val) return false;
+        if(root.right!=null && root.right.val!=root.val) return false;
+        if(left&&right) this.res++;
+        return left&&right;
+    }
+}
