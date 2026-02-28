2696ca729c243251bc170e921876e0c23d4b80ca
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-02-28 22:58:21 +0530

    Time: 33 ms (92.87%) | Memory: 95.1 MB (26.32%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -1,22 +1,23 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
-        Arrays.sort(nums);
-        int left = 0;
-        int ans = 0;
-        long curr = 0;
-        
-        for (int right = 0; right < nums.length; right++) {
+        Arrays.sort(nums); // We want to make most elements equal, sorting will help with that
+        int left = 0, ans = 0; // left pointer, best answer so far, sum of elements in our window
+        long currSum = 0;
+        for(int right = 0; right<nums.length; right++){
             long target = nums[right];
-            curr += target;
-            
-            while ((right - left + 1) * target - curr > k) {
-                curr -= nums[left];
+            currSum+=target;
+            // previous window * target - current sum is>k 
+            // i.e. consider array as 3 6 7 12 19 22 44 150
+            // consider right is 3, so target will become 12
+            // currentSum = 28, (target = 12) (target*4=48)
+            // If our k is greater than (48-28 ) i.e. 20 then we can take window otherwise we will shrink the window.
+            while((right-left+1)*target-currSum>k){ 
+                currSum-=nums[left];
                 left++;
             }
-            
-            ans = Math.max(ans, right - left + 1);
+            ans = Math.max(ans, right-left+1);
         }
-        
         return ans;
     }
 }
+
