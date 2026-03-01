8bf07d5bed92ca5ba0e9c6250ccdc282099568c2
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-01 16:21:47 +0530

    Time: 189 ms (10.65%) | Memory: 53.2 MB (26.45%) - LeetSync

---
+++ 753-open-the-lock/open-the-lock.java
@@ -0,0 +1,53 @@
+/*
+There would be 10000 Combinations, 
+Time complexity will become O(10000)
+
+Memory Complexity:
+Worst Case O(10000)
+*/
+
+class Solution {
+    static class Pair{
+        String digits;
+        int turns;
+        Pair(String digits, int turns){
+            this.digits = digits;
+            this.turns = turns;
+        }
+    }
+    public int openLock(String[] deadends, String target) {
+        Set<String> deadEnds = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+        for(String s:deadends) visited.add(s);
+        if(visited.contains("0000")) return -1;
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.add(new Pair("0000",0));
+
+        while(!queue.isEmpty()){
+            Pair p = queue.poll();
+            if(p.digits.equals(target)) return p.turns;                        
+            int turns = p.turns;
+            List<String> childrens = getChildrens(p.digits,visited);
+            for(String c:childrens){
+                if(visited.contains(c)) continue;
+                visited.add(c);
+                queue.add(new Pair(c,turns+1));
+            }
+        }
+        return -1;
+    }
+    private List<String> getChildrens(String s, Set<String> visited){
+        List<String> childrens = new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            int plus = ((s.charAt(i)-'0')+1)%10;
+            int minus = ((s.charAt(i)-'0'+10)-1)%10;
+            String plusCombination = s.substring(0,i)+(plus)+s.substring(i+1);
+            String minusCombination = s.substring(0,i)+(minus)+s.substring(i+1);
+            if(!visited.contains(plusCombination)) childrens.add(plusCombination);
+            if(!visited.contains(minusCombination)) childrens.add(minusCombination);
+        }
+        return childrens;
+    }
+
+}
