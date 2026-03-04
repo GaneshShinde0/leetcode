081b00c840e1366c4cccb4e27ca20a85c92c72ec
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-04 11:01:18 +0530

    Time: 6 ms (74.36%) | Memory: 47.3 MB (33.83%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,33 +1,67 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] inDegree = new int[numCourses];
-        HashMap<Integer, Set<Integer>> adj = new HashMap<>();
-        for(int i=0;i<numCourses;i++) adj.put(i, new HashSet<Integer>());
-        for(int[] preReq: prerequisites){
-            inDegree[preReq[0]]++;
-            if(preReq[0]==preReq[1]) return false;
-            adj.get(preReq[1]).add(preReq[0]);
+    // public boolean canFinish(int numCourses, int[][] prerequisites) {
+    //     HashMap<Integer, TreeSet<Integer>> hm = new HashMap<>();
+    //     for(int[] p:prerequisites){
+    //         hm.computeIfAbsent(p[0],k-> new TreeSet<>()).add(p[1]);
+    //         TreeSet<Integer> ts = hm.get(p[0]);
+    //         ts.addAll(hm.getOrDefault(p[1],new TreeSet<>()));
+    //         System.out.println(ts);
+    //         if(hm.containsKey(p[1])&&hm.get(p[1]).contains(p[0])) return false;
+    //     }
+    //     return true;
+    // }
+
+    public boolean canFinishInitial(int n, int[][] prerequisites) {
+        int[] indegree = new int[n];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++)adj.add(new ArrayList<Integer>());
+
+        for(int[] p:prerequisites){
+            // You must take course bi to complete ai
+            // So there is direction from bi->ai (as b needs to be completed first otherwise we cant reach a)
+            adj.get(p[1]).add(p[0]);
+            indegree[p[0]]++;
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=0;i<numCourses;i++){
-            if(inDegree[i]==0) queue.add(i);
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.add(i);
         }
-        int count = 0;
-        while(!queue.isEmpty()){
-            int curr = queue.poll();
+
+        int count = 0; // We can track processed nodes.
+        while(!q.isEmpty()){
+            int curr = q.remove();
             count++;
-            for(int node:adj.get(curr)){
-                inDegree[node]--;
-                if(inDegree[node]==0) queue.add(node);
+            for(int conn: adj.get(curr)){
+                indegree[conn]--;
+                if(indegree[conn]==0) q.add(conn);
             }
         }
-        return count==numCourses;
+        return count == n;
+    }
+    public boolean canFinish(int n, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++)adj.add(new ArrayList<>());
+        for(int[] p:prerequisites){
+            adj.get(p[0]).add(p[1]);
+        }
+        Set<Integer> visitSet = new HashSet<>();
+        for(int course =0;course<n;course++){
+            if(!dfs(visitSet,adj,course)) return false;
+        }
+        return true;
+    }
+
+    private boolean dfs(Set<Integer> visitSet,List<List<Integer>> adj, int course){
+        if(visitSet.contains(course)) return false;
+        if(adj.get(course).size()==0) return true;
+        visitSet.add(course);
+        for(int pre:adj.get(course)){
+            if(!dfs(visitSet,adj,pre))return false;
+        }
+        visitSet.remove(course);
+        adj.set(course,new ArrayList<>());
+        return true;
     }
 }
-
-/*
-1-> 0 , 2
-0 -> 1
-
-*/
