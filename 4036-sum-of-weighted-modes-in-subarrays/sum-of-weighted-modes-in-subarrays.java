c690d52486950e88395403c44869c20a95e968b4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-09 22:23:03 +0530

    Time: 255 ms (69.57%) | Memory: 138 MB (65.22%) - LeetSync

--- 4036-sum-of-weighted-modes-in-subarrays/sum-of-weighted-modes-in-subarrays.java
+++ 4036-sum-of-weighted-modes-in-subarrays/sum-of-weighted-modes-in-subarrays.java
@@ -1,6 +1,77 @@
 class Solution {
     public long modeWeight(int[] nums, int k) {
         long result = 0;
+        // Optimization: Use HashMap instead of TreeMap. 
+        // We will track maxFreq manually for O(1) access.
+        Map<Integer, Integer> numToFreq = new HashMap<>();
+        Map<Integer, TreeSet<Integer>> freqToNum = new HashMap<>();
+        
+        int maxFreq = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            // --- 1. Add current number (nums[i]) ---
+            int num = nums[i];
+            int oldFreq = numToFreq.getOrDefault(num, 0);
+            int newFreq = oldFreq + 1;
+            
+            numToFreq.put(num, newFreq);
+            
+            // Remove from old freq bucket
+            if (oldFreq > 0) {
+                removeFromFreqMap(freqToNum, oldFreq, num);
+            }
+            
+            // Add to new freq bucket
+            freqToNum.computeIfAbsent(newFreq, t -> new TreeSet<>()).add(num);
+            
+            // Update Max Freq (Bottleneck Optimization)
+            maxFreq = Math.max(maxFreq, newFreq);
+
+            // --- 2. Remove old number (slide window) ---
+            if (i >= k) { // Note: standard sliding window usually processes remove first or after result calculation
+                int oldNum = nums[i - k];
+                int prevFreqOfOldNum = numToFreq.get(oldNum);
+                int newFreqOfOldNum = prevFreqOfOldNum - 1;
+                
+                // Remove from current bucket
+                removeFromFreqMap(freqToNum, prevFreqOfOldNum, oldNum);
+                
+                // If this bucket was the maxFreq and is now empty, decrease maxFreq
+                if (prevFreqOfOldNum == maxFreq && !freqToNum.containsKey(maxFreq)) {
+                    maxFreq--;
+                }
+                
+                // Add to lower freq bucket
+                if (newFreqOfOldNum > 0) {
+                    numToFreq.put(oldNum, newFreqOfOldNum);
+                    freqToNum.computeIfAbsent(newFreqOfOldNum, t -> new TreeSet<>()).add(oldNum);
+                } else {
+                    numToFreq.remove(oldNum);
+                }
+            }
+
+            // --- 3. Calculate Result ---
+            if (i >= k - 1) {
+                // O(1) access to the max frequency bucket
+                long mode = freqToNum.get(maxFreq).first(); 
+                result += (long) maxFreq * mode;
+            }
+        }
+        return result;
+    }
+
+    // Helper to reduce code duplication (D in BUD)
+    private void removeFromFreqMap(Map<Integer, TreeSet<Integer>> map, int freq, int num) {
+        if (!map.containsKey(freq)) return;
+        TreeSet<Integer> set = map.get(freq);
+        set.remove(num);
+        if (set.isEmpty()) {
+            map.remove(freq);
+        }
+    }
+
+    public long modeWeightInitial(int[] nums, int k) {
+        long result = 0;
         HashMap<Integer,Integer> numToFreq = new HashMap<>();
         TreeMap<Integer,TreeSet<Integer>> freqToNum = new TreeMap<>();
         for(int i=0;i<nums.length;i++){
