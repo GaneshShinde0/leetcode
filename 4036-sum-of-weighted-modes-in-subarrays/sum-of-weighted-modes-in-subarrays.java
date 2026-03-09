9fe48a4fdaf3954a7940bc7bad6f5cde520e604d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-09 22:09:31 +0530

    Time: 386 ms (17.39%) | Memory: 150.2 MB (8.70%) - LeetSync

---
+++ 4036-sum-of-weighted-modes-in-subarrays/sum-of-weighted-modes-in-subarrays.java
@@ -0,0 +1,33 @@
+class Solution {
+    static class NumAndFreq{
+        int num;
+        int freq;
+        NumAndFreq(int num, int freq){
+            this.num=num;
+            this.freq=freq;
+        }
+    }
+    public long modeWeight(int[] nums, int k) {
+        long result = 0;
+        HashMap<Integer,Integer> numToFreq = new HashMap<>();
+        TreeMap<Integer,TreeSet<Integer>> freqToNum = new TreeMap<>();
+        for(int i=0;i<nums.length;i++){
+            int freq = numToFreq.getOrDefault(nums[i],0);
+            numToFreq.put(nums[i],freq+1);
+            freqToNum.computeIfAbsent(freq,t->new TreeSet<Integer>()).remove(nums[i]);
+            freqToNum.computeIfAbsent(freq+1,t->new TreeSet<Integer>()).add(nums[i]);
+            if(i>=k-1){
+                result += 1l*freqToNum.lastEntry().getKey()*freqToNum.lastEntry().getValue().first();
+                freq = numToFreq.get(nums[i-k+1]);
+                numToFreq.put(nums[i-k+1],freq-1);
+                freqToNum.computeIfAbsent(freq,t->new TreeSet<Integer>()).remove(nums[i-k+1]);
+                if(freqToNum.get(freq).size()==0) freqToNum.remove(freq);
+                freq = freq-1;
+                freqToNum.computeIfAbsent(freq,t->new TreeSet<Integer>()).add(nums[i-k+1]);
+                if(freq==0) freqToNum.remove(0);
+                if(numToFreq.get(nums[i-k+1])==0) numToFreq.remove(nums[i-k+1]);
+            }
+        }
+        return result;
+    }
+}
