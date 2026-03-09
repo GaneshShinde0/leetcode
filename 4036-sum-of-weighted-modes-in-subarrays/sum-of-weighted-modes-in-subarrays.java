f43914213c1178849ea800fe874f78fcbe881631
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-09 22:25:18 +0530

    Time: 404 ms (17.39%) | Memory: 159.7 MB (8.70%) - LeetSync

--- 4036-sum-of-weighted-modes-in-subarrays/sum-of-weighted-modes-in-subarrays.java
+++ 4036-sum-of-weighted-modes-in-subarrays/sum-of-weighted-modes-in-subarrays.java
@@ -3,47 +3,23 @@ class Solution {
         long result = 0;
         HashMap<Integer,Integer> numToFreq = new HashMap<>();
         TreeMap<Integer,TreeSet<Integer>> freqToNum = new TreeMap<>();
-        
-        for(int i=0; i<nums.length; i++){
-            int freq = numToFreq.getOrDefault(nums[i], 0);
-            numToFreq.put(nums[i], freq + 1);
-            
-            // Optimization 1: Only remove if freq > 0. 
-            // Prevents creating useless key '0' in freqToNum.
-            if (freq > 0) { 
-                freqToNum.computeIfAbsent(freq, t -> new TreeSet<Integer>()).remove(nums[i]);
-                // Optimization: Clean up empty sets immediately to keep map small
-                if (freqToNum.get(freq).isEmpty()) {
-                    freqToNum.remove(freq);
-                }
-            }
-            
-            freqToNum.computeIfAbsent(freq + 1, t -> new TreeSet<Integer>()).add(nums[i]);
-
-            if(i >= k - 1){
-                // Optimization 2: Call lastEntry() once
-                var entry = freqToNum.lastEntry();
-                result += 1L * entry.getKey() * entry.getValue().first();
-                
-                // Sliding window removal
-                int numToRemove = nums[i - k + 1];
-                freq = numToFreq.get(numToRemove);
-                
-                freqToNum.computeIfAbsent(freq, t -> new TreeSet<Integer>()).remove(numToRemove);
-                if(freqToNum.get(freq).isEmpty()) freqToNum.remove(freq);
-                
-                freq = freq - 1;
-                
-                // Optimization 3: Handle freq updates efficiently
-                if (freq == 0) {
-                    numToFreq.remove(numToRemove);
-                    // Do NOT add to freqToNum at key 0. It is useless work.
-                } else {
-                    numToFreq.put(numToRemove, freq);
-                    freqToNum.computeIfAbsent(freq, t -> new TreeSet<Integer>()).add(numToRemove);
-                }
+        for(int i=0;i<nums.length;i++){
+            int freq = numToFreq.getOrDefault(nums[i],0);
+            numToFreq.put(nums[i],freq+1);
+            freqToNum.computeIfAbsent(freq,t->new TreeSet<Integer>()).remove(nums[i]);
+            freqToNum.computeIfAbsent(freq+1,t->new TreeSet<Integer>()).add(nums[i]);
+            if(i>=k-1){
+                result += 1l*freqToNum.lastEntry().getKey()*freqToNum.lastEntry().getValue().first();
+                freq = numToFreq.get(nums[i-k+1]);
+                freqToNum.computeIfAbsent(freq,t->new TreeSet<Integer>()).remove(nums[i-k+1]);
+                if(freqToNum.get(freq).size()==0) freqToNum.remove(freq);
+                freq = freq-1;
+                numToFreq.put(nums[i-k+1],freq);
+                freqToNum.computeIfAbsent(freq,t->new TreeSet<Integer>()).add(nums[i-k+1]);
+                if(numToFreq.get(nums[i-k+1])==0) numToFreq.remove(nums[i-k+1]);
             }
         }
         return result;
     }
+
 }
