a8a8a1bc2201965f905d0904e4f39dca3edd6246
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-09 22:25:02 +0530

    Time: 250 ms (78.26%) | Memory: 138.7 MB (52.17%) - LeetSync

--- 4036-sum-of-weighted-modes-in-subarrays/sum-of-weighted-modes-in-subarrays.java
+++ 4036-sum-of-weighted-modes-in-subarrays/sum-of-weighted-modes-in-subarrays.java
@@ -1,96 +1,49 @@
 class Solution {
     public long modeWeight(int[] nums, int k) {
         long result = 0;
-        // Optimization: Use HashMap instead of TreeMap. 
-        // We will track maxFreq manually for O(1) access.
-        Map<Integer, Integer> numToFreq = new HashMap<>();
-        Map<Integer, TreeSet<Integer>> freqToNum = new HashMap<>();
-        
-        int maxFreq = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            // --- 1. Add current number (nums[i]) ---
-            int num = nums[i];
-            int oldFreq = numToFreq.getOrDefault(num, 0);
-            int newFreq = oldFreq + 1;
-            
-            numToFreq.put(num, newFreq);
-            
-            // Remove from old freq bucket
-            if (oldFreq > 0) {
-                removeFromFreqMap(freqToNum, oldFreq, num);
-            }
-            
-            // Add to new freq bucket
-            freqToNum.computeIfAbsent(newFreq, t -> new TreeSet<>()).add(num);
-            
-            // Update Max Freq (Bottleneck Optimization)
-            maxFreq = Math.max(maxFreq, newFreq);
-
-            // --- 2. Remove old number (slide window) ---
-            if (i >= k) { // Note: standard sliding window usually processes remove first or after result calculation
-                int oldNum = nums[i - k];
-                int prevFreqOfOldNum = numToFreq.get(oldNum);
-                int newFreqOfOldNum = prevFreqOfOldNum - 1;
-                
-                // Remove from current bucket
-                removeFromFreqMap(freqToNum, prevFreqOfOldNum, oldNum);
-                
-                // If this bucket was the maxFreq and is now empty, decrease maxFreq
-                if (prevFreqOfOldNum == maxFreq && !freqToNum.containsKey(maxFreq)) {
-                    maxFreq--;
-                }
-                
-                // Add to lower freq bucket
-                if (newFreqOfOldNum > 0) {
-                    numToFreq.put(oldNum, newFreqOfOldNum);
-                    freqToNum.computeIfAbsent(newFreqOfOldNum, t -> new TreeSet<>()).add(oldNum);
-                } else {
-                    numToFreq.remove(oldNum);
-                }
-            }
-
-            // --- 3. Calculate Result ---
-            if (i >= k - 1) {
-                // O(1) access to the max frequency bucket
-                long mode = freqToNum.get(maxFreq).first(); 
-                result += (long) maxFreq * mode;
-            }
-        }
-        return result;
-    }
-
-    // Helper to reduce code duplication (D in BUD)
-    private void removeFromFreqMap(Map<Integer, TreeSet<Integer>> map, int freq, int num) {
-        if (!map.containsKey(freq)) return;
-        TreeSet<Integer> set = map.get(freq);
-        set.remove(num);
-        if (set.isEmpty()) {
-            map.remove(freq);
-        }
-    }
-
-    public long modeWeightInitial(int[] nums, int k) {
-        long result = 0;
         HashMap<Integer,Integer> numToFreq = new HashMap<>();
         TreeMap<Integer,TreeSet<Integer>> freqToNum = new TreeMap<>();
-        for(int i=0;i<nums.length;i++){
-            int freq = numToFreq.getOrDefault(nums[i],0);
-            numToFreq.put(nums[i],freq+1);
-            freqToNum.computeIfAbsent(freq,t->new TreeSet<Integer>()).remove(nums[i]);
-            freqToNum.computeIfAbsent(freq+1,t->new TreeSet<Integer>()).add(nums[i]);
-            if(i>=k-1){
-                result += 1l*freqToNum.lastEntry().getKey()*freqToNum.lastEntry().getValue().first();
-                freq = numToFreq.get(nums[i-k+1]);
-                freqToNum.computeIfAbsent(freq,t->new TreeSet<Integer>()).remove(nums[i-k+1]);
-                if(freqToNum.get(freq).size()==0) freqToNum.remove(freq);
-                freq = freq-1;
-                numToFreq.put(nums[i-k+1],freq);
-                freqToNum.computeIfAbsent(freq,t->new TreeSet<Integer>()).add(nums[i-k+1]);
-                if(numToFreq.get(nums[i-k+1])==0) numToFreq.remove(nums[i-k+1]);
+        
+        for(int i=0; i<nums.length; i++){
+            int freq = numToFreq.getOrDefault(nums[i], 0);
+            numToFreq.put(nums[i], freq + 1);
+            
+            // Optimization 1: Only remove if freq > 0. 
+            // Prevents creating useless key '0' in freqToNum.
+            if (freq > 0) { 
+                freqToNum.computeIfAbsent(freq, t -> new TreeSet<Integer>()).remove(nums[i]);
+                // Optimization: Clean up empty sets immediately to keep map small
+                if (freqToNum.get(freq).isEmpty()) {
+                    freqToNum.remove(freq);
+                }
+            }
+            
+            freqToNum.computeIfAbsent(freq + 1, t -> new TreeSet<Integer>()).add(nums[i]);
+
+            if(i >= k - 1){
+                // Optimization 2: Call lastEntry() once
+                var entry = freqToNum.lastEntry();
+                result += 1L * entry.getKey() * entry.getValue().first();
+                
+                // Sliding window removal
+                int numToRemove = nums[i - k + 1];
+                freq = numToFreq.get(numToRemove);
+                
+                freqToNum.computeIfAbsent(freq, t -> new TreeSet<Integer>()).remove(numToRemove);
+                if(freqToNum.get(freq).isEmpty()) freqToNum.remove(freq);
+                
+                freq = freq - 1;
+                
+                // Optimization 3: Handle freq updates efficiently
+                if (freq == 0) {
+                    numToFreq.remove(numToRemove);
+                    // Do NOT add to freqToNum at key 0. It is useless work.
+                } else {
+                    numToFreq.put(numToRemove, freq);
+                    freqToNum.computeIfAbsent(freq, t -> new TreeSet<Integer>()).add(numToRemove);
+                }
             }
         }
         return result;
     }
-
 }
