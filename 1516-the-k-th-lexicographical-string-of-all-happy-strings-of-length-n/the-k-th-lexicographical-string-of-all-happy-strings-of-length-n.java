7741f81dc015b94ae323cf391949cc01d37dbe69
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-14 06:51:44 +0530

    Time: 22 ms (23.54%) | Memory: 46.8 MB (36.64%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -8,8 +8,6 @@ class Solution {
     private void recurse(int i, int n, int k, String s, List<String> li){
         if(s.length()==n){
             li.add(s);
-            i++;
-            if(i==k) return;
         }else{
             for(char c='a';c<='c';c++){
                 if(s.length()==0||s.charAt(s.length()-1)!=c){
