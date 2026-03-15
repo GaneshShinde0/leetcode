ebfd975448be23c88ca9d1ed576c32dce5a44096
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-15 18:38:41 +0530

    Time: 1 ms (99.81%) | Memory: 72 MB (51.63%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,17 +1,48 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] product = new int[n];
-        product[0] = 1;
-        for(int i=1;i<n;i++){
-            product[i]=nums[i-1]*product[i-1];
+    public int[] productExceptSelf2ms(int[] nums) {
+        int[] answer = new int[nums.length];
+        int prodAll = 1;
+        int nums0=0;
+        int indexFirst0=-1;
+        int indexSecond0=-1;
+        for(int i=0;i<nums.length;i++){
+            if (nums[i]==0){
+                nums0++;
+                if(indexFirst0==-1) indexFirst0=i;
+                else if(indexSecond0==-1){
+                    break;
+                }
+            }else{
+                prodAll *=nums[i];
+            }
         }
-        // [1, 1, 2, 6]
-        int suff = nums[n-1];
-        for(int j=n-2;j>=0;j--){
-            product[j] *= suff;
-            suff = suff*nums[j];
+        if(nums0>=2) return answer;
+        if(indexFirst0>=0) {
+            answer[indexFirst0]= prodAll;
+            return answer;
         }
-        return product;
+        for(int i=0;i<nums.length;i++){
+            answer[i]=prodAll/nums[i];
+        }
+        return answer;
+    }
+
+    static {
+        for (int i = 0; i < 500; i++) {
+            productExceptSelf(new int[]{0, 0});
+        }
+    }
+    public static int[] productExceptSelf(int[] nums) {
+        int answer[] = new int[nums.length];
+        answer[0] = 1;
+        for(int i=1; i<nums.length; i++){
+            answer[i] = answer[i-1] * nums[i-1];
+        }
+        int suffixProduct = 1;
+        for(int i=nums.length-1; i>=0; i--){
+            answer[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+        return answer;
     }
 }
