42c1936ded816be50da2c6b77776a02fd7026198
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-15 17:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (55.39%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,13 +1,26 @@
 class Solution {
-    public int hIndex(int[] citations) {
+    public int hIndexSorting(int[] citations) {
         Arrays.sort(citations);
         int n = citations.length;
         int res = 0;
         for(int i=0;i<citations.length;i++){
-            if(citations[i]>= n-i){
+            if(citations[i]>= n-i){// If this is greater than n-i then next elements will surely be as array is sorted.
                 return n-i;
             }
         }
         return 0;
+    }
+    public int hIndex(int[] citations){
+        int n = citations.length;
+        int[] papers = new int[n+1];
+        // Counting papers for each citation number.
+        for(int c:citations){
+            papers[Math.min(c,n)]++; // Storing citations for every index.
+        }
+        int res = n; // Consider result as max initially
+        for(int i=papers[n];res>i;i+=papers[res]){ // Start with how many there are at max.
+            res--;
+        }
+        return res;
     }
 }
