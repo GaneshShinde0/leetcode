b459f4abc75ecb291540f40bc940040269054324
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-15 18:37:59 +0530

    Time: 2 ms (94.56%) | Memory: 71.9 MB (58.45%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,41 +1,17 @@
 class Solution {
-    public int[] productExceptSelfWithDivision(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        int zeroCount = 0;
-        int product = 1;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                zeroCount++;
-                if(zeroCount == 2) return res;
-            }else{
-                product*=nums[i];
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(zeroCount == 1 && nums[i]!=0){
-                res[i]=0;
-            }else if(zeroCount==1 && nums[i]==0){
-                res[i]=product;
-            }else{
-                res[i]=product/nums[i];
-            }
-        }
-        return res;
-    }
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] res = new int[n];
-        res[0]=1;
+        int[] product = new int[n];
+        product[0] = 1;
         for(int i=1;i<n;i++){
-            res[i]=res[i-1]*nums[i-1];
+            product[i]=nums[i-1]*product[i-1];
         }
-        // System.out.println(Arrays.toString(res));
-        int suffix = 1;
-        for(int i=n-1;i>=0;i--){
-            res[i]*=suffix;
-            suffix*=nums[i];
+        // [1, 1, 2, 6]
+        int suff = nums[n-1];
+        for(int j=n-2;j>=0;j--){
+            product[j] *= suff;
+            suff = suff*nums[j];
         }
-        return res;
+        return product;
     }
 }
