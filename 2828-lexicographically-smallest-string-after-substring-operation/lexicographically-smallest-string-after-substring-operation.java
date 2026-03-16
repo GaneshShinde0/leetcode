6c634be97de6affddbaabaac794273538908eebd
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-16 23:53:23 +0530

    Time: 12 ms (33.51%) | Memory: 51.3 MB (41.24%) - LeetSync

--- 2828-lexicographically-smallest-string-after-substring-operation/lexicographically-smallest-string-after-substring-operation.java
+++ 2828-lexicographically-smallest-string-after-substring-operation/lexicographically-smallest-string-after-substring-operation.java
@@ -1,13 +1,32 @@
+/*
+Input: s = "cbabc"
+Output: "baabc";
+
+Input: s = "a"=> z
+
+Input: s = zzasdf
+Output: s = yyasdf
+
+Basically until we get A we can reduce.
+
+Input: s = "aaaaaa";
+we have a then we will reach till last character for which a is present and replace that with z, so we will get smallest string after operation
+Output: s = "aaaaaz"
+
+// We should be able to manipulate individual character;=> Transform string to char Array.
+*/
+
 class Solution {
     public String smallestString(String s) {
-        int i = 0, n = s.length();
+        int n = s.length(), i=0;
         char[] arr = s.toCharArray();
         while(i<n && arr[i]=='a') i++;
-        if(i==n) arr[n-1]='z';
+        if(i==n) arr[n-1]='z'; //  We have to change atleast one.
         while(i<n && arr[i]!='a'){
-            arr[i]-=1;
+            arr[i]--;
             i++;
         }
         return String.valueOf(arr);
+
     }
 }
