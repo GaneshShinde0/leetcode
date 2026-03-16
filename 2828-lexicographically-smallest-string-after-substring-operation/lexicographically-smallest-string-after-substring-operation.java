2a18431f8896f18d90a9625cb9464ef9673e6e72
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-16 23:46:13 +0530

    Time: 11 ms (92.78%) | Memory: 51.3 MB (41.24%) - LeetSync

---
+++ 2828-lexicographically-smallest-string-after-substring-operation/lexicographically-smallest-string-after-substring-operation.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String smallestString(String s) {
+        int i = 0, n = s.length();
+        char[] arr = s.toCharArray();
+        while(i<n && arr[i]=='a') i++;
+        if(i==n) arr[n-1]='z';
+        while(i<n && arr[i]!='a'){
+            arr[i]-=1;
+            i++;
+        }
+        return String.valueOf(arr);
+    }
+}
