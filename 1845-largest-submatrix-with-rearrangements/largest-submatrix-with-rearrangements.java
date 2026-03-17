03db0dad4d02ba9c06f211930c70cdf56dbfc398
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-17 08:37:48 +0530

    Time: 30 ms (11.30%) | Memory: 117.5 MB (6.96%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
@@ -1,4 +1,14 @@
 class Solution {
+
+/*
+
+Time Complexity
+O(m*n*log(n))
+Space Complexity: 
+    O(n) for extra array.
+    But we are also modifying matrix, so modifaction should be counted in space complexity.
+    O(m*n);
+*/
     public int largestSubmatrixModifiesInput(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         int res = 0;
@@ -17,8 +27,14 @@ class Solution {
         }
         return res;
     }
+/*
 
-    public int largestSubmatrix(int[][] matrix){
+Time Complexity
+    O(m*n*log(n))
+Space Complexity: 
+    O(n) for extra array.
+*/
+    public int largestSubmatrixSpaceOptimized(int[][] matrix){
         int m = matrix.length, n = matrix[0].length;
         int[] prevRow = new int[n];
         int ans =  0;
@@ -35,14 +51,36 @@ class Solution {
         }
         return ans;
     }
+
+    // Time and space optimized
+    // Pairing height and column
+    public int largestSubmatrix(int[][] matrix){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int res = 0;
+        List<Pair<Integer, Integer>> prevHeights = new ArrayList<>();
+        for(int row=0;row<m;row++){
+            List<Pair<Integer, Integer>> heights = new ArrayList<>();
+            boolean[] seen = new boolean[n];
+            for(Pair<Integer, Integer> pair: prevHeights){
+                int height = pair.getKey();
+                int col = pair.getValue();
+                if(matrix[row][col] == 1){
+                    heights.add(new Pair(height+1,col));
+                    seen[col]= true;
+                }
+            }
+            for(int col = 0; col<n; col++){
+                if(seen[col] == false && matrix[row][col]==1) heights.add(new Pair(1,col));
+            }
+
+            for(int i=0;i<heights.size();i++){
+                res = Math.max(res, heights.get(i).getKey()*(i+1));
+            }
+            prevHeights = heights;
+        }
+        return res;
+    }
+
 }
 
-/*
-
-Time Complexity
-O(m*n*log(n))
-Space Complexity: 
-    O(n) for extra array.
-    But we are also modifying matrix, so modifaction should be counted in space complexity.
-    O(m*n);
-*/
