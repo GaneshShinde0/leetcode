5451710045732d28adca6bbafd2ba44eda191328
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-17 08:07:57 +0530

    Time: 13 ms (86.96%) | Memory: 114.6 MB (37.39%) - LeetSync

---
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int largestSubmatrix(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int res = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i>0 && matrix[i][j]!=0) matrix[i][j]=matrix[i][j]+matrix[i-1][j];
+            }
+
+            int[] row = matrix[i].clone();
+            Arrays.sort(row);
+            for(int j=0;j<n;j++){
+                res = Math.max(res,row[j]*(n-j));
+            }
+        }
+        return res;
+    }
+}
