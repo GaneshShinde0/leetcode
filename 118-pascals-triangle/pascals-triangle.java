599e9fd9a8ec32f7476c6143a10802ee2aaabacd
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-19 00:51:31 +0530

    Time: 1 ms (95.14%) | Memory: 43.5 MB (48.13%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -37,22 +37,81 @@ class Solution {
         return pascalsTriangle;
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> pascalsTriangle = new ArrayList<>();
-        List<Integer> row = new ArrayList<>();
-        List<Integer> prev = new ArrayList<>();
-        row.add(1);
-        pascalsTriangle.add(row);
-        for(int i=1; i<numRows;i++){
-            row = new ArrayList<>();
-            row.add(1);
-            prev = pascalsTriangle.get(i-1);
-            for (int j=1;j<i;j++){
-                row.add(prev.get(j-1)+prev.get(j));
+        for(int i=0;i<numRows;i++){
+            List<Integer> row = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                if(j==0||j==i) row.add(1);
+                else{
+                    row.add(pascalsTriangle.get(i-1).get(j-1)+pascalsTriangle.get(i-1).get(j));
+                }
             }
-            row.add(1);
             pascalsTriangle.add(row);
         }
         return pascalsTriangle;
     }
 }
+
+/*
+To get element at n'th Row and r'th Column
+We can user nCr => n!/((n-r)!*r!)
+
+10 C 3
+
+res = 1;
+for(int i=0;i<r;i++){
+    res = res *(n-i)
+    res = res/(i+1);
+}
+
+Time: O(r); from nCr
+
+
+Find nth Row:
+
+Example 6th Row.
+
+1 5 10 10 5 1
+
+7th Row
+1 6 15 20 15 6 1
+
+List<Integer> result = new ArrayList<>();
+for(int i=0;i<=n;i++){
+    if(i==0||i==n) result.add(1);
+    else{
+        int ans = result.get(i-1);
+        ans = ans*(n-i);
+        ans = ans/(i); 
+    }
+}
+*/
