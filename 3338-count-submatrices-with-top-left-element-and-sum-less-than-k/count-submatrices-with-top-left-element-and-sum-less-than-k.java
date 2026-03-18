9c4c8468604693667727b91c00e8944d7a0e5790
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-18 11:20:30 +0530

    Time: 7 ms (50.81%) | Memory: 160.9 MB (91.94%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -2,6 +2,8 @@ class Solution {
     public int countSubmatrices(int[][] grid, int k) {
         int m = grid.length, n = grid[0].length;
         int result = 0;
+        // Question asks -> It asks for submatrices that include the top-left element of the grid (i.e., grid[0][0]).
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(i>0) grid[i][j]+=grid[i-1][j];
