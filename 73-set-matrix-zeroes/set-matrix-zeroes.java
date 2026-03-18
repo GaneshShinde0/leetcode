916821df4d3048aed2628032de9b753e4f102af3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-18 18:28:16 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (7.40%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -8,8 +8,8 @@ class Solution {
         for(int j=0;j<n;j++){
             if(matrix[0][j]==0) isFirstRowZero = true;
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
