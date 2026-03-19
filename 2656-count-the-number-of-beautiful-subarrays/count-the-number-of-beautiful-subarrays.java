f6db2a9f0a4aadc91d42e425ee76694cab613e69
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-19 14:39:52 +0530

    Time: 64 ms (19.13%) | Memory: 120.9 MB (9.84%) - LeetSync

--- 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.java
+++ 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.java
@@ -1,6 +1,32 @@
 class Solution {
     public long beautifulSubarrays(int[] nums) {
         HashMap<Long, Long> hm = new HashMap<>();
+        // Initial put(0, 1) removed
+        
+        long currXor = 0;
+        long res = 0;
+        
+        for (int num : nums) {
+            currXor ^= num;
+
+            // 1. Explicitly check if the prefix from the start is valid
+            if (currXor == 0) {
+                res++;
+            }
+
+            // 2. Check for subarrays starting after index 0
+            if (hm.containsKey(currXor)) {
+                res += hm.get(currXor);
+            }
+
+            // 3. Update the map
+            hm.put(currXor, hm.getOrDefault(currXor, 0L) + 1);
+        }
+        return res;
+    }
+
+    public long beautifulSubarrays1(int[] nums) {
+        HashMap<Long, Long> hm = new HashMap<>();
         hm.put(0l,1l);
         long currXor = 0;
         long res = 0;
@@ -14,6 +40,7 @@ class Solution {
 }
 
 /*
+0:1 
 4:1
 7:1
 6:1
