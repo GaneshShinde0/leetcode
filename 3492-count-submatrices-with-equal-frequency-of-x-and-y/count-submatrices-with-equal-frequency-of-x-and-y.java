f1c332fd219aaf936270bf580826c61bb098d78a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-19 08:34:42 +0530

    Time: 17 ms (98.08%) | Memory: 259.9 MB (15.38%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int numberOfSubmatrices(char[][] grid) {
+    public int numberOfSubmatrices2D(char[][] grid) {
         int m = grid.length, n =  grid[0].length, result = 0;
         int[][] freqX = new int[m+1][n+1];
         int[][] freqY = new int[m+1][n+1];
@@ -17,8 +17,37 @@ class Solution {
         }
         return result;
     }
+    public int numberOfSubmatrices(char[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int result = 0;
+        
+        // Stores vertical count of X and Y for each column up to the current row
+        int[] colX = new int[n];
+        int[] colY = new int[n];
+        
+        for (int i = 0; i < m; i++) {
+            // Running totals for the current submatrix (0,0) to (i,j)
+            int submatrixX = 0;
+            int submatrixY = 0;
+            
+            for (int j = 0; j < n; j++) {
+                // 1. Update vertical column counts
+                if (grid[i][j] == 'X') colX[j]++;
+                else if (grid[i][j] == 'Y') colY[j]++;
+                
+                // 2. Accumulate horizontal sum to get full submatrix count
+                submatrixX += colX[j];
+                submatrixY += colY[j];
+                
+                // 3. Check condition
+                if (submatrixX > 0 && submatrixX == submatrixY) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
 }
-
 /*
 Input: grid = [["X","Y","."],["Y",".","."]]
 
