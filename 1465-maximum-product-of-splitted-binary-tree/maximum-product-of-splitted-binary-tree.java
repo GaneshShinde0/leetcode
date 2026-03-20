dcc23db32dd5b549aceaa9cf01be1d053b2e2bb8
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-21 01:03:24 +0530

    Time: 14 ms (20.85%) | Memory: 64.3 MB (15.91%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -16,20 +16,20 @@
 class Solution {
     private static final int MOD = 1_000_000_007;
     public int maxProduct(TreeNode root) {
-        List<Long> sums = new ArrayList<>();
-        long totalSum = totalSum(root,sums);
-        long maxProd = 0;
-        for(long sum:sums){
-            maxProd = Math.max(maxProd,(totalSum-sum)*sum);
+        List<Long> sumAtNodes = new ArrayList<>();
+        long total = totalSum(sumAtNodes, root);
+        long result = 0;
+        for(long sum:sumAtNodes){
+            result = Math.max((total-sum)*sum,result);
         }
-        return (int) (maxProd%MOD);
+        return (int) (result%MOD);
     }
-    private long totalSum(TreeNode root,List<Long> sums){
+    private long totalSum(List<Long> sumAtNodes, TreeNode root){
         if(root==null) return 0;
-        long leftSum = totalSum(root.left,sums);
-        long rightSum = totalSum(root.right,sums);
+        long leftSum = totalSum(sumAtNodes,root.left);
+        long rightSum = totalSum(sumAtNodes,root.right);
         long currSum = root.val+leftSum+rightSum;
-        sums.add(currSum);
+        sumAtNodes.add(currSum);
         return currSum;
     }
 }
