088d8e01a397fa81b9f2cc7251195bb22c784a50
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-20 22:01:10 +0530

    Time: 2022 ms (5.00%) | Memory: 151.2 MB (5.00%) - LeetSync

---
+++ 1366-first-unique-number/first-unique-number.java
@@ -0,0 +1,30 @@
+class FirstUnique {
+    HashMap<Integer,Integer> hm = new HashMap<>();
+    Set<Integer> set = new LinkedHashSet<>();
+
+    public FirstUnique(int[] nums) {
+        for(int num:nums){
+            hm.put(num,hm.getOrDefault(num,0)+1);
+            if(hm.get(num)>1 && set.contains(num)) set.remove(num);
+            else if(hm.get(num)==1) set.add(num); 
+        }    
+    }
+    
+    public int showFirstUnique() {
+        if(set.isEmpty()) return -1;
+        else return new ArrayList<>(set).get(0);
+    }
+    
+    public void add(int num) {
+        hm.put(num,hm.getOrDefault(num,0)+1);
+        if(hm.get(num)>1 && set.contains(num)) set.remove(num);
+        else if(hm.get(num)==1) set.add(num); 
+    }
+}
+
+/**
+ * Your FirstUnique object will be instantiated and called as such:
+ * FirstUnique obj = new FirstUnique(nums);
+ * int param_1 = obj.showFirstUnique();
+ * obj.add(value);
+ */
