e2858408c6682f24a4ad1ddceb7b94ec7e37bf67
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-21 13:00:14 +0530

    Time: 14 ms (53.77%) | Memory: 47.1 MB (57.55%) - LeetSync

--- 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.java
+++ 255-verify-preorder-sequence-in-binary-search-tree/verify-preorder-sequence-in-binary-search-tree.java
@@ -4,6 +4,38 @@ Intuition:
 - Keeping a stack of nodes, of whcih we're still in the left subtree, if the next number is smaller than the last stack value, then we're still in the left subtree of all the stack node, so we will push new node onto the stack.
 - But before that, pop all smaller ancestor values as we must now be in the right subtrees (or evern further, -> RIght subtree of an ancestor). Also, use the popped values as a lower bound, since being in their right subtree means we must never come across a smaller number anymore.
 */
+/*
+Input: preorder = [5,2,1,3,6]
+
+Initially
+low = -1;
+path = []
+
+p = 5
+path = [5]
+low = -1
+
+p=2
+path = [5,2]
+low = -1;
+
+p=1
+path = [5,2,1]
+low = -1;
+
+p= 3 =>p>path.peek()=>3>1
+low = 1
+path = [5,2,3]
+
+p = 6 => 6>3=>
+low = 3
+path = [5,2,6]
+
+return true;
+
+
+
+*/
 class Solution {
     public boolean verifyPreorder(int[] preOrder) {
         int low = Integer.MIN_VALUE;
