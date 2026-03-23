8da62349a01e2b84e2b9fa0380a1966823079e83
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-23 22:14:45 +0530

    Time: 3 ms (68.20%) | Memory: 48 MB (85.22%) - LeetSync

--- 341-flatten-nested-list-iterator/flatten-nested-list-iterator.java
+++ 341-flatten-nested-list-iterator/flatten-nested-list-iterator.java
@@ -20,6 +20,7 @@ public class NestedIterator implements Iterator<Integer> {
     int i;
     int size;
     List<Integer> li;
+
     public NestedIterator(List<NestedInteger> nestedList) {
         this.i = 0;
         this.li = new ArrayList<>();
@@ -27,12 +28,12 @@ public class NestedIterator implements Iterator<Integer> {
         this.size = li.size();
     }
 
-    private void addIntegers(List<NestedInteger> nestedList){
-        for(int j=0;j<nestedList.size();j++){
+    private void addIntegers(List<NestedInteger> nestedList) {
+        for (int j = 0; j < nestedList.size(); j++) {
             NestedInteger curr = nestedList.get(j);
-            if(curr.isInteger()){
+            if (curr.isInteger()) {
                 this.li.add(curr.getInteger());
-            }else{
+            } else {
                 addIntegers(curr.getList());
             }
         }
@@ -40,16 +41,16 @@ public class NestedIterator implements Iterator<Integer> {
 
     @Override
     public Integer next() {
-        if(hasNext()){
+        if (hasNext()) {
             i++;
-            return li.get(i-1);
+            return li.get(i - 1);
         }
         return null;
     }
 
     @Override
     public boolean hasNext() {
-        return i<size;
+        return i < size;
     }
 }
 
