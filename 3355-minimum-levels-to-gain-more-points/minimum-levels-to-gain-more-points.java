f56177dd5fc2ec2ecdae027f0eda6fcae06780d6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-23 23:11:15 +0530

    Time: 9 ms (86.46%) | Memory: 166.9 MB (80.21%) - LeetSync

--- 3355-minimum-levels-to-gain-more-points/minimum-levels-to-gain-more-points.java
+++ 3355-minimum-levels-to-gain-more-points/minimum-levels-to-gain-more-points.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minimumLevels(int[] possible) {
+    public int minimumLevelsInitial(int[] possible) {
         int n = possible.length;
         
         int[] prefix = new int[n];
@@ -16,4 +16,22 @@ class Solution {
         return -1;
         
     }
+    public int minimumLevels(int[] possible) {
+        int n = possible.length;
+        
+        int sum = 0;
+        for (int i = 0; i < n; i++){
+            if (possible[i] == 0) sum = sum - 1;
+            else sum = sum + 1;
+        }
+        int rightSum = 0;
+        int res = -1;
+        for (int i = 0; i < n-1; i++){
+            rightSum = rightSum +((possible[i] == 0)?- 1:1);
+            if (sum-rightSum <rightSum) return i+1;
+        }
+        return res;
+        
+    }
+
 }
