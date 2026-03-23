56aecb565ac5d08dbad1aa6952710d40d0778a39
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-24 01:06:15 +0530

    Time: 50 ms (5.02%) | Memory: 83.9 MB (5.01%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<Integer> eventualSafeNodes(int[][] graph) {
+    public List<Integer> eventualSafeNodesInitial(int[][] graph) {
         List<Integer> result = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
         HashMap<Integer, Set<Integer>> adjList= new HashMap<>();
@@ -19,6 +19,34 @@ class Solution {
                 if(adjList.containsKey(parent)){
                     adjList.get(parent).remove(curr);
                     if(adjList.get(parent).size()==0) queue.add(parent);
+                }
+            }
+        }
+        Collections.sort(result);
+        return result;
+    }
+
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n = graph.length;
+        List<Integer> result = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int[] outDegree = new int[n+1];
+        HashMap<Integer, Set<Integer>> adjListChildToParent= new HashMap<>();
+        for(int i=0;i<graph.length;i++){
+            if(graph[i].length==0) queue.add(i);
+            for(int j:graph[i]){
+                outDegree[i]++;
+                adjListChildToParent.computeIfAbsent(j,k->new HashSet<Integer>()).add(i);
+            }
+        }
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            result.add(curr);
+            if(!adjListChildToParent.containsKey(curr)) continue;
+            for(int parent: adjListChildToParent.get(curr)){
+                if(outDegree[parent]>0){
+                    outDegree[parent]--;
+                    if(outDegree[parent]==0) queue.add(parent);
                 }
             }
         }
