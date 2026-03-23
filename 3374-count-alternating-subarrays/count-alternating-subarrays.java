85a411f3aa802b58b92d223cc3743286a7b1237a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-23 23:30:42 +0530

    Time: 3 ms (98.48%) | Memory: 90.3 MB (61.11%) - LeetSync

--- 3374-count-alternating-subarrays/count-alternating-subarrays.java
+++ 3374-count-alternating-subarrays/count-alternating-subarrays.java
@@ -1,14 +1,14 @@
 class Solution {
     public long countAlternatingSubarrays(int[] nums) {
         int n = nums.length;
-        long res =1, consecutive = 1;
+        long res =1, alternating = 1;
         for(int i=1;i<n;i++){
             if(nums[i]==nums[i-1]){
-                consecutive=1;
+                alternating=1;
             }else{
-                consecutive+=1;
+                alternating+=1;
             }
-            res += consecutive;
+            res += alternating;
         }
         return res;
     }
