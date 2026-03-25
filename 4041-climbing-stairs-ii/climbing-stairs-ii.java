fb6cbf9190e2d19151e96b118a29c845b2d8dc55
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-25 23:44:37 +0530

    Time: 11 ms (53.09%) | Memory: 110.6 MB (87.63%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int climbStairs(int n, int[] costs) {
+    public int climbStairsInitial(int n, int[] costs) {
         int cost = 0;
         int[] dp = new int[n+1];
         Arrays.fill(dp,Integer.MAX_VALUE);
@@ -10,6 +10,18 @@ class Solution {
             }
         }
         return dp[n];
+    }
+
+    public int climbStairs(int n, int[] costs){
+        int[] dp = new int[3];
+        for(int i=1;i<=n;i++){
+            int best = Integer.MAX_VALUE;
+            if(i>=1) best = Math.min(best, dp[(i-1)%3]+1);
+            if(i>=2) best = Math.min(best, dp[(i-2)%3]+4);
+            if(i>=3) best = Math.min(best, dp[(i-3)%3]+9);
+            dp[i%3] = best +costs[i-1];
+        }
+        return dp[n%3];
     }
 }
 
