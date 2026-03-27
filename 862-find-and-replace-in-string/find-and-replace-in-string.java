24c58842bbd50835fb3b4289939e24772ac70b0c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-27 08:35:39 +0530

    Time: 3 ms (46.92%) | Memory: 44 MB (38.15%) - LeetSync

--- 862-find-and-replace-in-string/find-and-replace-in-string.java
+++ 862-find-and-replace-in-string/find-and-replace-in-string.java
@@ -1,32 +1,53 @@
 class Solution {
-    class IndexSourceTarget{
+    class IndexSourceTarget {
         int index;
         String source;
         String target;
-        IndexSourceTarget(int index, String source, String target){
+
+        IndexSourceTarget(int index, String source, String target) {
             this.index = index;
             this.source = source;
             this.target = target;
         }
     }
-    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+
+    public String findReplaceStringInitial(String s, int[] indices, String[] sources, String[] targets) {
         StringBuilder sb = new StringBuilder(s);
         int m = sources.length;
-        int diff = 0;
         IndexSourceTarget[] arr = new IndexSourceTarget[m];
-        for(int i=0;i<m;i++){
-            arr[i]=new IndexSourceTarget(indices[i], sources[i], targets[i]);
+        int diff = 0;
+        for (int i = 0; i < m; i++) {
+            arr[i] = new IndexSourceTarget(indices[i], sources[i], targets[i]);
         }
-        Arrays.sort(arr,(a,b)->Integer.compare(a.index,b.index));
-        for(int i=0;i<m;i++){
+        Arrays.sort(arr, (a, b) -> Integer.compare(a.index, b.index));
+        for (int i = 0; i < m; i++) {
             IndexSourceTarget curr = arr[i];
             int idx = curr.index;
             String currSource = curr.source;
             String currTarget = curr.target;
-            if(s.indexOf(currSource,idx)==idx){
-                sb.delete(idx+diff,idx+diff+currSource.length());
-                sb.insert(idx+diff,currTarget);
-                diff+=currTarget.length()-currSource.length();
+            if (s.indexOf(currSource, idx) == idx) {
+                sb.delete(idx + diff, idx + diff + currSource.length());
+                sb.insert(idx + diff, currTarget);
+                diff += currTarget.length() - currSource.length();
+            }
+        }
+        return sb.toString();
+    }
+
+    public String findReplaceString(String s, int[] indexes, String[] sources, String[] targets) {
+        List<int[]> sorted = new ArrayList<>();
+        for (int i = 0; i < indexes.length; i++)
+            sorted.add(new int[] { indexes[i], i });
+        Collections.sort(sorted, (a,b)->Integer.compare(a[0],b[0]));
+        StringBuilder sb = new StringBuilder(s);
+        int diff = 0;
+        for (int[] ind : sorted) {
+            int idx = ind[0], j = ind[1];
+            String currSource = sources[j], currTarget = targets[j];
+            if (s.indexOf(currSource, idx) == idx) {
+                sb.delete(idx + diff, idx + diff + currSource.length());
+                sb.insert(idx + diff, currTarget);
+                diff += currTarget.length() - currSource.length();
             }
         }
         return sb.toString();
