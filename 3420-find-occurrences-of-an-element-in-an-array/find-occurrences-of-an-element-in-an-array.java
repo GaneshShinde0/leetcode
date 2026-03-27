06376a37f1f64bc2e7d802f6fe930f92c3ec1b5a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-27 15:32:00 +0530

    Time: 5 ms (83.11%) | Memory: 166.8 MB (64.17%) - LeetSync

--- 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
@@ -2,15 +2,14 @@ class Solution {
     public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
         int resultSize = queries.length;
         int numOfElements = nums.length;
-        int[] result = new int[resultSize];
         int count= 0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==x) count++;
         }
         int[] freqToIndex = new int[count];
-        count = 0;
-        for(int i=0;i<numOfElements;i++){
-            if(nums[i]==x) freqToIndex[count++] = i;
+        int[] result = new int[resultSize];
+        for(int i=0,j=0;i<numOfElements;i++){
+            if(nums[i]==x) freqToIndex[j++] = i;
         }
         for(int i=0;i<resultSize;i++){
             if(queries[i]>count) result[i]=-1;
