31da7236b3c1a0b900206f0c930c4d1053bb7375
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-28 13:49:05 +0530

    Time: 431 ms (92.86%) | Memory: 0B (100.00%) - LeetSync

--- 3301-friday-purchases-ii/friday-purchases-ii.sql
+++ 3301-friday-purchases-ii/friday-purchases-ii.sql
@@ -1,3 +1,5 @@
+/*
+-- Initial
 WITH NovemberDates AS( -- Generate November Column
     SELECT CAST('2023-11-01' as DATE) as purchase_date
     UNION ALL
@@ -9,3 +11,22 @@ RIGHT JOIN NovemberDates nd on p.purchase_date = nd.purchase_Date
 WHERE
 MONTH(CAST(nd.purchase_date as datetime)) = '11' AND YEAR(CAST(nd.purchase_date as datetime)) = 2023  
 GROUP BY nd.purchase_date HAVING DAY(CAST(nd.purchase_date as datetime))%7=3
+*/
+WITH NovemberDates AS (
+    -- Recursive CTE to generate all days in November 2023
+    SELECT CAST('2023-11-01' AS DATE) AS purchase_date
+    UNION ALL
+    SELECT DATEADD(DAY, 1, purchase_date)
+    FROM NovemberDates
+    WHERE purchase_date < '2023-11-30'
+)
+SELECT 
+    -- Standard formula for week of month (1-7 = Week 1, 8-14 = Week 2, etc.)
+    (DAY(nd.purchase_date)) / 7 + 1 AS week_of_month, 
+    nd.purchase_date, 
+    SUM(ISNULL(p.amount_spend, 0)) AS total_amount
+FROM NovemberDates nd
+LEFT JOIN Purchases p ON nd.purchase_date = p.purchase_date
+WHERE DATENAME(weekday, nd.purchase_date) = 'Friday'
+GROUP BY nd.purchase_date
+ORDER BY week_of_month;
