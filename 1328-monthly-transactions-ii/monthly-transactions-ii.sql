a13771081821d205b46c2e0ea4912eede4ff165b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-28 16:14:48 +0530

    Time: 420 ms (86.67%) | Memory: 0B (100.00%) - LeetSync

--- 1328-monthly-transactions-ii/monthly-transactions-ii.sql
+++ 1328-monthly-transactions-ii/monthly-transactions-ii.sql
@@ -1,10 +1,33 @@
 /* Write your T-SQL query statement below */
-SELECT Month, country, SUM(IIF(state='approved' ,1,0)) as approved_count, SUM(iif(state='approved' ,amount,0)) as approved_amount,
- SUM(IIF(state='chargeback ',1,0)) as chargeback_count, SUM(iif(state='chargeback ',amount,0)) as chargeback_amount FROM 
- (
-    SELECT LEFT(cb.trans_date,7) as MONTH, Country, 'chargeback' as state, amount 
-    FROM chargebacks cb  join transactions on cb.trans_id = transactions.id
-    UNION ALL
-    SELECT LEFT(trans_date,7) as Month, country, state, amount FROM Transactions Where state = 'approved'
- ) SRC 
- Group BY Month, COuntry
+SELECT 
+    Month, 
+    country, 
+    SUM(IIF(state = 'approved', 1, 0)) AS approved_count, 
+    SUM(IIF(state = 'approved', amount, 0)) AS approved_amount, 
+    SUM(IIF(state = 'chargeback', 1, 0)) AS chargeback_count, 
+    SUM(IIF(state = 'chargeback', amount, 0)) AS chargeback_amount 
+FROM (
+    SELECT 
+        LEFT(cb.trans_date, 7) AS Month, 
+        country, 
+        'chargeback' AS state, 
+        amount 
+    FROM chargebacks cb 
+    JOIN transactions ON cb.trans_id = transactions.id 
+    
+    UNION ALL 
+    
+    SELECT 
+        LEFT(trans_date, 7) AS Month, 
+        country, 
+        state, 
+        amount 
+    FROM Transactions 
+    --Where state = 'approved' this one or Having both work the same.
+) SRC 
+GROUP BY 
+    Month, 
+    country 
+HAVING 
+    SUM(IIF(state = 'approved', 1, 0)) > 0 
+    OR SUM(IIF(state = 'chargeback', 1, 0)) > 0;
