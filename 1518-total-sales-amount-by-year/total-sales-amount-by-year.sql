b175642756135ea0787f3a431547c596ab7a97c8
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-28 17:22:40 +0530

    Time: 277 ms (91.67%) | Memory: 0B (100.00%) - LeetSync

--- 1518-total-sales-amount-by-year/total-sales-amount-by-year.sql
+++ 1518-total-sales-amount-by-year/total-sales-amount-by-year.sql
@@ -1,4 +1,5 @@
 /* Write your T-SQL query statement below */
+/*
 DECLARE @max_date DATE = (SELECT MAX(period_end) FROM Sales);
 WITH RECURSIVE_CTE AS(
     SELECT MIN(PERIOD_START) as date FROM Sales
@@ -13,3 +14,27 @@ JOIN RECURSIVE_CTE cte ON s.period_start<=cte.date and s.period_end>=cte.date
 GROUP BY s.product_id, p.product_name, left(cte.date,4)
 ORDER BY 1, 3
 OPTION (MAXRECURSION 0);
+*/
+/* Write your T-SQL query statement below */
+with periods as (
+    select product_id, product_name, report_year
+    from Product
+    cross join (Values(2018), (2019), (2020)) as V(report_year)
+)
+
+select product_id,
+       product_name,
+       cast(report_year as varchar) as report_year,
+       total_days_in_report_period * average_daily_sales as total_amount
+from (
+    select p.product_id,
+           p.product_name,
+           s.average_daily_sales,
+           p.report_year,
+           datediff(day,
+                    IIF(s.period_start > datefromparts(report_year, 1, 1), s.period_start, datefromparts(report_year, 1, 1)),
+                    IIF(s.period_end < datefromparts(report_year, 12, 31), s.period_end, datefromparts(report_year, 12, 31))
+            ) + 1 as total_days_in_report_period
+    from periods p
+    join Sales s on p.product_id = s.product_id and p.report_year between year(period_start) and year(period_end)
+) t
