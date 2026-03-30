d2bbf4087c626cfc9022409092ec955c172dc4a5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-03-30 08:45:28 +0530

    Time: 7 ms (25.00%) | Memory: 47.9 MB (65.79%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean checkStrings(String source, String target) {
+    public boolean checkStringsMine(String source, String target) {
         int[] checkEvenIndices = new int[26];
         int[] checkOddIndices = new int[26];
         for(int i=0;i<source.length();i++){
@@ -20,4 +20,17 @@ class Solution {
         }
         return true;
     }
+
+    public boolean checkStrings(String s1, String s2){
+        if(s1.length()!=s2.length()) return false;
+        int[] count1 = new int[256];
+        int[] count2 = new int[256];
+        for(int i=0;i<s1.length();i++){
+            int offset = (i&1)<<7;
+            count1[offset+s1.charAt(i)]++;
+            count2[offset+s2.charAt(i)]++;
+        }
+        return Arrays.equals(count1,count2);
+    }
+
 }
