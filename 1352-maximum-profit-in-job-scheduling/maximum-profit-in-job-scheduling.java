189eaa84877d68916bc81af77fc1d21f39532df2
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-01 15:34:27 +0530

    Time: 1737 ms (5.03%) | Memory: 69 MB (57.41%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -2,27 +2,25 @@ class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         int n = startTime.length;
         int[][] startEndProfit = new int[n][3];
-        for(int i=0;i<n;i++){
-            startEndProfit[i][0]=startTime[i];
-            startEndProfit[i][1]=endTime[i];
-            startEndProfit[i][2]=profit[i];
+        for (int i = 0; i < n; i++) {
+            startEndProfit[i][0] = startTime[i];
+            startEndProfit[i][1] = endTime[i];
+            startEndProfit[i][2] = profit[i];
         }
-        Arrays.sort(startEndProfit,(a,b)->Integer.compare(a[1],b[1]));
+        Arrays.sort(startEndProfit, (a, b) -> Integer.compare(a[1], b[1]));
         int[] profitAt = new int[n];
-        profitAt[0]=startEndProfit[0][2];
+        profitAt[0] = startEndProfit[0][2];
         int result = startEndProfit[0][2];
-        for(int i=1;i<n;i++){
-            // System.out.println(Arrays.toString(startEndProfit[i]));
-            profitAt[i] = Math.max(startEndProfit[i][2],profitAt[i-1]);
-            for(int j=i-1;j>=0;j--){
-                if(startEndProfit[i][0]>=startEndProfit[j][1]){
-                    profitAt[i] = Math.max(profitAt[i], startEndProfit[i][2]+profitAt[j]);
+        for (int i = 1; i < n; i++) {
+            profitAt[i] = Math.max(startEndProfit[i][2], profitAt[i - 1]);
+            for (int j = i - 1; j >= 0; j--) {
+                if (startEndProfit[i][0] >= startEndProfit[j][1]) {
+                    profitAt[i] = Math.max(profitAt[i], startEndProfit[i][2] + profitAt[j]);
                     break;
                 }
             }
-            result = Math.max(profitAt[i],result);
+            result = Math.max(profitAt[i], result);
         }
-        // System.out.println(Arrays.toString(profitAt));
         return result;
     }
 }
