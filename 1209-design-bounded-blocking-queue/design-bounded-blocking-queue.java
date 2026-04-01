9ed58a3055f7cc23a7624c9d3ecc86b8cbe9b98b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-01 13:39:09 +0530

    Time: 10 ms (47.47%) | Memory: 44 MB (98.74%) - LeetSync

--- 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.java
+++ 1209-design-bounded-blocking-queue/design-bounded-blocking-queue.java
@@ -1,7 +1,7 @@
-class BoundedBlockingQueue {
+class BoundedBlockingQueueInitial {
     int capacity;
     Queue<Integer> queue = new LinkedList<>();
-    public BoundedBlockingQueue(int capacity) {
+    public BoundedBlockingQueueInitial(int capacity) {
         this.capacity = capacity;
     }
     
@@ -22,5 +22,58 @@ class BoundedBlockingQueue {
     
     public synchronized int size() {
         return queue.size();
+    }
+}
+
+class BoundedBlockingQueue {
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition full = lock.newCondition();
+    private Condition empty = lock.newCondition();
+
+    private int[] queue;
+    private int tail = 0;
+    private int head = 0;
+    private int size = 0;
+
+    public BoundedBlockingQueue(int capacity){
+        queue = new int[capacity];
+    }
+
+    public void enqueue(int element) throws InterruptedException{
+        lock.lock();
+        try{
+            while(size == queue.length){
+                full.await();
+            }
+            queue[tail++] = element;
+            tail%=queue.length;
+            size++;
+            empty.signal();
+        }finally{
+            lock.unlock();
+        }
+    }
+
+    public int dequeue() throws InterruptedException{
+        lock.lock();
+        try{
+            while(size==0) empty.await();
+            int res = queue[head++];
+            head %= queue.length;
+            size--;
+            full.signal();
+            return res;
+        }finally{
+            lock.unlock();
+        }
+    }
+
+    public int size() throws InterruptedException{
+        lock.lock();
+        try{
+            return this.size;
+        }finally{
+            lock.unlock();
+        }
     }
 }
