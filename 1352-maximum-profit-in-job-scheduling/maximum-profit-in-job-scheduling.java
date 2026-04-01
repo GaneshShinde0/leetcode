dc37deac74493c0e993e8bfce2f9eec202988953
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-01 15:55:24 +0530

    Time: 35 ms (55.55%) | Memory: 69 MB (57.41%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+    public int jobSchedulingInitial(int[] startTime, int[] endTime, int[] profit) {
         int n = startTime.length;
         int[][] startEndProfit = new int[n][3];
         for (int i = 0; i < n; i++) {
@@ -20,6 +20,42 @@ class Solution {
                 }
             }
             result = Math.max(profitAt[i], result);
+        }
+        return result;
+    }
+
+    
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int n = startTime.length;
+        int[][] startEndProfit = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            startEndProfit[i][0] = startTime[i];
+            startEndProfit[i][1] = endTime[i];
+            startEndProfit[i][2] = profit[i];
+        }
+        Arrays.sort(startEndProfit, (a, b) -> Integer.compare(a[1], b[1]));
+        int[] profitAt = new int[n];
+        profitAt[0] = startEndProfit[0][2];
+        int result = startEndProfit[0][2];
+        for (int i = 1; i < n; i++) {
+            profitAt[i] = Math.max(startEndProfit[i][2], profitAt[i - 1]);
+            int j = binarySearch(startEndProfit, i-1, startEndProfit[i][0]);
+            if(j!=-1) profitAt[i] = Math.max(profitAt[i], startEndProfit[i][2] + profitAt[j]);
+            result = Math.max(profitAt[i], result);
+        }
+        return result;
+    }
+
+    private int binarySearch(int[][] arr, int end, int targetStartTime){
+        int start = 0, result = -1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid][1]<=targetStartTime){
+                result = mid;
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
         }
         return result;
     }
