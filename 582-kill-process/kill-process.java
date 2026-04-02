dd47c6159c1a9b5c229c182bc74fb854dac69125
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-03 01:45:20 +0530

    Time: 25 ms (82.93%) | Memory: 55.5 MB (61.46%) - LeetSync

--- 582-kill-process/kill-process.java
+++ 582-kill-process/kill-process.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
-        HashMap<Integer, List<Integer>> hm = new HashMap<>();
-        for(int i=0;i<ppid.size();i++){
-            hm.computeIfAbsent(ppid.get(i),k->new ArrayList<Integer>()).add(pid.get(i));
+        HashMap<Integer, List<Integer>> parentToChild = new HashMap<>();
+        int i=0;
+        for(int parentId:ppid){
+            parentToChild.computeIfAbsent(parentId, k-> new ArrayList<Integer>()).add(pid.get(i));
+            i++;
         }
-        // System.out.println(hm);
-        List<Integer> res = new ArrayList<>();
-        dfs(hm, res, kill);
-        return res;
+        List<Integer> result = new ArrayList<>();
+        dfs(parentToChild,kill,result);
+        return result;
     }
-    private void dfs(HashMap<Integer, List<Integer>> hm, List<Integer> res, int kill){
-        res.add(kill);
-        if(!hm.containsKey(kill)) return;
-        for(int i:hm.get(kill)){
-            dfs(hm,res,i);
+    private void dfs(HashMap<Integer, List<Integer>> parentToChild, int currentKill, List<Integer> result){
+        result.add(currentKill);
+        if(!parentToChild.containsKey(currentKill)) return;
+        for(Integer child:parentToChild.get(currentKill)){
+            dfs(parentToChild,child,result);
         }
-    } 
+    }
 }
