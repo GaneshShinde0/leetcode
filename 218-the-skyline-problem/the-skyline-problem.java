9a5f9ab8d38ece32c94f67d9f708bf22982394bc
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-04 01:24:36 +0530

    Time: 39 ms (59.13%) | Memory: 53.5 MB (86.92%) - LeetSync

---
+++ 218-the-skyline-problem/the-skyline-problem.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        List<List<Integer>> result = new ArrayList<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            if(a[0]==b[0]) return Integer.compare(a[1],b[1]);
+            else return Integer.compare(a[0],b[0]);
+        });
+        for(int[] building:buildings){
+            pq.add(new int[]{building[0],building[2],0});
+            pq.add(new int[]{building[1],building[2],1});
+        }
+
+        TreeMap<Integer,Integer> queue = new TreeMap<>();
+        queue.put(0,1);
+        int prevMaxHeight = 0;
+        while(!pq.isEmpty()){
+            
+            int currentX = pq.peek()[0];
+
+            while(!pq.isEmpty() && pq.peek()[0]==currentX){
+                int[] curr = pq.poll();
+                if(curr[2]==0){
+                    queue.put(curr[1],queue.getOrDefault(curr[1],0)+1);
+                }else{
+                    queue.put(curr[1],queue.getOrDefault(curr[1],0)-1);
+                    if(queue.get(curr[1])==0) queue.remove(curr[1]);
+                }
+            }
+            int currMaxHeight = queue.lastKey();
+            if(prevMaxHeight!=currMaxHeight){
+                result.add(List.of(currentX,currMaxHeight));
+                prevMaxHeight = currMaxHeight;
+            }
+        }
+        return result;
+    }
+}
