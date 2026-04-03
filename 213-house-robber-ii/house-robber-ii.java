81978c967a211fcd837d70f4021b52053301571f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-03 23:07:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (14.68%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,5 +1,11 @@
+/*
+This is extension of house robber problem. Only difference is that the first and last houses are adjacent to each other and therefore, if the thief has robbed the first house, they cannot steal the last house and vice versa. 
+Houses are arranged in circular order.
+
+The problem becomes to rob either house1 to house n-1 or house2 to house n. -> Whatever offers more money.
+*/
 class Solution {
-    public int rob(int[] nums) {
+    public int robInitial(int[] nums) {
         int n = nums.length;
         if(n==1) return nums[0];
         int[] dp = new int[n];
@@ -17,5 +23,29 @@ class Solution {
             dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
         }
         return Math.max(res1,dp[n-1]);
+    }
+
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int max1 = rob_simple(nums, 0, nums.length - 2);
+        int max2 = rob_simple(nums, 1, nums.length - 1);
+
+        return Math.max(max1, max2);
+    }
+
+    public int rob_simple(int[] nums, int start, int end) {
+        int t1 = 0;
+        int t2 = 0;
+
+        for (int i = start; i <= end; i++) {
+            int temp = t1;
+            int current = nums[i];
+            t1 = Math.max(current + t2, t1);
+            t2 = temp;
+        }
+
+        return t1;
     }
 }
