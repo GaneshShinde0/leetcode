2e3ada3f596cfea1a2f5bb573500adb179b5196c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-04 09:24:32 +0530

    Time: 38 ms (26.04%) | Memory: 57.1 MB (94.08%) - LeetSync

--- 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -1,13 +1,13 @@
 class Solution {
     public String decodeCiphertext59ms(String encodedText, int rows) {
         int n = encodedText.length();
-        int skip = n/rows;
+        int cols = n/rows;
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<skip;i++){
+        for(int i=0;i<cols;i++){
             int curr = i;
             while(curr<n){
                 sb.append(encodedText.charAt(curr));
-                curr+=skip+1;
+                curr+=cols+1;
             }
         }
         while(sb.length()>0 && sb.charAt(sb.length()-1)==' '){
@@ -17,13 +17,13 @@ class Solution {
     }
     public String decodeCiphertext40ms(String encodedText, int rows) {
         int n = encodedText.length();
-        int skip = n/rows;
+        int cols = n/rows;
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<skip;i++){
+        for(int i=0;i<cols;i++){
             int curr = i;
             while(curr<n){
                 sb.append(encodedText.charAt(curr));
-                curr+=skip+1;
+                curr+=cols+1;
             }
         }
         return sb.toString().stripTrailing();
@@ -31,14 +31,14 @@ class Solution {
 
     public String decodeCiphertext36ms(String encodedText, int rows) {
         int n = encodedText.length();
-        int skip = n/rows;
+        int cols = n/rows;
         char[] encodedTextArr = encodedText.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<skip;i++){
+        for(int i=0;i<cols;i++){
             int curr = i;
             while(curr<n){
                 sb.append(encodedTextArr[curr]);
-                curr+=skip+1;
+                curr+=cols+1;
             }
         }
         return sb.toString().stripTrailing();
@@ -46,14 +46,14 @@ class Solution {
 
     public String decodeCiphertext(String encodedText, int rows) {
         int n = encodedText.length();
-        int skip = n/rows;
+        int cols = n/rows;
         char[] encodedTextArr = encodedText.toCharArray();
         StringBuilder sb = new StringBuilder(n);
-        for(int i=0;i<skip;i++){
+        for(int i=0;i<cols;i++){
             int curr = i;
             while(curr<n){
                 sb.append(encodedTextArr[curr]);
-                curr+=skip+1;
+                curr+=cols+1;
             }
         }
         int i = sb.length() - 1;
