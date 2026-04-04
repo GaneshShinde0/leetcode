a4c12a5ed8d45fde90627e5470d8854318c0d1af
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-04 09:15:00 +0530

    Time: 59 ms (7.10%) | Memory: 57.9 MB (72.78%) - LeetSync

---
+++ 2197-decode-the-slanted-ciphertext/decode-the-slanted-ciphertext.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String decodeCiphertext(String encodedText, int rows) {
+        int n = encodedText.length();
+        int skip = n/rows;
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<skip;i++){
+            int curr = i;
+            while(curr<n){
+                sb.append(encodedText.charAt(curr));
+                curr+=skip+1;
+            }
+        }
+        while(sb.length()>0 && sb.charAt(sb.length()-1)==' '){
+            sb.deleteCharAt(sb.length()-1);
+        }
+        return sb.toString();
+    }
+}
