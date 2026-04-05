922d8ecfd9c037cdd848d3d8ad7db362986b6286
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-06 00:32:34 +0530

    Time: 1 ms (99.84%) | Memory: 55.7 MB (40.29%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,8 @@
 class Solution {
+    // int[] temp = new int[100000000];
     public int majorityElement(int[] nums) {
         int count = 1, element = nums[0];
-        int[] temp = new int[10000000];
+        //int[] temp = new int[10000000]; // Can Do until 10^7
         for(int i=1;i<nums.length;i++){
             if(nums[i]==element)count++;
             else{
