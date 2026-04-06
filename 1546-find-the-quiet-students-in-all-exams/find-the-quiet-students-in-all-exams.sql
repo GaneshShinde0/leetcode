5ae81fc95b5f79bb5481c628a9fcb4287041e3ce
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-06 20:09:01 +0530

    Time: 449 ms (22.22%) | Memory: 0B (100.00%) - LeetSync

--- 1546-find-the-quiet-students-in-all-exams/find-the-quiet-students-in-all-exams.sql
+++ 1546-find-the-quiet-students-in-all-exams/find-the-quiet-students-in-all-exams.sql
@@ -2,8 +2,7 @@
 WITH MAX_MIN_SCORES AS(
     SELECT exam_id, max(score) as max, min(score) as min  FROM Exam GROUP BY exam_id
 ),
-EDGE_STUDENTS AS (SELECT s.student_id, s.student_name FROM Student s LEFT JOIN EXAM e on s.student_id = e.student_id
-INNER JOIN MAX_MIN_SCORES mms on e.exam_id = mms.exam_id
-WHERE (score=max or score=min )) 
-SELECT * FROM Student where student_id not in (select student_id from edge_students) and 
-student_id in (select distinct student_id from Exam);
+EDGE_STUDENTS as (SELECT distinct s.student_id, s.student_name FROM Student s LEFT JOIN EXAM e on s.student_id = e.student_id
+left JOIN MAX_MIN_SCORES mms on e.exam_id = mms.exam_id
+WHERE (score=max or score=min ) or e.student_id is null)
+SELECT * FROM Student WHERE student_id not in (SELECT student_id from EDGE_STUDENTS)
