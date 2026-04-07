1f8c896b8bc793db164b212a528919d9960f1eb8
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-07 09:54:55 +0530

    Time: 18 ms (5.19%) | Memory: 61.2 MB (7.79%) - LeetSync

--- 1200-remove-interval/remove-interval.java
+++ 1200-remove-interval/remove-interval.java
@@ -1,6 +1,20 @@
 class Solution {
-    public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
+    public List<List<Integer>> removeIntervalInitial(int[][] intervals, int[] toBeRemoved) {
         Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
+        List<List<Integer>> result = new ArrayList<>();
+        for(int[] interval:intervals){
+            if((interval[0]<toBeRemoved[0] && interval[1]>toBeRemoved[1])){
+                result.add(List.of(interval[0],toBeRemoved[0]));
+                result.add(List.of(toBeRemoved[1],interval[1]));
+            }else if(interval[0]>=toBeRemoved[0] && interval[1]<=toBeRemoved[1]) continue;
+            else if(interval[1]<=toBeRemoved[0]|| interval[0]>=toBeRemoved[1]) result.add(toList(interval));
+            else if(interval[0]<toBeRemoved[0] && interval[1]>=toBeRemoved[0]) result.add(List.of(interval[0],toBeRemoved[0]));
+            else if(interval[0]>=toBeRemoved[0] && interval[1]>toBeRemoved[1]) result.add(List.of(toBeRemoved[1],interval[1]));
+        }
+        return result;
+    }
+    public List<List<Integer>> removeInterval(int[][] intervals, int[] toBeRemoved) {
+        // Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
         List<List<Integer>> result = new ArrayList<>();
         for(int[] interval:intervals){
             if((interval[0]<toBeRemoved[0] && interval[1]>toBeRemoved[1])){
