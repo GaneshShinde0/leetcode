f2356ad2a37f212d7ed785d6096da298aa7d886a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-07 09:25:20 +0530

    Time: 61 ms (77.62%) | Memory: 57.9 MB (69.93%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
@@ -1,29 +1,31 @@
 class Robot{
-    private boolean moved = false;
-    private int idx = 0;
-    private List<int[]> pos = new ArrayList<>();
-    private List<Integer> dir = new ArrayList<>();
-    private Map<Integer, String> toDir = new HashMap<>();
-
+    int idx = 0;
+    boolean isMoved = false;
+    Map<Integer,String> edgeToDirs;
+    List<Integer> dir;
+    List<int[]> pos;
     public Robot(int width, int height){
-        toDir.put(0,"East");
-        toDir.put(1,"North");
-        toDir.put(2,"West");
-        toDir.put(3,"South");
+        this.edgeToDirs = new HashMap<>();
+        this.dir = new ArrayList<>();
+        this.pos = new ArrayList<>();
 
+        edgeToDirs.put(0,"East");
+        edgeToDirs.put(1,"North");
+        edgeToDirs.put(2,"West");
+        edgeToDirs.put(3,"South");
         for(int i=0;i<width;i++){
             pos.add(new int[]{i,0});
             dir.add(0);
         }
         for(int i=1;i<height;i++){
-            pos.add(new int[]{width-1,i});
             dir.add(1);
+            pos.add(new int[]{width-1,i});
         }
         for(int i=width-2;i>=0;i--){
-            pos.add(new int[]{i,height-1});
             dir.add(2);
+            pos.add(new int[]{i,height-1});
         }
-        for(int i= height-2;i>0;--i){
+        for(int i=height-2;i>=1;i--){
             pos.add(new int[]{0,i});
             dir.add(3);
         }
@@ -31,15 +33,15 @@ class Robot{
     }
 
     public void step(int num){
-        moved = true;
+        isMoved = true;
         idx = (idx+num)%pos.size();
     }
     public int[] getPos(){
         return pos.get(idx);
     }
     public String getDir(){
-        if(!moved) return "East";
-        return toDir.get(dir.get(idx));
+        if(!isMoved) return "East";
+        return edgeToDirs.get(dir.get(idx));
     }
 }
 
