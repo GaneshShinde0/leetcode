8dd87b36b5648cdc4ea34b0ced803ae1d315dc5a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-07 15:45:11 +0530

    Time: 38 ms (38.40%) | Memory: 93.8 MB (43.71%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int longestSubarray(int[] nums, int limit) {
+    public int longestSubarrayInitial(int[] nums, int limit) {
         int left = 0, n = nums.length;
         int result = 1;
         TreeMap<Integer, Integer> treeMap = new TreeMap<>();
@@ -14,6 +14,28 @@ class Solution {
                 left++;
             }
             result = Math.max(result, right - left + 1);
+        }
+        return result;
+    }
+
+    public int longestSubarray(int[] nums, int limit){
+        Deque<Integer> maxDeque = new LinkedList<>();
+        Deque<Integer> minDeque = new LinkedList<>();
+        int left = 0, n = nums.length;
+        int result = 1;
+        for(int right = 0; right < nums.length; right++){
+            while(!maxDeque.isEmpty() && maxDeque.peekLast()<nums[right]) maxDeque.pollLast();
+            maxDeque.offerLast(nums[right]);
+
+            while(!minDeque.isEmpty() && minDeque.peekLast()>nums[right]) minDeque.pollLast();
+            minDeque.offerLast(nums[right]);
+
+            while(maxDeque.peekFirst() - minDeque.peekFirst() > limit){
+                if(maxDeque.peekFirst() == nums[left]) maxDeque.pollFirst();
+                if(minDeque.peekFirst() == nums[left]) minDeque.pollFirst();
+                left++;
+            }
+            result = Math.max(result, right-left+1);
         }
         return result;
     }
