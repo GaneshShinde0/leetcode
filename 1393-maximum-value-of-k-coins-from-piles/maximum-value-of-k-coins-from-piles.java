a29331c1366b22cd225507a6330fd99d2786c2e9
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-07 23:04:58 +0530

    Time: 188 ms (67.14%) | Memory: 281.6 MB (5.00%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.java
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.java
@@ -1,23 +1,27 @@
 class Solution {
     int[][] dp = new int[1001][2001];
+
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
         // DP
         int n = piles.size();
-        return checkRemainingPiles(n-1,k,piles);
+        return checkRemainingPiles(n - 1, k, piles);
     }
 
-    private int checkRemainingPiles(int currentPile, int coinsLeft, List<List<Integer>> piles){
-        if(currentPile<0||coinsLeft==0) return 0;
+    private int checkRemainingPiles(int currentPile, int coinsLeft, List<List<Integer>> piles) {
+        if (currentPile < 0 || coinsLeft == 0)
+            return 0;
         int ans = dp[currentPile][coinsLeft];
-        if(ans!=0) return ans;
+        if (ans != 0)
+            return ans;
         int sumOfValuesFromTop = 0;
         List<Integer> currPileList = piles.get(currentPile);
         int currPileSize = currPileList.size();
-        for(int i=0;i<=currPileSize && i<=coinsLeft; i++){
-            ans = Math.max(ans, checkRemainingPiles(currentPile-1,coinsLeft-i,piles)+sumOfValuesFromTop);
-            if(i<currPileSize) sumOfValuesFromTop +=currPileList.get(i);
+        for (int i = 0; i <= currPileSize && i <= coinsLeft; i++) {
+            ans = Math.max(ans, checkRemainingPiles(currentPile - 1, coinsLeft - i, piles) + sumOfValuesFromTop);
+            if (i < currPileSize)
+                sumOfValuesFromTop += currPileList.get(i);
         }
-        dp[currentPile][coinsLeft]=ans;
+        dp[currentPile][coinsLeft] = ans;
         return ans;
     }
 }
