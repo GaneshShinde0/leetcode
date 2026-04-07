290d6cbb668df4f7a9b8293e31514f0ece59e4b4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-07 14:30:29 +0530

    Time: 821 ms (20.24%) | Memory: 105.9 MB (30.83%) - LeetSync

---
+++ 833-bus-routes/bus-routes.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        if(source==target) return 0;
+        HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
+        for(int i=0;i<routes.length;i++){
+            for(int stop:routes[i]){
+                // All the routes (or buses) that have this stop 
+                ArrayList<Integer> route = graph.getOrDefault(stop,new ArrayList<>());
+                route.add(i);
+                graph.put(stop, route);
+            }
+        }
+
+        if(graph.get(source)==null||graph.get(target)==null) return -1;
+
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> vis = new HashSet<Integer>();
+        
+        for(int route:graph.get(source)){
+            queue.add(route);
+            vis.add(route);
+        }
+
+        int busCount = 1;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                int route = queue.poll();
+                for(int stop:routes[route]){
+                    if(stop==target) return busCount;
+                    for(int nextRoute: graph.get(stop)){
+                        if(!vis.contains(nextRoute)){
+                            vis.add(nextRoute);
+                            queue.add(nextRoute);
+                        }
+                    }
+                }
+            }
+            busCount++;
+        }
+        return -1;
+    }
+}
