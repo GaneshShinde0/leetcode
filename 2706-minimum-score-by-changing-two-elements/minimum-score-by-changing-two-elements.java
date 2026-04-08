1d386a7c7f42ea92540cd969215fc0d989046084
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-08 10:02:27 +0530

    Time: 3 ms (99.12%) | Memory: 59.5 MB (96.49%) - LeetSync

--- 2706-minimum-score-by-changing-two-elements/minimum-score-by-changing-two-elements.java
+++ 2706-minimum-score-by-changing-two-elements/minimum-score-by-changing-two-elements.java
@@ -10,7 +10,7 @@ result = 6
 change  1 to 4
 
 */
-class Solution {
+class SolutionInitial {
     public int minimizeSum(int[] nums) {
         int n = nums.length;
         if(n==3) return 0;
@@ -22,7 +22,41 @@ class Solution {
         return maxMin;
     }
 }
+class Solution {
+    public int minimizeSum(int[] nums) {
+       int max1=Integer.MIN_VALUE;
+       int max2=Integer.MIN_VALUE;
+       int max3=Integer.MIN_VALUE;
+       int n=nums.length;
+       for(int i=0;i<n;i++){
+        if(nums[i]>max1){
+            max3=max2;
+            max2=max1;
+            max1=nums[i];
+        }
+        else if(nums[i]>max2){
+            max3=max2;
+            max2=nums[i];
+        }
+        else if(nums[i]>max3) max3=nums[i];
+       }
+       int min1=Integer.MAX_VALUE;
+       int min2=Integer.MAX_VALUE;
+       int min3=Integer.MAX_VALUE;
+       for(int i=0;i<n;i++){
+        if(nums[i]<min1){
+            min3=min2;
+            min2=min1;
+            min1=nums[i];
+        }
+        else if(nums[i]<min2){
+            min3=min2;
+            min2=nums[i];
+        }
+        else if(nums[i]<min3) min3=nums[i];
+       }
+       return Math.min(max3-min1,Math.min(max2-min2,max1-min3));
 
-/*
- 1 2 3 4 5 6
-*/
+        
+    }
+}
