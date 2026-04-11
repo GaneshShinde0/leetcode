b6c0e666a326ebe546eb196dd7ed061bb0c09f9d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-11 09:07:33 +0530

    Time: 8 ms (88.22%) | Memory: 44.5 MB (89.25%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,6 +1,6 @@
 class Solution {
     int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1}};
-    public int shortestPath(int[][] grid, int k) {
+    public int shortestPathInitial(int[][] grid, int k) {
         int m = grid.length, n = grid[0].length;
         boolean[][][] vis = new boolean[m][n][k+1];
         Queue<int[]> queue = new LinkedList<>();
@@ -31,6 +31,37 @@ class Solution {
                 if(grid[newI][newJ]==1) temp--;
                 if(temp<0)continue;
                 queue.add(new int[]{newI,newJ,d+1,temp});
+            }
+        }
+        return -1;
+    }
+
+    public int shortestPath(int[][] grid, int k) {
+
+        int m = grid.length, n = grid[0].length;
+        if (k >= m + n - 2) return m + n - 2;
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][][] vis = new boolean[m][n][k + 1];
+        queue.add(new int[]{0, 0, k, 0});
+        vis[0][0][k] = true;
+
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int r = curr[0], c = curr[1], remK = curr[2], dist = curr[3];
+            if (r == m - 1 && c == n - 1) {
+                return dist;
+            }
+
+            for (int[] dir : dirs) {
+                int nr = r + dir[0], nc = c + dir[1];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                    int nextK = remK - grid[nr][nc];
+                    if (nextK >= 0 && !vis[nr][nc][nextK]) {
+                        vis[nr][nc][nextK] = true;
+                        queue.add(new int[]{nr, nc, nextK, dist + 1});
+                    }
+                }
             }
         }
         return -1;
