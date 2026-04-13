11a23dfcc1d39690ca30f40ffec48c71805029ea
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-13 23:26:13 +0530

    Time: 7 ms (82.61%) | Memory: 47.2 MB (19.56%) - LeetSync

--- 87-scramble-string/scramble-string.java
+++ 87-scramble-string/scramble-string.java
@@ -5,6 +5,17 @@ class Solution {
         if(hashMap.containsKey(key)) return hashMap.get(key);
         int n = s1.length();
         if(s1.equals(s2)) return true;
+        int[] counts = new int[26];
+        for (int i = 0; i < n; i++) {
+            counts[s1.charAt(i) - 'a']++;
+            counts[s2.charAt(i) - 'a']--;
+        }
+        for (int count : counts) {
+            if (count != 0) {
+                hashMap.put(key, false);
+                return false;
+            }
+        }
         for(int i=1;i<s1.length();i++){
             boolean noSwap = isScramble(s1.substring(0,i),s2.substring(0,i)) && isScramble(s1.substring(i),s2.substring(i));
             boolean swap = isScramble(s1.substring(0,i), s2.substring(n-i)) && isScramble(s1.substring(i),s2.substring(0,n-i));
