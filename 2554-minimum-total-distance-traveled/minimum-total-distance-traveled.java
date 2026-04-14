821d943c36c5ac4ddd6429db837c3e129db060b7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-14 10:42:53 +0530

    Time: 159 ms (6.45%) | Memory: 75 MB (25.81%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.java
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.java
@@ -1,6 +1,61 @@
 class Solution {
 
-    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory){
+        Collections.sort(robot);
+        Arrays.sort(factory, Comparator.comparingInt(a->a[0]));
+        List<Integer> factoryPositions = flattonArray(factory);
+        int robotCount = robot.size();
+        int factoryCount = factoryPositions.size();
+        long[][] memo = new long[robotCount][factoryCount];
+        for(long[] row:memo) Arrays.fill(row,-1);
+        // Recursively calculate minimum distance with memoization
+        return calculateMinDistance(0,0, robot, factoryPositions, memo);
+    }
+
+    private long calculateMinDistance(int robotIdx, int factoryIdx, List<Integer> robots, List<Integer> factoryPositions, long[][] memo){
+        if(robotIdx==robots.size()) return 0;
+        if(factoryIdx == factoryPositions.size()) return (long) 1e12;
+        if(memo[robotIdx][factoryIdx]!=-1) return memo[robotIdx][factoryIdx];
+        // Option 1: Assign current robot to current factory.
+        long assign = Math.abs(robots.get(robotIdx)-factoryPositions.get(factoryIdx)) + calculateMinDistance(robotIdx+1,factoryIdx+1, robots, factoryPositions, memo);
+        long skip = calculateMinDistance(robotIdx, factoryIdx+1, robots, factoryPositions, memo);
+        memo[robotIdx][factoryIdx] = Math.min(assign, skip);
+        return memo[robotIdx][factoryIdx];
+    }
+
+    private List<Integer> flattonArray(int[][] arr){
+        List<Integer> flattenedList = new ArrayList<>();
+        for(int[] a: arr){
+            for(int i=0;i<a[1];i++){
+                flattenedList.add(a[0]);
+            }
+        }
+        return flattenedList;
+    }
+    public long minimumTotalDistanceRecursion(List<Integer> robot, int[][] factory){
+        Collections.sort(robot);
+        // Arrays.sort(facctory,(a,b)->Integer.compare(a[0],b[0]));
+        Arrays.sort(factory,Comparator.comparingInt(a->a[0]));
+        
+        // Flatten factory positions according to their capacities.
+        List<Integer> factoryPositions = new ArrayList<>();
+        for(int[] f:factory){
+            for(int i=0;i<f[1];i++){
+                factoryPositions.add(f[0]);
+            }
+        }
+        // Minimum distance starting from 0th robot and 0th factory position
+        return calculateMinDistance(0,0, robot, factoryPositions);
+    }
+    private long calculateMinDistance(int robotIdx, int factoryIdx, List<Integer> robot, List<Integer> factoryPositions){
+        if(robotIdx==robot.size()) return 0;
+        if(factoryIdx == factoryPositions.size()) return (long) 1e12;
+
+        long assign = Math.abs(robot.get(robotIdx)-factoryPositions.get(factoryIdx)) + calculateMinDistance(robotIdx+1, factoryIdx+1, robot, factoryPositions);
+        long skip = calculateMinDistance(robotIdx, factoryIdx+1, robot, factoryPositions);
+        return Math.min(assign, skip);
+    }
+    public long minimumTotalDistanceOptimized(List<Integer> robot, int[][] factory) {
         // Sort robots and factories by position
         Collections.sort(robot);
         Arrays.sort(factory, Comparator.comparingInt(a -> a[0]));
