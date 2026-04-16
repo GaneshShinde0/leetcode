63280abbb16fb6e2443f48a69b05dfbf9aaed8c6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-16 10:22:45 +0530

    Time: 179 ms (13.13%) | Memory: 204.1 MB (14.14%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.java
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<Integer> solveQueries(int[] nums, int[] queries) {
+    public List<Integer> solveQueriesInitial(int[] nums, int[] queries) {
         int n = nums.length;
         HashMap<Integer, TreeSet<Integer>> hm = new HashMap<>();
         for(int i=0;i<nums.length;i++){
@@ -25,6 +25,36 @@ class Solution {
             if(first!=pos && right == null) shortest = Math.min(shortest, n-pos+first);
             if(last!=pos && left == null) shortest = Math.min(shortest, pos+n-last);
             res.add(shortest);
+        }
+        return res;
+    }
+
+    public List<Integer> solveQueries(int[] nums, int[] queries) {
+        int n = nums.length;
+        HashMap<Integer, TreeSet<Integer>> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            hm.computeIfAbsent(nums[i],v->new TreeSet<Integer>()).add(i);
+        }
+        int k = queries.length;
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i=0;i<k;i++){
+            int pos = queries[i];
+            int curr = nums[queries[i]];
+            TreeSet<Integer> ts = hm.get(curr);
+            if(ts.size()<2){
+                res.add(-1);
+                continue;
+            }
+            Integer next = ts.higher(pos);
+            if (next == null) next = ts.first();
+
+            Integer prev = ts.lower(pos);
+            if (prev == null) prev = ts.last();
+
+            int distNext = (next > pos) ? (next - pos) : (n - pos + next);
+            int distPrev = (prev < pos) ? (pos - prev) : (pos + n - prev);
+
+            res.add(Math.min(distNext, distPrev));
         }
         return res;
     }
