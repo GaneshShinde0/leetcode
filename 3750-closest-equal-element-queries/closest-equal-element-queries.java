90e6a4b3bfb0c31c9482d629ecf31d702bbeae1c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-16 09:56:45 +0530

    Time: 207 ms (8.08%) | Memory: 204.4 MB (14.14%) - LeetSync

---
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> solveQueries(int[] nums, int[] queries) {
+        int n = nums.length;
+        HashMap<Integer, TreeSet<Integer>> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            hm.computeIfAbsent(nums[i],v->new TreeSet<Integer>()).add(i);
+        }
+        int k = queries.length;
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i=0;i<k;i++){
+            int pos = queries[i];
+            int curr = nums[queries[i]];
+            TreeSet<Integer> ts = hm.get(curr);
+            if(ts.size()<2){
+                res.add(-1);
+                continue;
+            }
+            Integer right = ts.ceiling(pos+1);
+            Integer left = ts.floor(pos-1);
+            Integer first = ts.first();
+            Integer last = ts.last();
+            int shortest = Integer.MAX_VALUE;
+            if(right != null) shortest = Math.min(shortest, right-pos);
+            if(left !=null) shortest = Math.min(shortest, pos-left);
+            if(first!=pos && right == null) shortest = Math.min(shortest, n-pos+first);
+            if(last!=pos && left == null) shortest = Math.min(shortest, pos+n-last);
+            res.add(shortest);
+        }
+        return res;
+    }
+}
