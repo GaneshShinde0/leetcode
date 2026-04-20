4a101883148f8530ddf556a5a0c105906c9bbf88
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-21 00:25:20 +0530

    Time: 3 ms (43.28%) | Memory: 43.4 MB (50.34%) - LeetSync

---
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.java
@@ -0,0 +1,47 @@
+class UnionFind{
+    int[] rank;
+    int[] parent;
+    UnionFind(int n){
+        this.rank = new int[n];
+        this.parent = new int[n];
+        for(int i=0;i<n;i++){
+            rank[i]=i;
+            parent[i]=i;
+        }
+    }
+
+    int find(int x){
+        if(parent[x]!=x) x= find(parent[x]);
+        return parent[x];
+    }
+
+    void union(int x, int y){
+        int xPar = find(x), yPar = find(y);
+        if(xPar==yPar){
+            return;
+        }else if(rank[xPar]<rank[yPar]){
+            parent[yPar]=xPar;
+        }else if(rank[yPar]<rank[xPar]){
+            parent[xPar]=yPar;
+        }else{
+            rank[xPar]++;
+            parent[yPar] = xPar;
+        }
+    }
+}
+
+class Solution {
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        UnionFind uf = new UnionFind(26);
+        for(int i=0;i<s1.length();i++){
+            int a = s1.charAt(i)-'a';
+            int b = s2.charAt(i)-'a';
+            uf.union(a,b);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<baseStr.length();i++){
+            sb.append((char)(uf.find(baseStr.charAt(i)-'a')+'a'));
+        }
+        return sb.toString();
+    }
+}
