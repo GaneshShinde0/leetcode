02454c382d4f485da8542f5e3cd36b345e1f520d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-21 12:48:29 +0530

    Time: 2 ms (90.79%) | Memory: 42.8 MB (96.50%) - LeetSync

--- 756-pour-water/pour-water.java
+++ 756-pour-water/pour-water.java
@@ -3,20 +3,25 @@ class Solution {
         while(volume>0){
             int index = k;
             int left = k-1;
+            // Some lower height might be present on left side.
             while(left>=0 && heights[left]<=heights[index]){
+                // Heights are equal lets check more on left
                 if(heights[left]==heights[index]){
                     left--;
-                }else{
+                }else{ // We can fill droplet probably here or on left.
                     index=left;
                     left--;
                 }
             }
             int right = k+1;
+            // Could not fill anything on left.
             if(index==k){
+                // Some lower height might be present on right side.
                 while(right<heights.length && heights[right]<=heights[index]){
+                    // Heights are equal lets check more on right.
                     if(heights[right]==heights[index]){
                         right++;
-                    }else{
+                    }else{ // We can fill droplet probably here or on right.
                         index = right;
                         right++;
                     }
