79a9c27a83d36448a466ec4b2414de31643f160f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-21 21:51:30 +0530

    Time: 6 ms (55.14%) | Memory: 43.9 MB (78.60%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.java
+++ 1503-reducing-dishes/reducing-dishes.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int maxSatisfaction(int[] satisfaction) {
+    public int maxSatisfactionInitial(int[] satisfaction) {
         int res = 0;
         int n = satisfaction.length;
         Arrays.sort(satisfaction);
@@ -11,6 +11,16 @@ class Solution {
                 k++;
             }
             res = Math.max(res, sum);
+        }
+        return res;
+    }
+
+    public int maxSatisfaction(int[] satisfaction){
+        Arrays.sort(satisfaction);
+        int res = 0, total = 0, n = satisfaction.length;
+        for(int i = n-1; i>=0 && satisfaction[i]+total>0;i--){
+            total += satisfaction[i];
+            res += total;
         }
         return res;
     }
