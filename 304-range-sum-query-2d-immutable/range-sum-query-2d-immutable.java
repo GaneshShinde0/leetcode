e58efd49e7f47f62b1d2363b7b6c48a4a32fdaf9
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-22 12:16:22 +0530

    Time: 102 ms (76.78%) | Memory: 141.8 MB (72.86%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,6 +1,6 @@
-class NumMatrix {
+class NumMatrixInitial {
     int[][] matrix1;
-    public NumMatrix(int[][] matrix) {
+    public NumMatrixInitial(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         this.matrix1 = new int[m][n];
@@ -41,3 +41,22 @@ class NumMatrix {
  * NumMatrix obj = new NumMatrix(matrix);
  * int param_1 = obj.sumRegion(row1,col1,row2,col2);
  */
+
+class NumMatrix {
+
+    private int[][] dp;
+    public NumMatrix(int[][] matrix){
+        int m = matrix.length, n = matrix[0].length;
+        if(m == 0 || n==0) return;
+        dp = new int[m+1][n+1];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i+1][j+1] = dp[i+1][j]+dp[i][j+1]+matrix[i][j]-dp[i][j];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2){
+        return dp[row2+1][col2+1] - dp[row1][col2+1] - dp[row2+1][col1]+dp[row1][col1];
+    }
+}
