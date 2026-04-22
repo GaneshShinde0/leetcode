0f0310c4fd6112ffb12d6715c586b7dfce54e4d7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-23 00:00:59 +0530

    Time: 106 ms (72.96%) | Memory: 55.4 MB (75.47%) - LeetSync

--- 1106-escape-a-large-maze/escape-a-large-maze.java
+++ 1106-escape-a-large-maze/escape-a-large-maze.java
@@ -1,41 +1,37 @@
 class Solution {
-    private int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+    int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+    int lowX = 0;
+    int lowY = 0;
+    private static final long MIL = 1_000_000;
+    long highX = MIL;
+    long highY = MIL;
     public boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
-        HashSet<String> blockedSet = new HashSet<>();
-        for(int[] block:blocked) blockedSet.add(block[0]+","+block[1]);
- 
-        return possiblePoints(source, target, blockedSet) && possiblePoints(target, source ,blockedSet);
+        Set<Long> blockedSet = new HashSet<>();
+        for(int[] block:blocked){
+            blockedSet.add(block[0]+MIL*block[1]);
+        }
+        return dfs(source, target, blockedSet) && dfs(target, source, blockedSet);
     }
-
-    private boolean possiblePoints(int[] source, int[] target, Set<String> blocked){
-        Set<String> hashSet = new HashSet<>();
+    public boolean dfs(int[] source, int[] target, Set<Long> blockedSet){
         Queue<int[]> queue = new LinkedList<>();
-        int xPosLim = 1_000_000;
-        int yPosLim = 1_000_000;
-        int xNegLim = Math.max(0,source[0]-200);
-        int yNegLim = Math.max(0,source[1]-200);
         queue.add(source);
+        Set<Long> visited = new HashSet<>();
+        visited.add(source[0]+MIL*source[1]);
         while(!queue.isEmpty()){
             int[] curr = queue.poll();
             int x = curr[0];
             int y = curr[1];
             for(int[] dir:dirs){
-                int newX = x+dir[0], newY=y+dir[1];
-                String currPoint = newX+","+newY;
-                if(newX<xNegLim||newY<yNegLim||newX>=xPosLim||newY>=yPosLim||hashSet.contains(currPoint)||blocked.contains(currPoint)) continue;
-                if(newX == target[0] && newY == target[1]) return true;
-                hashSet.add(currPoint);
+                int newX = x+dir[0];
+                int newY = y+dir[1];
+                long comb = newX+MIL*newY;
+                if(newX<lowX||newY<lowY||newX>=highX||newY>=highY||visited.contains(comb)||blockedSet.contains(comb)) continue;
+                if(newX == target[0] && newY==target[1]) return true;
                 queue.add(new int[]{newX,newY});
-                if(hashSet.size()==20000) return true;
+                visited.add(comb);
+                if(visited.size()>=20000) return true;
             }
         }
-        return hashSet.size()>=20000;
+        return false;
     }
 }
-
-/*
-(0,2)
-(0,1)
-(0,0),(1,0)
-
-*/
