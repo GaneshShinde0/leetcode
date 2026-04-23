7a0e6ae17495fa38ca162f07226e4e1c10d962ff
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-23 11:33:37 +0530

    Time: 3 ms (100.00%) | Memory: 43.7 MB (50.10%) - LeetSync

--- 284-peeking-iterator/peeking-iterator.java
+++ 284-peeking-iterator/peeking-iterator.java
@@ -3,34 +3,35 @@
 
 class PeekingIterator implements Iterator<Integer> {
     Iterator<Integer> iter;
-    Integer peekedValue;
+    Integer peekingValue;
 	public PeekingIterator(Iterator<Integer> iterator) {
+	    // initialize any member here.
         this.iter = iterator;
-        peekedValue=null;
+        this.peekingValue = null;
 	}
 	
     // Returns the next element in the iteration without advancing the iterator.
 	public Integer peek() {
-        if (peekedValue == null) {
-            peekedValue = iter.next();
+        if(peekingValue==null){
+            peekingValue = iter.next();
         }
-        return peekedValue;
+        return peekingValue;
 	}
 	
 	// hasNext() and next() should behave the same as in the Iterator interface.
 	// Override them if needed.
 	@Override
 	public Integer next() {
-        if(peekedValue != null){
-            Integer toReturn = peekedValue;
-            peekedValue = null;
+	    if(peekingValue!=null){
+            Integer toReturn = peekingValue;
+            peekingValue = null;
             return toReturn;
         }
-	    return iter.next();
+        return iter.next();
 	}
 	
 	@Override
 	public boolean hasNext() {
-	    return peekedValue != null ||  iter.hasNext();
+	    return peekingValue!=null || iter.hasNext();
 	}
 }
