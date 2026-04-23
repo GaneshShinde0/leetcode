181a8739df91395e3cb98548bc5069209a3166d7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-23 22:18:45 +0530

    Time: 2 ms (50.00%) | Memory: 46.4 MB (25.00%) - LeetSync

--- 2593-number-of-distinct-binary-strings-after-applying-operations/number-of-distinct-binary-strings-after-applying-operations.java
+++ 2593-number-of-distinct-binary-strings-after-applying-operations/number-of-distinct-binary-strings-after-applying-operations.java
@@ -1,5 +1,16 @@
 class Solution {
     private static final int MOD = 1_000_000_007;
+    public int countDistinctStringsInitial(String s, int k) {
+        int n = s.length()-k+1;
+        int ans = 1;
+        while(n>0){
+            ans = ans<<1;
+            n--;
+            ans%=MOD;
+        }
+        return ans;
+    }
+
     public int countDistinctStrings(String s, int k) {
         int n = s.length()-k+1;
         int ans = 1;
