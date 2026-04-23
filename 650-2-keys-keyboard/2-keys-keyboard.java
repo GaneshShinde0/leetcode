d8c8c8f79970f3a3ecbf4031fe035a05a9a86415
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-23 12:36:16 +0530

    Time: 32 ms (9.69%) | Memory: 42.1 MB (66.09%) - LeetSync

---
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -0,0 +1,30 @@
+/*
+1=>0
+2=>2
+3=>3
+4=>4
+5=>5
+6=>3+2=>5
+7=>
+*/
+
+class Solution {
+    public int minSteps(int n) {
+        if(n==1) return 0;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        for(int i=0;i<=n;i++){
+            if(i<6){
+                dp[i]=i;
+            }else{
+                dp[i]=i;
+                for(int j=2;j<i;j++){
+                    if(i%j==0){
+                        dp[i]=Math.min(dp[i],dp[j]+i/j);
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+}
