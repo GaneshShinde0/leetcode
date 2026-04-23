ecbeb241ed2a04d4acc12b63ddccf3c4ca5cdde5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-23 14:59:31 +0530

    Time: 715 ms (28.73%) | Memory: 106.3 MB (28.36%) - LeetSync

--- 993-tallest-billboard/tallest-billboard.py
+++ 993-tallest-billboard/tallest-billboard.py
@@ -1,5 +1,5 @@
 class Solution:
-    def tallestBillboard(self, rods: List[int]) -> int:
+    def tallestBillboardWorks(self, rods: List[int]) -> int:
         N = len(rods)
         INF = 10**10
         @cache
@@ -11,4 +11,31 @@ class Solution:
             return max(calc(index+1,total+rods[index])+rods[index],
             calc(index+1,total-rods[index]),
             calc(index+1,total))
+        return calc(0,0)
+
+    
+    def tallestBillboard(self, rods: List[int]) -> int:
+        N = len(rods)
+        max_sum = sum(rods)
+        delta = max_sum//2
+        INF = 10**10
+        
+        has_cache = [[False]*(max_sum+1) for _ in range(N)]
+        cache = [[None]*(max_sum+1) for _ in range(N)]
+        # index -> 0 to N
+        # total -> 0 to 5000
+        def calc(index, total):
+            if index ==N:
+                if(total==0):
+                    return 0
+                return -INF
+            if(total*2>max_sum):
+                return -INF   
+            if has_cache[index][total+delta]:
+                return cache[index][total+delta]    
+            has_cache[index][total+delta] = True    
+            cache[index][total+delta] =  max(calc(index+1,total+rods[index])+rods[index],
+            calc(index+1,total-rods[index]),
+            calc(index+1,total))
+            return cache[index][total+delta]
         return calc(0,0)
