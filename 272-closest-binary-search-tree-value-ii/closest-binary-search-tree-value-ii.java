62741f7a77c0f5c990636711a14ad8c915c4ed93
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-24 22:42:29 +0530

    Time: 6 ms (15.08%) | Memory: 46.7 MB (90.62%) - LeetSync

--- 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.java
+++ 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.java
@@ -13,6 +13,41 @@
  *     }
  * }
  */
+
+
+class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        Queue<Integer> treeNodes = new PriorityQueue<>((b,a)->Double.compare(Math.abs(a-target), Math.abs(b-target)));
+        inOrder(root, treeNodes, k);
+        return new ArrayList<>(treeNodes);
+    }
+
+    void inOrder(TreeNode root, Queue<Integer> treeNodes, int k){
+        if(root==null) return;
+        inOrder(root.left, treeNodes,k);
+        treeNodes.add(root.val);
+        if(treeNodes.size()>k) treeNodes.poll();
+        inOrder(root.right, treeNodes, k);
+    }
+}
+ 
+class SolutionOptimizeInitial {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> treeNodes = new ArrayList<>();
+        inOrder(root, treeNodes);
+        Collections.sort(treeNodes,(a,b)->{
+            return Double.compare(Math.abs(a-target), Math.abs(b-target));
+        });
+        return treeNodes.subList(0,k);
+    }
+
+    void inOrder(TreeNode root, List<Integer> treeNodes){
+        if(root==null) return;
+        inOrder(root.left, treeNodes);
+        treeNodes.add(root.val);
+        inOrder(root.right, treeNodes);
+    }
+}
 class SolutionInitial {
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
         List<Integer> treeNodes = new ArrayList<>();
@@ -41,21 +76,3 @@ class SolutionInitial {
     }
 }
 
-
-class Solution {
-    public List<Integer> closestKValues(TreeNode root, double target, int k) {
-        List<Integer> treeNodes = new ArrayList<>();
-        inOrder(root, treeNodes);
-        Collections.sort(treeNodes,(a,b)->{
-            return Double.compare(Math.abs(a-target), Math.abs(b-target));
-        });
-        return treeNodes.subList(0,k);
-    }
-
-    void inOrder(TreeNode root, List<Integer> treeNodes){
-        if(root==null) return;
-        inOrder(root.left, treeNodes);
-        treeNodes.add(root.val);
-        inOrder(root.right, treeNodes);
-    }
-}
