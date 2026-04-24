ad5a1a5e2ac45f85a7a8ebaf191a38d55e6d204f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-24 16:54:29 +0530

    Time: 4 ms (33.67%) | Memory: 47 MB (64.99%) - LeetSync

--- 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.java
+++ 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+class SolutionInitial {
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
         List<Integer> treeNodes = new ArrayList<>();
         inOrder(root, treeNodes);
@@ -31,6 +31,25 @@ class Solution {
         List<Integer> result = new ArrayList<>();
         while(!pq.isEmpty()) result.add(pq.poll());
         return result;
+    }
+
+    void inOrder(TreeNode root, List<Integer> treeNodes){
+        if(root==null) return;
+        inOrder(root.left, treeNodes);
+        treeNodes.add(root.val);
+        inOrder(root.right, treeNodes);
+    }
+}
+
+
+class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> treeNodes = new ArrayList<>();
+        inOrder(root, treeNodes);
+        Collections.sort(treeNodes,(a,b)->{
+            return Double.compare(Math.abs(a-target), Math.abs(b-target));
+        });
+        return treeNodes.subList(0,k);
     }
 
     void inOrder(TreeNode root, List<Integer> treeNodes){
