a729ec1ee77971266c91d5bdbd4f696344d018f9
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-24 22:49:04 +0530

    Time: 1 ms (90.95%) | Memory: 47 MB (64.99%) - LeetSync

--- 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.java
+++ 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.java
@@ -14,8 +14,28 @@
  * }
  */
 
+class Solution{
+    public List<Integer> closestKValues(TreeNode root, double target, int k){
+        Deque<Integer> queue = new LinkedList<>();
+        dfs(root, queue, k, target);
+        return new ArrayList<>(queue);
+    }
 
-class Solution {
+    public void dfs(TreeNode node, Deque<Integer> queue, int k, double target){
+        if(node == null) return;
+        dfs(node.left, queue, k, target);
+        queue.add(node.val);
+        if(queue.size()>k){
+            if(Math.abs(target-queue.peekFirst())<=Math.abs(target-queue.peekLast())){
+                queue.removeLast();
+            }else{
+                queue.removeFirst();
+            }
+        }
+        dfs(node.right, queue,k,target);
+    }
+}
+class SolutionOptimizeInitial2{
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
         Queue<Integer> treeNodes = new PriorityQueue<>((b,a)->Double.compare(Math.abs(a-target), Math.abs(b-target)));
         inOrder(root, treeNodes, k);
