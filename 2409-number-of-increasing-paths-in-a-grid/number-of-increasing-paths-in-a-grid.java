6f26de9a1b44e8cfe8c20cdb2cee35a94a612b6b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-26 19:59:29 +0530

    Time: 35 ms (49.04%) | Memory: 89.4 MB (70.38%) - LeetSync

--- 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.java
+++ 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.java
@@ -1,33 +1,31 @@
 class Solution {
-    long res;
     private static final long MOD = 1_000_000_007;
     int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
 
     public int countPaths(int[][] matrix) {
-        this.res = 0;
         int m = matrix.length, n = matrix[0].length;
-        int[][] maxPath = new int[m][n];
+        int[][] paths = new int[m][n];
         long totalPaths = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                totalPaths = (totalPaths + dfs(i, j, matrix, maxPath)) % MOD;
+                totalPaths = (totalPaths + dfs(i, j, matrix, paths)) % MOD;
             }
         }
         return (int) totalPaths;
     }
-    private int dfs(int i, int j, int[][] matrix, int[][] maxPath){
-        if(maxPath[i][j]>0){
-            return maxPath[i][j];
+    private int dfs(int i, int j, int[][] matrix, int[][] paths){
+        if(paths[i][j]>0){
+            return paths[i][j];
         }
-        maxPath[i][j] = 1;
+        paths[i][j] = 1;
         long neighbourSum = 1;
         for(int[] dir:dirs){
             int newI = i+dir[0];
             int newJ = j+dir[1];
             if(newI<0||newJ<0||newI>=matrix.length||newJ>=matrix[0].length || matrix[newI][newJ]<=matrix[i][j]) continue;
-            maxPath[newI][newJ] = dfs(newI, newJ, matrix, maxPath);
-            neighbourSum = (neighbourSum +maxPath[newI][newJ])%MOD;
+            paths[newI][newJ] = dfs(newI, newJ, matrix, paths);
+            neighbourSum = (neighbourSum +paths[newI][newJ])%MOD;
         }
-        return maxPath[i][j]=(int) neighbourSum;
+        return paths[i][j]=(int) neighbourSum;
     }
 }
