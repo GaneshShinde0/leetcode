50865999699cb415ed1dc91416327f4c724bd0af
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-26 22:47:41 +0530

    Time: 33 ms (20.61%) | Memory: 77.9 MB (7.29%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -1,5 +1,50 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if (n < 2) {
+            ArrayList<Integer> centroids = new ArrayList<>();
+            for (int i = 0; i < n; i++)
+                centroids.add(i);
+            return centroids;
+        }
+
+        ArrayList<Set<Integer>> neighbors = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            neighbors.add(new HashSet<Integer>());
+
+        for (int[] edge : edges) {
+            Integer start = edge[0], end = edge[1];
+            neighbors.get(start).add(end);
+            neighbors.get(end).add(start);
+        }
+
+        ArrayList<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            if (neighbors.get(i).size() == 1)
+                leaves.add(i);
+
+        // Trim the leaves until reaching the centroids
+        int remainingNodes = n;
+        while (remainingNodes > 2) {
+            remainingNodes -= leaves.size();
+            ArrayList<Integer> newLeaves = new ArrayList<>();
+
+            // remove the current leaves along with the edges
+            for (Integer leaf : leaves) {
+                // the only neighbor left for the leaf node
+                Integer neighbor = neighbors.get(leaf).iterator().next();
+
+                // remove the edge along with the leaf node
+                neighbors.get(neighbor).remove(leaf);
+                if (neighbors.get(neighbor).size() == 1)
+                    newLeaves.add(neighbor);
+            }
+            leaves = newLeaves;
+        }
+        return leaves;
+    }
+}
+class SolutionInitial {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
         if(edges.length==0) return List.of(0);
         int[] degrees = new int[n];
         Map<Integer, HashSet<Integer>> graph = new HashMap<>();
