4a99bcc822738b25970454d691c24f04cd7a2fb4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-26 23:35:08 +0530

    Time: 576 ms (77.78%) | Memory: 0B (100.00%) - LeetSync

--- 2484-compute-the-rank-as-a-percentage/compute-the-rank-as-a-percentage.sql
+++ 2484-compute-the-rank-as-a-percentage/compute-the-rank-as-a-percentage.sql
@@ -1,11 +1,6 @@
+/* Write your T-SQL query statement below */
 
-with cte as(
-    select student_id,department_id,
-    rank() over (partition by department_id order by mark desc) as rn,
-    count(*) over ( partition by department_id) as cnt
-    from Students
-)
-select student_id,department_id,
-case when cnt>1 then round((rn-1.0)/(cnt-1.0)*100,2) else 0.00 end as percentage
-
-from cte
+SELECT student_id,
+    department_id,
+    ISNULL(ROUND((drnk - 1)*100.0/NULLIF(cnt - 1, 0), 2), 0) AS percentage
+FROM (SELECT *, RANK() OVER(PARTITION BY department_id ORDER BY mark DESC) AS drnk, COUNT(*) OVER(PARTITION BY department_id) AS cnt FROM Students) AS S;
