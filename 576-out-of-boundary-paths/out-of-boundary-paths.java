2c9f7cf944e9d4af32a87c8a4ab4ecf84d2b3d63
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-27 14:22:27 +0530

    Time: 4 ms (46.38%) | Memory: 43.6 MB (87.00%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.java
+++ 576-out-of-boundary-paths/out-of-boundary-paths.java
@@ -1,4 +1,42 @@
 class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+    int maxMove,m,n;
+    int[][][] memo;
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        if(maxMove==0) return 0;
+        this.m = m;
+        this.n = n;
+        this.maxMove = maxMove;
+        this.memo = new int[m][n][maxMove+1];
+        for(int[][] arr:memo){
+            for(int[] a:arr) Arrays.fill(a,-1);
+        }
+        Stack<int[]> stack = new Stack<>();
+        stack.add(new int[]{startRow, startColumn, 0});
+        dfs(startRow,startColumn, 0);
+        return memo[startRow][startColumn][0];
+    }
+    private int dfs(int row, int col, int currMoves){
+        if(memo[row][col][currMoves]!=-1){
+            return memo[row][col][currMoves]%MOD;
+        }
+        int count = 0;
+        if(currMoves == maxMove) return 0;
+        for(int[] dir:dirs){
+            int newR = row+dir[0];
+            int newC = col+dir[1];
+            if(newR<0||newC<0||newR>=m||newC>=n){
+                count = (count+1)%MOD;
+                continue;
+            }
+            count = (count+dfs(newR,newC, currMoves+1))%MOD;
+        }
+        return memo[row][col][currMoves] = count;
+    }
+}
+class SolutionWorks {
     int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
     private static final int MOD = 1_000_000_007;
     int maxMove;
