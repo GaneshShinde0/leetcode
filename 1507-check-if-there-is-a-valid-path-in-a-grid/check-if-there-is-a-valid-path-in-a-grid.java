e5efd82534cf1269d06d8a2785e8cc00fc4073fb
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-27 09:46:46 +0530

    Time: 27 ms (47.26%) | Memory: 83.2 MB (94.97%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
@@ -1,4 +1,53 @@
 class Solution {
+    // Maps each street type (1-6) to its two allowed [row_diff, col_diff]
+    int[][][] pipeDirs = {
+        {}, // Placeholder for index 0
+        {{0, -1}, {0, 1}},  // Type 1: Left, Right
+        {{-1, 0}, {1, 0}},  // Type 2: Up, Down
+        {{0, -1}, {1, 0}},  // Type 3: Left, Down
+        {{0, 1}, {1, 0}},   // Type 4: Right, Down
+        {{0, -1}, {-1, 0}}, // Type 5: Left, Up
+        {{0, 1}, {-1, 0}}   // Type 6: Right, Up
+    };
+
+    public boolean hasValidPath(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        Stack<int[]> stack = new Stack<>();
+        
+        stack.push(new int[]{0, 0});
+        visited[0][0] = true;
+
+        while (!stack.isEmpty()) {
+            int[] curr = stack.pop();
+            int r = curr[0], c = curr[1];
+            if (r == m - 1 && c == n - 1) return true;
+
+            int type = grid[r][c];
+            for (int[] dir : pipeDirs[type]) {
+                int nr = r + dir[0], nc = c + dir[1];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
+                    // Check if the neighbor pipe has an opening pointing back to (r, c)
+                    if (canConnect(nr, nc, r, c, grid)) {
+                        visited[nr][nc] = true;
+                        stack.push(new int[]{nr, nc});
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean canConnect(int nr, int nc, int r, int c, int[][] grid) {
+        int neighborType = grid[nr][nc];
+        for (int[] dir : pipeDirs[neighborType]) {
+            if (nr + dir[0] == r && nc + dir[1] == c) return true;
+        }
+        return false;
+    }
+}
+class SolutionInitial{
     int[][] dirs = {{0,1},{0,-1},{-1,0},{1,0}};
     public boolean hasValidPath(int[][] grid) {
         int m = grid.length, n = grid[0].length;
