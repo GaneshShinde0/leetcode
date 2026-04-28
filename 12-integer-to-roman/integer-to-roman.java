47d780ed3451486f11a4abb9f75feed2e1c41b92
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-28 13:10:17 +0530

    Time: 7 ms (12.37%) | Memory: 46.6 MB (26.85%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,10 +1,9 @@
 class Solution {
     public String intToRoman(int num) {
-        String ones[]={"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        String tens[]= {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        String hundreds[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        String thousands[] = {"","M","MM","MMM"};
-        StringBuilder sb = new StringBuilder();
-        return sb.append(thousands[num/1000]).append(hundreds[(num%1000)/100]).append(tens[(num%100)/10]).append(ones[num%10]).toString();
+        String[] ones = {"","I","II","III","IV","V","VI","VII","VIII","IX","X"};
+        String[] tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC","C"};
+        String[] hundreds = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM","M"};
+        String[] thousands = {"","M","MM","MMM"};
+        return thousands[num/1000]+hundreds[(num%1000)/100]+tens[(num%100)/10]+ones[num%10];
     }
 }
