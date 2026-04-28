10b2901415445957f9f300999bd1ab2e8a19b778
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-28 14:19:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (86.40%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,33 +1,12 @@
 class Solution {
-    public int climbStairsUsingTwoVariables(int n) {
-        int n1 = 0, n2 =1;
-        int i=1;
-        if(n==1) return 1;
-        while(i<=n){
-            i++;
-            int temp = n2;
-            n2=n1+n2;
-            n1=temp;
+    public int climbStairs(int n) {
+        if(n<2) return n;
+        int t1 = 1, t2 = 2;
+        for(int i=2;i<n;i++){
+            int temp = t2;
+            t2 = t1+t2;
+            t1 = temp;
         }
-        return n2;
-    }
-    public int climbStairsDPArray(int n) {
-        int[] dp = new int[n+2];
-        dp[1] = 1;
-        for(int i = 2;i<=n+1;i++){
-            dp[i]=dp[i-2]+dp[i-1];
-        }
-        return dp[n+1];
-    }
-
-    public int climbStairs(int n){
-        int one=1;
-        int two =1;
-        for(int i=1;i<n;i++){
-            int temp = one;
-            one = one+two;
-            two = temp;
-        }
-        return one;
+        return t2;
     }
 }
