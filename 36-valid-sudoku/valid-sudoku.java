edfd1bf1d5e0f6cb74542cbed0fb768ddb3a569d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-28 14:16:27 +0530

    Time: 3 ms (54.71%) | Memory: 46.5 MB (58.28%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,27 +1,22 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        int n = board.length;
-        HashSet<Character>[] rows = new HashSet[n];
-        HashSet<Character>[] cols = new HashSet[n];
-        HashSet<Character>[] boxes = new HashSet[n];
-        for(int i=0;i<n;i++){
-            rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
+        List<Set<Character>> rows = new ArrayList<>(9);
+        List<Set<Character>> cols = new ArrayList<>(9);
+        List<Set<Character>> cell = new ArrayList<>(9);
+        for(int i=0;i<9;i++){
+            rows.add(new HashSet<Character>());
+            cols.add(new HashSet<Character>());
+            cell.add(new HashSet<Character>());
         }
-
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
                 if(board[i][j]=='.') continue;
-                char val = board[i][j];
-                int boxIndex = (i/3)*3+j/3;
-                if(rows[i].contains(val)||cols[j].contains(val)||boxes[boxIndex].contains(val)) return false;
-                rows[i].add(val);
-                cols[j].add(val);
-                boxes[boxIndex].add(val);
+                if(rows.get(i).contains(board[i][j])|| cols.get(j).contains(board[i][j])||  cell.get(3*(i/3)+(j/3)).contains(board[i][j])) return false;
+                rows.get(i).add(board[i][j]);
+                cols.get(j).add(board[i][j]);
+                cell.get(3*(i/3)+(j/3)).add(board[i][j]);
             }
         }
-
         return true;
     }
 }
