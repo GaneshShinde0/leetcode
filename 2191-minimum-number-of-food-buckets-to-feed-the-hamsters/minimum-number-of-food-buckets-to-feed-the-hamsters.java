7e740c76b912af912842170cebaa75eb801fe5c1
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-29 14:24:08 +0530

    Time: 8 ms (16.99%) | Memory: 46.9 MB (49.04%) - LeetSync

---
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumBuckets(String hamsters) {
+        int res = 0;
+        int n = hamsters.length();
+        char[] arr = hamsters.toCharArray();
+        for(int i=0;i<n;i++){
+            int prev = Math.max(0,i-1);
+            int next = Math.min(n-1,i+1);
+            if(arr[i]=='.'||arr[i]=='D') continue;
+            else if(arr[i]=='H' && arr[prev]=='H' && arr[next]=='H') return -1;
+            else if(arr[next]=='.'&&arr[prev]!='D'){
+                arr[next]='D';
+                res++;
+            }else if(arr[next]=='H' && arr[prev]=='.'){
+                arr[prev]='D';
+                res++;
+            }
+        }
+        return res;
+    }
+}
