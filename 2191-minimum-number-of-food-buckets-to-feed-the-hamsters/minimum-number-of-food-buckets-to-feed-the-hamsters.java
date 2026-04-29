ab9ffca7e3dddc086dc4d775d1ce6cf940471e0d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-29 15:10:23 +0530

    Time: 5 ms (56.73%) | Memory: 46.2 MB (98.72%) - LeetSync

--- 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.java
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.java
@@ -1,5 +1,34 @@
 class Solution {
     public int minimumBuckets(String hamsters) {
+        int n = hamsters.length();
+        int buckets = 0;
+        int lastBucketPos = -2; // Tracks the position of the last placed bucket
+
+        for (int i = 0; i < n; i++) {
+            if (hamsters.charAt(i) == 'H') {
+                if (lastBucketPos == i - 1) {
+                    continue;
+                }
+
+                // 2. Greedy choice: Try placing a bucket at i+1
+                if (i + 1 < n && hamsters.charAt(i + 1) == '.') {
+                    buckets++;
+                    lastBucketPos = i + 1; // Mark bucket position
+                } 
+                // 3. Forced choice: Try placing a bucket at i-1
+                else if (i - 1 >= 0 && hamsters.charAt(i - 1) == '.') {
+                    buckets++;
+                    lastBucketPos = i - 1; // Mark bucket position
+                } 
+                // 4. Impossible case
+                else {
+                    return -1;
+                }
+            }
+        }
+        return buckets;
+    }
+    public int minimumBucketsInitial(String hamsters) {
         int res = 0;
         int n = hamsters.length();
         char[] arr = hamsters.toCharArray();
