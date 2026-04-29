ef543f960030a170a99932bba7454806292b54a6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-29 14:05:44 +0530

    Time: 229 ms (33.58%) | Memory: 133 MB (8.03%) - LeetSync

--- 2075-brightest-position-on-street/brightest-position-on-street.java
+++ 2075-brightest-position-on-street/brightest-position-on-street.java
@@ -5,16 +5,15 @@ class Solution {
             low = Math.min(low, light[0]-light[1]);
             low = Math.min(low, light[0]+light[1]);
         }
-        int offSet = low;
         TreeMap<Integer,Integer> hm = new TreeMap<>();
         for(int[] light:lights){
-            int start = light[0]-light[1]-offSet;
-            int end = light[0]+light[1]-offSet+1;
+            int start = light[0]-light[1];
+            int end = light[0]+light[1]+1;
             hm.put(start, hm.getOrDefault(start,0)+1);
             hm.put(end, hm.getOrDefault(end,0)-1);
         }
 
-        int res = offSet, curr = 0, max=0;
+        int res = 0, curr = 0, max=0;
 
         for(Map.Entry<Integer,Integer> e:hm.entrySet()){
             curr+=e.getValue();
@@ -23,6 +22,6 @@ class Solution {
                 res = e.getKey();
             }
         }
-        return res+offSet;
+        return res;
     }
 }
