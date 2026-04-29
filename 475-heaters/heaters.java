13be127640f21c968e19f9383e0dd50abf956f3c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-04-29 11:59:11 +0530

    Time: 19 ms (71.43%) | Memory: 48.1 MB (33.06%) - LeetSync

--- 475-heaters/heaters.java
+++ 475-heaters/heaters.java
@@ -1,5 +1,26 @@
 class Solution {
     public int findRadius(int[] houses, int[] heater) {
+        Arrays.sort(houses);
+        Arrays.sort(heater);
+        int radius = 0;
+        for(int h:houses){
+            int left = 0, right = heater.length-1;
+            while(left<right){
+                int mid = (left+right)/2;
+                if(heater[mid]<h){
+                    left = mid+1;
+                }else{
+                    right = mid;
+                }
+            }
+            int dist = Math.abs(heater[left]-h);
+            if(left>0) dist = Math.min(dist, Math.abs(heater[left-1]-h));
+            radius = Math.max(radius, dist);
+        }
+        return radius;
+
+    }
+    public int findRadiusUsingTreeSet(int[] houses, int[] heater) {
         if(houses.length==0) return 0;
         TreeSet<Integer> set = new TreeSet<>();
         int res = 0;
