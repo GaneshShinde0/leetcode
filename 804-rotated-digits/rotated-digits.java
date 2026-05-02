1308526dffe42cf98c13de995372a1ea8e32d3ba
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-02 07:41:37 +0530

    Time: 4 ms (59.28%) | Memory: 42.8 MB (22.85%) - LeetSync

---
+++ 804-rotated-digits/rotated-digits.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    public int rotatedDigits(int n) {
+        int count = 0;
+        int[] dp = new int[n+1];
+        for(int i=0;i<=n;i++){
+            if(i<10){
+                if(i==2||i==5||i==6||i==9){
+                    count++;
+                    dp[i]=2;
+                }else if(i==0||i==1||i==8){
+                    dp[i]=1;
+                }else{
+                    dp[i]=0;
+                }
+            }else{
+                int a = dp[i/10];// Digits until last 
+                int b = dp[i%10];// Last digit
+                if(a==1 && b==1){
+                    dp[i]=1;
+                }else if(a>=1 && b>=1){
+                    dp[i]=2;
+                    count++;
+                }else{
+                    dp[i]=0;
+                }
+            }
+        }
+        return count;
+    }
+}
