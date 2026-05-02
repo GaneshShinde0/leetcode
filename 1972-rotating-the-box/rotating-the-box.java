d60ac82b67f13dd698da5ab91c30f41ae0d39f2a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-02 20:28:53 +0530

    Time: 6 ms (99.50%) | Memory: 124.8 MB (60.55%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -5,8 +5,8 @@ class Solution {
         for(char[] row:res) Arrays.fill(row,'.');
 
         for(int i=0;i<rows;i++){
-            int chooseRow = cols-1;
-            int chooseCol = rows-i-1;
+            int chooseRow = cols-1; // Only row will get changed
+            int chooseCol = rows-i-1; // Wont change.
             for(int j=cols-1;j>=0;j--){
                 if(grid[i][j]=='#'){
                     res[chooseRow][chooseCol]='#';
