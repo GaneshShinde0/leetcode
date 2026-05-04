7dcf3e8011aa0f8aeabed3c2555fc2c520d877a4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-04 08:59:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (51.87%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,79 +1,26 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        for(int[] arr: matrix){
-            reverse(arr);
-        }
+        for(int[] row:matrix) reverse(row);
         for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-i-1;j++){
+            for(int j=0;j<n-i;j++){
                 swap(matrix,i,j,n-j-1,n-i-1);
             }
         }
     }
-    private void swap(int[][] nums, int i, int j, int k, int l){
-        int temp = nums[i][j];
-        nums[i][j] = nums[k][l];
-        nums[k][l] = temp;
-    }
-    private void reverse(int[] nums){
-        int left = 0, right = nums.length-1;
-        while(left<right){
-            int temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++;
-            right--;
+    private void reverse(int[] arr){
+        int start = 0, end = arr.length-1;
+        while(start<end){
+            int temp = arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
         }
     }
-}
-
-/*
-Input: matrix = 
-[
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-
-[
-    [3,2,1],
-    [6,5,4],
-    [9,8,7]
-]
-
-0,1 -> 1,2
-
-Reverse All the rows.
-Flip the elements which are in middle positions
-
-
-Input: matrix = 
-[
-    [5,1,9,11],
-    [2,4,8,10],
-    [13,3,6,7],
-    [15,14,12,16]
-]
-
-After Reverse:
-[
-    [11,9,1,5],
-    [10,8,4,2],
-    [7,6,3,13],
-    [16,12,14,15]
-]
-
-Now Swap diagonals
-    i=1, j=1 ; k=2,l = 2;
-    i=0,j=2, k=4-2-1=1, l = 4-0-1=3
-    
-for(int i=0;i<m;i++){
-    for(int j=0;j<n-1;j++){
-        swap(nums,i,j,n-j-1,n-i-1);
+    private void swap(int[][] matrix, int i, int j, int newI, int newJ){
+        int temp = matrix[i][j];
+        matrix[i][j]=matrix[newI][newJ];
+        matrix[newI][newJ]=temp;
     }
 }
-
-
-This can also be done with transpose and reverse.
-
-*/
