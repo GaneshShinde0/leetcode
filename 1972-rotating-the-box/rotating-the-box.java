5638065be331356b4b259dbbd6bb0d281b46c2e5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-06 08:01:45 +0530

    Time: 6 ms (99.52%) | Memory: 124.9 MB (28.12%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,19 +1,18 @@
 class Solution {
-    public char[][] rotateTheBox(char[][] grid) {
-        int rows = grid.length, cols = grid[0].length;
-        char[][] res = new char[cols][rows];
-        for(char[] row:res) Arrays.fill(row,'.');
-
-        for(int i=0;i<rows;i++){
-            int chooseRow = cols-1; // Only row will get changed
-            int chooseCol = rows-i-1; // Wont change.
-            for(int j=cols-1;j>=0;j--){
-                if(grid[i][j]=='#'){
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m = boxGrid.length, n = boxGrid[0].length;
+        char[][] res = new char[n][m];
+        for(char[] arr:res) Arrays.fill(arr, '.');
+        for(int i=0;i<m;i++){
+            int chooseRow = n-1;
+            int chooseCol = m-i-1;
+            for(int j=n-1;j>=0;j--){// Row;
+                if(boxGrid[i][j]=='#'){
                     res[chooseRow][chooseCol]='#';
                     chooseRow--;
-                }else if(grid[i][j]=='*'){
-                    res[j][chooseCol]='*';
-                    chooseRow = j-1;
+                }else if(boxGrid[i][j]=='*'){
+                    res[j][chooseCol] = '*';
+                    chooseRow=j-1;
                 }
             }
         }
