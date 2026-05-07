b0177b4ee45b50c3033661766d6100822d91deb3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-07 10:18:32 +0530

    Time: 21 ms (20.97%) | Memory: 189.7 MB (91.94%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.java
+++ 3981-jump-game-ix/jump-game-ix.java
@@ -1,4 +1,7 @@
-class Solution {
+/*
+Approach 1: Interval Divide and Conquer
+*/
+class Solution1 {
     record Item(int value, int index){}
     public int[] maxValue(int[] nums) {
         int n = nums.length;
@@ -26,6 +29,29 @@ class Solution {
         }
         if(pivotIdx==0) return;
         process(pivotIdx-1,nextRightMin, currMax, maxFromLeft, ans,nums);
+    }
+}
 
+
+class Solution {
+    record Item(int value, int left, int right){}
+    public int[] maxValue(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        List<Item> stack = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            Item curr = new Item(nums[i],i,i);
+            while(!stack.isEmpty() && stack.getLast().value()>nums[i]){
+                Item top = stack.removeLast();
+                curr = new Item(Math.max(curr.value(), top.value()), top.left(), curr.right());
+            }
+            stack.add(curr);
+        }
+        for(int i=0;i<stack.size();i++){
+            for(int j=stack.get(i).left(); j<=stack.get(i).right(); j++){
+                ans[j]=stack.get(i).value();
+            }
+        }
+        return ans;
     }
 }
