60cfbe0edc4e7bb9959345e0bf0e8ee2f092492b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-08 23:52:57 +0530

    Time: 17 ms (30.53%) | Memory: 53.3 MB (29.34%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.java
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.java
@@ -10,6 +10,26 @@ class Solution {
             result.add(new ArrayList<>(curr));
         }
         for(int i = start; i<nums.length;i++){
+            if(curr.size()==0|| curr.get(curr.size()-1)<=nums[i]){
+                curr.add(nums[i]);
+                recurse(i+1,nums, curr, result);
+                curr.remove(curr.size()-1);
+            }
+        }
+    }
+}
+class SolutionInitial {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        recurse(0,nums,new ArrayList<>(), result);
+        return new ArrayList<>(result);
+    }
+
+    private void recurse(int start, int[] nums,List<Integer> curr, Set<List<Integer>> result){
+        if(start<=nums.length && curr.size()>=2){
+            result.add(new ArrayList<>(curr));
+        }
+        for(int i = start; i<nums.length;i++){
             boolean addedTwo = false;
             boolean addedOne = false;
             if(curr.size()==0 &&start<i && nums[i]>=nums[start]){
