f6a23230d4032ccf54d3021cc80a54ef9cc69737
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-09 23:46:32 +0530

    Time: 7 ms (98.51%) | Memory: 47.5 MB (98.09%) - LeetSync

--- 4131-minimum-flips-to-make-binary-string-coherent/minimum-flips-to-make-binary-string-coherent.java
+++ 4131-minimum-flips-to-make-binary-string-coherent/minimum-flips-to-make-binary-string-coherent.java
@@ -1,11 +1,21 @@
 class Solution {
+    public int minFlipsInitialInContest(String s) {
+        int count1 = 0,count0=0;
+        for(char c:s.toCharArray()){
+            if(c=='1')count1++;
+            else count0++;
+        }
+        if(count0==0) return 0;
+        if(count1>=2 && s.charAt(0)=='1' && s.charAt(s.length()-1)=='1') return Math.min(Math.max(0,count1-2),count0);
+        return Math.min(Math.max(0,count1-1),count0);
+    }
     public int minFlips(String s) {
         int count1 = 0,count0=0;
         for(char c:s.toCharArray()){
             if(c=='1')count1++;
             else count0++;
         }
-        if(count1>=2 && s.charAt(0)=='1' && s.charAt(s.length()-1)=='1') return Math.min(Math.max(0,count1-2),count0);
+        if(s.charAt(0)=='1' && s.charAt(s.length()-1)=='1') return Math.min(Math.max(0,count1-2),count0);
         return Math.min(Math.max(0,count1-1),count0);
     }
 }
