ad46ba4f9e4336d7dba924b9487986483aded37b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-11 22:43:06 +0530

    Time: 25 ms (62.50%) | Memory: 91.8 MB (50.00%) - LeetSync

---
+++ 3148-sum-of-remoteness-of-all-cells/sum-of-remoteness-of-all-cells.java
@@ -0,0 +1,39 @@
+class Solution {
+    private final int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+    int m,n;
+    int currVis;
+    public long sumRemoteness(int[][] grid) {
+        long totalSum = 0, result = 0;
+        this.m = grid.length;
+        this.n = grid[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]!=-1){
+                    totalSum+=grid[i][j];
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]!=-1){
+                    this.currVis = 0;
+                    result += (totalSum-dfs(grid,i,j))*this.currVis;
+                }
+            }
+        }
+        return result;
+    }
+
+    private long dfs(int[][] grid, int i, int j){
+        long sum=grid[i][j];
+        this.currVis++;
+        grid[i][j] = -1;
+        for(int[] dir:dirs){
+            int newI = i+dir[0];
+            int newJ = j+dir[1];
+            if(newI<0||newJ<0||newI>=m||newJ>=n||grid[newI][newJ]==-1) continue;
+            sum+=dfs(grid,newI,newJ);
+        }
+        return sum;
+    }
+}
