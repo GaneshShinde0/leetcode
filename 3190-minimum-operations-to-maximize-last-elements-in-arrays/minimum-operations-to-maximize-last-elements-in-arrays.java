a3196b8cff2c54dfe1e260ebb4f30a6f37f08ded
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-12 22:15:20 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (81.16%) - LeetSync

--- 3190-minimum-operations-to-maximize-last-elements-in-arrays/minimum-operations-to-maximize-last-elements-in-arrays.java
+++ 3190-minimum-operations-to-maximize-last-elements-in-arrays/minimum-operations-to-maximize-last-elements-in-arrays.java
@@ -1,4 +1,5 @@
-class Solution {
+
+class Solution1 {
     public int minOperations(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int count1 = 0;// When last elements are not swapped.
@@ -12,5 +13,42 @@ class Solution {
             if(n1>nums2[n-1]||n2>nums1[n-1]) count2++; // When last elements are swapped.
         }
         return Math.min(count1,count2);
+    }
+}
+class Solution {
+    public int minOperations(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int a = nums1[n-1];
+        int b = nums2[n-1];
+
+        int swap1 = 0;
+        int flag1 = 1;
+        // option1 : if we don't swap the last elements of both array
+        for(int i=0;i<n-1;i++){
+            if(nums1[i]<=a && nums2[i]<=b)
+                continue;
+            else if(nums2[i]<=a && nums1[i]<=b)
+                swap1++;
+            else{
+                flag1 = 0;
+                break;
+            }
+        }
+        // option2 : if we swap the last elements of both array, i.e. count2 = 1
+        int swap2 = 1;
+        int flag2 = 1;
+        for(int i=0;i<n-1;i++){
+            if(nums1[i]<=b && nums2[i]<=a)
+                continue;
+            else if(nums2[i]<=b && nums1[i]<=a)
+                swap2++;
+            else{
+                flag2 = 0;
+                break;
+            }
+        }
+        if(flag1==0 && flag2==0)
+            return -1;
+        return Math.min(swap1,swap2);
     }
 }
