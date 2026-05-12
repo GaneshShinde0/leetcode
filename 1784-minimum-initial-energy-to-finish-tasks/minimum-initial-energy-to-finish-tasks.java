0aca470d4dc722ac7e744748718045ee16366b54
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-12 08:27:25 +0530

    Time: 23 ms (88.46%) | Memory: 114 MB (29.81%) - LeetSync

--- 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.java
@@ -1,4 +1,15 @@
 class Solution {
+
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) -> a[1] - a[0] - (b[1] - b[0]));
+        int ans = 0;
+        for (int[] task : tasks) {
+            ans = Math.max(ans + task[0], task[1]);
+        }
+        return ans;
+    }
+}
+class SolutionInitial {
     public int minimumEffort(int[][] tasks) {
         int min = 0, max = 0;
         Arrays.sort(tasks,(a,b)->Integer.compare(a[1]-a[0],b[1]-b[0]));
