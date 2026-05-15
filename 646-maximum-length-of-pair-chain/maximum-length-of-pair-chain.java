99e6db648edee5e56758417bb49dba204ec0ed50
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-15 14:58:00 +0530

    Time: 9 ms (94.31%) | Memory: 47.1 MB (50.12%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.java
@@ -1,5 +1,17 @@
 class Solution {
     public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs,(a,b)-> Integer.compare(a[1],b[1]));
+        int curr = Integer.MIN_VALUE;
+        int res = 0;
+        for(int i=0;i<pairs.length;i++){
+            if(pairs[i][0]>curr){
+                res++;
+                curr = pairs[i][1];
+            }
+        }
+        return res;
+    }
+    public int findLongestChainInitial(int[][] pairs) {
         Arrays.sort(pairs,(a,b)-> Integer.compare(a[0],b[0]));
         int n = pairs.length;
         int[] dp = new int[n];
