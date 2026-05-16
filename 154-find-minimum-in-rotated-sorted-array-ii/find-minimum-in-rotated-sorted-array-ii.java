2346f314eead923b72800508a083880412184864
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-16 10:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (36.28%) - LeetSync

---
+++ 154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -0,0 +1,13 @@
+
+class Solution{
+    public int findMin(int[] nums){
+        int start = 0, end = nums.length-1;
+        while(start<end){
+            int mid = (start+end)/2;
+            if(nums[mid]<nums[end]) end = mid;
+            else if (nums[mid]>nums[end]) start = mid+1;
+            else end-=1;
+        }
+        return nums[start];
+    }
+}
