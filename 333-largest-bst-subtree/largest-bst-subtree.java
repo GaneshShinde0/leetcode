25031ffda2f3ddac4c9235dd0afeaa17480c1d0f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-17 15:04:20 +0530

    Time: 0 ms (100.00%) | Memory: 45.3 MB (53.24%) - LeetSync

--- 333-largest-bst-subtree/largest-bst-subtree.java
+++ 333-largest-bst-subtree/largest-bst-subtree.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+class Solution1 {
     private boolean isValidBST(TreeNode root){
         if(root==null) return true;
         int leftMax = findMax(root.left);
@@ -38,5 +38,32 @@ class Solution {
         if(root==null) return 0;
         if(isValidBST(root)) return countNodes(root);
         return Math.max(largestBSTSubtree(root.left), largestBSTSubtree(root.right));
+    }
+}
+
+class NodeValue{
+    public int maxNode, minNode, maxSize;
+    NodeValue(int minNode, int maxNode, int maxSize){
+        this.maxNode = maxNode;
+        this.minNode = minNode;
+        this.maxSize = maxSize;
+    }
+}
+
+class Solution{
+    public NodeValue largestBSTSubtreeHelper(TreeNode root){
+        if(root==null) return new NodeValue(Integer.MAX_VALUE, Integer.MIN_VALUE, 0);
+        NodeValue left= largestBSTSubtreeHelper(root.left);
+        NodeValue right= largestBSTSubtreeHelper(root.right);
+
+        if(left.maxNode<root.val && root.val<right.minNode){
+            return new NodeValue(Math.min(root.val,left.minNode), Math.max(root.val, right.maxNode), left.maxSize+right.maxSize+1);
+        }
+
+        return new NodeValue(Integer.MIN_VALUE, Integer.MAX_VALUE, Math.max(left.maxSize,right.maxSize));
+    }
+
+    public int largestBSTSubtree(TreeNode root){
+        return largestBSTSubtreeHelper(root).maxSize;
     }
 }
