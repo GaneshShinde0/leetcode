1fe9ad38865de1c137ef2207c885a63df886c1b1
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-17 06:14:29 +0530

    Time: 1 ms (99.73%) | Memory: 52.3 MB (40.50%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,15 +1,22 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
-        if(start<0|| start>=arr.length||arr[start]<0) {
-            return false;
-        }
-        if(arr[start]==0){
-            return true;
-        }
-
-        int jumps=arr[start];
-        arr[start]=-arr[start];
-
-        return canReach(arr,start + jumps)||canReach(arr,start - jumps);
+        return helper(arr,start);
+    }
+    public boolean helper(int[] arr, int start){
+        if(start<0||start>=arr.length || arr[start]==-1) return false;
+        if(arr[start]==0) return true;
+        int temp = arr[start];
+        arr[start] = -1;
+        return helper(arr,start+temp) || helper(arr,start-temp);
     }
 }
+/*
+arr = [4,2,3,0,3,1,2], start = 5;
+arr = [4,2,3,0,3,1,2], start = 5;
+temp = 1;
+arr = [4,2,3,0,3,-1,2], start = 4;
+temp = 3
+arr[]
+arr = [4,2,3,0,3,-1,2], start = 6;
+
+*/
