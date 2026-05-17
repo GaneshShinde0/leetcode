a00d09e87d853285ce2597fbf5fc593b8f965a35
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-17 07:53:49 +0530

    Time: 1 ms (93.44%) | Memory: 44.4 MB (68.52%) - LeetSync

--- 360-sort-transformed-array/sort-transformed-array.java
+++ 360-sort-transformed-array/sort-transformed-array.java
@@ -1,6 +1,39 @@
 class Solution {
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
         int n = nums.length;
+        int[] res = new int[n];
+        int left = 0, right = n - 1;
+        
+        int i = a >= 0 ? n - 1 : 0;
+        int diff = a >= 0 ? -1 : 1;
+        
+        while (left <= right) {
+            int leftCalc = calc(nums[left], a, b, c);
+            int rightCalc = calc(nums[right], a, b, c);
+            
+            boolean pickLeft = (a >= 0) ? (leftCalc > rightCalc) : (leftCalc < rightCalc);
+            
+            if (pickLeft) {
+                res[i] = leftCalc;
+                left++;
+            } else {
+                res[i] = rightCalc;
+                right--;
+            }
+            
+            i += diff; 
+        }
+        
+        return res;
+    }
+    
+    private int calc(int x, int a, int b, int c) {
+        return x * x * a + x * b + c;
+    }
+}
+class Solution1 {
+    public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+        int n = nums.length;
         int left = 0, right = n-1;
         int[] res = new int[n];
         int i = 0;
