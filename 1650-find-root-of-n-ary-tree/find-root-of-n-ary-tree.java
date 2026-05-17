8c2ddaabf3c612d46a585bf5ca896a7fb0534f2e
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-17 15:53:31 +0530

    Time: 20 ms (32.46%) | Memory: 54.2 MB (5.26%) - LeetSync

---
+++ 1650-find-root-of-n-ary-tree/find-root-of-n-ary-tree.java
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    
+    public Node() {
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val) {
+        val = _val;
+        children = new ArrayList<Node>();
+    }
+    
+    public Node(int _val,ArrayList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public Node findRoot(List<Node> tree) {
+        HashMap<Node,HashSet<Node>> childToParent = new HashMap<>();
+        for(Node node:tree){
+            for(Node child: node.children){
+                childToParent.computeIfAbsent(child, x->new HashSet<Node>()).add(node);
+            }
+        }
+        for(Node node:tree){
+            if(!childToParent.containsKey(node)) return node;
+        }
+        return null;
+    }
+}
