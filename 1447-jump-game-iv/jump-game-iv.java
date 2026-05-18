b2ef3de66b3706dae229f80035c8b47e453e5c38
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-18 08:47:04 +0530

    Time: 65 ms (29.82%) | Memory: 103.7 MB (9.15%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        int n = arr.length;
+        boolean[] vis = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+        HashMap<Integer,Set<Integer>> graph = new HashMap<>();
+        for(int i=0;i<n;i++){
+            graph.computeIfAbsent(arr[i],x->new HashSet<>()).add(i);
+        }
+        vis[0] = true;
+        queue.add(0);
+        int depth = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                int curr = queue.poll();
+                if(curr==n-1) return depth;
+                if(curr+1<n && !vis[curr+1]){
+                    vis[curr+1] = true;
+                    queue.add(curr+1);
+                }
+                if(curr-1>=0 && !vis[curr-1]){
+                    vis[curr-1] = true;
+                    queue.add(curr-1);
+                }
+                if(!graph.containsKey(arr[curr])) continue;
+                for(int jumps:graph.get(arr[curr])){
+                    if(!vis[jumps]){
+                        vis[jumps] = true;
+                        queue.add(jumps);
+                    }                    
+                }
+                graph.remove(arr[curr]);
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
