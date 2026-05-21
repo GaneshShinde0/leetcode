d24dd2091aeeb031f4da9229236d0a61f353746a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-22 00:36:56 +0530

    Time: 8 ms (12.12%) | Memory: 96.8 MB (38.18%) - LeetSync

---
+++ 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int sumOfBeauties(int[] nums) {
+        int n = nums.length;
+        int[] maxLeft = new int[n];
+        int[] minRight = new int[n];
+        Arrays.fill(minRight,Integer.MAX_VALUE);
+        int beautySum = 0;
+        for(int i=1;i<n;i++){
+            maxLeft[i] = Math.max(nums[i-1],maxLeft[i-1]);
+            minRight[n-i-1] = Math.min(nums[n-i],minRight[n-i]);
+        }
+        for(int i=1;i<n-1;i++){
+            if(maxLeft[i]<nums[i] && nums[i]<minRight[i]) beautySum+=2;
+            else if(nums[i-1]<nums[i] && nums[i]<nums[i+1]) beautySum+=1;
+        }
+        return beautySum;
+    }
+}
