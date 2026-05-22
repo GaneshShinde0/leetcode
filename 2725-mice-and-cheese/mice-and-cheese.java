6649d22fc17c245d3b151c23462d5a69be90279b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-22 23:58:49 +0530

    Time: 15 ms (92.80%) | Memory: 83.8 MB (26.70%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.java
+++ 2725-mice-and-cheese/mice-and-cheese.java
@@ -1,11 +1,13 @@
 class Solution {
     public int miceAndCheese(int[] reward1, int[] reward2, int k) {
         int res = 0, n = reward1.length, diff[] = new int[n];
+        // Take all from second array, meaning 
         for(int i=0;i<n;i++){
             diff[i] = reward1[i]-reward2[i];
             res+=reward2[i];
         }
         Arrays.sort(diff);
+        // Take k largest from the sequence and sum up.
         for(int i=0;i<k;i++){
             res+=diff[n-1-i];
         }
