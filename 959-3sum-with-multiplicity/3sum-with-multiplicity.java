bd633c2acaad0119293307eafb0b4960c44b2f23
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-23 14:42:46 +0530

    Time: 3 ms (71.22%) | Memory: 45.4 MB (72.30%) - LeetSync

--- 959-3sum-with-multiplicity/3sum-with-multiplicity.java
+++ 959-3sum-with-multiplicity/3sum-with-multiplicity.java
@@ -18,7 +18,7 @@ class Solution {
         return res;
     }
     
-    public int threeSumMulti(int[] arr, int target) {
+    public int threeSumMultiBetter(int[] arr, int target) {
         int n = arr.length;
         int res = 0;
         int[] hm1 = new int[target+1];
@@ -35,5 +35,20 @@ class Solution {
             res=(res+twoSumRes)%MOD;
         }
         return res;
+    }
+    public int threeSumMulti(int[] arr, int target) {
+        long[] c = new long[101];
+        for(int a:arr) c[a]++;
+        long res = 0;
+        for(int i=0;i<=100;i++){
+            for(int j=i;j<=100;j++){
+                int k = target-i-j;
+                if(k>100||k<0) continue;
+                if(i==j && j==k) res+=c[i]*(c[i]-1)*(c[i]-2)/6;
+                else if(i==j && j!=k) res += c[i]*(c[i]-1)/2*c[k];
+                else if(j<k) res +=c[i]*c[j]*c[k];
+            }
+        }
+        return (int) (res%MOD);
     }
 }
