dddd73b0d214b90a3d8b4fadecc315824ab4c458
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-23 14:36:30 +0530

    Time: 99 ms (17.63%) | Memory: 46.6 MB (15.47%) - LeetSync

--- 959-3sum-with-multiplicity/3sum-with-multiplicity.java
+++ 959-3sum-with-multiplicity/3sum-with-multiplicity.java
@@ -1,6 +1,6 @@
 class Solution {
     private final int MOD = 1_000_000_007;
-    public int threeSumMulti(int[] arr, int target) {
+    public int threeSumMultiInitial(int[] arr, int target) {
         int n = arr.length;
         int res = 0;
         HashMap<Integer, Integer> hm1 = new HashMap<>();
@@ -12,6 +12,25 @@ class Solution {
             for(int j=i+1;j<n;j++){
                 if(hm.containsKey(newTarget-arr[j])) twoSumRes+=hm.get(newTarget-arr[j]);
                 hm.put(arr[j],hm.getOrDefault(arr[j],0)+1);
+            }
+            res=(res+twoSumRes)%MOD;
+        }
+        return res;
+    }
+    
+    public int threeSumMulti(int[] arr, int target) {
+        int n = arr.length;
+        int res = 0;
+        int[] hm1 = new int[target+1];
+        for(int i=0;i<n;i++){
+            if(arr[i]<=target) hm1[arr[i]]++;
+            int[] hm = new int[target+1];
+            int newTarget = target-arr[i];
+            int twoSumRes = 0;
+            for(int j=i+1;j<n;j++){
+                int temp = newTarget-arr[j];
+                if(temp>=0 && temp<=target && hm[temp]>=1) twoSumRes+=hm[temp];
+                if(arr[j]>=0 && arr[j]<=target) hm[arr[j]]++;
             }
             res=(res+twoSumRes)%MOD;
         }
