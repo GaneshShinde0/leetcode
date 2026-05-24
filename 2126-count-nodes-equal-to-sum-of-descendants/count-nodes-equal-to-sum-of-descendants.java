50a2c7d154d3d1bce799674bbeb22046ac24ca34
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-24 17:52:47 +0530

    Time: 6 ms (100.00%) | Memory: 106.1 MB (78.95%) - LeetSync

--- 2126-count-nodes-equal-to-sum-of-descendants/count-nodes-equal-to-sum-of-descendants.java
+++ 2126-count-nodes-equal-to-sum-of-descendants/count-nodes-equal-to-sum-of-descendants.java
@@ -15,6 +15,7 @@
  */
 class Solution {
     int res = 0;
+    
     public int equalToDescendants(TreeNode root) {
         helper(root);
         return res;
@@ -23,6 +24,17 @@ class Solution {
         if(root ==null) return 0;
         int leftSum = helper(root.left);
         int rightSum = helper(root.right);
+        if(root.val == leftSum+rightSum) res++;
+        return leftSum+rightSum+root.val;
+    }
+    public int equalToDescendantsInitial(TreeNode root) {
+        helper1(root);
+        return res;
+    }
+    private int helper1(TreeNode root){
+        if(root ==null) return 0;
+        int leftSum = helper1(root.left);
+        int rightSum = helper1(root.right);
         if(root.val == leftSum+rightSum) res++;
         if(root.left==null && root.right==null) return root.val;
         return leftSum+rightSum+root.val;
