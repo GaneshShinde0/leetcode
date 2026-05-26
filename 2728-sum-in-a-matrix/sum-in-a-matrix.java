a21d2a067d2e10b6069d296f1b9079a902acc058
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-26 11:44:54 +0530

    Time: 17 ms (99.23%) | Memory: 77.9 MB (86.34%) - LeetSync

--- 2728-sum-in-a-matrix/sum-in-a-matrix.java
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.java
@@ -18,7 +18,7 @@ class Solution {
         }
         return res;
     }
-    public int matrixSum(int[][] nums) {
+    public int matrixSumSecond(int[][] nums) {
         int m = nums.length, n = nums[0].length;
         List<Integer>[] rowPQ= new List[m];
         for(int i=0;i<m;i++){
@@ -33,6 +33,20 @@ class Solution {
             int max = 0;
             for(int i=0;i<m;i++){
                 max = Math.max(max,(int)rowPQ[i].get(n-j-1));
+            }
+            res+=max;
+        }
+        return res;
+    }
+    
+    public int matrixSum(int[][] nums) {
+        int m = nums.length, n = nums[0].length;
+        for(int[] row:nums) Arrays.sort(row);
+        int res = 0;
+        for(int j=0;j<n;j++){
+            int max = 0;
+            for(int i=0;i<m;i++){
+                max = Math.max(max,nums[i][n-j-1]);
             }
             res+=max;
         }
