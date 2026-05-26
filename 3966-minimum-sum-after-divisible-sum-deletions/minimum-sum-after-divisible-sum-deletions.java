2fad044116227602343b9e5cc67a3f20bd566dce
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-26 09:01:04 +0530

    Time: 21 ms (57.58%) | Memory: 112.6 MB (80.30%) - LeetSync

--- 3966-minimum-sum-after-divisible-sum-deletions/minimum-sum-after-divisible-sum-deletions.java
+++ 3966-minimum-sum-after-divisible-sum-deletions/minimum-sum-after-divisible-sum-deletions.java
@@ -2,10 +2,21 @@
 USE DP to track the result, 
 dp[prefix%k] stores the smallest result with prefix sum mod k.
 
-Explaination:
+Intuition:
 - Iterate through the array, maintaining an evolving sum res.
 - If res reaches a value that shares a remainder with a smaller previously seen prefix sum, the algorithm instantly deletes the difference
 - By setting res to that smaller value.
+
+Explaination:
+- Evolving sum res isn't just a running total, it represents minimum possible sum of the array prefix so far, after optimal deletions.
+    res = dp[remainder] = Math.min(dp[remainder], res);
+- This compact line looks up the smallest past sum with the same remainder (int) (res%k);
+- It compares this past sum with current res and updates DP with new minimum
+- It replaces res with this minimum value, which is equivalent to performing optimal deletion. The final value of res after the loop is the minimum possible sum for the entire array.
+
+Time & Space Complexity:
+Time: O(N)
+Space: O(k)
 */
 class Solution {
     public long minArraySum(int[] nums, int k) {
@@ -17,7 +28,7 @@ class Solution {
             res+=num;
             int remainder = (int) (res%k);
             dp[remainder] = Math.min(dp[remainder], res);
-            res = Math.min(dp[remainder], res);
+            res = dp[remainder];
         }
         return res;
     }
