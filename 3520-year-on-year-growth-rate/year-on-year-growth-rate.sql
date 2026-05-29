38916cd6da4d0dead0b0d6117acd537f19e928fa
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-05-29 11:03:28 +0530

    Time: 367 ms (70.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3520-year-on-year-growth-rate/year-on-year-growth-rate.sql
@@ -0,0 +1,8 @@
+WITH CTE AS (
+    SELECT YEAR(transaction_date) as year, product_id, sum(spend) as curr_year_spend FROM user_transactions GROUP BY YEAR(transaction_date), PRODUCT_ID
+),
+CTE2 as (
+    SELECT *, lag(curr_year_spend) OVER (partition by product_id ORDER BY year asc) as prev_year_spend FROM CTE
+)
+SELECT *, ROUND((curr_year_spend-prev_year_spend)/prev_year_spend*100.00,2) as yoy_rate FROM CTE2 
+ORDER BY product_id, year
