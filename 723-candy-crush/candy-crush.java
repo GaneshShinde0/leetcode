74a08fca50479a7841b48974ded4c1a309038995
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-06-01 13:31:12 +0530

    Time: 21 ms (8.24%) | Memory: 52.8 MB (5.88%) - LeetSync

---
+++ 723-candy-crush/candy-crush.java
@@ -0,0 +1,70 @@
+class Solution {
+    record Pair(int key, int value){}
+    public int[][] candyCrush(int[][] board) {
+        int m = board.length, n = board[0].length;
+        while(true){
+            HashSet<Pair> clear = new HashSet<>();
+            for(int i=0;i<m;i++){
+                int streak = 0, last = -1;
+                for(int j=0;j<n;j++){
+                    if(board[i][j]==0){
+                        last =-1;
+                        continue;
+                    }
+                    if(board[i][j]==last) streak+=1;
+                    else{
+                        last = board[i][j];
+                        streak = 1;
+                    }
+                    if (streak == 3) {
+                        clear.add(new Pair(i, j));
+                        clear.add(new Pair(i, j - 1)); // Add the 2nd candy
+                        clear.add(new Pair(i, j - 2)); // Add the 1st candy
+                    } else if (streak > 3) {
+                        clear.add(new Pair(i, j)); // 1st and 2nd are already in the set
+                    }
+                }
+            }
+            for(int j=0;j<n;j++){
+                int streak = 0, last = -1;
+                for(int i=0;i<m;i++){
+                    if(board[i][j]==0){
+                        last =-1;
+                        continue;
+                    }
+                    if(board[i][j]==last) streak+=1;
+                    else{
+                        last = board[i][j];
+                        streak = 1;
+                    }
+                    if (streak == 3) {
+                        clear.add(new Pair(i, j));
+                        clear.add(new Pair(i - 1, j)); // Add the 2nd candy
+                        clear.add(new Pair(i - 2, j)); // Add the 1st candy
+                    } else if (streak > 3) {
+                        clear.add(new Pair(i, j)); // 1st and 2nd are already in the set
+                    }
+                }
+            }
+            if(clear.size()==0) return board;
+            for(Pair p:clear){
+                board[p.key()][p.value()]=0;
+            }
+            // We can use two pointers to drop.
+            for(int j=0;j<n;j++){
+                int ptr = m-1;
+                for(int i=m-1;i>=0;i--){
+                    if(board[i][j]!=0){
+                        board[ptr][j]=board[i][j];
+                        ptr--;
+                    }
+                }
+                while(ptr>=0){
+                    board[ptr][j]=0;
+                    ptr--;
+                }
+
+            }
+        }
+    }
+}
