d5ffae1a444a558bfd12acee8f8c12b1090269de
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-06-01 11:50:53 +0530

    Time: 6 ms (75.15%) | Memory: 42.9 MB (82.72%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,44 @@
+/*
+Dry Run 
+Input: amount = 5, coins = [1,2,5]
+ways = > [1,0,0,0,0,0]
+i =1
+ways = > [1,1,0,0,0,0]
+i=2 => 
+ways = > [1,1,2,0,0,0]
+i=3
+ways = > [1,1,2,3,0,0]
+i=4
+ways = > [1,1,2,3,5,0]
+i=5
+ways = > [1,1,2,3,5,9]
+*/
+class SolutionWillGiveNumberOfPermutations{
+    public int change(int amount, int[] coins) {
+        int[] ways = new int[amount+1];
+        ways[0]=1;
+        for(int i=0;i<=amount;i++){
+            for(int coin:coins){
+                if(i-coin>=0){
+                    ways[i]+=ways[i-coin];
+                }
+            }
+        }
+        return ways[amount];
+    }
+}
+
+class Solution{
+    public int change(int amount, int[] coins) {
+        int[] ways = new int[amount+1];
+        ways[0]=1;
+        for(int coin:coins){
+            for(int i=0;i<=amount;i++){
+                if(i-coin>=0){
+                    ways[i]+=ways[i-coin];
+                }
+            }
+        }
+        return ways[amount];
+    }
+}
