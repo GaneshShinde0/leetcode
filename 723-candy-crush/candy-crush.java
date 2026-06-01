469f74f3046995eed73d2241c597cb53de3abce3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-06-01 13:36:12 +0530

    Time: 21 ms (8.24%) | Memory: 52.8 MB (5.88%) - LeetSync

--- 723-candy-crush/candy-crush.java
+++ 723-candy-crush/candy-crush.java
@@ -7,20 +7,20 @@ class Solution {
             for(int i=0;i<m;i++){
                 int streak = 0, last = -1;
                 for(int j=0;j<n;j++){
-                    if(board[i][j]==0){
-                        last =-1;
-                        continue;
-                    }
+                    // if(board[i][j]==0){
+                    //     last =-1;
+                    //     continue;
+                    // }
                     if(board[i][j]==last) streak+=1;
                     else{
                         last = board[i][j];
                         streak = 1;
                     }
-                    if (streak == 3) {
+                    if (streak == 3 && last>0) {
                         clear.add(new Pair(i, j));
                         clear.add(new Pair(i, j - 1)); // Add the 2nd candy
                         clear.add(new Pair(i, j - 2)); // Add the 1st candy
-                    } else if (streak > 3) {
+                    } else if (streak > 3  && last>0) {
                         clear.add(new Pair(i, j)); // 1st and 2nd are already in the set
                     }
                 }
@@ -28,20 +28,20 @@ class Solution {
             for(int j=0;j<n;j++){
                 int streak = 0, last = -1;
                 for(int i=0;i<m;i++){
-                    if(board[i][j]==0){
-                        last =-1;
-                        continue;
-                    }
+                    // if(board[i][j]==0){
+                    //     last =-1;
+                    //     continue;
+                    // }
                     if(board[i][j]==last) streak+=1;
                     else{
                         last = board[i][j];
                         streak = 1;
                     }
-                    if (streak == 3) {
+                    if (streak == 3 && last>0) {
                         clear.add(new Pair(i, j));
                         clear.add(new Pair(i - 1, j)); // Add the 2nd candy
                         clear.add(new Pair(i - 2, j)); // Add the 1st candy
-                    } else if (streak > 3) {
+                    } else if (streak > 3  && last>0 ) {
                         clear.add(new Pair(i, j)); // 1st and 2nd are already in the set
                     }
                 }
