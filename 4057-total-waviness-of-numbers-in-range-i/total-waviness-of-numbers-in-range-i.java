cf6adac3ae2147e0f38d1f82438772e4a6d25974
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-06-04 08:39:41 +0530

    Time: 31 ms (23.79%) | Memory: 46.7 MB (8.18%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -1,25 +1,18 @@
 class Solution {
-    public int iswavi(int n){
-        String str=Integer.toString(n);
-        if(str.length()<2){
-            return 0;
-        }
-        int count=0;
-        for(int i=0;i<str.length()-2;i++){
-            int ch1=str.charAt(i)-'0';
-            int ch2=str.charAt(i+1)-'0';
-            int ch3=str.charAt(i+2)-'0';
-            if((ch1<ch2&&ch2>ch3)||(ch1>ch2&&ch3>ch2)){
-                count++;
-            }
-        }
-        return count;
-    }
     public int totalWaviness(int num1, int num2) {
-        int count=0;
+        int res = 0;
         for(int i=num1;i<=num2;i++){
-            count+=iswavi(i);
+            res+=isWavi(i);
         }
-        return count;
+        return res;
+    }
+    private int isWavi(int num){
+        String s = ""+num;
+        int w = 0;
+        if(s.length()<=2) return w;
+        for(int i=0;i<s.length()-2;i++){
+            if((s.charAt(i)>s.charAt(i+1) && s.charAt(i+1)<s.charAt(i+2))||(s.charAt(i)<s.charAt(i+1) && s.charAt(i+1)>s.charAt(i+2))) w++;
+        }
+        return w;
     }
 }
