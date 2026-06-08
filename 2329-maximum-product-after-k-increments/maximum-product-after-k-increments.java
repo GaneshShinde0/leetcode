cc2c5e6b5762c813c438fb732128f4033750854c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-06-08 11:49:06 +0530

    Time: 318 ms (92.32%) | Memory: 92.8 MB (6.08%) - LeetSync

--- 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.java
+++ 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.java
@@ -1,6 +1,24 @@
 class Solution {
     private final int MOD = 1_000_000_007;
+    
     public int maximumProduct(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        if(nums.length==1) return nums[0]+k;
+        for(int i:nums) pq.add(i);
+        while(k>0){
+            int num1 = pq.poll();
+            int num2 = pq.peek();
+            int diff = Math.min(k,num2-num1+1);
+            k-=diff;
+            pq.add(num1+diff);
+        }
+        long product = 1;
+        while(!pq.isEmpty()){
+            product = (1l*product*pq.poll())%MOD;
+        }
+        return (int) product;
+    }
+    public int maximumProductInitial(int[] nums, int k) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i:nums) pq.add(i);
         while(k>0){
