3cbec4785c6c073b68475a31b6f3f4d4c4a630a7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-06-13 23:55:13 +0530

    Time: 45 ms (89.19%) | Memory: 66.1 MB (20.27%) - LeetSync

---
+++ 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
@@ -0,0 +1,32 @@
+/*
+Sort the input
+1,2,5,8,13,21
+
+Selet k elements such that their difference is maximum.
+
+
+
+*/
+class Solution {
+    public int maximumTastiness(int[] price, int k) {
+        Arrays.sort(price);
+        int low = 0, high = price[price.length-1];
+        while(low<high){
+            int mid = low+(high-low)/2;
+            if(check(mid,price,k)) low = mid+1;
+            else high = mid;
+        }
+        return low-1;
+    }
+    private boolean check(int x, int[] price, int k){
+        int prev = price[0], count = 1, i = 1;
+        while(count<k && i<price.length){
+            if(price[i]-prev>=x){
+                prev = price[i];
+                count++;
+            }
+            i++;
+        }
+        return count == k;
+    }
+}
