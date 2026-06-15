ebd9bd75cf9e1d9d837c1ba90aa9236d5e27565c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-06-15 09:21:32 +0530

    Time: 63 ms (51.78%) | Memory: 72.1 MB (80.08%) - LeetSync

--- 901-advantage-shuffle/advantage-shuffle.java
+++ 901-advantage-shuffle/advantage-shuffle.java
@@ -1,6 +1,29 @@
 class Solution {
     public int[] advantageCount(int[] nums1, int[] nums2) {
         int n = nums1.length;
+        Arrays.sort(nums1);
+        // 2. Create and sort indices based on nums2 descending
+        Integer[] indices = new Integer[n];
+        for(int i=0;i<n;i++){
+            indices[i] = i;
+        }
+        Arrays.sort(indices, (i,j)->Integer.compare(nums2[j], nums2[i]));
+
+        int[] result = new int[n];
+        int left = 0, right = n-1;
+        for(int i:indices){
+            if(nums1[right]>nums2[i]){
+                result[i] = nums1[right];
+                right--;
+            }else{
+                result[i] = nums1[left];
+                left++;
+            }
+        }
+        return result;
+    }
+    public int[] advantageCountInitial(int[] nums1, int[] nums2) {
+        int n = nums1.length;
         TreeMap<Integer,Integer> tm = new TreeMap<>();
         for(int num:nums1){
             tm.put(num, tm.getOrDefault(num,0)+1);
