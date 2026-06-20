82ec8d001d65c988e3ead1d39f87d5689fa75ee0
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-06-20 23:38:48 +0530

    Time: 67 ms (9.52%) | Memory: 124 MB (14.29%) - LeetSync

---
+++ 3458-maximum-number-of-upgradable-servers/maximum-number-of-upgradable-servers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] maxUpgrades(int[] count, int[] upgrade, int[] sell, int[] money) {
+        int n = count.length;
+        int[] res = new int[n];
+        for(int i=0;i<n;i++){
+            res[i] = getBestScenario(count[i],upgrade[i], sell[i], money[i]);
+        }
+        return res;
+    }
+
+    private int getBestScenario(long count, long upgrade, long sell,long money){
+        long left = 0, right = count, ans = 0;
+        while(left<=right){
+            long mid = (left+right)/2;
+            long newMoney = money + (count-mid)*sell;
+            if(newMoney/upgrade>=mid){
+                ans = left;
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return (int) (left-1);
+    }
+}
