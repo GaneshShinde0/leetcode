01a840cbff27014bd44a71ab87d0888f60620213
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-06-25 09:06:15 +0530

    Time: 18 ms (82.84%) | Memory: 63.7 MB (98.40%) - LeetSync

--- 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.java
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.java
@@ -1,5 +1,24 @@
 class Solution {
+    
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        int n = capacity.length, res = 0;
+        int[] diff = new int[n];
+        for(int i=0;i<n;i++){
+            diff[i] = capacity[i]-rocks[i];
+        }
+        Arrays.sort(diff);
+        for(int i=0;i<n;i++){
+            if(additionalRocks>=diff[i]){
+                additionalRocks-=diff[i];
+                res++;
+            }else{
+                break;
+            }
+        }
+        
+        return res;
+    }
+    public int maximumBagsUsingPQAddsOverhead(int[] capacity, int[] rocks, int additionalRocks) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         int n = capacity.length, res = 0;
         for(int i=0;i<n;i++){
