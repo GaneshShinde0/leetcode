b7463056d5d091d5de187b7cef88f77d315a4158
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-06-27 08:57:28 +0530

    Time: 55 ms (67.44%) | Memory: 68.8 MB (36.05%) - LeetSync

---
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximumLength(int[] nums) {
+        HashMap<Integer,Integer> freq = new HashMap<>();
+        for(int num:nums){
+            freq.put(num, freq.getOrDefault(num,0)+1);
+        }
+        int res = 1;
+        for(Map.Entry<Integer,Integer> e:freq.entrySet()){
+            int key = e.getKey(), value = e.getValue();
+            if(key==1){
+                if(value%2==1) res = Math.max(value,res);
+                else res = Math.max(value-1,res);
+                continue;
+            }
+            int local=0;
+            while(freq.containsKey(key) && freq.get(key)>=2){
+                key = key*key;
+                local+=2;
+            }
+            if(freq.containsKey(key)){
+                res = Math.max(res, local+1);
+            }else{
+                res = Math.max(res, local-1);
+            }
+        }
+        return res;
+    }
+}
