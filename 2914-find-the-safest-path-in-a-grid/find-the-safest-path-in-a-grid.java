e5ab92cfcd3b2ed99b55577c17b06fbd8962e422
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-07-01 19:26:43 +0530

    Time: 93 ms (91.75%) | Memory: 110.9 MB (96.83%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
@@ -1,61 +1,51 @@
 class Solution {
-    static final int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
+    static final int[][] dirs = {{1,0},{0,1},{-1,0},{0,-1}};
     public int maximumSafenessFactor(List<List<Integer>> grid) {
         int n = grid.size();
+        if(grid.get(0).get(0)==1 || grid.get(n-1).get(n-1)==1) return 0;
 
-        if (grid.get(0).get(0) == 1 || grid.get(n - 1).get(n - 1) == 1) return 0;
-        int[][] A = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                A[i][j] = grid.get(i).get(j);
+        int[][] arr = new int[n][n];
 
         Queue<int[]> q = new LinkedList<>();
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                if (A[i][j] == 1)
-                    q.add(new int[]{i, j});
-
-        while (q.size() > 0) {
-            int[] head = q.poll();
-            int i = head[0];
-            int j = head[1];
-            int v = A[i][j];
-
-            for (int[] d : dirs) {
-                int x = i + d[0];
-                int y = j + d[1];
-
-                if (Math.min(x, y) >= 0 && Math.max(x, y) < n && A[x][y] == 0) {
-                    A[x][y] = v + 1;
-                    q.add(new int[]{x, y});
-                }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = grid.get(i).get(j);
+                if(arr[i][j]==1) q.add(new int[]{i,j});
             }
         }
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        pq.add(new int[]{A[0][0], 0, 0});
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int i = curr[0], j= curr[1];
+            int v = arr[i][j];
 
-        while (pq.size() > 0) {
-            int[] head = pq.poll();
-            int sf = head[0];
-            int i = head[1];
-            int j = head[2];
-
-            if (i == n - 1 && j == n - 1)
-                return sf - 1;
-
-            for (int[] d : dirs) {
-                int x = i + d[0];
-                int y = j + d[1];
-
-                if (Math.min(x, y) >= 0 && Math.max(x, y) < n && A[x][y] > 0) {
-                    pq.add(new int[]{Math.min(sf, A[x][y]), x, y});
-                    A[x][y] *= -1;
-                }
+            for(int[] dir: dirs){
+                int x = i + dir[0];
+                int y = j + dir[1];
+                if(x<0||y<0||x>=n||y>=n||arr[x][y]!=0) continue;
+                arr[x][y] = v+1;
+                q.add(new int[]{x,y});
             }
         }
 
-        return A[n - 1][n - 1] - 1;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> Integer.compare(b[0],a[0]));
+        pq.add(new int[]{arr[0][0], 0, 0});
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int dist = curr[0];
+            int i = curr[1], j = curr[2];
+            if(i==n-1 && j==n-1) return dist - 1;
+
+            for(int[] dir:dirs){
+                int x = i+dir[0];
+                int y = j+dir[1];
+                if(x<0||y<0||x>=n||y>=n||arr[x][y]<=0) continue;
+                pq.add(new int[]{Math.min(dist, arr[x][y]), x, y});
+                arr[x][y] *= -1;
+            }
+        }
+
+        return arr[n - 1][n - 1] - 1;
     }
 }
