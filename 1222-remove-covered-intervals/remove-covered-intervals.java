3d76225098ffec191642ac612195e36b83050ce2
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-07-06 09:31:49 +0530

    Time: 6 ms (93.21%) | Memory: 46.2 MB (92.44%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -18,7 +18,7 @@ class Solution {
         }
         return li.size();
     }
-    public int removeCoveredIntervals(int[][] intervals) {
+    public int removeCoveredIntervals1(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->{
             if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
             else return Integer.compare(a[1],b[1]);
@@ -34,6 +34,30 @@ class Solution {
             }else if(curr[1]<=prevEnd){
                 continue;
             }
+        }
+        return count;
+    }
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+        @Override
+        public int compare(int[] o1, int[] o2) {
+            // Sort by start point.
+            // If two intervals share the same start point,
+            // put the longer one to be the first.
+            return o1[0] == o2[0] ? o2[1] - o1[1]: o1[0] - o2[0];
+        }
+        });
+
+        int count = 0;
+        int end, prev_end = 0;
+        for (int[] curr : intervals) {
+        end = curr[1];
+        // if current interval is not covered
+        // by the previous one
+        if (prev_end < end) {
+            ++count;
+            prev_end = end;
+        }
         }
         return count;
     }
