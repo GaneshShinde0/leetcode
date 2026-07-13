496bcb712385b9761643f651c53eadee0a09da3d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-07-13 10:23:16 +0530

    Time: 1 ms (28.06%) | Memory: 42 MB (97.63%) - LeetSync

--- 1212-sequential-digits/sequential-digits.java
+++ 1212-sequential-digits/sequential-digits.java
@@ -1,35 +1,31 @@
 class Solution {
-    public List<Integer> sequentialDigitsSimpleSolution(int low, int high) {
-        int[] allNums = {12,23,34,45,56,67,78,89,
-                         123,234,345,456,567,678,789,
-                         1234,2345,3456,4567,5678,6789,
-                         12345,23456,34567,45678,56789,
-                         123456,234567,345678,456789,
-                         1234567,2345678,3456789,
-                         12345678,23456789,
-                         123456789};
-        List<Integer> res = new ArrayList<>();
-        int n = allNums.length;
-        for (int i = 0; i < n; i++) {
-            if (allNums[i] < low) continue;
-            if (allNums[i] > high) break;
-            res.add(allNums[i]);
-        }
-        return res;
-    }
-
-    public List<Integer> sequentialDigits(int low, int high){
-        String digits = "123456789";
-        List<Integer> res = new ArrayList<>();
-        int nl = String.valueOf(low).length();
-        int nh = String.valueOf(high).length();
-
-        for(int i = nl; i<=nh; i++){
-            for(int j=0; j<10-i;j++){
-                int num = Integer.parseInt(digits.substring(j, j+i));
-                if(num>=low && num<=high) res.add(num);
+    public List<Integer> sequentialDigits(int low, int high) {
+        int initialLow = low;
+        List<Integer> result = new ArrayList<>();
+        while(low<=high){
+            int curr = formSequence(low);
+            int lenLow = String.valueOf(low).length();
+            int lenCurr = String.valueOf(curr).length();
+            if(lenCurr<lenLow) low = (int) Math.pow(10,lenLow);
+            else{
+                if(curr>=initialLow && curr<=high){
+                    result.add(curr);
+                }
+                low = curr+(int) Math.pow(10, (int) Math.log10(curr));
             }
         }
-        return res;
+        return result;
+    }
+
+    private int formSequence(int num){
+        String s = num+"";
+        int len = s.length(), ptr = 0;
+        char beginning = s.charAt(0);
+        StringBuilder sb = new StringBuilder();
+        while(ptr<s.length() && beginning<='9'){
+            sb.append(beginning++);
+            ptr++;
+        }
+        return Integer.parseInt(sb.toString());
     }
 }
