6e6d58025a66d3b72f0c13dd7d08958d26240d13
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-01 11:42:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (57.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,4 +1,23 @@
-class Solution {
+class Solution{
+    int[][] memo;
+
+    private int maxDiff(int[] nums, int left, int right){
+        if(memo[left][right]!=-1) return memo[left][right];
+        if(left == right) return nums[left];
+        int scoreByLeft = nums[left] -maxDiff(nums, left+1, right);
+        int scoreByRight = nums[right] - maxDiff(nums, left, right-1);
+        memo[left][right] = Math.max(scoreByLeft, scoreByRight);
+        return memo[left][right];
+    }
+
+    public boolean predictTheWinner(int[] nums){
+        int n = nums.length;
+        memo = new int[n][n];
+        for(int i=0;i<n;i++) Arrays.fill(memo[i],-1);
+        return maxDiff(nums,0,n-1)>=0;
+    }
+}
+class SolutionRecursion{
     public boolean predictTheWinner(int[] nums) {
         boolean turn = true;
         int scoreA=0, scoreB=0;
