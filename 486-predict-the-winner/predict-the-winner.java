f019bec16926c1fa32c36542ec5c4e5e50846ff3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-01 09:38:47 +0530

    Time: 2 ms (37.40%) | Memory: 42.8 MB (41.06%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -8,7 +8,9 @@ class Solution {
     public boolean helper(boolean turn, int i, int j, int a, int b, int[] nums){
         if(i>j && a>=b) return true;
         else if(i>j && b>a) return false;
+        // Player 1 will choose whichever choice works for him.
         if(j>=0 && i<nums.length &&turn) return helper(!turn,i+1,j,a+nums[i],b,nums) || helper(!turn,i,j-1, a+nums[j],b,nums);
+        // Player 2 will choose choice that results in false as he wants to make sure player 1 loses. (So need to take && )
         if(j>=0 && i<nums.length &&!turn) return helper(!turn,i+1,j,a,nums[i]+b,nums) && helper(!turn,i,j-1, a,nums[j]+b,nums);
         return false;
     }
