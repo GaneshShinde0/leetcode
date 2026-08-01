354dbe89c0ee14a4943c6c31f8f8901f69b18388
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-01 11:47:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (57.54%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,4 +1,18 @@
-class Solution{
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[] dp = Arrays.copyOf(nums, n);
+
+        for(int diff = 1; diff<n; diff++){
+            for(int left = 0; left<n-diff; left++){
+                int right = left+diff;
+                dp[left] = Math.max(nums[left]-dp[left+1], nums[right]-dp[left]);
+            }
+        }
+        return dp[0]>=0;
+    }
+}   
+class SolutionDPBottomUp{
     public boolean predictTheWinner(int[] nums){
         int n = nums.length;
         int[][] dp = new int[n][n];
