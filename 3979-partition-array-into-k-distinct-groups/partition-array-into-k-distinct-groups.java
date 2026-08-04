f6653d96c61bb6cacc380a3f6da190815b36aef7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-04 09:40:39 +0530

    Time: 107 ms (5.23%) | Memory: 178 MB (5.88%) - LeetSync

--- 3979-partition-array-into-k-distinct-groups/partition-array-into-k-distinct-groups.java
+++ 3979-partition-array-into-k-distinct-groups/partition-array-into-k-distinct-groups.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean partitionArray(int[] nums, int k) {
         HashMap<Integer,Integer> freq = new HashMap<>();
+        int maxFreq = 0;
         for(int i:nums){
             freq.put(i, freq.getOrDefault(i,0)+1);
+            maxFreq = Math.max(freq.get(i), maxFreq);
         }
-        List<Integer> li = new ArrayList<>(freq.values());
-        Collections.sort(li);
-        int left = 0, right = li.size()-1;
-        return (1l*li.get(right)*k<=nums.length && nums.length%k==0 );
+        return (1l*maxFreq*k<=nums.length && nums.length%k==0 );
     }
 }
