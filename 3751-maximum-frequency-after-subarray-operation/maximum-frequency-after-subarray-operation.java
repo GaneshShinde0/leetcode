8556d95620501290398898a86173908416a015b3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-06 11:28:21 +0530

    Time: 3 ms (99.04%) | Memory: 83.1 MB (80.77%) - LeetSync

--- 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
@@ -1,4 +1,14 @@
-class Solution {
+class Solution{
+    public int maxFrequency(int[] A, int k) {
+        int count[] = new int[51], res = 0;
+        for (int a : A) {
+            count[a] = Math.max(count[a], count[k]) + 1;
+            res = Math.max(res, count[a] - count[k]);
+        }
+        return count[k] + res;
+    }    
+}
+class Solution1 {
     public int maxFrequency(int[] A, int k) {
         Map<Integer, Integer> count = new HashMap<>();
         for (int a : A) {
