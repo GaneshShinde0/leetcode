7174fee8b4530bbc252fb2d21f7185be666b2c71
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-08 21:42:46 +0530

    Time: 99 ms (82.25%) | Memory: 80.2 MB (24.24%) - LeetSync

---
+++ 2742-group-by/group-by.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+
+Array.prototype.groupBy = function(fn) {
+    let obj={};
+    for(const item of this){
+        const key=fn(item);
+        if(key===undefined) continue;
+        if(!obj[key]){
+            obj[key]=[];
+        }
+        obj[key].push(item);
+    }
+    return obj;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
