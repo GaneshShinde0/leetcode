5f79ac87a7be777c39c62276aa1fe44380f6aa41
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-08 20:50:24 +0530

    Time: 16 ms (85.71%) | Memory: 46.1 MB (14.29%) - LeetSync

---
+++ 3949-minimum-number-of-primes-to-sum-to-target/minimum-number-of-primes-to-sum-to-target.java
@@ -0,0 +1,33 @@
+class Solution {
+    private static List<Integer> primes = getPrimes();
+    public int minNumberOfPrimes(int n, int m) {
+        int[] minNums = new int[Math.max(n + 1, 8000)];
+        Arrays.fill(minNums,100000);
+        minNums[0]=0;
+        for(int k=0;k<m;k++){
+            int p = primes.get(k);
+            minNums[p] = 1;
+            for(int i=p;i<=n;i++){
+                minNums[i] = Math.min(minNums[i-p]+1, minNums[i]);
+            }
+        }
+        return minNums[n]==100000?-1:minNums[n];
+    }
+
+    private static List<Integer> getPrimes(){
+        boolean[] temp = new boolean[10000];
+        for(int i=2;i<=1000;i++){
+            int curr = 2*i;
+            while(curr<temp.length){
+                temp[curr] = true;
+                curr+=i;
+            }
+        }
+        List<Integer> li = new ArrayList<>();
+        for(int i=2;i<10000;i++){
+            if(!temp[i]) li.add(i);
+        }
+        return li;
+
+    }
+}
