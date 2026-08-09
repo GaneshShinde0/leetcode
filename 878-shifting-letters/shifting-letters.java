69ac87da9b9866d23f326c550a1c274b8f23a149
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-09 16:33:14 +0530

    Time: 14 ms (11.14%) | Memory: 73.8 MB (19.96%) - LeetSync

--- 878-shifting-letters/shifting-letters.java
+++ 878-shifting-letters/shifting-letters.java
@@ -10,6 +10,7 @@ class Solution {
         }
         return sb.toString();
     }
+    // Following reduces time from 718 to 13 ms
     public String shiftingLetters(String s, int[] shifts) {
         int n = shifts.length;
         long suffixSum = shifts[n-1];
@@ -17,7 +18,7 @@ class Solution {
         for(int i=n-1;i>=0;i--){
             char c = (char)('a'+ (s.charAt(i)-'a' + (suffixSum%26))%26);
             sb.append(c);
-            if(i>0) suffixSum+=shifts[i-1];
+            if(i>0) suffixSum=(suffixSum+shifts[i-1])%26;
         }
         return sb.reverse().toString();
     }
