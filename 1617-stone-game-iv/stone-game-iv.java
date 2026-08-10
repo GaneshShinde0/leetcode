65177d54f4be03b789aff0f8c73d41c83fe1c6db
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-10 10:08:47 +0530

    Time: 1225 ms (5.22%) | Memory: 81.6 MB (5.48%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,14 +1,14 @@
 class Solution {
     public boolean winnerSquareGame(int n) {
         HashMap<Integer, Boolean> memo = new HashMap<>();
-        return recurse(memo, n, 1);
+        return recurse(memo, n);
     }
-    private boolean recurse(HashMap<Integer,Boolean> memo, int n, int turn){
+    private boolean recurse(HashMap<Integer,Boolean> memo, int n){
         if(memo.containsKey(n)) return memo.get(n);
         if(n==0) return false;
         boolean res = false;
         for(int i=1;i<=Math.sqrt(n);i++){
-            res |= !recurse(memo,n-i*i, turn+1);
+            res |= !recurse(memo,n-i*i);
         }
         memo.put(n,res);
         return res;
