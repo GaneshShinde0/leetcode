6b84e9994c478fece8599dcd58baaf2d000728a8
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-10 12:54:21 +0530

    Time: 117 ms (54.55%) | Memory: 72.1 MB (81.82%) - LeetSync

---
+++ 2912-deep-object-filter/deep-object-filter.js
@@ -0,0 +1,48 @@
+var deepFilter = function(obj, fn) {
+  function dfs(node) {
+    if(node === null) {
+      if(fn(node)) return node;
+      return undefined;
+    }
+    if(typeof node !== 'object' ) {
+      if(fn(node)) return node;
+      return undefined;
+    }
+
+    if(Array.isArray(node)) {
+      const newArr = [];
+
+      for(let i = 0;i < node.length; i++) {
+        const curr = node[i];
+        const subRes = dfs(curr);
+
+        if(subRes!==undefined) {
+          newArr.push(subRes);
+        }
+      }
+
+      if(newArr.length === 0) {
+        return undefined;
+      }
+
+      return newArr;
+    }
+
+    const newObj = {};
+    let isEmpty = true
+
+    for(const key in node) {
+      const subRes = dfs(node[key]);
+      if(subRes!== undefined) {
+        newObj[key] = subRes;
+        isEmpty = false
+      }
+    }
+
+    if(isEmpty) return undefined;
+
+    return newObj;
+  }
+
+  return dfs(obj);
+}
