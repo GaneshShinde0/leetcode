b585da48ede9bec9ed45bf2190876ef14cd0734a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-12 11:38:08 +0530

    Time: 8 ms (44.59%) | Memory: 42.9 MB (56.80%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -19,7 +19,7 @@ Algorithm:
 
 To Make this more efficient, we ignore all the invalid characters after the mid index as well thereby reducing the number of recursive calls.
 */
-class Solution {
+class SolutionDivideAndConquer {
     public int longestSubstring(String s, int k) {
         return longestSubstringUtil(s, 0, s.length(), k);
     }
@@ -38,5 +38,69 @@ class Solution {
                 longestSubstringUtil(s, midNext, end, k));
         }
         return (end-start);
+    }
+}
+/*
+Time Complexity: O(N*N)
+*/
+
+/*
+Approach: SLiding Window
+Intuition: 
+Window slides over the string s and validates each character, Based on certain conditions, the sliding window either expands or shrinks.
+
+The sbustring is valid if each character has atleast k frequency. The main idea is to find all the valid substrings with a different number of unique characters and track the maximum length. 
+
+Algorithm:
+
+1. FInd the number of unique characters in the string s and store the count in variable maxUnique. For s = "aabcbacad", the unique characters are a,b,c,d and maxUnique = 4.
+2. Iterate over the string s with the value of currUnique ranging from 1 to maxUnique in each iteration, currUnique is the maximum number of unique characters that must be present in the sliding window.
+3. The sliding window starts at index windowStart and ends at index windowEnd and slides over string s until windowEnd reaches the end of string s. At any point, we shrink or expand the window to ensure that the number of unique characters is not greater than currUnique. 
+    -  If the number of unique character in the sliding window is less than or equal to currUnique, expand the window from right by adding a character to the end of the window given by windowEnd.
+    - Otherwise, shrink the window from left by removing a charcter from the start of the window given by windowStart.
+4. Keep track of the number of unique characters in teh current sliding window having atleast k frequency given by countAtLeastK. Update the result if all the characters in the window hasve atleast k frequency.
+*/
+class Solution {
+    public int longestSubstring(String s, int k) {
+        char[] str = s.toCharArray();
+        int[] freq = new int[26];
+        int maxUnique = getMaxUniqueLetters(s);
+        int result = 0, n = s.length();
+
+        for(int currUnique = 1; currUnique <=maxUnique; currUnique++){
+            Arrays.fill(freq, 0);
+            int start = 0, end = 0, idx = 0, unique = 0, countAtLeastK = 0;
+            while(end<n){
+                if(unique<=currUnique){
+                    idx = str[end]-'a';
+                    if(freq[idx]==0) unique++;
+                    freq[idx]++;
+                    if(freq[idx]==k) countAtLeastK++;
+                    end++;
+                }else{
+                    idx = str[start] - 'a';
+                    if(freq[idx]==k) countAtLeastK--;
+                    freq[idx]--;
+                    if(freq[idx]==0) unique--;
+                    start++;
+                }
+                if(unique == currUnique && unique == countAtLeastK){
+                    result = Math.max(end-start, result);
+                }
+            }
+        }
+        return result;
+    }
+    
+    private int getMaxUniqueLetters(String s){
+        boolean[] map = new boolean[26];
+        int maxUnique = 0;
+        for(int i=0;i<s.length();i++){
+            if(!map[s.charAt(i)-'a']){
+                maxUnique++;
+                map[s.charAt(i)-'a'] = true;
+            }
+        }
+        return maxUnique;
     }
 }
