1360953eccc10a7b090700588a69519382697d9b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-12 20:54:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (87.37%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.java
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.java
@@ -1,6 +1,6 @@
 class Solution {
     int[][] dirs = {{0,1},{-1,0},{0,-1},{1,0}};
-    public boolean isRobotBounded(String instructions) {
+    public boolean isRobotBoundedInitial(String instructions) {
         int x = 0, y = 0, dirPtr=0;
         for(char c:instructions.toCharArray()){
             if(c=='G'){
@@ -14,6 +14,20 @@ class Solution {
         }
         if((x==0 && y==0) || dirPtr%4!=0) return true;
         else return false;
+    }
+    public boolean isRobotBounded(String instructions) {
+        int x = 0, y = 0, dirPtr=0;
+        for(char c:instructions.toCharArray()){
+            if(c=='G'){
+                x+=dirs[dirPtr][0];
+                y+=dirs[dirPtr][1];
+            }else if(c=='L'){
+                dirPtr = (dirPtr+4-1)%4;
+            }else if(c=='R'){
+                dirPtr = (dirPtr+1)%4;
+            }
+        }
+        return ((x==0 && y==0) || dirPtr!=0);
     }
 }
 
