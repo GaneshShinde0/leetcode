dae5287bd6ee7d1383893f6145acb2eae193f070
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-12 22:08:03 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (69.91%) - LeetSync

--- 828-chalkboard-xor-game/chalkboard-xor-game.java
+++ 828-chalkboard-xor-game/chalkboard-xor-game.java
@@ -1,3 +1,17 @@
+/*
+Intuition And Algorithm:
+Problem Statement: If the XOR of the entrie array is 0, then Alice wins.
+
+If the XOR condition is never triggered, then clearly Alice wins iff there are even number of elements, as every player has a move.
+
+Actually, Alice always has a move when there are even number of elements... If Xor is not zero, but there are no possible moves then 0xor S !=0 , causes a contradition.
+
+Similarly if there are an odd number of elements, then Bob always faces an even number of elements and has a move, so the answer is just the parity of the number of elemnts in the array.
+
+
+
+*/
+
 class Solution {
     public boolean xorGame(int[] nums) {
         int x = 0;
