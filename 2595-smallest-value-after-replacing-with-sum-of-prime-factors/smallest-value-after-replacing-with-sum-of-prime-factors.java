305b9d59daa5a11123857720f530d11602ce8dbc
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-12 15:33:57 +0530

    Time: 2 ms (42.23%) | Memory: 42.1 MB (62.14%) - LeetSync

--- 2595-smallest-value-after-replacing-with-sum-of-prime-factors/smallest-value-after-replacing-with-sum-of-prime-factors.java
+++ 2595-smallest-value-after-replacing-with-sum-of-prime-factors/smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int smallestValue(int n) {
+    public int smallestValueUsingRecursion(int n) {
         int sumOfPrimeFactors = 0;
         int temp = n;
         for(int i=2;i<=temp;i++){
@@ -10,5 +10,19 @@ class Solution {
         }
         if(n==sumOfPrimeFactors) return n;
         else return smallestValue(sumOfPrimeFactors);
+    }
+    public int smallestValue(int n) {
+        while(true){
+            int sumOfPrimeFactors = 0;
+            int temp = n;
+            for(int i=2;i<=temp;i++){
+                while(temp%i==0){
+                    temp/=i;
+                    sumOfPrimeFactors += i;
+                }
+            }
+            if(n==sumOfPrimeFactors) return n;
+            n=sumOfPrimeFactors;
+        }
     }
 }
