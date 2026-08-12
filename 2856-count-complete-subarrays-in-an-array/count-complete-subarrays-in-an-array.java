d59447beba82084ba84df265ba5ca22b34fd0f39
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-12 15:21:56 +0530

    Time: 17 ms (36.42%) | Memory: 47.5 MB (9.16%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int countCompleteSubarrays(int[] nums) {
+    public int countCompleteSubarraysNxN(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for(int i:nums) set.add(i);
         int dist = set.size(), n = nums.length, res = 0;
@@ -8,6 +8,30 @@ class Solution {
             for(int j=i;j<n;j++){
                 set2.add(nums[j]);
                 if(set2.size()==dist) res++;
+            }
+        }
+        return res;
+    }
+
+    public int countCompleteSubarrays(int[] nums){
+        int res = 0;
+        Map<Integer, Integer> cnt = new HashMap<>();
+        int n = nums.length, right = 0;
+        int dist = new HashSet<>(Arrays.asList(Arrays.stream(nums).boxed().toArray(Integer[]::new))).size();
+        
+        for(int left = 0; left<n; left++){
+            if(left>0){
+                int remove = nums[left-1];
+                cnt.put(remove,cnt.get(remove)-1);
+                if(cnt.get(remove)==0) cnt.remove(remove);
+            }
+            while(right<n && cnt.size()<dist){
+                int add = nums[right];
+                cnt.put(add, cnt.getOrDefault(add,0)+1);
+                right++;
+            }
+            if(cnt.size()==dist){
+                res+=(n-right+1);
             }
         }
         return res;
