60af6838d2b3b8d8013eb69bd1669c311778d930
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-12 16:54:04 +0530

    Time: 8 ms (58.65%) | Memory: 83.8 MB (57.69%) - LeetSync

--- 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.java
+++ 1107-minimum-swaps-to-group-all-1s-together/minimum-swaps-to-group-all-1s-together.java
@@ -5,6 +5,7 @@ class Solution {
         for(int num:nums) if(num==1) n++;
         int left = 0, right = 0, max = 0, curr = 0;
         while(right<nums.length){
+            // We want window of size n which has maximum possible 1s... We can get the by finding maximum 1's in current window.
             while(right<nums.length && (right-left)<n){
                 if(nums[right]==1){
                     curr++;
