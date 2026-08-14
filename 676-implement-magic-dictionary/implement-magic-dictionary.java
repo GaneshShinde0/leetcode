eae1bf8ffc41c2ded38a10949d09402992835bcc
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-14 07:57:02 +0530

    Time: 35 ms (38.94%) | Memory: 48.8 MB (29.65%) - LeetSync

--- 676-implement-magic-dictionary/implement-magic-dictionary.java
+++ 676-implement-magic-dictionary/implement-magic-dictionary.java
@@ -1,8 +1,8 @@
-class MagicDictionary {
+class MagicDictionaryUsingHashMap {
     HashMap<String,Integer> mapOfComb;
     Set<String> words;
 
-    public MagicDictionary() {
+    public MagicDictionaryUsingHashMap() {
         this.mapOfComb = new HashMap<>();
         this.words = new HashSet<>();
     }
@@ -31,6 +31,65 @@ class MagicDictionary {
     }
 }
 
+class MagicDictionary {
+    
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEndOfWord = false;
+    }
+    
+    TrieNode root;
+
+    public MagicDictionary() {
+        root = new TrieNode();
+    }
+    
+    public void buildDict(String[] dictionary) {
+        for (String word : dictionary) {
+            TrieNode curr = root;
+            for (char c : word.toCharArray()) {
+                if (curr.children[c - 'a'] == null) {
+                    curr.children[c - 'a'] = new TrieNode();
+                }
+                curr = curr.children[c - 'a'];
+            }
+            curr.isEndOfWord = true;
+        }
+    }
+    
+    public boolean search(String searchWord) {
+        // Start the DFS from the root, at index 0, with 0 modifications made so far
+        return dfs(root, searchWord, 0, false);
+    }
+    
+    private boolean dfs(TrieNode node, String word, int index, boolean modified) {
+        // Base case: If we've reached the end of the word
+        if (index == word.length()) {
+            // It must be a valid word in the dictionary AND exactly 1 character was modified
+            return node.isEndOfWord && modified;
+        }
+        char currentChar = word.charAt(index);
+        // Check all possible next characters in the Trie
+        for (int i = 0; i < 26; i++) {
+            if (node.children[i] != null) {
+                // If the character matches, continue DFS without changing 'modified' status
+                if (i == currentChar - 'a') {
+                    if (dfs(node.children[i], word, index + 1, modified)) {
+                        return true;
+                    }
+                } 
+                // If the character does NOT match, we can only proceed if we haven't modified yet
+                else if (!modified) {
+                    if (dfs(node.children[i], word, index + 1, true)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        // If no valid path was found, return false
+        return false; 
+    }
+}
 /**
  * Your MagicDictionary object will be instantiated and called as such:
  * MagicDictionary obj = new MagicDictionary();
