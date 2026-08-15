9b0e1c293ea2a566863848f1616b782bf74191d7
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-15 17:54:48 +0530

    Time: 3 ms (87.60%) | Memory: 52.5 MB (22.07%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,85 +1,27 @@
 class Solution {
-    private static final int[][] directions = {
-        {0,1},
-        {0,-1},
-        {1,0},
-        {-1,0}
-    };
-    
-    public int numIslandsInitial(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int res = 0;
-        boolean[][] visited = new boolean[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(visited[i][j]==false&&grid[i][j]=='1'){
-                    res++;
-                    dfsInitial(grid, visited,i,j,m,n);
-                }
-            }
-        }
-        return res;
-    }
-
-    private void dfsInitial(char[][] grid, boolean[][] visited, int i, int j,int m, int n){
-        if(visited[i][j]) return;
-        visited[i][j]=true;
-        if(grid[i][j]=='1'){
-            for(int[] dir:directions){
-                int newI = i+dir[0];
-                int newJ = j+dir[1];
-                if(newI>=m||newI<0||newJ>=n||newJ<0) continue;
-                dfsInitial(grid,visited,newI,newJ,m,n);
-            }
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+    int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+    int m,n;
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int count = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        this.m = grid.length;
+        this.n = grid[0].length;
+        int numOfIslands = 0;
+        for(int i=0; i<m; i++){
+            for(int j =0; j<n; j++){
                 if(grid[i][j]=='1'){
-                    dfs(grid, i, j);
-                    count++;
+                    dfs(grid,i,j);
+                    numOfIslands++;
                 }
             }
         }
-        return count;
+        return numOfIslands;
     }
-    
     private void dfs(char[][] grid, int i, int j){
-        grid[i][j] = '0';
-        for(int[] dir:directions){
-            int nx = i+dir[0];
-            int ny = j+dir[1];
-            if(nx>=grid.length||ny>=grid[0].length||nx<0||ny<0||grid[nx][ny]=='0') continue;
-            dfs(grid,nx,ny);
+        grid[i][j]='0';
+        for(int[] dir:dirs){
+            int newI = i+dir[0];
+            int newJ = j+dir[1];
+            if(newI>=m||newJ>=n||newI<0||newJ<0||grid[newI][newJ]=='0') continue;
+            dfs(grid,newI,newJ);
         }
     }
 }
