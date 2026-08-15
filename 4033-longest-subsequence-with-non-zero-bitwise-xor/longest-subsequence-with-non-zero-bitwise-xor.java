3b4d842191d2e240ec0b5ce696ea788cde9ef046
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-15 17:23:25 +0530

    Time: 2 ms (68.38%) | Memory: 133.5 MB (52.21%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
@@ -1,14 +1,28 @@
+/*
+0^1 => 1
+1^0 => 1
+0^0 => 0
+1^1 => 0
+
+If All elements are zero => return 0;
+All Elements are same=> 
+    If number of elements is odd then return n
+    else return n-1
+
+Running XOR
+
+*/
+
 class Solution {
     public int longestSubsequence(int[] nums) {
-        int xor = 0;
-        boolean allZero = true;
-        int n = nums.length;
-        for(int num:nums){
-            xor^=num;
-            if(num!=0)allZero=false;
+        int xor = nums[0], first = nums[0], n = nums.length;
+        boolean allZero = nums[0]==0;
+        for(int i=1;i<n;i++){
+            xor^=nums[i];
+            if(nums[i]!=0) allZero = false;
         }
         if(allZero) return 0;
-        if(xor>0)return n;
-        else return n-1;
+        if(xor==0) return n-1;
+        else return n;
     }
 }
