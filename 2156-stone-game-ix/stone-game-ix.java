d9b4cd964fa0db6d88a0e55261312848bd8fc4e6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-16 10:58:52 +0530

    Time: 7 ms (29.05%) | Memory: 114.6 MB (82.43%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.java
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -1,4 +1,17 @@
-class Solution {
+class Solution{
+    public boolean stoneGameIX(int[] stones){
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for(int val:stones){
+            int type = val%3;
+            if(type == 0) cnt0++;
+            else if(type==1) cnt1++;
+            else cnt2++;
+        }
+        if(cnt0%2==0) return cnt1>=1 && cnt2>=1;
+        return cnt1-cnt2>2 || cnt2-cnt1>2;
+    }
+}
+class SolutionInitial {
     public boolean stoneGameIX(int[] stones) {
         int[] counts = new int[3];
         for (int stone : stones) counts[stone % 3]++;
@@ -34,6 +47,10 @@ class Solution {
         // 3. Did Alice win?
         // She wins if the game didn't end by running out of stones, 
         // AND the last move was made by Bob (meaning Bob is forced to make a losing move).
+        // movesMade are the safe moves...
+            // If there are safe moves at Alices Turn then Alice will win.
+                // ALices Turn movesMade%2==1
+            // If last safe move was from bob then bob will win
         // Since movesMade is the total valid moves, think about what movesMade % 2 should be for Alice to win!
         return movesMade%2==1 && movesMade<totalStones;
     }
