d2c83a1176aaed6b7a6dbdc914aa50962d39b19b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-16 18:00:30 +0530

    Time: 36 ms (70.71%) | Memory: 47.4 MB (19.19%) - LeetSync

--- 2877-shortest-string-that-contains-three-strings/shortest-string-that-contains-three-strings.java
+++ 2877-shortest-string-that-contains-three-strings/shortest-string-that-contains-three-strings.java
@@ -16,7 +16,7 @@ class Solution {
         return li.get(0);
 
     }
-    private String merge(String s1, String s2){
+    private String mergeInitial(String s1, String s2){
         if(s1.contains(s2)) return s1;
         int m = s1.length(), n = s2.length();
         StringBuilder sb = new StringBuilder(s1);
@@ -25,5 +25,32 @@ class Solution {
             if(sb.indexOf(s2)!=-1) return sb.toString();
         }
         return sb.toString();
+    }
+
+    // Merge Using KMP Algorithm
+    private String merge(String s1, String s2){
+        if(s1.contains(s2)) return s1;
+        String combined = s2 + '#' + s1;
+        int[] lps = new int[combined.length()];
+        // Standard KMP LPS (Longest Prefix Suffix) building logic
+        int len = 0; // Length of the previous longest pprefix suffix.
+        int i=1;
+
+        while(i<combined.length()){
+            if(combined.charAt(i)==combined.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }else{
+                if(len!=0){
+                    len = lps[len-1];
+                }else{
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        int overlap = lps[combined.length()-1];
+        return s1+s2.substring(overlap);
     }
 }
