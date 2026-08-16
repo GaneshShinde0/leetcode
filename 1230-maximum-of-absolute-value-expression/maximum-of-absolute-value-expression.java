41a216bc6a7ab3076f80f88c1f3878d597fcefd6
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-16 16:12:30 +0530

    Time: 19 ms (7.14%) | Memory: 53.3 MB (54.08%) - LeetSync

--- 1230-maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.java
+++ 1230-maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.java
@@ -6,6 +6,39 @@ class Solution {
     public int maxAbsValExpr(int[] arr1, int[] arr2) {
         int[] sign={-1,1};
         int res = Integer.MIN_VALUE;
+        int[] max = new int[4];
+        Arrays.fill(max, Integer.MIN_VALUE);
+        int[] min = new int[4];
+        Arrays.fill(min, Integer.MAX_VALUE);
+        
+        for(int i=0;i<arr1.length;i++){
+            int v0,v1,v2,v3;
+            v0 = arr1[i] + arr2[i] + i;
+            v1 = arr1[i] - arr2[i] + i;
+            v2 = -arr1[i] + arr2[i] + i;
+            v3 = -arr1[i] - arr2[i] + i;
+            max[0] = Math.max(max[0], v0);
+            min[0] = Math.min(min[0], v0);
+            
+            max[1] = Math.max(max[1], v1);
+            min[1] = Math.min(min[1], v1);
+            
+            max[2] = Math.max(max[2], v2);
+            min[2] = Math.min(min[2], v2);
+            
+            max[3] = Math.max(max[3], v3);
+            min[3] = Math.min(min[3], v3);
+        } 
+
+        for (int k = 0; k < 4; k++) {
+            res = Math.max(res, max[k] - min[k]);
+        }
+
+        return res;
+    }
+    public int maxAbsValExprInitial(int[] arr1, int[] arr2) {
+        int[] sign={-1,1};
+        int res = Integer.MIN_VALUE;
         for(int s1:sign){
             for(int s2:sign){
                 for(int s3:sign){
