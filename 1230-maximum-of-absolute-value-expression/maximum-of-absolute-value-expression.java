bf9b6b8918d8eb078a4f01b0a57a98a34d83d23d
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-16 16:16:19 +0530

    Time: 5 ms (81.63%) | Memory: 52.9 MB (94.90%) - LeetSync

--- 1230-maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.java
+++ 1230-maximum-of-absolute-value-expression/maximum-of-absolute-value-expression.java
@@ -3,7 +3,40 @@
 - Can be represented in 8 ways if we remove modulo.
 */
 class Solution {
-    public int maxAbsValExpr(int[] arr1, int[] arr2) {
+    public int maxAbsValExpr(int[] x, int[] y) {
+        int res = 0, n = x.length, P[] = {-1,1};
+        for (int p : P) {
+            for (int q : P) {
+                int smallest = p * x[0] + q * y[0] + 0;
+                for (int i = 1; i < n; ++i) {
+                    int cur = p * x[i] + q * y[i] + i;
+                    res = Math.max(res, cur - smallest);
+                    smallest = Math.min(smallest, cur);
+                }
+            }
+        }
+        return res;
+    }
+    public int maxAbsValExprInitial(int[] arr1, int[] arr2) {
+        int[] sign={-1,1};
+        int res = Integer.MIN_VALUE;
+        for(int s1:sign){
+            for(int s2:sign){
+                for(int s3:sign){
+                    // max and min should be calculated using same sign;, considering sign of i-j as well
+                    int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+                    for(int i=0;i<arr1.length;i++){
+                        int temp1 = s1*arr1[i]+s2*arr2[i]+s3*i;
+                        max = Math.max(max, temp1);
+                        min = Math.min(min, temp1);
+                        res = Math.max(res, max-min);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    public int maxAbsValExprOther(int[] arr1, int[] arr2) {
         int[] sign={-1,1};
         int res = Integer.MIN_VALUE;
         int[] max = new int[4];
@@ -36,23 +69,5 @@ class Solution {
 
         return res;
     }
-    public int maxAbsValExprInitial(int[] arr1, int[] arr2) {
-        int[] sign={-1,1};
-        int res = Integer.MIN_VALUE;
-        for(int s1:sign){
-            for(int s2:sign){
-                for(int s3:sign){
-                    // max and min should be calculated using same sign;, considering sign of i-j as well
-                    int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-                    for(int i=0;i<arr1.length;i++){
-                        int temp1 = s1*arr1[i]+s2*arr2[i]+s3*i;
-                        max = Math.max(max, temp1);
-                        min = Math.min(min, temp1);
-                        res = Math.max(res, max-min);
-                    }
-                }
-            }
-        }
-        return res;
-    }
+    
 }
