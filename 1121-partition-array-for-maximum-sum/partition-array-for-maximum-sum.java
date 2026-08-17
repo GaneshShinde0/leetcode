668cb3f729969e1ba4d6d64ed830f1409ea979a0
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-17 16:01:58 +0530

    Time: 6 ms (94.60%) | Memory: 43.7 MB (90.00%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -4,7 +4,7 @@ class Solution {
         int[] dp = new int[n+1];
         for(int i=1;i<=n;i++){
             int maxVal = 0;
-            for(int j=1;j<=k && i>=j;j++){
+            for(int j=1;j<=k && j<=i;j++){
                 maxVal = Math.max(maxVal, arr[i-j]);
                 dp[i] = Math.max(dp[i], dp[i-j]+maxVal*j);
                 // System.out.println(Arrays.toString(dp));
