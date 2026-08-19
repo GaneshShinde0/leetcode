ec40b65c53525e086dbe140b1e105b73f5f11c01
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-19 10:38:59 +0530

    Time: 371 ms (49.52%) | Memory: 0B (100.00%) - LeetSync

--- 4103-find-churn-risk-customers/find-churn-risk-customers.sql
+++ 4103-find-churn-risk-customers/find-churn-risk-customers.sql
@@ -9,9 +9,9 @@ SUBSCRIBER_FOR_60_DAYS AS(
     SELECT DISTINCT user_id, DATEDIFF(DAY, MIN(event_date),MAX(EVENT_DATE)) as days_as_subscriber  FROM subscription_events 
     GROUP BY user_id
 ),
-ACTIVE_USERS AS (SELECT se.user_id, se.plan_name as current_plan , se.monthly_amount as current_monthly_amount, le.max_historical_amount, sf60.days_as_subscriber  FROM subscription_events se right join LAST_EVENT le on le.user_id = se.user_id and le.last_event = se.event_id 
+CHURN_USERS AS (SELECT se.user_id, se.plan_name as current_plan , se.monthly_amount as current_monthly_amount, le.max_historical_amount, sf60.days_as_subscriber  FROM subscription_events se right join LAST_EVENT le on le.user_id = se.user_id and le.last_event = se.event_id 
 JOIN SUBSCRIBER_FOR_60_DAYS sf60 on se.user_id = sf60.user_id 
 JOIN ONE_DOWNGRADE od on se.user_id = od.user_id
 where se.event_type <>'cancel' AND sf60.days_as_subscriber>=60 AND se.monthly_amount/le.max_historical_amount<0.5)
-SELECT * FROM ACTIVE_USERS
+SELECT * FROM CHURN_USERS
 ORDER BY days_as_subscriber DESC, user_id ASC
