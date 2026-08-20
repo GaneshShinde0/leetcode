e55c98211693ba1d0278cc32905180a944449283
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-20 09:23:35 +0530

    Time: 72 ms (5.96%) | Memory: 46.7 MB (20.00%) - LeetSync

---
+++ 2318-maximum-points-in-an-archery-competition/maximum-points-in-an-archery-competition.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] maximumBobPoints(int numArrows, int[] aliceArrows) {
+        int n = aliceArrows.length;//12 given in question
+        int[] bobArrows = new int[n];
+        for(int mask=0;mask<(1<<n);mask++){
+            int currArrows = numArrows;
+            int[] temp = new int[n];
+            for(int j=0;j<n;j++){
+                if(currArrows>aliceArrows[j] && ((1<<j) & mask) !=0){
+                    temp[j] = aliceArrows[j]+1;
+                    currArrows-=temp[j];
+                }
+            }
+            if(check(temp, bobArrows)){
+                bobArrows = temp;
+                bobArrows[0]+=currArrows;
+            }
+        }
+        System.out.println(Arrays.toString(bobArrows));
+        return bobArrows;
+    }
+    private boolean check(int[] a, int[] b){
+        int aScore = 0, bScore = 0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]>b[i]){
+                aScore+=i;
+            }else if(a[i]<b[i]){
+                bScore+=i;
+            }
+        }
+        return aScore>bScore;
+    }
+}
