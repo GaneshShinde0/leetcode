58f18a7f4e16c0d4afcf873a56e215ac8d3fe3b4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-22 22:29:24 +0530

    Time: 33 ms (33.31%) | Memory: 98.9 MB (77.74%) - LeetSync

---
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.java
@@ -0,0 +1,45 @@
+class RandomizedCollection {
+    List<Integer> li;
+    HashMap<Integer,HashSet<Integer>> valToIdx;
+    public RandomizedCollection() {
+        this.li = new ArrayList<>();
+        this.valToIdx = new HashMap<Integer, HashSet<Integer>>();
+    }
+    
+    public boolean insert(int val) {
+        valToIdx.computeIfAbsent(val, x->new HashSet<>()).add(li.size());
+        li.add(val);
+        return valToIdx.get(val).size()==1;
+    }
+    
+    public boolean remove(int val) {
+        if(!valToIdx.containsKey(val)) return false;
+        
+        int removeIdx = valToIdx.get(val).iterator().next();
+        valToIdx.get(val).remove(removeIdx);
+
+        int last = li.get(li.size()-1);
+        li.set(removeIdx, last);
+
+        valToIdx.get(last).add(removeIdx);
+        valToIdx.get(last).remove(li.size()-1);
+        li.remove(li.size()-1);
+        
+        if(valToIdx.get(val).size()==0) valToIdx.remove(val);
+
+        return true;
+    }
+    
+    public int getRandom() {
+        int idx = (int) (Math.random()*li.size());
+        return li.get(idx);
+    }
+}
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
