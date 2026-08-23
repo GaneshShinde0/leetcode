5d96cd25b0754b8816bfbc63c9a831db979337fd
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-23 22:58:40 +0530

    Time: 1 ms (100.00%) | Memory: 45 MB (88.27%) - LeetSync

--- 2754-maximum-strength-of-a-group/maximum-strength-of-a-group.java
+++ 2754-maximum-strength-of-a-group/maximum-strength-of-a-group.java
@@ -1,5 +1,32 @@
 class Solution {
     public long maxStrength(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        
+        long res = 1;
+        int maxNeg = Integer.MIN_VALUE, posCount = 0, negCount = 0;
+        
+        for (int num : nums) {
+            if (num > 0) {
+                res *= num;
+                posCount++;
+            } else if (num < 0) {
+                res *= num;
+                maxNeg = Math.max(maxNeg, num);
+                negCount++;
+            }
+        }
+        
+        // If there are no positive numbers, and 1 or 0 negative numbers, 
+        // the remaining elements must be zeros. The max product is 0.
+        if (posCount == 0 && negCount <= 1) return 0;
+        
+        // If the product of all non-zero numbers is negative, remove the largest negative
+        return res < 0 ? res / maxNeg : res;
+    }
+}
+
+class SolutionInitial {
+    public long maxStrength(int[] nums) {
         if(nums.length==1) return nums[0];
         long res = 1, maxNeg = Integer.MIN_VALUE, negCount=0, posCount=0,zeroCount=0;
         Arrays.sort(nums);
