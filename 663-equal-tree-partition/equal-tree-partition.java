ca69357b9ca58f574beaf2c3a6b18499fd61b0f3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-23 22:00:44 +0530

    Time: 4 ms (43.90%) | Memory: 47.2 MB (29.27%) - LeetSync

--- 663-equal-tree-partition/equal-tree-partition.java
+++ 663-equal-tree-partition/equal-tree-partition.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+class SolutionHashMap {
     HashMap<Integer,Integer> hm;
     public boolean checkEqualTree(TreeNode root) {
         this.hm = new HashMap<>();
@@ -27,5 +27,29 @@ class Solution {
         int sum = root.val+sum(root.left)+sum(root.right);
         hm.put(sum,hm.getOrDefault(sum,0)+1);
         return sum;
+    }
+}
+class Solution {
+    HashSet<Integer> hs;
+    
+    public boolean checkEqualTree(TreeNode root) {
+        hs = new HashSet<>();
+        
+        // Calculate total sum while only adding proper subtrees to the HashSet
+        int totalSum = root.val + sum(root.left) + sum(root.right);
+        
+        // If the sum is odd (positive or negative), it can't be split equally
+        if (totalSum % 2 != 0) return false;
+        
+        // Because the root's total sum isn't in the HashSet, 
+        // a target sum will only be found if a proper subtree sums is present.
+        return hs.contains(totalSum / 2);
+    }
+    
+    private int sum(TreeNode root) {
+        if (root == null) return 0;
+        int currentSum = root.val + sum(root.left) + sum(root.right);
+        hs.add(currentSum); // Add proper subtree sum
+        return currentSum;
     }
 }
