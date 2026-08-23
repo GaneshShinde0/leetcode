f4231785bb5e82482bc9847428b32271851b373f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-23 07:42:36 +0530

    Time: 8 ms (53.28%) | Memory: 46.9 MB (83.94%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -1,23 +1,35 @@
 class Solution {
     public boolean sumGame(String num) {
-        int left = 0, right = 0, leftQ=0, rightQ=0, n=num.length(),mid = n/2;
+        int n = num.length(), leftQ=0, rightQ=0, mid = n/2,leftSum=0, rightSum=0;
         for(int i=0;i<n;i++){
-            char c = num.charAt(i);
             if(i<mid){
-                if(c=='?') leftQ++;
-                else left += (c-'0');
+                if(num.charAt(i)=='?') leftQ++;
+                else leftSum += num.charAt(i)-'0';
             }else{
-                if(c=='?') rightQ++;
-                else right += (c-'0');
+                if(num.charAt(i)=='?') rightQ++;
+                else rightSum += num.charAt(i)-'0';
             }
         }
+        
         // If the total number of '?' is odd, Alice makes the last move and guarantees a win.
         if((leftQ+rightQ)%2!=0) return true;
 
         // If the total ? is even, Bob wins only if the difference in sums can be 
         // Perfectly baancced by the difference in '?'.
         // Every 2 extra '?' on one side can beforced by bob to add exactly 9.
-        return (left-right)*2 != (rightQ-leftQ)*9;
-
+        return (leftSum-rightSum)*2 != (rightQ-leftQ)*9;
     }
 }
+
+/*
+99?11?
+
+ALice will try to increase the gap, bob will try to reduce the gap.
+11??111?
+
+if(leftSum-rightSum>10) Alice will always win...
+
+1329
+
+599?
+*/
