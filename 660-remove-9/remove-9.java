ce1a2e1cdbe8c9127fc8a2187d3a0d860a184216
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-23 22:05:54 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (80.00%) - LeetSync

--- 660-remove-9/remove-9.java
+++ 660-remove-9/remove-9.java
@@ -3,8 +3,29 @@ Until 10 -> 9
 Until 100 -> 80
 */
 
-class Solution {
+class SolutionEasy {
     public int newInteger(int n) {
         return Integer.parseInt(Integer.toString(n, 9));
+    }
+}
+
+class Solution {
+    public int newInteger(int n) {
+        int ans = 0;
+        int placeValue = 1; // Represents the 1s, 10s, 100s position in our final decimal number
+        
+        while (n > 0) {
+            // Get the current base-9 digit
+            int digit = n % 9;
+            
+            // Place it in the correct position of our result
+            ans += digit * placeValue;
+            
+            // Move to the next digit
+            n /= 9;
+            placeValue *= 10;
+        }
+        
+        return ans;
     }
 }
