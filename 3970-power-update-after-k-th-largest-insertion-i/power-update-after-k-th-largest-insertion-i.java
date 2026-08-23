d29984cad718b9307444e9e4e8e3dcf36be1d5dc
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-23 11:59:17 +0530

    Time: 198 ms (70.00%) | Memory: 85.8 MB (60.00%) - LeetSync

--- 3970-power-update-after-k-th-largest-insertion-i/power-update-after-k-th-largest-insertion-i.java
+++ 3970-power-update-after-k-th-largest-insertion-i/power-update-after-k-th-largest-insertion-i.java
@@ -1,17 +1,35 @@
-import java.math.BigInteger;
-
 class Solution {
-    public List<Integer> powerUpdate(int[] nums, int p, int[][] queries) {
-        List<Integer> ans = new ArrayList<Integer>();
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            list.add(nums[i]);
+    // Custom modular exponentiation method
+    private int modPow(long base, long exp) {
+        long res = 1;
+        long mod = 1_000_000_007;
+        base %= mod;
+        
+        while (exp > 0) {
+            // If exp is odd, multiply the base with the result
+            if ((exp & 1) == 1) {
+                res = (res * base) % mod;
+            }
+            // Square the base and halve the exponent
+            base = (base * base) % mod;
+            exp >>= 1;
         }
+        return (int) res;
+    }
+
+    public List<Integer> powerUpdate(int[] nums, int p, int[][] queries) {
+        List<Integer> ans = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for (int num : nums) {
+            list.add(num);
+        }
+        
         for (int i = 0; i < queries.length; i++) {
             int low = 0, high = list.size() - 1;
             while (low <= high) {
-                int mid = (low + high) / 2;
+                int mid = low + (high - low) / 2;
                 if (list.get(mid) > queries[i][0]) {
                     high = mid - 1;
                 } else {
@@ -19,14 +37,14 @@ class Solution {
                 }
             }
             list.add(low, queries[i][0]);
+            
             int kthLargestValue = list.get(list.size() - queries[i][1]);
-            BigInteger t = BigInteger.valueOf(p).modPow(BigInteger.valueOf(kthLargestValue),
-                    BigInteger.valueOf(1000000007));
-            p = t.intValue();
+            
+            // Use the custom modPow method instead of BigInteger
+            p = modPow(p, kthLargestValue);
             ans.add(p);
         }
 
         return ans;
-
     }
 }
