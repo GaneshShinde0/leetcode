3db1517c865c2e22aa93328db1502bc00aca7be4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-26 17:32:36 +0530

    Time: 87 ms (11.11%) | Memory: 164.3 MB (11.11%) - LeetSync

--- 3992-longest-semi-repeating-subarray/longest-semi-repeating-subarray.java
+++ 3992-longest-semi-repeating-subarray/longest-semi-repeating-subarray.java
@@ -3,19 +3,20 @@ class Solution {
         int res = 0, left = 0, right = 0, n = nums.length, windowSize = 0;
         HashMap<Integer, Integer> hm1 = new HashMap<>();
         HashMap<Integer, Integer> hm2 = new HashMap<>();
-        for(;right<n; right++){
-            if(hm1.containsKey(nums[right])) hm2.put(nums[right], hm2.getOrDefault(nums[right],0)+1);
-            hm1.put(nums[right], hm1.getOrDefault(nums[right],0)+1);
-            while(left<right && hm2.size()>k){
-                hm1.put(nums[left],hm1.getOrDefault(nums[left],0)-1);
-                if(hm1.get(nums[left])==0) hm1.remove(nums[left]);
-                hm2.put(nums[left],hm2.getOrDefault(nums[left],0)-1);
-                if(hm2.get(nums[left])<=0) hm2.remove(nums[left]);
+        for (; right < n; right++) {
+            if (hm1.containsKey(nums[right]))
+                hm2.put(nums[right], hm2.getOrDefault(nums[right], 0) + 1);
+            hm1.put(nums[right], hm1.getOrDefault(nums[right], 0) + 1);
+            while (left < right && hm2.size() > k) {
+                hm1.put(nums[left], hm1.getOrDefault(nums[left], 0) - 1);
+                if (hm1.get(nums[left]) == 0)
+                    hm1.remove(nums[left]);
+                hm2.put(nums[left], hm2.getOrDefault(nums[left], 0) - 1);
+                if (hm2.get(nums[left]) <= 0)
+                    hm2.remove(nums[left]);
                 left++;
             }
-            // System.out.println(hm1);
-            // System.out.println(hm2);
-            res = Math.max(res, right-left+1);
+            res = Math.max(res, right - left + 1);
         }
         return res;
     }
