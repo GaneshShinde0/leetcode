81b8234d092aead5737ae7f8c173c886da66a665
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-26 17:34:42 +0530

    Time: 11 ms (100.00%) | Memory: 131.1 MB (55.56%) - LeetSync

--- 3992-longest-semi-repeating-subarray/longest-semi-repeating-subarray.java
+++ 3992-longest-semi-repeating-subarray/longest-semi-repeating-subarray.java
@@ -1,5 +1,31 @@
 class Solution {
     public int longestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        int[] count = new int[100001]; // nums[i] up to 1e5
+        int res = 0, left = 0, repeatCount = 0;
+
+        for (int right = 0; right < n; right++) {
+            count[nums[right]]++;
+            if (count[nums[right]] == 2) {
+                repeatCount++; // this value just became a repeating element
+            }
+
+            while (repeatCount > k) {
+                count[nums[left]]--;
+                if (count[nums[left]] == 1) {
+                    repeatCount--; // this value is no longer repeating
+                }
+                left++;
+            }
+
+            res = Math.max(res, right - left + 1);
+        }
+
+        return res;
+    }
+}
+class SolutionInitial {
+    public int longestSubarray(int[] nums, int k) {
         int res = 0, left = 0, right = 0, n = nums.length, windowSize = 0;
         HashMap<Integer, Integer> hm1 = new HashMap<>();
         HashMap<Integer, Integer> hm2 = new HashMap<>();
