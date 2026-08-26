8ffe8a8a2a87451e376f003b182a033d4d60503c
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-26 23:11:44 +0530

    Time: 6 ms (81.60%) | Memory: 46.1 MB (28.70%) - LeetSync

---
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -0,0 +1,29 @@
+/*
+0 1 1
+0 1 1
+
+Lets see how much each point has to travel forward to get match.
+
+Input: 
+nums1 = [2,5,1,2,5], 
+nums2 = [10,5,2,1,5,2]
+
+2 4 
+*/
+
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(nums1[i]==nums2[j]){
+                    dp[i+1][j+1] = 1+dp[i][j];
+                }else{
+                    dp[i+1][j+1] = Math.max(dp[i][j+1],dp[i+1][j]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
