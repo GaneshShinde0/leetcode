3ff20a4d972d0dd322445ddbddfd14b48fb91157
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-31 00:00:41 +0530

    Time: 2318 ms (26.58%) | Memory: 82.5 MB (6.26%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,8 +1,57 @@
 /*
+Dynamic Programming - Top Down (Optimized Space)
+In Approach 2, We used a 2D array for memoCuts, on careful observation, we notice that when we update or access the stored values to or from memoCuts, the value of the variable end always remains the same.
+
+Thus, we only need to track and update the start index when finding the minimum number of cuts. The memoCuts[start] will determine the minimum number of cuts. The memoCuts[start] will determine the minimum number of cuts for substring starting at index start and ending at length(s-1).
+
+
+*/
+class Solution{
+    private Boolean memoPalindrome[][];
+    private Integer memoCuts[];
+    public int minCut(String s) {
+        int n = s.length();
+        memoPalindrome = new Boolean[n][n];
+        memoCuts = new Integer[n];
+        return minimumCuts(s,0, n-1, n-1);
+    }
+    private int minimumCuts(String s, int start, int end, int minimumCuts){
+        if(memoCuts[start]!=null) return memoCuts[start];
+        if(start==end||isPalindrome(s,start,end)) return 0;
+        for(int currEndIndex = start; currEndIndex<=end; currEndIndex++){
+            if(isPalindrome(s,start,currEndIndex)){
+                minimumCuts = Math.min(minimumCuts, 1+minimumCuts(s,currEndIndex+1, end, minimumCuts));
+            }
+        }
+        memoCuts[start] = minimumCuts;
+        return minimumCuts;
+    }
+    
+    private boolean isPalindrome(String s, int start, int end){
+        if(memoPalindrome[start][end]!=null) return memoPalindrome[start][end];
+        int startTemp =start, endTemp = end;
+        while(start<end){
+            if(s.charAt(start++)!=s.charAt(end--)){
+                memoPalindrome[startTemp][endTemp] = false;
+                return false;
+            }
+        }
+        memoPalindrome[startTemp][endTemp] = true;
+        return true;
+    }
+}
+
+/*
 Dynamic Programming+ Top Down (Recursion, Memoization)
 Time Complexity: O(N^2, N) where N is the length of the string s.
+Every Sub result is calculated once.
+We know that size N has N^2 possible substrings. Thus worst case time complexity of the recursive method findMinimumCut is O(N^2)
+
+Additionally, within each recursive call, we are also checking if a substring is palindrome or not. The worst case time complexity for isPalindrome is O(N/2). This gives us the total time complexity as O(N^2) . O(N/2) = O(N^3)
+
+Space N^2... 
 */
-class Solution {
+class SolutionTopDownDP {
     private Integer memoCuts[][];
     private Boolean memoPalindrome[][];
     public int minCut(String s) {
