1ecd1bab4629a9bfbd67a8db0373aa7f9cd0b80b
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-30 11:01:08 +0530

    Time: 1874 ms (5.12%) | Memory: 46.8 MB (93.92%) - LeetSync

---
+++ 4380-minimum-operations-to-form-subset-sum-ii/minimum-operations-to-form-subset-sum-ii.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int minOperations(int[] nums, int sum) {
+        final int INF = 1_000_000;
+
+        int[] dp = new int[sum + 1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+
+        for (int x : nums) {
+            // (value, minimum operations)
+            List<int[]> options = new ArrayList<>();
+            long value = x;
+            int divCost = 0;
+            while(value > 0){
+                long curr = value;
+                int cost = divCost;
+
+                while(curr<=sum){
+                    options.add(new int[]{(int)curr, cost});
+                    curr*=2;
+                    cost++;
+                }
+
+                value/=2;
+                divCost++;
+            }
+
+            // Remove duplicate resulting values, keeping minimum cost.
+            
+            // System.out.println(best);
+            int[] next = Arrays.copyOf(dp, sum + 1);
+
+            for (int s = 0; s <= sum; s++) {
+                if (dp[s] == INF) continue;
+
+                for (int[] option: options) {
+                    int val = option[0];
+                    int cost = option[1];
+
+                    if (s + val <= sum) {
+                        next[s + val] = Math.min(
+                            next[s + val],
+                            dp[s] + cost
+                        );
+                    }
+                }
+            }
+
+            dp = next;
+        }
+
+        return dp[sum] == INF ? -1 : dp[sum];
+    }
+}
