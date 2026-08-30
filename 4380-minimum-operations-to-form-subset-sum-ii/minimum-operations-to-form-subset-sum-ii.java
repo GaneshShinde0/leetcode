9bcee06a295035c40bb6fb4742f75cb0fd417757
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-30 11:06:40 +0530

    Time: 1792 ms (5.12%) | Memory: 47.1 MB (49.60%) - LeetSync

--- 4380-minimum-operations-to-form-subset-sum-ii/minimum-operations-to-form-subset-sum-ii.java
+++ 4380-minimum-operations-to-form-subset-sum-ii/minimum-operations-to-form-subset-sum-ii.java
@@ -14,29 +14,61 @@ class Solution {
             while(value > 0){
                 long curr = value;
                 int cost = divCost;
-
                 while(curr<=sum){
                     options.add(new int[]{(int)curr, cost});
                     curr*=2;
                     cost++;
                 }
-
                 value/=2;
                 divCost++;
             }
+            for (int s = sum; s >=0; s--) {
+                if (dp[s] == INF) continue;
+                for (int[] option: options) {
+                    int val = option[0];
+                    int cost = option[1];
+                    if (s + val <= sum) {
+                        dp[s + val] = Math.min(dp[s + val], dp[s] + cost);
+                    }
+                }
+            }
 
-            // Remove duplicate resulting values, keeping minimum cost.
-            
-            // System.out.println(best);
+        }
+
+        return dp[sum] == INF ? -1 : dp[sum];
+    }
+}
+
+class SolutionInitial {
+    public int minOperationsInititial(int[] nums, int sum) {
+        final int INF = 1_000_000;
+        int[] dp = new int[sum + 1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+
+        for (int x : nums) {
+            // (value, minimum operations)
+            List<int[]> options = new ArrayList<>();
+            long value = x;
+            int divCost = 0;
+            while(value > 0){
+                long curr = value;
+                int cost = divCost;
+                while(curr<=sum){
+                    options.add(new int[]{(int)curr, cost});
+                    curr*=2;
+                    cost++;
+                }
+                value/=2;
+                divCost++;
+            }
             int[] next = Arrays.copyOf(dp, sum + 1);
 
             for (int s = 0; s <= sum; s++) {
                 if (dp[s] == INF) continue;
-
                 for (int[] option: options) {
                     int val = option[0];
                     int cost = option[1];
-
                     if (s + val <= sum) {
                         next[s + val] = Math.min(
                             next[s + val],
@@ -48,7 +80,6 @@ class Solution {
 
             dp = next;
         }
-
         return dp[sum] == INF ? -1 : dp[sum];
     }
 }
