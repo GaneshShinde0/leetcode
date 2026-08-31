ef10d0be57abc053637432ce6614a76a69a9d943
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-08-31 10:44:37 +0530

    Time: 28 ms (24.86%) | Memory: 49.9 MB (39.06%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -8,7 +8,27 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution{
+    public int[] nextLargerNodes(ListNode head){
+        Stack<int[]> stk = new Stack<>();
+        List<Integer> answer = new ArrayList<>();
+        int cnt = 0;
+
+        while(head!=null){
+            answer.add(0);
+            while(!stk.isEmpty() && head.val>stk.peek()[1]){
+                int[] curr = stk.peek();
+                int id = curr[0], val = curr[1];
+                stk.pop();
+                answer.set(id,head.val);
+            }
+            stk.add(new int[]{cnt++, head.val});
+            head = head.next;
+        }
+        return answer.stream().mapToInt(i->i).toArray();
+    }
+}
+class SolutionTwoPass {
     public int[] nextLargerNodes(ListNode head) {
         ListNode temp = new ListNode(-1);
         temp.next = head;
