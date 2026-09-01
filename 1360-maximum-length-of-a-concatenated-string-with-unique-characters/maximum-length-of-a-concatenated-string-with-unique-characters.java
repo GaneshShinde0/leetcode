304bcb8382d5a3666c5f5350e2cd343fe49b88a5
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-01 23:22:49 +0530

    Time: 28 ms (15.59%) | Memory: 51.5 MB (5.48%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,4 +1,4 @@
-class Solution {
+class SolutionMine {
     public int maxLength(List<String> arr) {
         int n = arr.size();
         List<Integer> li = new ArrayList<>();
@@ -33,5 +33,36 @@ class Solution {
             res = Math.max(res, Integer.bitCount(curr));
         }
         return res;
+    }
+}
+
+class Solution {
+    public int maxLength(List<String> arr) {
+        // Initialize results with an empty string
+        // from which to build all future results
+        List<String> results = new ArrayList<>();
+        results.add("");
+        int best = 0;
+        for (String word : arr) {
+            // We only want to iterate through results
+            // that existed prior to this loop
+            int resultsLen = results.size();
+            for (int i = 0; i < resultsLen; i++) {
+                // Form a new result combination and
+                // use a set to check for duplicate characters
+                String newRes = results.get(i) + word;
+                Set<Character> newResSet = new HashSet<>();
+                for (char c : newRes.toCharArray())
+                    newResSet.add(c);
+                if (newRes.length() != newResSet.size())
+                    continue;
+
+                // Add valid options to results and
+                // keep track of the longest so far
+                results.add(newRes);
+                best = Math.max(best, newRes.length());
+            }
+        }
+        return best;
     }
 }
