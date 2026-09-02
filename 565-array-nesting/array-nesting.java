f0fc0d32631ccfdf7b45034e2c5468d791797a14
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-02 20:59:57 +0530

    Time: 45 ms (6.66%) | Memory: 89.5 MB (8.60%) - LeetSync

--- 565-array-nesting/array-nesting.java
+++ 565-array-nesting/array-nesting.java
@@ -1,4 +1,28 @@
-class Solution {
+class Solution{
+    public int arrayNesting(int[] nums) {
+        int res = 0;
+        HashSet<Integer> visited = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(visited.contains(nums[i])) continue;
+            int start = nums[i], count = 0;
+            do{
+                visited.add(start);
+                start = nums[start];
+                count++;
+            }while(start!=nums[i]);
+            res = Math.max(res, count);
+        }
+        return res;
+    }
+    private int dfs(int[] nums, int curr, HashMap<Integer, Integer> visited){
+        if(visited.containsKey(curr)) return visited.get(curr);
+        visited.put(curr,0);
+        int sub = 1+dfs(nums, nums[curr], visited);
+        visited.put(curr, sub);
+        return sub;
+    }
+}
+class SolutionInitial{
     public int arrayNesting(int[] nums) {
         int res = 0;
         HashMap<Integer,Integer> visited = new HashMap<>();
