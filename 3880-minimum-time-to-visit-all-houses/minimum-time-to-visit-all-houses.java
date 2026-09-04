ae49dbf6238220801b399de559c9fc84a32d0663
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-04 09:17:23 +0530

    Time: 5 ms (100.00%) | Memory: 86.2 MB (59.09%) - LeetSync

--- 3880-minimum-time-to-visit-all-houses/minimum-time-to-visit-all-houses.java
+++ 3880-minimum-time-to-visit-all-houses/minimum-time-to-visit-all-houses.java
@@ -5,15 +5,14 @@ class Solution {
         long[] reversePref = new long[n+1];
         for(int i=0;i<n;i++){
             forwardPref[i+1] = forwardPref[i]+forward[i];
-            reversePref[i+1] = reversePref[i]+ ((i<n-1)?backward[i+1]:backward[0]);
+            reversePref[i+1] = reversePref[i]+ ((i<n-1)?backward[i+1]:backward[0]); // Taking from +1 side as we can go from +1 to 0 and not the other way.
         }
-
         long res = 0;
         int curr = 0;
         for(int q:queries){
             int u = curr, v = q;
-            long fd = u<=v?forwardPref[v]-forwardPref[u] : forwardPref[n]-(forwardPref[u]-forwardPref[v]);
-            long bd = u>=v?reversePref[u]-reversePref[v] : reversePref[n]-(reversePref[v]-reversePref[u]);
+            long fd = u<=v?forwardPref[v]-forwardPref[u] : forwardPref[n]-forwardPref[u]+forwardPref[v];
+            long bd = u>=v?reversePref[u]-reversePref[v] : reversePref[n]-reversePref[v]+reversePref[u];
             res += Math.min(fd, bd);
             curr = v;
         }
