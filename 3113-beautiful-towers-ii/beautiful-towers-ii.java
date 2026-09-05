acf30ae29bfa05bd03e2662b65f621d1094b4d5a
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-05 14:09:43 +0530

    Time: 83 ms (33.33%) | Memory: 101.9 MB (13.61%) - LeetSync

---
+++ 3113-beautiful-towers-ii/beautiful-towers-ii.java
@@ -0,0 +1,54 @@
+/*
+[5,3,4,1,1]
+[5,3,3,1,1]
+[1,1,1,1,1]
+
+
+[6,5,3,9,2,7]
+*/
+
+class Solution {
+    public long maximumSumOfHeights(List<Integer> maxHeights) {
+        return maximumSumOfHeights(maxHeights.stream().mapToInt(Integer::intValue).toArray());
+    }
+    public long maximumSumOfHeights(int[] heights) {
+        int n = heights.length;
+        long[] totalBricksOnLeft = new long[n];
+        Stack<Integer> stk = new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!stk.isEmpty() && heights[stk.peek()]>heights[i]){
+                stk.pop();
+            }
+            if(stk.isEmpty()){
+                totalBricksOnLeft[i] = 1l*(i+1)*heights[i];
+            }else{
+                int j = stk.peek();
+                totalBricksOnLeft[i] = totalBricksOnLeft[j]+1l*(i-j)*heights[i];
+            }
+            stk.push(i);
+        }
+
+        long[] totalBricksOnRight = new long[n];
+        stk.clear();
+        for(int i=n-1;i>=0;i--){
+            while(!stk.isEmpty() && heights[stk.peek()]>heights[i]){
+                stk.pop();
+            }
+            if(stk.isEmpty()){
+                totalBricksOnRight[i] = 1l*(n-i)*heights[i];
+            }else{
+                int j = stk.peek();
+                totalBricksOnRight[i] = totalBricksOnRight[j]+1l*(j-i)*heights[i];
+            }
+            stk.push(i);
+        }
+
+        // System.out.println(Arrays.toString(totalBricksOnLeft));
+        // System.out.println(Arrays.toString(totalBricksOnRight));
+        long res = 0;
+        for(int i=0;i<n;i++){
+            res = Math.max(res, totalBricksOnLeft[i]+totalBricksOnRight[i]-heights[i]);
+        }
+        return res;
+    }
+}
