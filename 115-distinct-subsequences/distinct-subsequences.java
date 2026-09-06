cf09c2f7a6b83787144f4148c42f7f04b40e7732
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-06 10:19:12 +0530

    Time: 19 ms (69.08%) | Memory: 54.2 MB (73.01%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,13 +1,13 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int slen = s.length(), tlen = t.length();
-        int[][] dp = new int[slen+1][tlen+1];
-        dp[0][0] = 1; // At begining empty string can match with empty string.
-        for (int i = 0; i <= slen; i++) {
-            dp[i][0] = 1; //base: empty target can always be formed exactly 1 way
+        int sLen = s.length(), tLen = t.length();
+        int[][] dp = new int[sLen+1][tLen+1];
+        for(int i=0;i<=sLen;i++){
+            dp[i][0] = 1;
         }
-        for(int i=0;i<slen;i++){
-            for(int j=0;j<tlen;j++){
+
+        for(int i=0;i<sLen;i++){
+            for(int j=0;j<tLen;j++){
                 if(s.charAt(i)==t.charAt(j)){
                     dp[i+1][j+1] = dp[i][j+1]+dp[i][j];
                 }else{
@@ -15,6 +15,6 @@ class Solution {
                 }
             }
         }
-        return dp[slen][tlen];
+        return dp[sLen][tLen];
     }
 }
