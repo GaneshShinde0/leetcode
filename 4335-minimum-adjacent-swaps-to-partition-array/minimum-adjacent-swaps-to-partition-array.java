d8b209ddbe5a4985d50055eb4589f13cc237ba60
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-12 12:22:40 +0530

    Time: 10 ms (22.77%) | Memory: 96.5 MB (93.80%) - LeetSync

--- 4335-minimum-adjacent-swaps-to-partition-array/minimum-adjacent-swaps-to-partition-array.java
+++ 4335-minimum-adjacent-swaps-to-partition-array/minimum-adjacent-swaps-to-partition-array.java
@@ -5,7 +5,7 @@ All 1s should go after last 0...
 All -1 should go after first 0....
 */
 class Solution {
-    public int minAdjacentSwaps(int[] nums, int a, int b) {
+    public int minAdjacentSwaps1(int[] nums, int a, int b) {
         // Lets simplify problem;
         int[] arr = Arrays.copyOf(nums,nums.length);
         int firstZero = -1, lastZero = -1;
@@ -19,14 +19,37 @@ class Solution {
         for(int i=0;i<arr.length;i++){
             if(arr[i]==0){
                 zero++;
-                res+=ones;
+                res+=ones; // When we see zero that zero needs to go to left by one count.
             }else if(arr[i]==1){
                 ones++;
             }else{
-                res+=zero+ones;
+                res+=zero+ones; // When you see  a negative one you need to move it to left by zero+ones count..
             }
         }
         return (int) (res%1_000_000_007);
     }
 
+    public int minAdjacentSwaps(int[] nums, int a, int b) {
+        // Lets simplify problem;
+        int[] arr = Arrays.copyOf(nums,nums.length);
+        int firstZero = -1, lastZero = -1;
+        for(int i=0;i<arr.length;i++){
+            arr[i] = nums[i]>b?1:nums[i]<a?-1:0;
+        }
+        // System.out.println(Arrays.toString(arr));
+        // Now bascally we have to get this array sorted.. so that all 0's come in middle ones come to right, and -1s come to left.
+        long res = 0;
+        int negOnes = 0, ones = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==-1){
+                res+= i-negOnes;
+                negOnes++;
+            }else if(arr[i]==0){
+                res+=ones;
+            }else{
+                ones++;
+            }
+        }
+        return (int) (res%1_000_000_007);
+    }
 }
