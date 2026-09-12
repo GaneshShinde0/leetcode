fd72b1f4d4bf7fd1f75992a9ab57bbe2f3f9f391
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-12 12:15:21 +0530

    Time: 56 ms (5.05%) | Memory: 110.2 MB (6.52%) - LeetSync

---
+++ 4335-minimum-adjacent-swaps-to-partition-array/minimum-adjacent-swaps-to-partition-array.java
@@ -0,0 +1,32 @@
+/*
+100000-1
+
+All 1s should go after last 0...
+All -1 should go after first 0....
+*/
+class Solution {
+    public int minAdjacentSwaps(int[] nums, int a, int b) {
+        // Lets simplify problem;
+        int[] arr = Arrays.copyOf(nums,nums.length);
+        int firstZero = -1, lastZero = -1;
+        for(int i=0;i<arr.length;i++){
+            arr[i] = nums[i]>b?1:nums[i]<a?-1:0;
+        }
+        System.out.println(Arrays.toString(arr));
+        // Now bascally we have to get this array sorted.. so that all 0's come in middle ones come to right, and -1s come to left.
+        long res = 0;
+        int zero = 0, ones = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
+                zero++;
+                res+=ones;
+            }else if(arr[i]==1){
+                ones++;
+            }else{
+                res+=zero+ones;
+            }
+        }
+        return (int) (res%1_000_000_007);
+    }
+
+}
