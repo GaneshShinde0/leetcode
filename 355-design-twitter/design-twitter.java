6a27c5864f15514a6b9f12e9a23c7f4921b8487f
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-12 13:57:14 +0530

    Time: 35 ms (38.55%) | Memory: 57.3 MB (8.26%) - LeetSync

---
+++ 355-design-twitter/design-twitter.java
@@ -0,0 +1,47 @@
+class Twitter {
+
+    private Map<Integer, Set<Integer>> userAndFollowing;
+    private Map<Integer, Set<int[]>> tweets;
+    int timestamp;
+    public Twitter() {
+        userAndFollowing = new HashMap<>();
+        tweets = new HashMap<>();
+        timestamp=0;
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        tweets.computeIfAbsent(userId, x->new HashSet<>()).add(new int[]{timestamp++,tweetId});
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[0]-a[0]); // Compare by most recent timestamps.
+        Set<Integer> followees = userAndFollowing.getOrDefault(userId, new HashSet<>());
+        followees.add(userId);
+        for (int followee : followees) {
+            Set<int[]> tweetList = tweets.getOrDefault(followee, new HashSet<>());
+            pq.addAll(tweetList);
+        }
+        List<Integer> res = new ArrayList<>();
+        while (!pq.isEmpty() && res.size() < 10) {
+            res.add(pq.poll()[1]);
+        }
+        return res;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        userAndFollowing.computeIfAbsent(followerId, x->new HashSet<Integer>()).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if(userAndFollowing.containsKey(followerId)) userAndFollowing.get(followerId).remove(followeeId);
+    }
+}
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
