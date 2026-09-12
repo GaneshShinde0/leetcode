aef75be546fe2b2e8a10f001446468859c416cd3
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-12 17:07:17 +0530

    Time: 33 ms (66.67%) | Memory: 54 MB (27.78%) - LeetSync

---
+++ 2570-maximize-total-tastiness-of-purchased-fruits/maximize-total-tastiness-of-purchased-fruits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxTastiness(int[] price, int[] tastiness, int maxAmount, int maxCoupons) {
+        int n = price.length;
+        int[][][] dp = new int[n+1][maxAmount+1][maxCoupons+1];
+        int res = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=maxAmount;j++){
+                for(int k=0;k<=maxCoupons;k++){
+                    dp[i+1][j][k] = dp[i][j][k];
+                    if(j>=price[i]){
+                        dp[i+1][j][k] = Math.max(dp[i+1][j][k], tastiness[i]+dp[i][j-price[i]][k]);
+                    }
+                    if(k>0 && j>=price[i]/2){
+                        dp[i+1][j][k] = Math.max(dp[i+1][j][k], tastiness[i]+dp[i][j-price[i]/2][k-1]);
+                    }
+                }
+            }
+        }
+        return dp[n][maxAmount][maxCoupons];
+    }
+}
