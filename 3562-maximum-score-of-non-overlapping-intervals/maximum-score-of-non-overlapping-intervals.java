7d2a89b65aa7efe8d3376a42d5540035e28735d4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-12 10:55:30 +0530

    Time: 150 ms (70.37%) | Memory: 234.7 MB (22.22%) - LeetSync

--- 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.java
+++ 3562-maximum-score-of-non-overlapping-intervals/maximum-score-of-non-overlapping-intervals.java
@@ -23,6 +23,7 @@ class Solution {
         for(int i=0;i<n;i++){
             int l = arr[i][0], weight = arr[i][2],idx = arr[i][3];
             //Use binary search to find intervals whose right endpoints are smaller than 1.
+            // Get index of element who are ending on or before current start.
             int k = binarySearch(arr, i, l);
             for(int j=1;j<5;j++){
                 long s1 = dp[i][j];
