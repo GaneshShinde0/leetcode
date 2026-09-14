8f140d0d5c0a2e6ba2b60492a65aed30eb5b18dd
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-14 09:25:07 +0530

    Time: 17 ms (14.76%) | Memory: 48.2 MB (99.37%) - LeetSync

--- 554-brick-wall/brick-wall.java
+++ 554-brick-wall/brick-wall.java
@@ -1,18 +1,17 @@
 class Solution {
     public int leastBricks(List<List<Integer>> wall) {
-        HashMap<Long,Integer> hm = new HashMap<>();
-        int layers = wall.size();
-        long maxAligned = 0;
-        long width = 0;
-        for(int i:wall.get(0)) width+=i;
-        for(List<Integer> w:wall){
-            long currEnd = 0;
-            for(int e:w){
-                currEnd+=e;
-                hm.put(currEnd,hm.getOrDefault(currEnd,0)+1);
-                if(currEnd!=width) maxAligned = Math.max(hm.get(currEnd),maxAligned);
+        HashMap<Long, Long> hm = new HashMap<>();
+        long width = 0, maxCuts = 0;
+        for(int curr: wall.get(0)) width+=curr;
+        long temp = 0;
+        for(int i=0;i<wall.size();i++){
+            long curr = 0;
+            for(int j:wall.get(i)){
+                curr+=j;
+                if(curr!=width) hm.put(curr, hm.getOrDefault(curr,0l)+1);
+                temp = Math.max(temp, hm.getOrDefault(curr,0l));
             }
         }
-        return (int)(layers-maxAligned);
+        return (int) (wall.size()-temp);
     }
 }
