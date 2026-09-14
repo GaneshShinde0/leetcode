a3d04fcf161fa146dcaff26b63e8ba28ac7166b4
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-14 17:14:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (28.83%) - LeetSync

--- 1040-maximum-binary-tree-ii/maximum-binary-tree-ii.java
+++ 1040-maximum-binary-tree-ii/maximum-binary-tree-ii.java
@@ -17,22 +17,13 @@ class Solution {
     public TreeNode insertIntoMaxTree(TreeNode root, int val) {
         if(root==null) return new TreeNode(val);
         else if(root.val<val){
-            TreeNode node =new TreeNode(val);
+            TreeNode node = new TreeNode(val);
             node.left = root;
             return node;
-        }else if(root.right!=null && root.right.val<val){ 
-            TreeNode temp = root.right;
-            TreeNode node =new TreeNode(val);
-            root.right = node;
-            node.left = temp;
-            return root;
-        }else if(root.right!=null && root.right.val>val){
-            insertIntoMaxTree(root.right,val);
-        }else if(root.right==null){
-            TreeNode node =new TreeNode(val);
-            root.right = node;
-            return root;
+        }else{
+            root.right = insertIntoMaxTree(root.right, val);
         }
         return root;
     }
+
 }
