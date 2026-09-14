552f2d7f2f8c23b43d73626f4b429c684df70120
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-14 14:26:08 +0530

    Time: 16 ms (13.46%) | Memory: 47.4 MB (51.92%) - LeetSync

---
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValidPalindrome(String s, int k) {
+        String rev = new StringBuilder(s).reverse().toString();
+        int n = s.length();
+        int[][] dp = new int[n+1][n+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(s.charAt(i)==rev.charAt(j)){
+                    dp[i+1][j+1] = 1+dp[i][j];
+                }else{
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return (n-dp[n][n])<=k;
+    }
+}
