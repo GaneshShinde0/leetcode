b401df54d5558a80e1ab026577a59ae44645e12e
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-15 23:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (21.95%) - LeetSync

--- 1033-broken-calculator/broken-calculator.java
+++ 1033-broken-calculator/broken-calculator.java
@@ -1,14 +1,10 @@
 class Solution {
     public int brokenCalc(int startValue, int target) {
-        int ans = 0;
-        while(target>startValue){
-            ans++;
-            if(target%2==1){
-                target++;
-            }else{
-                target/=2;
-            }
+        if(startValue >= target) return startValue - target;
+        if(target%2==0){
+            return 1+brokenCalc(startValue,target/2);
+        }else{
+            return 1+brokenCalc(startValue, target+1);
         }
-        return ans+startValue-target;
     }
 }
