b52c77b7019b1b17b9cad7a1e3c4a4a65d58fa93
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-15 10:50:16 +0530

    Time: 4 ms (79.41%) | Memory: 82.5 MB (52.45%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.java
@@ -1,19 +1,15 @@
 class Solution {
     public int[] findBuildings(int[] heights) {
         List<Integer> li = new ArrayList<>();
-        int n = heights.length;
-        li.add(n-1);
-        int max = n-1;
-        for(int i=n-2;i>=0;i--){
-            if(heights[i]>heights[max]){
-                max = i;
-                li.add(i);
-            }
+        int rightMax = 0, n = heights.length;
+        for(int i=n-1;i>=0;i--){
+            if(heights[i]>rightMax) li.add(i);
+            rightMax = Math.max(rightMax, heights[i]);
         }
-        int[] res = new int[li.size()];
-        int resSize = li.size();
-        for(int i=0;i<resSize;i++){
-            res[i]=li.get(resSize-1-i);
+        int m = li.size();
+        int[] res = new int[m];
+        for(int i=0;i<m;i++){
+            res[i] = li.get(m-i-1);
         }
         return res;
     }
