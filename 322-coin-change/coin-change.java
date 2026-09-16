6e0bb3f5394964e1268377808c5680d7c840689e
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-16 22:43:54 +0530

    Time: 15 ms (83.75%) | Memory: 46.3 MB (73.38%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,15 +1,16 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0] = 0;
-        for(int i=0;i<=amount;i++){
-            for(int c:coins){
-                if(i-c>=0 && dp[i-c]!=amount+1){
-                    dp[i] = Math.min(dp[i],dp[i-c]+1);
+        int MAX = amount+1;
+        Arrays.fill(dp, MAX);
+        dp[0] = 0; // Number of coins to form 0 ammount;
+        for(int i=1;i<MAX;i++){
+            for(int coin:coins){
+                if(i-coin>=0 && dp[i-coin]!=MAX){
+                    dp[i] = Math.min(dp[i], dp[i-coin]+1);
                 }
             }
         }
-        return dp[amount]==amount+1?-1:dp[amount];
+        return dp[MAX-1]==MAX?-1:dp[MAX-1];
     }
 }
