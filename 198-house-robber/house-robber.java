66466d7f0d388716e186423aedd53b341d92ce78
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-16 22:21:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (94.28%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,13 +1,24 @@
 class Solution {
+    
     public int rob(int[] nums) {
-        int n = nums.length;
+        int n=nums.length;
         if(n==1) return nums[0];
-        int t1 = nums[0], t2 = Math.max(nums[1],nums[0]);
-        for(int i=2;i<nums.length;i++){
-            int temp = t2;
-            t2 = Math.max(t1+nums[i],t2);
-            t1 = temp;
-        }   
-        return Math.max(t2,t1);
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[1],nums[0]);
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(nums[i]+dp[i-2], dp[i-1]);
+        }
+        return Math.max(dp[n-2], dp[n-1]);
+    }
+    public int robCheck(int[] nums) {
+        int r1 = nums[0], r2 = nums[1], i=2, n=nums.length;
+        while(i<n){
+            int temp = r1;
+            r1 = Math.max(r1+nums[i],r2);
+            r2 = Math.max(temp+nums[i], r2);
+            i++;
+        }
+        return Math.max(r1,r2);
     }
 }
