da9ee21a47759ae6b8d04cb8d481fe3f15095864
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-16 22:11:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (17.47%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,11 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n<2) return n;
-        int t1 = 1, t2 = 2;
-        for(int i=2;i<n;i++){
-            int temp = t2;
-            t2 = t1+t2;
-            t1 = temp;
+        if(n<=2) return n;
+        int t1=1, t2=2, i=2;
+        while(i<n){
+            i++;
+            int temp = t1;
+            t1 = t2;
+            t2 = temp+t2;
         }
         return t2;
     }
