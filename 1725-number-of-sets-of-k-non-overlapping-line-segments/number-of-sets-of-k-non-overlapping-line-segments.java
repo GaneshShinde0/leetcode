d7cc70a6acaca7090782cd92893d1d959bada2b0
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-16 11:35:46 +0530

    Time: 24 ms (78.06%) | Memory: 48.1 MB (77.42%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.java
@@ -0,0 +1,22 @@
+/*
+n - Points
+k - segments.
+
+*/
+class Solution {
+    private int MOD = 1_000_000_007;
+    public int numberOfSets(int n, int k) {
+        // Number of ways to have j segments ending at ith point.
+        int[][] dp = new int[n][k+1];
+        // Number of ways to have 0 segments ending at i.
+        for(int i=0;i<n;i++) dp[i][0] = 1;
+        for(int j=1;j<=k;j++){
+            int sum = 0;
+            for(int i=1;i<n;i++){
+                sum = (sum+dp[i-1][j-1])%MOD;
+                dp[i][j] = (dp[i-1][j] + sum)%MOD;
+            }
+        }
+        return (int) (dp[n-1][k]%MOD);
+    }
+}
