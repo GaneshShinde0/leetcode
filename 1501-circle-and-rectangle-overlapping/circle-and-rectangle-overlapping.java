80aea556ebaab9ecc6483251f6c3c7d151b1d049
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-19 08:53:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (94.96%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
@@ -1,17 +1,40 @@
 class Solution {
     public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
-        int dist = 0;
-        
-        // int closestX = Math.abs(x1-xCenter)<Math.abs(x2-xCenter)?x1:x2;
-        // int closestY = Math.abs(y1-yCenter)<Math.abs(y2-yCenter)?y1:y2;
         int closestX = Math.max(x1, Math.min(x2,xCenter));
         int closestY = Math.max(y1, Math.min(y2,yCenter));
-        return (closestX-xCenter)*(closestX-xCenter) + (closestY-yCenter)*(closestY-yCenter)<=radius*radius;
+        return dist(closestX, closestY, xCenter, yCenter) <=radius; //Kind of like pythagoras Theoresm
+    }
+
+    public boolean checkOverlapByCountingDistance(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        double dist = 0;
+        if(xCenter<x1 || xCenter>x2){
+            dist += Math.min(Math.pow(x1-xCenter,2), Math.pow(x2-xCenter,2));
+        }
+        if(yCenter<y1 || yCenter>y2){
+            dist += Math.min(Math.pow(y1-yCenter,2), Math.pow(y2-yCenter,2));
+        }
+        return dist<=radius*radius; //Kind of like pythagoras Theoresm
+    }
+
+
+    int[][] dirs = {{0,1},{1,0},{-1,0},{0,-1}};
+    public boolean checkOverlapDoesNotWork(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        boolean touches = false;
+        // Four Corner Distance With Center
+        touches|=dist(xCenter, yCenter, x1,y1)<=radius;
+        touches|=dist(xCenter, yCenter, x1,y2)<=radius;
+        touches|=dist(xCenter, yCenter, x2,y1)<=radius;
+        touches|=dist(xCenter, yCenter, x2,y2)<=radius;
+        touches|=xCenter>=x1 &&xCenter<=x2 && yCenter>=y1 && yCenter<=y2;
+        for(int[] dir:dirs){
+            int newX = xCenter+dir[0]*radius, newY = yCenter+dir[1]*radius;
+            touches|= newX>=x1 &&newX<=x2 && newY>=y1 && newY<=y2;
+            if(touches) return true;
+        }
+        return false;
+    }
+    
+    private double dist(int x1, int y1, int x2, int y2){
+        return Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
     }
 }
-
-/*
-Closest x and closest y..
-
-
-*/
