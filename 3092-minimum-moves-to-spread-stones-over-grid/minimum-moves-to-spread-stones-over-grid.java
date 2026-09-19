882c53d8b92625a6ff7f7fbdb459a368e7f7fc07
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-19 11:36:58 +0530

    Time: 4 ms (78.20%) | Memory: 44 MB (49.83%) - LeetSync

--- 3092-minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.java
+++ 3092-minimum-moves-to-spread-stones-over-grid/minimum-moves-to-spread-stones-over-grid.java
@@ -23,7 +23,7 @@ class Solution {
         for (int mask = 0; mask < (1 << n); mask++) {
             if (dp[mask] == Integer.MAX_VALUE) continue;
             int i = Integer.bitCount(mask); // next source index to place
-            // if (i >= n) continue;
+            if (i >= n) continue; // Not Needed
             
             for (int j = 0; j < n; j++) {
                 if ((mask & (1 << j)) != 0) continue; // target j already used
