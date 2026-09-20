1732b7b91486b1af5072809a35e52e95141d95f8
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-20 22:03:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (33.70%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int minOpen= 0, maxOpen = 0;
+        for(char c:s.toCharArray()){
+            if(c=='*'){
+                maxOpen++;
+                minOpen--;
+            }else if(c=='('){
+                maxOpen++;
+                minOpen++;
+            }else{
+                maxOpen--;
+                minOpen--;
+            }
+            if(maxOpen<0) return false;
+            minOpen = Math.max(minOpen,0);
+        }
+        return minOpen==0;
+    }
+}
