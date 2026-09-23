9dffa3da5fff51450e4b9c65af402b321fcad2bc
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-23 11:05:46 +0530

    Time: 6 ms (21.32%) | Memory: 100.5 MB (97.62%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -1,18 +1,22 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int total = 0;
-        for(int num:nums) total += num;
-        int n = nums.length, maxI = -1, left = 0, curr = 0;
-        for(int right = 0;right<n;right++){
-            curr += nums[right];
-            while(curr>total-x && left<=right){
-                curr -= nums[left];
+        int sum = 0, n = nums.length;
+        for(int num:nums) sum+= num;
+        int longestSubArr = longestSubArrayLength(nums,sum-x);
+        if(longestSubArr==-1) return -1;
+        return nums.length-longestSubArr;
+    }
+    private int longestSubArrayLength(int[] nums, int target){
+        int left = 0, right = 0, n= nums.length, sum = 0, res = -1;
+        while(right<n){
+            sum+=nums[right];
+            while(left<=right && sum>target){
+                sum-=nums[left];
                 left++;
             }
-            if(curr == total-x){
-                maxI = Math.max(maxI, right-left+1);
-            }
+            if(sum==target)res = Math.max(res,right-left+1);
+            right++;
         }
-        return maxI !=-1 ?n-maxI : -1;
+        return res;
     }
 }
