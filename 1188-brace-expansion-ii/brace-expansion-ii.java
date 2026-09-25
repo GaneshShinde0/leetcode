b908eda702c8d6c8b8ad3ca240c2cd626d8eb9b2
GaneshShinde0 <61266528+GaneshShinde0@users.noreply.github.com>
2026-09-25 10:30:22 +0530

    Time: 9 ms (82.40%) | Memory: 47.3 MB (45.60%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.java
+++ 1188-brace-expansion-ii/brace-expansion-ii.java
@@ -1,52 +1,58 @@
-import java.util.*;
-
 class Solution {
+    String expression;
+    int idx;
     public List<String> braceExpansionII(String expression) {
-        List<Set<String>> stk = new ArrayList<>();
-        stk.add(new HashSet<>(Arrays.asList("")));
+        this.expression = expression;
+        this.idx = idx;
+        Set<String> res = expr();
+        return new ArrayList<String>(res);
+    }
 
-        for (char c : expression.toCharArray()) {
-            if (c == ',') {
-                // stk[-2:] = [stk[-2] | stk[-1], {""}]
-                Set<String> top = stk.remove(stk.size() - 1);
-                Set<String> second = stk.remove(stk.size() - 1);
-                Set<String> merged = new HashSet<>(second);
-                merged.addAll(top);
-                stk.add(merged);
-                stk.add(new HashSet<>(Arrays.asList("")));
-            } else if (c == '}') {
-                // stk[-3:] = [{l+r for r in (stk[-1]|stk[-2]) for l in stk[-3]}]
-                Set<String> top = stk.remove(stk.size() - 1);
-                Set<String> second = stk.remove(stk.size() - 1);
-                Set<String> third = stk.remove(stk.size() - 1);
-
-                Set<String> union = new HashSet<>(second);
-                union.addAll(top);
-
-                Set<String> product = new HashSet<>();
-                for (String l : third) {
-                    for (String r : union) {
-                        product.add(l + r);
-                    }
-                }
-                stk.add(product);
-            } else if (c == '{') {
-                // stk += [set(), {""}]
-                stk.add(new HashSet<>());
-                stk.add(new HashSet<>(Arrays.asList("")));
-            } else {
-                // stk[-1] = {pre + ch for pre in stk[-1]}
-                Set<String> top = stk.remove(stk.size() - 1);
-                Set<String> newTop = new HashSet<>();
-                for (String pre : top) {
-                    newTop.add(pre + c);
-                }
-                stk.add(newTop);
+    // expr => term | term, expr
+    private Set<String> expr(){
+        Set<String> res = new TreeSet<String>();
+        while(true){
+            res.addAll(term());
+            if(idx < expression.length() && expression.charAt(idx)==','){
+                idx++;
+                continue;
+            }else{
+                break;
             }
         }
+        return res;
+    }
 
-        List<String> res = new ArrayList<>(stk.get(stk.size() - 1));
-        Collections.sort(res);
+    // term => iterm | item term
+    private Set<String> term(){
+        Set<String> res = new TreeSet<>();
+        res.add("");
+        // An item starts with { or a lowercase letter; continue matching only when this condition is met
+        while(idx < expression.length() && (expression.charAt(idx)=='{'||Character.isLetter(expression.charAt(idx)))){
+            Set<String> sub = item();
+            Set<String> tmp = new TreeSet<String>();
+            for(String s1:res){
+                for(String s2:sub){
+                    tmp.add(s1+s2);
+                }
+            }
+            res = tmp;
+        }
+        return res;
+    }
+
+    // item => letter | {expr}
+    private Set<String> item(){
+        Set<String> res = new TreeSet<String>();
+        if(expression.charAt(idx)=='{'){
+            idx++;
+            res = expr();
+        }else{
+            StringBuilder sb = new StringBuilder();
+            sb.append(expression.charAt(idx));
+            res.add(sb.toString());
+        }
+        idx++;
         return res;
     }
 }
